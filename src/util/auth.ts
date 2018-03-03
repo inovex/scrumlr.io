ca37b83223a7b9f41c79fd36c5e7fae50784e378
bitionaire <johann.boehler@googlemail.com>
2018-03-03 22:33:01 +0100

    Extracted duplicate loginAnonymously

---
+++ src/util/auth.ts
@@ -0,0 +1,11 @@
+const md5 = require('blueimp-md5');
+
+export const loginAnonymously = (firebase: any, email: string) => {
+  return firebase.auth().signInAnonymously().then((auth: firebase.UserInfo) => {
+    const mailHash = email ? md5(email) : md5(`${auth.uid}@scrumlr.io`);
+    return firebase.auth().currentUser.updateProfile({
+      displayName: email,
+      photoURL: `https://www.gravatar.com/avatar/${mailHash}?s=32&d=retro`
+    });
+  });
+};
