82cd061d90a7e1706af3434ff4db5e3f31677f0a
bitionaire <johann.boehler@googlemail.com>
2018-03-11 21:54:20 +0100

    Fixed button selector

--- features/step_definitions/input.steps.js
+++ features/step_definitions/input.steps.js
@@ -2,7 +2,7 @@ const {When} = require('cucumber');
 const webdriver = require('selenium-webdriver');
 
 When(/^I click on the "([^"]*)" button$/, function (buttonText, next) {
-  this.driver.findElement(webdriver.By.className('new-board__action-button'))
+  this.driver.findElement(webdriver.By.xpath(`//button[text()='${buttonText}']`))
     .then((button) => {
       button.click().then(next);
     })
