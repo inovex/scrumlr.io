0de74a57eaad67a14137b2466574513d678addb8
bitionaire <johann.boehler@googlemail.com>
2018-04-05 19:02:39 +0200

    Fixed: Display number of votes in vote phase

--- src/components/Card/Dot/Variants/DownvoteDot.tsx
+++ src/components/Card/Dot/Variants/DownvoteDot.tsx
@@ -4,23 +4,30 @@ import './DownvoteDot.css';
 import Icon from '../../../Icon';
 
 export interface DownvoteDotProps {
-  onClick: any;
+  onClick: () => void;
 }
 
-export class DownvoteDot extends React.Component<DownvoteDotProps, any> {
-  render() {
-    const { onClick } = this.props;
-
+export const DownvoteDot: React.SFC<DownvoteDotProps> = ({
+  onClick,
+  children
+}) => {
+  if (children) {
     return (
       <Dot animate size="small" onClick={onClick} className="dot__downvote">
-        <Icon
-          name="minus"
-          aria-hidden="true"
-          className="dot_downvote-icon"
-          width={28}
-          height={28}
-        />
+        {children}
       </Dot>
     );
   }
-}
+
+  return (
+    <Dot animate size="small" onClick={onClick} className="dot__downvote">
+      <Icon
+        name="minus"
+        aria-hidden="true"
+        className="dot_downvote-icon"
+        width={28}
+        height={28}
+      />
+    </Dot>
+  );
+};
