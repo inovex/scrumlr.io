3dbc0fa0d05852a6902989cb285261d4bca8fe50
bitionaire <johann.boehler@masin.io>
2020-01-22 22:21:30 +0100

    Added test for disabled state of sign in

--- src/domain/auth/components/SignIn.test.tsx
+++ src/domain/auth/components/SignIn.test.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-const withMocks = (testMethod: (SignIn: React.FC) => void, config: { authConfig?: any } = {}) => {
+interface firebase {
+    auth: {
+        signInWithRedirect: jest.Mock;
+    };
+    firebase: {
+        auth: {
+            OAuthProvider: jest.Mock;
+            GithubAuthProvider: jest.Mock;
+            GoogleAuthProvider: jest.Mock;
+            SAMLAuthProvider: jest.Mock;
+        };
+    };
+}
+
+const withMocks = (testMethod: (SignIn: React.FC, firebase: firebase) => void, config: { authConfig?: any } = {}) => {
     jest.isolateModules(() => {
         jest.doMock('../../../config/authConfig', () => ({
             enableAnonymousIdentity: true,
@@ -17,183 +31,260 @@ const withMocks = (testMethod: (SignIn: React.FC) => void, config: { authConfig?
             ...config.authConfig
         }));
 
+        const firebase = {
+            auth: {
+                signInWithRedirect: jest.fn()
+            },
+            firebase: {
+                auth: {
+                    OAuthProvider: jest.fn(),
+                    GithubAuthProvider: jest.fn(),
+                    GoogleAuthProvider: jest.fn(),
+                    SAMLAuthProvider: jest.fn()
+                }
+            }
+        };
+
+        jest.doMock('../../firebase', () => firebase);
+
         const SignIn = require('./SignIn').default;
-        testMethod(SignIn);
+        testMethod(SignIn, firebase);
     });
 };
 
-describe('available auth providers', () => {
-    describe('disabled', () => {
-        test('it should not render google sign in when disabled', () => {
+describe('<SignIn />', () => {
+    describe('disabled state of sign in', () => {
+        test('all buttons should be enabled', () => {
             withMocks(
                 (SignIn) => {
                     const component = render(<SignIn />);
-                    expect(component.queryByText(/Google/i)).not.toBeInTheDocument();
+                    expect(component.getByText(/Google/i)).toHaveProperty('disabled', false);
+                    expect(component.getByText(/Apple/i)).toHaveProperty('disabled', false);
+                    expect(component.getByText(/Microsoft/i)).toHaveProperty('disabled', false);
+                    expect(component.getByText(/GitHub/i)).toHaveProperty('disabled', false);
+                    expect(component.getByText(/scrumlr.io/i)).toHaveProperty('disabled', false);
+                    expect(component.getByText(/anonymous/i)).toHaveProperty('disabled', false);
                 },
                 {
                     authConfig: {
-                        enableGoogleIdentity: false
-                    }
-                }
-            );
-        });
-
-        test('it should not render apple sign in when disabled', () => {
-            withMocks(
-                (SignIn) => {
-                    const component = render(<SignIn />);
-                    expect(component.queryByText(/Apple/i)).not.toBeInTheDocument();
-                },
-                {
-                    authConfig: {
-                        enableAppleIdentity: false
-                    }
-                }
-            );
-        });
-
-        test('it should not render microsoft sign in when disabled', () => {
-            withMocks(
-                (SignIn) => {
-                    const component = render(<SignIn />);
-                    expect(component.queryByText(/Microsoft/i)).not.toBeInTheDocument();
-                },
-                {
-                    authConfig: {
-                        enableMicrosoftIdentity: false
-                    }
-                }
-            );
-        });
-
-        test('it should not render github sign in when disabled', () => {
-            withMocks(
-                (SignIn) => {
-                    const component = render(<SignIn />);
-                    expect(component.queryByText(/GitHub/i)).not.toBeInTheDocument();
-                },
-                {
-                    authConfig: {
-                        enableGithubIdentity: false
-                    }
-                }
-            );
-        });
-
-        test('it should not render SAML sign in when disabled', () => {
-            withMocks(
-                (SignIn) => {
-                    const component = render(<SignIn />);
-                    expect(component.queryByText(/scrumlr.io/i)).not.toBeInTheDocument();
-                },
-                {
-                    authConfig: {
-                        enableSamlIdentity: false,
+                        enableAnonymousIdentity: true,
+                        enableGoogleIdentity: true,
+                        enableAppleIdentity: true,
+                        enableMicrosoftIdentity: true,
+                        enableGithubIdentity: true,
+                        enableSamlIdentity: true,
+                        samlProviderId: 'saml.sso',
                         samlProviderName: 'scrumlr.io'
                     }
                 }
             );
         });
 
-        test('it should not render anonymous sign in when disabled', () => {
+        test('all buttons should be disabled', () => {
             withMocks(
-                (SignIn) => {
+                async (SignIn, firebase) => {
                     const component = render(<SignIn />);
-                    expect(component.queryByText(/anonymous/i)).not.toBeInTheDocument();
+
+                    firebase.auth.signInWithRedirect.mockReturnValue(new Promise(() => {}));
+                    fireEvent.click(component.getByText(/Google/i));
+
+                    expect(await component.getByText(/Google/i)).toHaveProperty('disabled', true);
+                    expect(await component.getByText(/Apple/i)).toHaveProperty('disabled', true);
+                    expect(await component.getByText(/Microsoft/i)).toHaveProperty('disabled', true);
+                    expect(await component.getByText(/GitHub/i)).toHaveProperty('disabled', true);
+                    expect(await component.getByText(/scrumlr.io/i)).toHaveProperty('disabled', true);
+                    expect(await component.getByText(/anonymous/i)).toHaveProperty('disabled', true);
                 },
                 {
                     authConfig: {
-                        enableAnonymousIdentity: false
+                        enableAnonymousIdentity: true,
+                        enableGoogleIdentity: true,
+                        enableAppleIdentity: true,
+                        enableMicrosoftIdentity: true,
+                        enableGithubIdentity: true,
+                        enableSamlIdentity: true,
+                        samlProviderId: 'saml.sso',
+                        samlProviderName: 'scrumlr.io'
                     }
                 }
             );
         });
     });
 
-    describe('enabled', () => {
-        test('it should render google sign in when enabled', () => {
-            withMocks(
-                (SignIn) => {
-                    const component = render(<SignIn />);
-                    expect(component.queryByText(/Google/i)).toBeInTheDocument();
-                },
-                {
-                    authConfig: {
-                        enableGoogleIdentity: true
+    describe('available auth providers', () => {
+        describe('disabled', () => {
+            test('it should not render google sign in when disabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+                        expect(component.queryByText(/Google/i)).not.toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableGoogleIdentity: false
+                        }
                     }
-                }
-            );
+                );
+            });
+
+            test('it should not render apple sign in when disabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+                        expect(component.queryByText(/Apple/i)).not.toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableAppleIdentity: false
+                        }
+                    }
+                );
+            });
+
+            test('it should not render microsoft sign in when disabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+                        expect(component.queryByText(/Microsoft/i)).not.toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableMicrosoftIdentity: false
+                        }
+                    }
+                );
+            });
+
+            test('it should not render github sign in when disabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+                        expect(component.queryByText(/GitHub/i)).not.toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableGithubIdentity: false
+                        }
+                    }
+                );
+            });
+
+            test('it should not render SAML sign in when disabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+                        expect(component.queryByText(/scrumlr.io/i)).not.toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableSamlIdentity: false,
+                            samlProviderName: 'scrumlr.io'
+                        }
+                    }
+                );
+            });
+
+            test('it should not render anonymous sign in when disabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+                        expect(component.queryByText(/anonymous/i)).not.toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableAnonymousIdentity: false
+                        }
+                    }
+                );
+            });
         });
 
-        test('it should render apple sign in when enabled', () => {
-            withMocks(
-                (SignIn) => {
-                    const component = render(<SignIn />);
-                    expect(component.queryByText(/Apple/i)).toBeInTheDocument();
-                },
-                {
-                    authConfig: {
-                        enableAppleIdentity: true
+        describe('enabled', () => {
+            test('it should render google sign in when enabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+                        expect(component.queryByText(/Google/i)).toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableGoogleIdentity: true
+                        }
                     }
-                }
-            );
-        });
+                );
+            });
 
-        test('it should render microsoft sign in when enabled', () => {
-            withMocks(
-                (SignIn) => {
-                    const component = render(<SignIn />);
-                    expect(component.queryByText(/Microsoft/i)).toBeInTheDocument();
-                },
-                {
-                    authConfig: {
-                        enableMicrosoftIdentity: true
+            test('it should render apple sign in when enabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+                        expect(component.queryByText(/Apple/i)).toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableAppleIdentity: true
+                        }
                     }
-                }
-            );
-        });
+                );
+            });
 
-        test('it should render github sign in when enabled', () => {
-            withMocks(
-                (SignIn) => {
-                    const component = render(<SignIn />);
-                    expect(component.queryByText(/GitHub/i)).toBeInTheDocument();
-                },
-                {
-                    authConfig: {
-                        enableGithubIdentity: true
+            test('it should render microsoft sign in when enabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+                        expect(component.queryByText(/Microsoft/i)).toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableMicrosoftIdentity: true
+                        }
                     }
-                }
-            );
-        });
+                );
+            });
 
-        test('it should render SAML sign in when enabled', () => {
-            withMocks(
-                (SignIn) => {
-                    const component = render(<SignIn />);
-                    expect(component.queryByText(/scrumlr.io/i)).toBeInTheDocument();
-                },
-                {
-                    authConfig: {
-                        enableSamlIdentity: true,
-                        samlProviderName: 'scrumlr.io'
+            test('it should render github sign in when enabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+                        expect(component.queryByText(/GitHub/i)).toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableGithubIdentity: true
+                        }
                     }
-                }
-            );
-        });
+                );
+            });
 
-        test('it should render anonymous sign in when enabled', () => {
-            withMocks(
-                (SignIn) => {
-                    const component = render(<SignIn />);
-                    expect(component.queryByText(/anonymous/i)).toBeInTheDocument();
-                },
-                {
-                    authConfig: {
-                        enableAnonymousIdentity: true
+            test('it should render SAML sign in when enabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+                        expect(component.queryByText(/scrumlr.io/i)).toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableSamlIdentity: true,
+                            samlProviderName: 'scrumlr.io'
+                        }
                     }
-                }
-            );
+                );
+            });
+
+            test('it should render anonymous sign in when enabled', () => {
+                withMocks(
+                    (SignIn) => {
+                        const component = render(<SignIn />);
+
+                        expect(component.queryByText(/anonymous/i)).toBeInTheDocument();
+                    },
+                    {
+                        authConfig: {
+                            enableAnonymousIdentity: true
+                        }
+                    }
+                );
+            });
         });
     });
 });
