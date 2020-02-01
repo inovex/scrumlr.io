c64e816f2b4ea96bfaa7f431c85460bf8611e839
bitionaire <johann.boehler@masin.io>
2020-02-01 20:32:01 +0100

    Persist user profile on login

--- src/domain/auth/sessionStore.ts
+++ src/domain/auth/sessionStore.ts
@@ -1,6 +1,7 @@
-import { observable, action } from 'mobx';
+import { observable, action, when } from 'mobx';
 import { RootStore } from '../rootStore';
 import { User } from 'firebase';
+import { Document } from 'firestorter';
 
 class SessionStore {
     public rootStore: RootStore;
@@ -8,12 +9,32 @@ class SessionStore {
     @observable
     public authUser: User | null = null;
 
+    @observable
+    public userProfile: Document | null = null;
+
     constructor(rootStore: RootStore) {
         this.rootStore = rootStore;
+        when(() => !!this.authUser, this.saveOrUpdateUserProfile);
     }
 
     @action setAuthUser = (authUser: User | null): void => {
         this.authUser = authUser;
+        if (!!authUser) {
+            this.userProfile = new Document(`users/${authUser.uid}`);
+        } else {
+            this.userProfile = null;
+        }
+    };
+
+    saveOrUpdateUserProfile = () => {
+        this.userProfile?.set(
+            {
+                displayName: this.authUser?.displayName,
+                photoURL: this.authUser?.photoURL,
+                ...this.userProfile.data
+            },
+            { merge: true }
+        );
     };
 }
 
