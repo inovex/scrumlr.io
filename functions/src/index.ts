9a67a57ddeab47efdcfa662b18d5bf618ce8e1bf
bitionaire <johann.boehler@scrumlr.io>
2020-04-15 12:53:45 +0200

    Fixed linting issues with functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,10 +6,15 @@ const db = admin.firestore();
 
 exports.onResetVoting = functions.firestore.document('boards/{boardId}').onUpdate((change, context) => {
    if (change.before.data()!.voting && !change.after.data()!.voting) {
-       db.collection(`boards/${context.params.boardId}/members`).get().then((collection) => {
+       const boardId = context.params.boardId;
+       db.collection(`boards/${boardId}/members`).get().then((collection) => {
            collection.forEach((member) => {
-               member.ref.update({ votes: null });
+               member.ref.update({ votes: null }).catch((reason) => {
+                   console.error(`failed to reset votes for member ${member.id} on board ${boardId}`, reason);
+               });
            });
-       })
+       }).catch((reason) => {
+           console.error(`could not retrieve members from board ${boardId}`, reason);
+       });
    }
 });
