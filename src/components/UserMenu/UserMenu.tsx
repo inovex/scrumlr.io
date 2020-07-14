b508b3a196efa51ac6e17d86ddfc36a731299bbd
Anna <asinitsyna@inovex.de>
2020-07-14 10:23:44 +0200

    Refactor layout to adapt to new CSV functionality

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
-const reactDDMenu = require('react-dd-menu');
-const DropdownMenu = reactDDMenu.DropdownMenu;
-
 import './UserMenu.scss';
 import Icon from '../Icon/Icon';
 import Input from '../Input/Input';
 import MenuItem from './MenuItem';
 import { ModalType } from '../../types';
+
+const reactDDMenu = require('react-dd-menu');
+const DropdownMenu = reactDDMenu.DropdownMenu;
+
 import classNames = require('classnames');
 
 export interface UserMenuProps {
@@ -159,25 +160,41 @@ export class UserMenu extends React.Component<UserMenuProps, UserMenuState> {
 
         {isLastPhase && (
           <li key="user-menu__export" className="user_menu__li--hidden-mobile">
-            <MenuItem name="Export" icon="download" onClick={onExport} />
+            <div className="user-menu__with-buttons">
+              <Icon className="menu-item__button-icon" name="download" />
+              <span
+                className={classNames(
+                  'menu-item__button-text',
+                  'user-menu__with-buttons__text'
+                )}
+              >
+                Export
+              </span>
+              <button
+                className="user-menu__with-buttons__button"
+                onClick={onExport}
+              >
+                PDF
+              </button>
+            </div>
           </li>
         )}
 
         {admin && (
           <>
             <li key="user-menu__timer" className="user_menu__li-hidden-mobile">
-              <div className="user-menu__timer">
+              <div className="user-menu__with-buttons">
                 <Icon className="menu-item__button-icon" name="timer" />
                 <span
                   className={classNames(
                     'menu-item__button-text',
-                    'user-menu__timer__text'
+                    'user-menu__with-buttons__text'
                   )}
                 >
                   Timer
                 </span>
                 <button
-                  className="user-menu__timer__button"
+                  className="user-menu__with-buttons__button"
                   onClick={() => {
                     onSetTimer(3 * 60);
                     this.setState({ isOpen: false });
@@ -186,7 +203,7 @@ export class UserMenu extends React.Component<UserMenuProps, UserMenuState> {
                   3 min.
                 </button>
                 <button
-                  className="user-menu__timer__button"
+                  className="user-menu__with-buttons__button"
                   onClick={() => {
                     onSetTimer(5 * 60);
                     this.setState({ isOpen: false });
@@ -195,7 +212,7 @@ export class UserMenu extends React.Component<UserMenuProps, UserMenuState> {
                   5
                 </button>
                 <button
-                  className="user-menu__timer__button"
+                  className="user-menu__with-buttons__button"
                   onClick={() => {
                     onSetTimer(10 * 60);
                     this.setState({ isOpen: false });
