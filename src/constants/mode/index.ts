0985c9ec7d9ee814e3ca82b04f2831875863844b
bitionaire <johann.boehler@masin.io>
2020-07-30 11:00:00 +0200

    Readded meetup mode in case some boards exist

--- src/constants/mode/index.ts
+++ src/constants/mode/index.ts
@@ -5,6 +5,7 @@ import simpleRetroModeConfiguration from './PositiveNegativeMode';
 import startStopContinueRetroModeConfiguration from './StartStopContinueMode';
 import plusDeltaConfiguration from './PlusDeltaMode';
 import fourLconfiguration from './4L';
+import meetupConfiguration from './MeetupMode';
 import { PhaseConfiguration } from '../Retrospective';
 
 interface RetroModeConfiguration {
@@ -15,6 +16,7 @@ interface RetroModeConfiguration {
   kalm: PhaseConfiguration[];
   plusDelta: PhaseConfiguration[];
   fourL: PhaseConfiguration[];
+  meetup: PhaseConfiguration[];
 }
 
 export const retroModes: RetroModeConfiguration = {
@@ -24,7 +26,8 @@ export const retroModes: RetroModeConfiguration = {
   startStopContinue: startStopContinueRetroModeConfiguration,
   kalm: kalmConfiguration,
   plusDelta: plusDeltaConfiguration,
-  fourL: fourLconfiguration
+  fourL: fourLconfiguration,
+  meetup: meetupConfiguration
 };
 
 export type RetroMode = keyof RetroModeConfiguration;
