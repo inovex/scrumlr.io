73e59cb3c8a24be1fda75014c049dec27a534406
Manuel Brandstetter <brandstetterma@aol.com>
2020-09-04 09:58:35 +0200

    Update GitHub workflow and delete unnecessary App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
