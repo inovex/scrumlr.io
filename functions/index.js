4f913a99d92fdafe3478e9b4f7a936b8b251e6ad
Anna <asinitsyna@inovex.de>
2020-09-24 15:43:56 +0200

    Implement firebase function for deleting and obfuscating user data

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+import * as Raven from "raven-js";
+
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 
@@ -44,6 +46,42 @@ exports.scheduledDatabaseCleanup = functions.pubsub.schedule('30 3 * * *').onRun
   });
 });
 
+exports.deleteUserData = functions.https.onCall((data, context) => {
+  const userId = data.userId;
+
+  const toUpdate = [];
+
+  const app = admin.initializeApp();
+  const query = admin.database().ref('/boards');
+  return query.once("value")
+      .then((snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+          const childData = childSnapshot.val();
+
+          if (childData.private.users[userId]) {
+
+            toUpdate.push(childSnapshot.child('private/users/' + userId).remove());
+
+            // Obfuscate username and userId in cards written by that user
+            Object.keys(childData.private.cards).forEach((cardId) => {
+              if (childData.private.cards[cardId].authorUid === userId) {
+                toUpdate.push(childSnapshot.child('private/cards/' + cardId).update({
+                  authorUid: '0000000',
+                  author: '???'
+                }));
+              }
+            })
+          }
+        });
+      }).then(() => {
+        Promise.all(toUpdate).then((values) => {
+          console.log(`deleted ${values.length} entries`);
+          app.delete().catch((error) => {
+            console.error('unable to stop app', error);
+          });
+        }).then(() => exitFunction());
+      });
+});
 
 
 
