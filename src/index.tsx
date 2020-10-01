c4509a5784e6b9cd46cb56be913901ac0bbc934a
Manuel Brandstetter <brandstetterma@aol.com>
2020-10-01 08:00:13 +0200

    Change import order

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-
 import { Provider } from 'react-redux';
-
 import {ReactReduxFirebaseProvider } from 'react-redux-firebase';
 import { createFirestoreInstance } from 'redux-firestore';
-
 import firebase from './firebase';
 import store from './store/store';
+import './index.scss';
 
 const rrfProps = {
   firebase,
