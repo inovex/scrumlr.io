a06181f4bd5b3f9747171e6a6c49267ca0e11e0c
Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>
2020-10-01 09:50:39 +0200

    Add Firebase configuration to React (#187)
    * Add firebase environment setup
    
    * Add .env template
    
    * Move firebase initialization to own file and delete unnecessary service worker
    
    * Fix yarn.lock
    
    * Move redux stuff into own folder
    
    * Move react redux firebase config
    
    * Remove config since we will save users manually in their corresponding boards
    
    * Change import order

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from './firebase';
+import store from './store/store';
 import './index.scss';
-import * as serviceWorker from './serviceWorker';
+
+const rrfProps = {
+  firebase,
+  config: {},
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
