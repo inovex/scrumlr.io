93fb300c8ff4c689caa008835d019643de956797
Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>
2020-10-12 11:52:29 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import {ReactReduxFirebaseProvider } from 'react-redux-firebase';
 import { createFirestoreInstance } from 'redux-firestore';
-<<<<<<< HEAD
 import './index.scss';
 import firebase from './firebaseSetup';
 import store from './store/store';
@@ -18,28 +17,12 @@ const rrfProps = {
   dispatch: store.dispatch,
   createFirestoreInstance
 };
-=======
-import firebase from './firebase';
-import store from './store/store';
-import './index.scss';
-
-const rrfProps = {
-  firebase,
-  config: {},
-  dispatch: store.dispatch,
-  createFirestoreInstance
-};
-
->>>>>>> main
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ReactReduxFirebaseProvider {...rrfProps}>
-<<<<<<< HEAD
         <Router/>
-=======
->>>>>>> main
       </ReactReduxFirebaseProvider>
     </Provider>
   </React.StrictMode>,
