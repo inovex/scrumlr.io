df85f9bf81ab9aeacc074bd3b680edeb0bd7a93a
bitionaire <jboehler@scrumlr.io>
2020-11-02 16:22:16 +0100

    Improved performance

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './Board.scss';
 import {getColorClassName} from "../../constants/colors";
 import {ColumnProps} from "../Column/Column";
@@ -8,13 +8,16 @@ export interface BoardProps {
 }
 
 const Board = ({ children }: BoardProps) => {
-    let columns: boolean[] = [];
+    const [ showNavigation, setShowNavigation ] = useState<boolean>(false);
+    const boardRef = useRef<HTMLDivElement>(null);
+    const columnVisibilityStatesRef = useRef<boolean[]>([]);
 
     useEffect(() => {
         const board: HTMLDivElement = boardRef.current!;
 
-        columns = new Array(board.childElementCount - 2);
-        columns.fill(false);
+        columnVisibilityStatesRef.current = new Array(board.childElementCount - 2);
+        const columnVisibilityStates = columnVisibilityStatesRef.current;
+        columnVisibilityStates.fill(false);
 
         const observerOptions = {
             root: boardRef.current,
@@ -25,8 +28,9 @@ const Board = ({ children }: BoardProps) => {
         const observerCallback: IntersectionObserverCallback = (entries) => {
             entries.forEach((entry) => {
                 const index = Array.prototype.indexOf.call(board.children, entry.target) - 1;
-                columns[index] = entry.isIntersecting;
-            })
+                columnVisibilityStates[index] = entry.isIntersecting;
+            });
+            setShowNavigation(columnVisibilityStates.filter(value => !value).length > 0);
         }
 
         const observer = new IntersectionObserver(observerCallback, observerOptions);
@@ -41,24 +45,25 @@ const Board = ({ children }: BoardProps) => {
         }
     }, []);
 
-    const boardRef = useRef<HTMLDivElement>(null);
     const colors = React.Children.map(children, (child) => child.props.color);
 
     if (!children || React.Children.count(children) === 0) {
         return <div className="board--empty">Empty board</div>;
     }
 
+    const columnVisibilityStates = columnVisibilityStatesRef.current;
+
     const handlePreviousClick = () => {
-        const firstVisibleColumnIndex = columns.findIndex((value) => value);
+        const firstVisibleColumnIndex = columnVisibilityStates.findIndex((value) => value);
         const scrollToIndex = firstVisibleColumnIndex > 0 ? firstVisibleColumnIndex : colors.length;
         boardRef.current!.children[scrollToIndex].scrollIntoView({ inline: 'start', behavior: 'smooth' });
     }
 
     const handleNextClick = () => {
-        if (columns[columns.length - 1]) {
+        if (columnVisibilityStates[columnVisibilityStates.length - 1]) {
             boardRef.current!.children[1].scrollIntoView({ inline: 'start', behavior: 'smooth'});
         } else {
-            const firstVisibleColumnIndex = columns.findIndex((value) => value);
+            const firstVisibleColumnIndex = columnVisibilityStates.findIndex((value) => value);
             const scrollToIndex = firstVisibleColumnIndex <= colors.length - 1 ? firstVisibleColumnIndex + 2 : 1;
             boardRef.current!.children[scrollToIndex].scrollIntoView({ inline: 'start', behavior: 'smooth' });
         }
@@ -69,13 +74,13 @@ const Board = ({ children }: BoardProps) => {
             <style>
                 {`.board { --board__columns: ${React.Children.count(children)} }`}
             </style>
-            <button className="board__navigation-left" onClick={handlePreviousClick}>Previous</button>
+            {showNavigation && <button className="board__navigation-left" onClick={handlePreviousClick}>Previous</button>}
             <div className="board" ref={boardRef}>
                 <div className={`board__spacer-left ${getColorClassName(colors[0])}`} />
                     {children}
                 <div className={`board__spacer-right ${getColorClassName(colors[colors.length - 1])}`} />
             </div>
-            <button className="board__navigation-right" onClick={handleNextClick}>Next</button>
+            {showNavigation && <button className="board__navigation-right" onClick={handleNextClick}>Next</button>}
         </>
     )
 };
