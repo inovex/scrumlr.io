6e4298cb72e79b9627283e2c9b1f78565f056669
bitionaire <jboehler@scrumlr.io>
2020-11-02 12:26:09 +0100

    Added navigation buttons

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './Board.scss';
 import {getColorClassName} from "../../constants/colors";
 import {ColumnProps} from "../Column/Column";
@@ -8,21 +8,75 @@ export interface BoardProps {
 }
 
 const Board = ({ children }: BoardProps) => {
+    const [columns, setColumns] = useState<boolean[]>([]);
+    useEffect(() => {
+        const board: HTMLDivElement = boardRef.current!;
+
+        const columnsState: boolean[] = new Array(board.childElementCount - 2);
+        columnsState.fill(false);
+        setColumns(columnsState);
+
+        const observerOptions = {
+            root: boardRef.current,
+            rootMargin: '0px',
+            threshold: 1.0
+        }
+
+        const observerCallback: IntersectionObserverCallback = (entries) => {
+            entries.forEach((entry) => {
+                const index = Array.prototype.indexOf.call(board.children, entry.target) - 1;
+                columnsState[index] = entry.isIntersecting;
+            })
+            setColumns([...columnsState]);
+        }
+
+        const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+        const children = board.children;
+        for (let i = 1; i < children.length - 1; i++) {
+            observer.observe(children[i]);
+        }
+
+        return () => {
+            observer.disconnect();
+        }
+    }, []);
+
+    const boardRef = useRef<HTMLDivElement>(null);
+    const colors = React.Children.map(children, (child) => child.props.color);
+
     if (!children || React.Children.count(children) === 0) {
         return <div className="board--empty">Empty board</div>;
     }
-    const colors = React.Children.map(children, (child) => child.props.color);
+
+    const handlePreviousClick = () => {
+        const firstVisibleColumnIndex = columns.findIndex((value) => value);
+        const scrollToIndex = firstVisibleColumnIndex > 0 ? firstVisibleColumnIndex : colors.length;
+        boardRef.current!.children[scrollToIndex].scrollIntoView({ inline: 'start', behavior: 'smooth' });
+    }
+
+    const handleNextClick = () => {
+        if (columns[columns.length - 1]) {
+            boardRef.current!.children[1].scrollIntoView({ inline: 'start', behavior: 'smooth'});
+        } else {
+            const firstVisibleColumnIndex = columns.findIndex((value) => value);
+            const scrollToIndex = firstVisibleColumnIndex <= colors.length - 1 ? firstVisibleColumnIndex + 2 : 1;
+            boardRef.current!.children[scrollToIndex].scrollIntoView({ inline: 'start', behavior: 'smooth' });
+        }
+    }
 
     return (
         <>
             <style>
                 {`.board { --board__columns: ${React.Children.count(children)} }`}
             </style>
-            <div className="board">
+            <button className="board__navigation-left" onClick={handlePreviousClick}>Previous</button>
+            <div className="board" ref={boardRef}>
                 <div className={`board__spacer-left ${getColorClassName(colors[0])}`} />
                     {children}
                 <div className={`board__spacer-right ${getColorClassName(colors[colors.length - 1])}`} />
             </div>
+            <button className="board__navigation-right" onClick={handleNextClick}>Next</button>
         </>
     )
 };
