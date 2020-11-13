911a6aa65634bbf05e435ce870e5b04e4277e369
Manuel Brandstetter <brandstetterma@aol.com>
2020-11-13 18:45:40 +0100

    Add error handling to authentication methods with provisional messages

--- src/utils/authentication/AuthenticationManager.ts
+++ src/utils/authentication/AuthenticationManager.ts
@@ -1,21 +1,25 @@
 import firebase from 'firebaseSetup';
+import {Toast} from 'utils/Toast';
 
 export const AuthenticationManager = {
     signInAnonymously,
 };
 
-function updateProfile(displayName?: string, photoURL?: string) {
-    firebase.auth().currentUser?.updateProfile({
-        displayName,
-        photoURL
-    });
-
-    //TODO: Add Toast to display Success/Error Messages
+async function updateProfile(displayName?: string, photoURL?: string) {
+    try {
+        await firebase.auth().currentUser?.updateProfile({displayName,photoURL});
+        Toast.success("Successfully updated profile");
+    } catch(err) {
+        Toast.error("Your profile name and/or picture could not be updated");
+    }    
 }
 
 async function signInAnonymously(displayName?: string, photoURL?: string) {
-    await firebase.auth().signInAnonymously();
-    updateProfile(displayName, photoURL);
-
-    //TODO: Add Toast to display Success/Error Messages
+    try {
+        await firebase.auth().signInAnonymously();
+        updateProfile(displayName, photoURL);
+        Toast.success("Successfully signed in");
+    } catch(err) {
+        Toast.error("There occured a problem while signing in");
+    }   
 }
