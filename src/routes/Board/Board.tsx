0a53e3d7d6278e2ebf94496791f594d163f850bb
Manuel Brandstetter <brandstetterma@aol.com>
2020-12-04 18:33:56 +0100

    Initial commit with beginning/basic functionality

--- src/routes/Board/Board.tsx
+++ src/routes/Board/Board.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { useSelector } from 'react-redux';
 import LoadingScreen from "components/LoadingScreen/LoadingScreen";
-import {useEffect} from "react";
 import {ApplicationState} from "../../types/store";
 import store from "../../store";
 import {ActionFactory} from "../../store/action";
@@ -10,14 +9,6 @@ import {ActionFactory} from "../../store/action";
 export interface BoardProps extends RouteComponentProps<{id: string}> {}
 
 function Board(props: BoardProps) {
-    useEffect( () => {
-        const boardId = props.match.params.id;
-        store.dispatch(ActionFactory.joinBoard(boardId));
-
-        return () => {
-            store.dispatch(ActionFactory.leaveBoard());
-        }
-    }, [ props.match.params.id ]);
 
     const state: any = useSelector((state: ApplicationState) => ({
         board: state.board,
---
+++ src/routes/Board/BoardGuard.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect} from 'react';
+import store from "store";
+import {ActionFactory} from "store/action";
+import {RouteComponentProps} from "react-router";
+import Board from "./Board";
+
+// Redux Stuff
+import {ApplicationState} from "types/store";
+import {useSelector} from "react-redux";
+
+//Additional components
+import LoadingScreen from "components/LoadingScreen/LoadingScreen";
+
+export interface BoardGuardProps extends RouteComponentProps<{id: string}> {}
+
+const BoardGuard = (props: BoardGuardProps) => {
+
+    const boardStatus = useSelector((state: ApplicationState) => state.board.status);
+
+    useEffect( () => {
+        const boardId = props.match.params.id;
+        store.dispatch(ActionFactory.joinBoard(boardId));
+
+        return () => {
+            store.dispatch(ActionFactory.leaveBoard());
+        }
+    }, [ props.match.params.id ]);
+
+    if (boardStatus === 'accepted' || boardStatus === 'ready') {
+        return (<Board {...props}/>);
+    } else if (boardStatus === 'rejected') {
+        return 'You have been rejected';
+    } else {
+        //TODO Add msg prop to LoadingScreen to display a message underneath loading spinner
+        return (<LoadingScreen/>);
+    }
+}
+
+export default BoardGuard;
