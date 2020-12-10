bafd0f4324a840c011af91ab79725e1d8a620117
alphapfote <alphapfote@gmail.com>
2020-12-10 17:47:15 +0100

    Component NoteInput

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Column.scss';
 import {Color, getColorClassName} from "constants/colors";
-import {ReactComponent as PlusIcon} from "assets/icon-add.svg";
+import NoteInput from "components/NoteInput/NoteInput";
 
 export interface ColumnProps {
     color: Color;
@@ -16,10 +16,7 @@ const Column = ({ color, children } : ColumnProps) => {
                     <h1 className="column__header-text">{children}</h1>
                     <p className="column__header-card-number">5</p>
                 </div>
-                <div className="note-input">
-                    <p className="note-input__text">Add your note...</p>
-                    <PlusIcon className="note-input__icon"/>
-                </div>
+                <NoteInput/>
             </div>
         </section>
     );
