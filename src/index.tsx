1e9a1a5bf79ef5ab42ad0dd62918f5b539933bf5
Johann Böhler <bitionaire@users.noreply.github.com>
2020-12-10 09:20:10 +0100

    Configurable server API URL (#244)

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { ToastContainer } from 'react-toastify';
 import Parse from 'parse';
 
 Parse.initialize('Scrumlr');
-Parse.serverURL = 'http://localhost:4000/api';
+Parse.serverURL = process.env.REACT_APP_SERVER_API_URL || 'http://localhost:4000';
 
 ReactDOM.render(
   <React.StrictMode>
