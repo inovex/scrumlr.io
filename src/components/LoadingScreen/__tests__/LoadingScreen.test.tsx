692b2d636c7b000e748c95e53d0808a4643c36ad
Manuel Brandstetter <brandstetterma@aol.com>
2020-12-14 12:36:49 +0100

    Update loading screen test and snapshots

--- src/components/LoadingScreen/__tests__/LoadingScreen.test.tsx
+++ src/components/LoadingScreen/__tests__/LoadingScreen.test.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import {render} from '@testing-library/react';
 import LoadingScreen from '../LoadingScreen';
 
-test('render loading screen', () => {
+test('render loading screen without info text', () => {
     const {container} = render(<LoadingScreen/>);
+    expect(container.firstChild).toMatchSnapshot();
+});
+
+test('render loading screen with info text', () => {
+    const {container} = render(<LoadingScreen info='Test'/>);
     expect(container.firstChild).toMatchSnapshot();
 });
