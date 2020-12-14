1e58943645ee4647c0fa4a35b74ce4b2fee9d165
Manuel Brandstetter <brandstetterma@aol.com>
2020-12-14 21:58:42 +0100

    Add waiting queue mechanism for join requests (unstyled)

--- src/routes/Board/Board.tsx
+++ src/routes/Board/Board.tsx
@@ -4,22 +4,44 @@ import LoadingScreen from "components/LoadingScreen/LoadingScreen";
 import BoardComponent from "components/Board/Board";
 import Column from "components/Column/Column";
 import {ApplicationState} from "types/store";
+import store from "../../store";
+import {ActionFactory} from "../../store/action";
+
 
 function Board() {
 
     const state = useSelector((state: ApplicationState) => ({
-        board: state.board
+        board: state.board,
+        joinRequests: state.joinRequests,
+        users: state.users
     }));
+
+    let waitingUser;
+    if (true) {
+        const pendingJoinRequests = state.joinRequests.filter(joinRequest => joinRequest.status === 'pending');
+        if (pendingJoinRequests && pendingJoinRequests.length > 0) {
+            waitingUser = (<div>
+                {JSON.stringify(pendingJoinRequests[0])}
+                <button onClick={_ => store.dispatch(ActionFactory.acceptJoinRequest(pendingJoinRequests[0].id, pendingJoinRequests[0].boardId, pendingJoinRequests[0].userId))}>Accept</button>
+                <button onClick={_ => store.dispatch(ActionFactory.rejectJoinRequest(pendingJoinRequests[0].id, pendingJoinRequests[0].boardId, pendingJoinRequests[0].userId))}>Reject</button>
+            </div>);
+        }
+    }
 
     if (state.board.status === 'pending') {
         return <LoadingScreen/>;
     } else if (state.board.status === 'ready') {
         return (
-            <BoardComponent>
-                {
-                    state.board.data!.columns.map((column) => (<Column color="pink">{column.name}</Column>))
-                }
-            </BoardComponent>);
+            <>
+                {waitingUser}
+                <BoardComponent>
+                    {
+                        state.board.data!.columns.map((column) => (<Column color="pink">{column.name}</Column>))
+                    }
+                </BoardComponent>);
+
+                </>
+        )
     } else {
         return <LoadingScreen/>;
     }
