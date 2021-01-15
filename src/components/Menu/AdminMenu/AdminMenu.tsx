75f703e8eaa506a85b5dbe7677175424ebfdce63
Manuel Brandstetter <brandstetterma@aol.com>
2021-01-15 11:15:04 +0100

    Enhance css

--- src/components/Menu/AdminMenu/AdminMenu.tsx
+++ src/components/Menu/AdminMenu/AdminMenu.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './AdminMenu.scss';
+import {ReactComponent as TimerIcon} from "assets/icon-timer-white.svg";
 
 function AdminMenu() {
 
     return (<div className='admin-menu'>
-
+        <div className='admin-menu__items'>
+            <button className='admin-menu__item'>
+                <TimerIcon className='admin-menu__item-icon'/>
+            </button>
+            <button className='admin-menu__item'>
+                <TimerIcon className='admin-menu__item-icon'/>
+            </button>
+            <button className='admin-menu__item'>
+                <TimerIcon className='admin-menu__item-icon'/>
+            </button>
+            <button className='admin-menu__item'>
+                <TimerIcon className='admin-menu__item-icon'/>
+            </button>
+        </div>
     </div>);
 }
 
