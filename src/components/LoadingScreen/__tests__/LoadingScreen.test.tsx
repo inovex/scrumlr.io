033d44a9cdf05cdd3e4d4c91c76ba902c944bc72
Manuel Brandstetter <brandstetterma@aol.com>
2021-01-18 12:53:56 +0100

    Change test

--- src/components/LoadingScreen/__tests__/LoadingScreen.test.tsx
+++ src/components/LoadingScreen/__tests__/LoadingScreen.test.tsx
@@ -2,12 +2,7 @@ import * as React from 'react';
 import {render} from '@testing-library/react';
 import LoadingScreen from '../LoadingScreen';
 
-test('render loading screen without info text', () => {
+test('render loading screen', () => {
     const {container} = render(<LoadingScreen/>);
-    expect(container.firstChild).toMatchSnapshot();
-});
-
-test('render loading screen with info text', () => {
-    const {container} = render(<LoadingScreen info='Test'/>);
     expect(container.firstChild).toMatchSnapshot();
 });
