4bfa7b744f2b94c4080336cc4c3504ff33eca715
Manuel Brandstetter <brandstetterma@aol.com>
2021-01-25 14:23:04 +0100

    Add red and white colored icons for different color schemes

--- src/components/Menu/AdminMenu/AdminMenu.tsx
+++ src/components/Menu/AdminMenu/AdminMenu.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import './AdminMenu.scss';
-import {ReactComponent as TimerIcon} from "assets/icon-timer-white.svg";
+import {ReactComponent as TimerIconWhite} from "assets/icon-timer-white.svg";
+import {ReactComponent as TimerIconRed} from "assets/icon-timer-red.svg";
 
 function AdminMenu() {
 
     return (<div className='admin-menu'>
         <div className='admin-menu__items'>
             <button className='admin-menu__item'>
-                <TimerIcon className='admin-menu__item-icon'/>
+                <TimerIconWhite className='admin-menu__item-icon--white'/>
+                <TimerIconRed className='admin-menu__item-icon--red'/>
             </button>
             <button className='admin-menu__item'>
-                <TimerIcon className='admin-menu__item-icon'/>
+                <TimerIconWhite className='admin-menu__item-icon--white'/>
+                <TimerIconRed className='admin-menu__item-icon--red'/>
             </button>
             <button className='admin-menu__item'>
-                <TimerIcon className='admin-menu__item-icon'/>
+                <TimerIconWhite className='admin-menu__item-icon--white'/>
+                <TimerIconRed className='admin-menu__item-icon--red'/>
             </button>
             <button className='admin-menu__item'>
-                <TimerIcon className='admin-menu__item-icon'/>
+                <TimerIconWhite className='admin-menu__item-icon--white'/>
+                <TimerIconRed className='admin-menu__item-icon--red'/>
             </button>
         </div>
     </div>);
