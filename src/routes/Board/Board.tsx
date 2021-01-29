46f3893305f8ac313eccb597de9fba9ed62e4698
alphapfote <alphapfote@gmail.com>
2021-01-29 12:10:56 +0100

    Add note (WIP)

--- src/routes/Board/Board.tsx
+++ src/routes/Board/Board.tsx
@@ -3,7 +3,7 @@ import { RouteComponentProps } from 'react-router';
 import { useSelector } from 'react-redux';
 import LoadingScreen from "components/LoadingScreen/LoadingScreen";
 import BoardComponent from "components/Board/Board";
-import Column from "components/Column/Column";
+// import Column from "components/Column/Column";
 import {useEffect} from "react";
 import {ApplicationState} from "../../types/store";
 import store from "../../store";
@@ -22,25 +22,47 @@ function Board(props: BoardProps) {
         }
     }, [ props.match.params.id ]);
 
-    const state = useSelector((state: ApplicationState) => ({
+    //const state = useSelector((state: ApplicationState) => ({
+    const state:any = useSelector((state: ApplicationState) => ({
         board: state.board,
         notes: state.notes,
         users: state.users.all
     }));
 
+    const onAddNote = () => {
+        store.dispatch(ActionFactory.addNote(state.board.data.columns[0].id, 'Test'));
+    }
+
+    const onDeleteNote = (id: string) => {
+        store.dispatch(ActionFactory.deleteNote(id));
+    }
+
     if (state.board.status === 'pending') {
         return <LoadingScreen/>;
     } else if (state.board.status === 'ready') {
         return (
-            <BoardComponent>
-                {
-                    state.board.data!.columns.map((column) => (<Column color="pink">{column.name}</Column>))
-                }
-            </BoardComponent>);
-        
+            <>
+            <ul>
+                <li>{ JSON.stringify(state.board.data) }</li>
+                <li>
+                    <ul>
+                        {state.notes.map((note: any, index: number) => <li key={index}>
+                            {JSON.stringify(note)}
+                            <button onClick={() => { onDeleteNote(note.id) }}>Delete Note</button>
+                        </li>)}
+                    </ul>
+                </li>
+                <button onClick={onAddNote}>Add Note</button>
+            </ul>);
+                <BoardComponent>
+                    {
+                        // state.board.data!.columns.map((column) => (<Column color="pink">{column.name}</Column>))
+                    }
+                </BoardComponent>);
+            </>
+        )
     } else {
         return <LoadingScreen/>;
     }
 }
-
 export default Board;
