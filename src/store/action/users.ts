3bea6038c53ec3080b49e8985fd389ae6e1731fd
Muhannad Ali <mali@inovex.de>
2021-02-19 11:44:34 +0100

    fix merge

--- src/store/action/users.ts
+++ src/store/action/users.ts
@@ -14,24 +14,23 @@ export const UsersActionType = {
 
 /** Factory or creator class of internal Redux board users object specific actions. */
 export const UsersActionFactory = {
-    /*
-     * ATTENTION:
-     * Each action creator should be also listed in the type `UsersReduxAction`, because
-     * the type inference won't work otherwise (e.g. in reducers).
-     */
-    /**
-     * Creates an action which should be dispatched when the server notifies about a changed list of users
-     * for the current board.
-     *
-     * @param users the list of users
-     * @param admin flag which indicates whether the list of users contains all admins or basic users
-     */
-    setUsers: (users: UserClientModel[], admin: boolean) => ({
-        type: UsersActionType.SetUsers,
-        users,
-        admin
-    })
-}
+  /*
+   * ATTENTION:
+   * Each action creator should be also listed in the type `UsersReduxAction`, because
+   * the type inference won't work otherwise (e.g. in reducers).
+   */
+  /**
+   * Creates an action which should be dispatched when the server notifies about a changed list of users
+   * for the current board.
+   *
+   * @param users the list of users
+   * @param admin flag which indicates whether the list of users contains all admins or basic users
+   */
+  setUsers: (users: UserClientModel[], admin: boolean) => ({
+    type: UsersActionType.SetUsers,
+    users,
+    admin,
+  }),
 
   setUserStatus: (userId: string, status: boolean) => ({
     type: UsersActionType.SetUserStatus,
