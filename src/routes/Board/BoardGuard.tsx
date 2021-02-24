b9d2a67884e096e085dbff79c7e8083a6a40a3dc
Johann Böhler <bitionaire@users.noreply.github.com>
2021-02-24 10:42:14 +0100

    Major upgrade of dependencies (#282)

--- src/routes/Board/BoardGuard.tsx
+++ src/routes/Board/BoardGuard.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import {useEffect} from 'react';
 import store from "store";
 import {ActionFactory} from "store/action";
 import {RouteComponentProps} from "react-router";
