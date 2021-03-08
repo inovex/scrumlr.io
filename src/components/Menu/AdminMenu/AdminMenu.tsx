2a3aa8e256ca383dc0b40c96aae7479528519c9e
Manuel Brandstetter <brandstetterma@aol.com>
2021-03-08 16:15:45 +0100

    Add admin menu icons and change labels

--- src/components/Menu/AdminMenu/AdminMenu.tsx
+++ src/components/Menu/AdminMenu/AdminMenu.tsx
@@ -1,21 +1,24 @@
 import './AdminMenu.scss';
-import {ReactComponent as TimerIcon} from "assets/icon-timer-white.svg";
+import {ReactComponent as TimerIcon} from "assets/icon-timer.svg";
+import {ReactComponent as ColumnIcon} from 'assets/icon-column.svg';
+import {ReactComponent as FocusIcon} from 'assets/icon-focus.svg';
+import {ReactComponent as VoteIcon} from 'assets/icon-vote.svg';
 import MenuToggle from "../MenuToggle/MenuToggle";
 
 function AdminMenu() {
     return (<div className='admin-menu'>
         <div className='admin-menu__items'>
-            <MenuToggle index={1} direction='left' toggleStartLabel='START TIMER' toggleStopLabel='END TIMER'>
+            <MenuToggle index={1} direction='left' toggleStartLabel='START COLUMN MODE' toggleStopLabel='END COLUMN MODE'>
+                <ColumnIcon className='menu-toggle__icon menu-toggle__icon--start'/>
+            </MenuToggle>
+            <MenuToggle index={2} direction='left' toggleStartLabel='START TIMER' toggleStopLabel='STOP TIMER'>
                 <TimerIcon className='menu-toggle__icon menu-toggle__icon--start'/>
             </MenuToggle>
-            <MenuToggle index={2} direction='left' toggleStartLabel='START TIMER' toggleStopLabel='END TIMER'>
-                <TimerIcon className='menu-toggle__icon menu-toggle__icon--start'/>
+            <MenuToggle index={3} direction='left' toggleStartLabel='START VOTING PHASE' toggleStopLabel='END VOTING PHASE'>
+                <VoteIcon className='menu-toggle__icon menu-toggle__icon--start'/>
             </MenuToggle>
-            <MenuToggle index={3} direction='left' toggleStartLabel='START TIMER' toggleStopLabel='END TIMER'>
-                <TimerIcon className='menu-toggle__icon menu-toggle__icon--start'/>
-            </MenuToggle>
-            <MenuToggle index={4} direction='left' toggleStartLabel='START TIMER' toggleStopLabel='END TIMER'>
-                <TimerIcon className='menu-toggle__icon menu-toggle__icon--start'/>
+            <MenuToggle index={4} direction='left' toggleStartLabel='START FOCUSED MODE' toggleStopLabel='END FOCUSED MODE'>
+                <FocusIcon className='menu-toggle__icon menu-toggle__icon--start'/>
             </MenuToggle>
         </div>
     </div>);
