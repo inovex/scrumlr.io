d0166c295b85f0b174a17750c5289d857fe405b6
alphapfote <alphapfote@gmail.com>
2021-03-15 01:41:04 +0100

    Add testing

--- src/components/Column/__tests__/Column.test.tsx
+++ src/components/Column/__tests__/Column.test.tsx
@@ -30,20 +30,43 @@ const createColumn = () => {
 
 describe('Column', () => {
     describe('should render correctly', () => {
-
-        test('show column with correct accent-color', () => {
+        test('column has correct accent-color', () => {
             const { container } = render(createColumn());
             expect(container.firstChild).toHaveClass("column accent-color__pink");
         });
 
-        test('show header', () => {
+        test('column content is present', () => {
+            const { container } = render(createColumn());
+            expect(container.querySelector(".column").firstChild).toHaveClass("column__content");
+        });
+
+        test('column header is present', () => {
+            const { container } = render(createColumn());
+            expect(container.querySelector(".column__content").firstChild).toHaveClass("column__header");
+        });
+
+        test('column header title is present', () => {
+            const { container } = render(createColumn());
+            expect(container.querySelector(".column__header").firstChild).toHaveClass("column__header-title");
+        });
+
+        test('column header text is present', () => {
+            const { container } = render(createColumn());
+            expect(container.querySelector(".column__header-title").firstChild).toHaveClass("column__header-text");
+        });
+
+        test('column header cardnumber is present', () => {
+            const { container } = render(createColumn());
+            expect(container.querySelector(".column__header-title").lastChild).toHaveClass("column__header-card-number");
+        });
+
+        test('header text has correct title', () => {
             const { container } = render(createColumn());
             expect(container.firstChild).toHaveTextContent('Testheader');
         });
     });
 
     describe('should have correct style', () => {
-
         test('show column with correct style', () => {
             const { container } = render(createColumn());            
             expect(container.firstChild).toMatchSnapshot();
