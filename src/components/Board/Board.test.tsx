b964f29a2e06a85599a4c50b22d085776097a71f
Manuel Brandstetter <brandstetterma@aol.com>
2021-03-19 10:56:08 +0100

    Add test for Menu component, fix Board tests and add snapshots

--- src/components/Board/Board.test.tsx
+++ src/components/Board/Board.test.tsx
@@ -5,6 +5,7 @@ import Column from "components/Column/Column";
 import { Color } from "constants/colors";
 import configureStore from "redux-mock-store";
 import { Provider } from "react-redux";
+import Parse from 'parse';
 
 const mockStore = configureStore();
 
@@ -37,6 +38,8 @@ const createBoardWithColumns = (...colors: Color[]) => {
 
 describe("basic", () => {
   beforeEach(() => {
+    const mockCurrentUser = jest.fn(() => ({id: 'testId'}));
+    Parse.User.current = mockCurrentUser;
     window.IntersectionObserver = jest.fn(
       () =>
         ({
@@ -135,6 +138,8 @@ describe("basic", () => {
 
 describe("navigation", () => {
   beforeEach(() => {
+    const mockCurrentUser = jest.fn(() => ({id: 'testId'}));
+    Parse.User.current = mockCurrentUser;
     window.IntersectionObserver = jest.fn(
       () =>
         ({
