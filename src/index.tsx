c26986a93fa626da3ddb772666619b21c28735cb
bitionaire <johann.boehler@scrumlr.io>
2021-03-31 21:51:30 +0200

    Test linting

--- src/index.tsx
+++ src/index.tsx
@@ -11,10 +11,8 @@ Parse.initialize('Scrumlr');
 Parse.serverURL = process.env.REACT_APP_SERVER_API_URL || 'http://localhost:4000';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router/>
-      <ToastContainer/>
+  <React.StrictMode><Provider store={store}><Router/>
+  <ToastContainer/>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
