ea76854c9045b5686da035b7b56124ced1da611b
bitionaire <johann.boehler@masin.io>
2021-04-01 10:04:30 +0200

    Testfoo

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ Parse.serverURL = process.env.REACT_APP_SERVER_API_URL || 'http://localhost:4000
 
 ReactDOM.render(
   <React.StrictMode><Provider store={store}><Router/>
-  <ToastContainer/>
+<ToastContainer/>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
