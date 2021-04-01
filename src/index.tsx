ef2f0471458dbc6e273df46a55188595ea8f16c6
bitionaire <johann.boehler@masin.io>
2021-04-01 10:25:38 +0200

    Test

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import './index.scss';
-import store from './store';
-import Router from './routes/Router';
-import { ToastContainer } from 'react-toastify';
-import Parse from 'parse';
+import React from "react"
+import ReactDOM from "react-dom"
+import {Provider} from "react-redux"
+import "./index.scss"
+import {ToastContainer} from "react-toastify"
+import Parse from "parse"
+import store from "./store"
+import Router from "./routes/Router"
 
-Parse.initialize('Scrumlr');
-Parse.serverURL = process.env.REACT_APP_SERVER_API_URL || 'http://localhost:4000';
+Parse.initialize("Scrumlr")
+Parse.serverURL = process.env.REACT_APP_SERVER_API_URL || "http://localhost:4000"
 
 ReactDOM.render(
-  <React.StrictMode><Provider store={store}><Router/>
-<ToastContainer/>
-                   </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router />
+      <ToastContainer />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
+  document.getElementById("root")
+)
