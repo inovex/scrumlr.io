32071721fac0f399d17c524afe2037c58331c7d6
bitionaire <johann.boehler@masin.io>
2021-04-06 12:29:14 +0200

    Added semi true

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import {Provider} from "react-redux"
-import "./index.scss"
-import {ToastContainer} from "react-toastify"
-import Parse from "parse"
-import store from "./store"
-import Router from "./routes/Router"
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import "./index.scss";
+import {ToastContainer} from "react-toastify";
+import Parse from "parse";
+import store from "./store";
+import Router from "./routes/Router";
 
-Parse.initialize("Scrumlr")
-Parse.serverURL = process.env.REACT_APP_SERVER_API_URL || "http://localhost:4000"
+Parse.initialize("Scrumlr");
+Parse.serverURL = process.env.REACT_APP_SERVER_API_URL || "http://localhost:4000";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,4 +18,4 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
-)
+);
