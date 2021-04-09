215e77718f99b0b74c1a26d2c40655518ac39224
alphapfote <alphapfote@gmail.com>
2021-04-09 17:21:24 +0200

    Fix structure

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,9 +1,6 @@
 import "./Column.scss";
 import {Color, getColorClassName} from "constants/colors";
 import NoteInput from "components/NoteInput/NoteInput";
-import Note from "components/Note/Note";
-import {ApplicationState} from "types/store";
-import {useSelector} from "react-redux";
 import React from "react";
 
 export interface ColumnProps {
@@ -13,16 +10,7 @@ export interface ColumnProps {
   children?: React.ReactNode;
 }
 
-const Column = ({id, name, color, children}: ColumnProps) => {
-  const state = useSelector((state: ApplicationState) => ({
-    board: state.board,
-    notes: state.notes,
-    users: state.users,
-  }));
-
-  const notesInColumn = state.notes.filter((note) => note.columnId === id);
-
-  return (
+const Column = ({id, name, color, children}: ColumnProps) => (
     <section className={`column ${getColorClassName(color)}`}>
       <div className="column__content">
         <header className="column__header">
@@ -32,13 +20,8 @@ const Column = ({id, name, color, children}: ColumnProps) => {
           </div>
           <NoteInput columnId={id} />
         </header>
-        <ul className="column__note-list">
-          {notesInColumn.map((note: any, index: number) => (
-            <Note key={note.id} text={note.text} authorId={note.author} noteId={note.id} />
-          ))}
-        </ul>
+        <ul className="column__note-list">{children}</ul>
       </div>
     </section>
   );
-};
 export default Column;
