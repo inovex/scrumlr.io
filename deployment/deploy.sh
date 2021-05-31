02c2f1be1769b1ec8033523eb42bb282c459c49c
Manuel <accounts@manuelbrandstetter.de>
2021-05-31 20:54:39 +0200

    Enhance deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+echo "\n[1] Searching for required docker images\n"
+
+if [[ "$(docker images -q scrumlr-frontend)" == "" ]]; 
+then
+    echo "Image scrumlr-frontend: missing";
+    cd ..
+    docker build -f Dockerfile -t scrumlr-frontend .
+    cd deployment
+else
+    echo "Image scrumlr-frontend: found";
+fi
+
+if [[ "$(docker images -q scrumlr-parse)" == "" ]]; 
+then
+    echo "Image scrumlr-parse: missing";
+    cd ../server
+    docker build -f ../deployment/scrumlr-server/Dockerfile -t scrumlr-parse .
+    cd ../deployment
+else
+    echo "Image scrumlr-parse: found";
+fi
+
+if [[ "$(docker images -q scrumlr-proxy)" == "" ]]; 
+then
+    echo "Image scrumlr-proxy: missing";
+    cd scrumlr-proxy
+    docker build -f Dockerfile -t scrumlr-proxy .
+    cd ..
+else
+    echo "Image scrumlr-proxy: found";
+fi
+
+echo "\n[2] Create Kubernetes resources (e.g. deployments, services, configmaps, secrets)\n"
+
 # scrumlr-database
 kubectl create -f scrumlr-database/service.yaml
 kubectl create -f scrumlr-database/deployment.yaml
@@ -32,10 +66,14 @@ kubectl create -f scrumlr-dashboard/deployment.yaml
 kubectl create -f scrumlr-frontend/service.yaml
 kubectl create -f scrumlr-frontend/deployment.yaml
 
-
 # Wait until all deployments are available
+echo "\n[3] Wait until all deployments are available\n"
 kubectl wait --for=condition=available --timeout=60s --all deployments
 
 # Port-forwarding to service/scrumlr-proxy
+echo "\n[4] Port-forward from localhost:4000 to proxy\n"
 kubectl port-forward service/scrumlr-proxy 4000:4000
+
+# Shut down resources
+# for d in ./*/ ; do (cd "$d" && kubectl delete -f . ); done
 
