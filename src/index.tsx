10cf74f4d01d5c1b851336eb3ba011282f45cbde
Manuel <accounts@manuelbrandstetter.de>
2021-06-10 11:36:58 +0200

    Add index enviroment default api url:

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import Router from "./routes/Router";
 
 Parse.initialize("Scrumlr");
 Parse.serverURL = process.env.REACT_APP_SERVER_API_URL || "http://localhost:4000/api";
-Parse.liveQueryServerURL = process.env.REACT_APP_LIVEQUERY_URL || "ws://kubernetes.docker.internal/ws";
+Parse.liveQueryServerURL = process.env.REACT_APP_LIVEQUERY_URL || "ws://localhost:4000/api";
 
 ReactDOM.render(
   <React.StrictMode>
