aa004515c610e2d132cfd3b4f6a2dd1c33b36f61
Andreas Kraus <akraus931@google.com>
2021-07-15 12:59:23 +0200

    Animal names and adjectives for all letters supplemented.

--- src/constants/Name.ts
+++ src/constants/Name.ts
@@ -7,182 +7,115 @@ export interface Name {
   F: string[];
   G: string[];
   H: string[];
+  I: string[];
+  J: string[];
   K: string[];
   L: string[];
   M: string[];
+  N: string[];
   O: string[];
   P: string[];
+  Q: string[];
   R: string[];
   S: string[];
   T: string[];
+  U: string[];
+  V: string[];
   W: string[];
+  X: string[];
+  Y: string[];
   Z: string[];
 }
 
-
 export const ANIMAL_NAMES: Name = {
-    A: ['Alligator', 'Alpaca', 'Antelope', 'Ape'],
-    B: ['Bat', 'Bear', 'Beaver', 'Bee', 'Bird', 'Butterfly'],
-    C: ['Camel', 'Cat', 'Chicken', 'Coyote', 'Crow'],
-    D: ['Deer', 'Dinosaur', 'Dog', 'Dolphin', 'Donkey', 'Duck'],
-    E: ['Eagle', 'Elephant', 'Emu'],
-    F: ['Falcon', 'Fish', 'Flamingo', 'Fly', 'Frog'],
-    G: ['Giraffe', 'Goat', 'Goose', 'Gorilla', 'Grasshopper'],
-    H: ['Hamster', 'Hedgehog', 'Hornet', 'Horse'],
-    K: ['Koala', 'Kangaroo'],
-    L: ['Leopard', 'Lion', 'Llama'],
-    M: ['Mole', 'Monkey', 'Moose', 'Mouse', 'Mule'],
-    O: ['Opossum', 'Owl'],
-    P: ['Panda', 'Parrot', 'Penguin', 'Pig', 'Pigeon'],
-    R: ['Rabbit', 'Raccoon', 'Rat', 'Raven', 'Reindeer'],
-    S: [
-      'Seal',
-      'Seastar',
-      'Shark',
-      'Sheep',
-      'Skunk',
-      'Snake',
-      'Spider',
-      'Squirrel',
-      'Swan'
-    ],
-    T: ['Termite', 'Tiger', 'Turkey', 'Turtle'],
-    W: ['Walrus', 'Wasp', 'Weasel', 'Whale', 'Wolf', 'Wombat'],
-    Z: ['Zebra']
-  };
+  A: ["Alligator", "Alpaca", "Antelope", "Ape"],
+  B: ["Bat", "Bear", "Beaver", "Bee", "Bird", "Butterfly"],
+  C: ["Camel", "Cat", "Chicken", "Coyote", "Crow", "Clam", "Crocodile"],
+  D: ["Deer", "Dinosaur", "Dog", "Dolphin", "Donkey", "Duck", "Dragon"],
+  E: ["Eagle", "Elephant", "Emu", "Elk", "Eel"],
+  F: ["Falcon", "Fish", "Flamingo", "Fly", "Frog"],
+  G: ["Giraffe", "Goat", "Goose", "Gorilla", "Grasshopper"],
+  H: ["Hamster", "Hedgehog", "Hornet", "Horse"],
+  I: ["Ibis", "Impala", "Iguana"],
+  J: ["Jaguar", "Jackal", "Jackrabbit", "Jay", "Jellyfish"],
+  K: ["Koala", "Kangaroo", "Kiwi", "Kingfisher", "Krill"],
+  L: ["Leopard", "Lion", "Llama", "Lemur", "Lizard", "Lobster", "Lemming", "Lynx"],
+  M: ["Mole", "Monkey", "Moose", "Mouse", "Mule", "Mockingbird"],
+  N: ["Nandu", "Newt", "Narwhal", "Nighthawk", "Nutria"],
+  O: ["Opossum", "Owl", "Octopus", "Orca", "Ostrich", "Otter", "Orangutan", "Okapi", "Ocelot", "Ox", "Oyster"],
+  P: ["Panda", "Parrot", "Penguin", "Pig", "Pigeon"],
+  Q: ["Quail", "Quokka"],
+  R: ["Rabbit", "Raccoon", "Rat", "Raven", "Reindeer"],
+  S: ["Seal", "Seastar", "Shark", "Sheep", "Skunk", "Snake", "Spider", "Squirrel", "Swan"],
+  T: ["Termite", "Tiger", "Turkey", "Turtle"],
+  U: ["Unicorn", "Urchin", "Unau", "Urial"],
+  V: ["Vulture", "Vervet", "Volvox"],
+  W: ["Walrus", "Wasp", "Weasel", "Whale", "Wolf", "Wombat"],
+  X: ["Xenops", "Xerus", "Xenopus"],
+  Y: ["Yak", "Yabby"],
+  Z: ["Zebra", "Zebu"],
+};
 
+export const ADJECTIVES: Name = {
+  A: ["Adorable", "Adventurous", "Acrobatic", "Afraid", "Aggressive", "Agile", "Amazing", "Angry", "Anxious", "Ashamed", "Awesome", "Awful", "Awkward"],
+  B: ["Bad", "Beautiful", "Beloved", "Best", "Black", "Bold", "Brave", "Brilliant", "Busy"],
+  C: ["Calm", "Charming", "Cheap", "Cold", "Concerned", "Cool", "Crazy", "Creative", "Critical", "Curly", "Cute"],
+  D: ["Dangerous", "Dark", "Deadly", "Delicious", "Difficult", "Direct", "Dirty", "Dizzy"],
+  E: ["Eager", "Educated", "Elegant", "Excellent", "Excited"],
+  F: ["Fabulous", "Fake", "Fast", "Fearless", "Focused", "Foolish", "Fresh", "Funky", "Funny", "Furious", "Fearsome"],
+  G: ["Giant", "Gifted", "Good", "Grand", "Great", "Grown", "Grumpy"],
+  H: ["Hairy", "Handsome", "Happy", "Heavy", "Hot", "Huge"],
+  I: [
+    "Icy",
+    "Idealistic",
+    "Illogical",
+    "Immoral",
+    "Immortal",
+    "Imperial",
+    "Indestructible",
+    "Impeccable",
+    "Improper",
+    "Inconceivable",
+    "Incredible",
+    "Infernal",
+    "Inspiring",
+    "Intense",
+    "Irate",
+    "Interesting",
+    "Intricate",
+  ],
+  J: ["Jealous", "Jazzy", "Jurassic", "Jumpy", "Jacobean", "Jowly", "Jarring", "Justified", "Joyful", "Juvenile", "Judgmental", "Jinxed", "Jeering", "Jovial", "Jesting"],
+  K: ["Keen", "Kind", "Kissy", "Knowing", "Kinetic", "Knightly"],
+  L: ["Lame", "Large", "Lazy", "Lean", "Little", "Lonely", "Lovely", "Loyal"],
+  M: ["Mad", "Majestic", "Major", "Massive", "Mean", "Modern", "Mysterious"],
+  N: ["Naive", "Naked", "Nameless", "Narcissistic", "Nerdy", "Noble", "Nutty", "Nosy"],
+  O: ["Oily", "Odd", "Old", "Optimistic", "Ordinary", "Outstanding"],
+  P: ["Pale", "Peaceful", "Perfect", "Phony", "Pink", "Polite", "Poor", "Positive", "Pretty", "Proud"],
+  Q: ["Quaint", "Quirky", "Quixotic", "Qabalistic", "Qualified", "Quarantined", "Quarrelsome", "Queasy", "Queer", "Querulous", "Quick", "Quiet", "Quotable", "Quizzical"],
+  R: ["Rare", "Raw", "Reckless", "Remote", "Rich", "Rough", "Royal", "Rude"],
+  S: ["Sad", "Sarcastic", "Scared", "Scary", "Secret", "Sexy", "Shiny", "Silly", "Sour", "Spicy", "Strong", "Stylish", "Super", "Suspicious"],
+  T: ["Tasty", "Tempting", "Terrible", "Thin", "Tiny", "Tough", "Twin"],
+  U: ["Unstoppable", "Unbearable", "Unmovable", "Unbreakable", "Uber", "Unique", "Upbeat", "Ultra", "Ultrasonic", "Underrated", "Understanding", "Urban"],
+  V: ["Vagabond", "Valiant", "Vehement", "Velvety", "Verbose", "Versatile", "Vindictive", "Violent", "Vulnerable"],
+  W: ["Wealthy", "Weak", "Weird", "Wet", "White", "Wicked", "Wild", "Wise", "Worst"],
+  X: ["Xanthic", "Xenial"],
+  Y: ["Yappy", "Yodeling", "Youthful", "Yummy", "Yearling", "Yelping", "Young", "Yucky"],
+  Z: ["Zany", "Zealous", "Zigzag", "Zazzy", "Zappy", "Zestful", "Zen", "Zingy", "Zippy"],
+};
 
-  
-  export const ADJECTIVES: Name = {
-    A: [
-      'Adorable',
-      'Adventurous',
-      'Acrobatic',
-      'Afraid',
-      'Aggressive',
-      'Agile',
-      'Amazing',
-      'Angry',
-      'Anxious',
-      'Ashamed',
-      'Awesome',
-      'Awful',
-      'Awkward'
-    ],
-    B: [
-      'Bad',
-      'Beautiful',
-      'Beloved',
-      'Best',
-      'Black',
-      'Bold',
-      'Brave',
-      'Brilliant',
-      'Busy'
-    ],
-    C: [
-      'Calm',
-      'Charming',
-      'Cheap',
-      'Cold',
-      'Concerned',
-      'Cool',
-      'Crazy',
-      'Creative',
-      'Critical',
-      'Curly',
-      'Cute'
-    ],
-    D: [
-      'Dangerous',
-      'Dark',
-      'Deadly',
-      'Delicious',
-      'Difficult',
-      'Direct',
-      'Dirty',
-      'Dizzy'
-    ],
-    E: ['Eager', 'Educated', 'Elegant', 'Excellent', 'Excited'],
-    F: [
-      'Fabulous',
-      'Fake',
-      'Fast',
-      'Fearless',
-      'Focused',
-      'Foolish',
-      'Fresh',
-      'Funky',
-      'Funny'
-    ],
-    G: ['Giant', 'Gifted', 'Good', 'Grand', 'Great', 'Grown', 'Grumpy'],
-    H: ['Hairy', 'Handsome', 'Happy', 'Heavy', 'Hot', 'Huge'],
-    K: ['Keen', 'Kind'],
-    L: ['Lame', 'Large', 'Lazy', 'Lean', 'Little', 'Lonely', 'Lovely', 'Loyal'],
-    M: ['Mad', 'Majestic', 'Major', 'Massive', 'Mean', 'Modern', 'Mysterious'],
-    O: ['Oily', 'Odd', 'Old', 'Optimistic', 'Ordinary', 'Outstanding'],
-    P: [
-      'Pale',
-      'Peaceful',
-      'Perfect',
-      'Phony',
-      'Pink',
-      'Polite',
-      'Poor',
-      'Positive',
-      'Pretty',
-      'Proud'
-    ],
-    R: ['Rare', 'Raw', 'Reckless', 'Remote', 'Rich', 'Rough', 'Royal', 'Rude'],
-    S: [
-      'Sad',
-      'Sarcastic',
-      'Scared',
-      'Scary',
-      'Secret',
-      'Sexy',
-      'Shiny',
-      'Silly',
-      'Sour',
-      'Spicy',
-      'Strong',
-      'Stylish',
-      'Super',
-      'Suspicious'
-    ],
-    T: ['Tasty', 'Tempting', 'Terrible', 'Thin', 'Tiny', 'Tough', 'Twin'],
-    W: [
-      'Wealthy',
-      'Weak',
-      'Weird',
-      'Wet',
-      'White',
-      'Wicked',
-      'Wild',
-      'Wise',
-      'Worst'
-    ],
-    Z: ['Zany', 'Zealous', 'Zigzag']
-  };
-  
-  export const getRandomName = () => {
-    const keys = Object.keys(ANIMAL_NAMES);
-    const randomKey = keys[(keys.length * Math.random()) << 0];
+export const getRandomName = () => {
+  const keys = Object.keys(ANIMAL_NAMES);
+  const randomKey = keys[(keys.length * Math.random()) << 0];
 
-    const randomAnimal =
-      ANIMAL_NAMES[randomKey][
-        (ANIMAL_NAMES[randomKey].length * Math.random()) << 0
-      ];
-    const randomAdjective =
-      ADJECTIVES[randomKey][(ADJECTIVES[randomKey].length * Math.random()) << 0];
-  
-    return `${randomAdjective} ${randomAnimal}`;
-  };
+  const randomAnimal = ANIMAL_NAMES[randomKey][(ANIMAL_NAMES[randomKey].length * Math.random()) << 0];
+  const randomAdjective = ADJECTIVES[randomKey][(ADJECTIVES[randomKey].length * Math.random()) << 0];
 
-  export const getInitials = (name: string): string => {
-    return name.split(' ')
-      .map(n => n[0])
-      .join('');
-  }
-  
-  export default getRandomName;
+  return `${randomAdjective} ${randomAnimal}`;
+};
+
+export const getInitials = (name: string): string => name
+    .split(" ")
+    .map((n) => n[0])
+    .join("");
+
+export default getRandomName;
