8b2db55436f09f094085f482adff9c1b8fdd0acb
Andreas Kraus <akraus931@google.com>
2021-07-23 09:16:36 +0200

    Removed unknown animal names and adjectives; Added MYTHICAL_CREATURES

--- src/constants/Name.ts
+++ src/constants/Name.ts
@@ -15,45 +15,74 @@ export interface Name {
   N: string[];
   O: string[];
   P: string[];
-  Q: string[];
+  // Q: string[];
   R: string[];
   S: string[];
   T: string[];
   U: string[];
   V: string[];
   W: string[];
-  X: string[];
+  // X: string[];
   Y: string[];
   Z: string[];
 }
 
 export const ANIMAL_NAMES: Name = {
   A: ["Alligator", "Alpaca", "Antelope", "Ape"],
-  B: ["Bat", "Bear", "Beaver", "Bee", "Bird", "Butterfly"],
+  B: ["Bat", "Bear", "Beaver", "Bee", "Bird", "Butterfly", "Bookworm"],
   C: ["Camel", "Cat", "Chicken", "Coyote", "Crow", "Clam", "Crocodile"],
   D: ["Deer", "Dinosaur", "Dog", "Dolphin", "Donkey", "Duck", "Dragon"],
   E: ["Eagle", "Elephant", "Emu", "Elk", "Eel"],
   F: ["Falcon", "Fish", "Flamingo", "Fly", "Frog"],
-  G: ["Giraffe", "Goat", "Goose", "Gorilla", "Grasshopper"],
+  G: ["Giraffe", "Goat", "Goose", "Gorilla", "Grasshopper", "Griffin", "Gargoyle"],
   H: ["Hamster", "Hedgehog", "Hornet", "Horse"],
   I: ["Ibis", "Impala", "Iguana"],
-  J: ["Jaguar", "Jackal", "Jackrabbit", "Jay", "Jellyfish"],
+  J: ["Jaguar", "Jackal", "Jackrabbit", "Jellyfish"],
   K: ["Koala", "Kangaroo", "Kiwi", "Kingfisher", "Krill"],
   L: ["Leopard", "Lion", "Llama", "Lemur", "Lizard", "Lobster", "Lemming", "Lynx"],
   M: ["Mole", "Monkey", "Moose", "Mouse", "Mule", "Mockingbird"],
-  N: ["Nandu", "Newt", "Narwhal", "Nighthawk", "Nutria"],
+  N: ["Nandu", "Newt", "Narwhal", "Nighthawk"],
   O: ["Opossum", "Owl", "Octopus", "Orca", "Ostrich", "Otter", "Orangutan", "Okapi", "Ocelot", "Ox", "Oyster"],
   P: ["Panda", "Parrot", "Penguin", "Pig", "Pigeon"],
-  Q: ["Quail", "Quokka"],
+  // Q: ["Quail", "Quokka"],
   R: ["Rabbit", "Raccoon", "Rat", "Raven", "Reindeer"],
   S: ["Seal", "Seastar", "Shark", "Sheep", "Skunk", "Snake", "Spider", "Squirrel", "Swan"],
   T: ["Termite", "Tiger", "Turkey", "Turtle"],
-  U: ["Unicorn", "Urchin", "Unau", "Urial"],
-  V: ["Vulture", "Vervet", "Volvox"],
+  U: ["Unicorn", "Urchin"],
+  V: ["Vulture", "Volvox"],
   W: ["Walrus", "Wasp", "Weasel", "Whale", "Wolf", "Wombat"],
-  X: ["Xenops", "Xerus", "Xenopus"],
-  Y: ["Yak", "Yabby"],
-  Z: ["Zebra", "Zebu"],
+  // X: ["Xenops", "Xerus", "Xenopus"],
+  Y: ["Yak"],
+  Z: ["Zebra"],
+};
+
+export const MYTHICAL_CREATURES: Name = {
+  A: ["Alien", "Angel"],
+  B: ["Banshee", "Basilisk", "Bigfoot", "Bogeyman", "Boggart", "Bookworm"],
+  C: ["Centaur", "Cerberus", "Chimera", "Chtulhu", "Cyclops"],
+  D: ["Dementor", "Demon", "Devil", "Doppelganger", "Dragon", "Drake", "Draugr", "Dryad", "Dwarf"],
+  E: ["Elf", "Erlking"],
+  F: ["Fairy"],
+  G: ["Gargoyle", "Genie", "Ghost", "Ghoul", "Giant", "Gnome", "Goblin", "Golem", "Gorgon", "Gremlin", "Griffin"],
+  H: ["Hag", "Harpy", "Hellhound", "Hippogriff", "Hobgoblin", "Hydra"],
+  I: ["Ifrit", "Imp", "Incubus"],
+  J: ["Jabberwocky", "Jackalope", "Jinn"],
+  K: ["Kappa", "Kelpie", "Kobold", "Kraken"],
+  L: ["Leprechaun", "Leviathan", "Lich", "Lindworm"],
+  M: ["Magician", "Mandrake", "Manticore", "Medusa", "Merman", "Mermaid", "Minotaur", "Mummy"],
+  N: ["Naga", "Nessy", "Nymph"],
+  O: ["Ogre", "Orc", "Ouroboros"],
+  P: ["Pegasus", "Phoenix", "Pixie", "Poltergeist"],
+  // Q: [],
+  R: ["Redcap", "Revenant", "Roc", "Rusalka"],
+  S: ["Santa", "Sasquatch", "Satan", "Satyr", "Siren", "Skinwalker", "Sorcerer", "Sorceress", "Sphinx", "Sprite", "Succubus"],
+  T: ["Thunderbird", "Troll"],
+  U: ["Undead", "Unicorn"],
+  V: ["Valkyrie", "Vampire", "Veela"],
+  W: ["Warlock", "Wendigo", "Werewolf", "Witch", "Witcher", "Wizard", "Wraith", "Wyvern"],
+  // X: [],
+  Y: ["Yeti"],
+  Z: ["Zombie"],
 };
 
 export const ADJECTIVES: Name = {
@@ -91,18 +120,19 @@ export const ADJECTIVES: Name = {
   N: ["Naive", "Naked", "Nameless", "Narcissistic", "Nerdy", "Noble", "Nutty", "Nosy"],
   O: ["Oily", "Odd", "Old", "Optimistic", "Ordinary", "Outstanding"],
   P: ["Pale", "Peaceful", "Perfect", "Phony", "Pink", "Polite", "Poor", "Positive", "Pretty", "Proud"],
-  Q: ["Quaint", "Quirky", "Quixotic", "Qabalistic", "Qualified", "Quarantined", "Quarrelsome", "Queasy", "Queer", "Querulous", "Quick", "Quiet", "Quotable", "Quizzical"],
+  // Q: ["Quaint", "Quirky", "Quixotic", "Qabalistic", "Qualified", "Quarantined", "Quarrelsome", "Queasy", "Queer", "Querulous", "Quick", "Quiet", "Quotable", "Quizzical"],
   R: ["Rare", "Raw", "Reckless", "Remote", "Rich", "Rough", "Royal", "Rude"],
   S: ["Sad", "Sarcastic", "Scared", "Scary", "Secret", "Sexy", "Shiny", "Silly", "Sour", "Spicy", "Strong", "Stylish", "Super", "Suspicious"],
   T: ["Tasty", "Tempting", "Terrible", "Thin", "Tiny", "Tough", "Twin"],
   U: ["Unstoppable", "Unbearable", "Unmovable", "Unbreakable", "Uber", "Unique", "Upbeat", "Ultra", "Ultrasonic", "Underrated", "Understanding", "Urban"],
   V: ["Vagabond", "Valiant", "Vehement", "Velvety", "Verbose", "Versatile", "Vindictive", "Violent", "Vulnerable"],
   W: ["Wealthy", "Weak", "Weird", "Wet", "White", "Wicked", "Wild", "Wise", "Worst"],
-  X: ["Xanthic", "Xenial"],
+  // X: ["Xanthic", "Xenial"],
   Y: ["Yappy", "Yodeling", "Youthful", "Yummy", "Yearling", "Yelping", "Young", "Yucky"],
   Z: ["Zany", "Zealous", "Zigzag", "Zazzy", "Zappy", "Zestful", "Zen", "Zingy", "Zippy"],
 };
 
+/* random animal names
 export const getRandomName = () => {
   const keys = Object.keys(ANIMAL_NAMES);
   const randomKey = keys[(keys.length * Math.random()) << 0];
@@ -112,7 +142,18 @@ export const getRandomName = () => {
 
   return `${randomAdjective} ${randomAnimal}`;
 };
+*/
 
+// random mythical creature names
+export const getRandomName = () => {
+  const keys = Object.keys(MYTHICAL_CREATURES);
+  const randomKey = keys[(keys.length * Math.random()) << 0];
+
+  const randomAnimal = MYTHICAL_CREATURES[randomKey][(MYTHICAL_CREATURES[randomKey].length * Math.random()) << 0];
+  const randomAdjective = ADJECTIVES[randomKey][(ADJECTIVES[randomKey].length * Math.random()) << 0];
+
+  return `${randomAdjective} ${randomAnimal}`;
+};
 export const getInitials = (name: string): string =>
   name
     .split(" ")
