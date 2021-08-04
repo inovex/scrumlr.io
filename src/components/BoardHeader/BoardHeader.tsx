3f6ac8608ae99c6063f4091f6ca080e7c179dc13
Manuel <accounts@manuelbrandstetter.de>
2021-08-04 11:56:11 +0200

    Add header info hover effect

--- src/components/BoardHeader/BoardHeader.tsx
+++ src/components/BoardHeader/BoardHeader.tsx
@@ -2,7 +2,6 @@ import "./BoardHeader.scss";
 import lock from "assets/icon-lock.svg";
 import BoardUsers from "components/BoardUsers/BoardUsers";
 import HeaderLogo from "./HeaderLogo/HeaderLogo";
-import {HeaderMenu} from "./HeaderMenu/HeaderMenu";
 
 export interface BoardHeaderProps {
   boardstatus: string;
@@ -13,10 +12,12 @@ const BoardHeader = ({name, boardstatus}: BoardHeaderProps) => (
   <div className="board-header">
     <HeaderLogo />
     <div className="board-header__infos">
-      <p className="board-header__status">{boardstatus}</p>
-      <div className="board-header__title-block">
-        <img className="board-header__status-image" src={lock} alt="Private Session" />
-        <h1 className="board-header__title">{name}</h1>
+      <div className="info-block">
+        <p className="board-header__status">{boardstatus}</p>
+        <div className="board-header__title-block">
+          <img className="board-header__status-image" src={lock} alt="Private Session" />
+          <h1 className="board-header__title">{name}</h1>
+        </div>
       </div>
     </div>
     <div className="board-header__users">
