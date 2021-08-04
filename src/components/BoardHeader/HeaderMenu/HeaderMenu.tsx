7ea2b175b68adbf197f528028a28cd3eb25efdc2
Manuel <accounts@manuelbrandstetter.de>
2021-08-04 17:01:50 +0200

    Style header menu

--- src/components/BoardHeader/HeaderMenu/HeaderMenu.tsx
+++ src/components/BoardHeader/HeaderMenu/HeaderMenu.tsx
@@ -1,19 +1,51 @@
+import {useState} from "react";
 import "./HeaderMenu.scss";
 import Portal from "components/Portal/Portal";
+import {ReactComponent as DeleteIcon} from "assets/icon-delete.svg";
+import {ReactComponent as CheckIcon} from "assets/icon-check.svg";
+import classNames from "classnames";
 
 type HeaderMenuProps = {
+  boardName: string;
+  accessMode: string;
   open: boolean;
   onClose: () => void;
 };
 
 const HeaderMenu = (props: HeaderMenuProps) => {
+  const [showAuthorNames, setShowAuthorNames] = useState(false);
   if (!props.open) {
     return null;
   }
 
   return (
-    <Portal onClose={props.onClose}>
-      <div className="header-menu" />
+    <Portal onClose={props.onClose} dark={false}>
+      <ul className="header-menu">
+        <li className="header-menu__info">
+          <span className="info__board-name">{props.boardName}</span>
+          <span className="info__access-mode">{props.accessMode}</span>
+        </li>
+        <li className="header-menu__item">
+          <button className="menu__item-button" onClick={() => setShowAuthorNames(!showAuthorNames)}>
+            <div className="item-button__toggle-container">
+              <div className={classNames("item-button__toggle", {"item-button__toggle--left": !showAuthorNames}, {"item-button__toggle--right": showAuthorNames})} />
+            </div>
+            <label className="item-button__label">{showAuthorNames ? "Hide" : "Show"} authors of card</label>
+          </button>
+        </li>
+        <li className="header-menu__item">
+          <button className="menu__item-button">
+            <CheckIcon className="item-button__icon" />
+            <label className="item-button__label">Share board</label>
+          </button>
+        </li>
+        <li className="header-menu__item">
+          <button className="menu__item-button">
+            <DeleteIcon className="item-button__icon" />
+            <label className="item-button__label">Delete board</label>
+          </button>
+        </li>
+      </ul>
     </Portal>
   );
 };
