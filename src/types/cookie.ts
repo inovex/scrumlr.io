b7d65a42f3ddaa3d035651ae6079df54777fe461
Andreas Kraus <akraus931@google.com>
2021-08-12 08:14:22 +0200

    Deleted not unnecessary cookieServerModel

--- src/types/cookie.ts
+++ src/types/cookie.ts
@@ -1,11 +1,3 @@
-import Parse from "parse";
-
-/** The representation of a note on the server. */
-export interface CookieServerModel extends Parse.Object {
-  name: string;
-  value: boolean;
-}
-
 /** The representation of a note on the client. */
 export interface CookieClientModel {
   /** The cookie name: i.e. "scrumlr_cookieConsent". */
@@ -13,8 +5,3 @@ export interface CookieClientModel {
   /** The cookie value which tells if cookies are allowed. */
   value: boolean | null;
 }
-
-export const mapCookieServerModel = (cookie: CookieServerModel): CookieClientModel => ({
-  name: cookie.get("cookieName"),
-  value: cookie.get("value"),
-});
