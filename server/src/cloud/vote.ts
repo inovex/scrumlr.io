5abee3d33fbb50f73de32a936302735a87ddee1d
Manuel <accounts@manuelbrandstetter.de>
2021-08-24 08:43:40 +0200

    Add voting rules to parse cloud function

--- server/src/cloud/vote.ts
+++ server/src/cloud/vote.ts
@@ -7,15 +7,30 @@ export interface VoteRequest {
 }
 
 export const initializeVoteFunctions = () => {
-  api<VoteRequest, boolean>("addVote", async (user, request) => {
+  api<VoteRequest, {status: string; description: string}>("addVote", async (user, request) => {
     await requireValidBoardMember(user, request.board);
 
-    const board = Parse.Object.extend("Board").createWithoutData(request.board);
+    const board = await new Parse.Query("Board").get(request.board, {useMasterKey: true});
+    // Check if the board exists
+    if (!board) {
+      return {status: "Error", description: `Board '${request.board}' does not exist`};
+    }
+    // Check if 'voting' is active
+    if (board.get("voting") === "disabled") {
+      return {status: "Error", description: "Currently the vote is disabled"};
+    }
+    // Check if user will exceed vote limit
+    const voteQuery = new Parse.Query("Vote");
+    voteQuery.equalTo("board", board);
+    voteQuery.equalTo("user", user);
+    if ((await voteQuery.count({useMasterKey: true})) >= board.get("voteLimit")) {
+      return {status: "Error", description: "You have already cast all your votes"};
+    }
+
     const note = Parse.Object.extend("Note").createWithoutData(request.note);
     const vote = newObject("Vote", {board, note, user}, {readRoles: [getMemberRoleName(request.board)]});
-
     await vote.save(null, {useMasterKey: true});
-    return true;
+    return {status: "Success", description: "Your vote has been added"};
   });
 
   api<VoteRequest, boolean>("removeVote", async (user, request) => {
