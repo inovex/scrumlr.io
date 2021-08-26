0cd0fa3e066baef29f03d2adec6b72b3ddb6440a
Manuel <accounts@manuelbrandstetter.de>
2021-08-26 12:04:54 +0200

    Prevent participants to withdraw votes if voting is disabled

--- server/src/cloud/vote.ts
+++ server/src/cloud/vote.ts
@@ -17,7 +17,7 @@ export const initializeVoteFunctions = () => {
     }
     // Check if 'voting' is active
     if (board.get("voting") === "disabled") {
-      return {status: "Error", description: "Currently the vote is disabled"};
+      return {status: "Error", description: "You cannot vote while voting is disabled"};
     }
     // Check if user will exceed vote limit
     const voteQuery = new Parse.Query("Vote");
@@ -26,25 +26,34 @@ export const initializeVoteFunctions = () => {
     if ((await voteQuery.count({useMasterKey: true})) >= board.get("voteLimit")) {
       return {status: "Error", description: "You have already cast all your votes"};
     }
-
+    // Create vote for corresponding note, user and board
     const note = Parse.Object.extend("Note").createWithoutData(request.note);
     const vote = newObject("Vote", {board, note, user}, {readRoles: [getMemberRoleName(request.board)]});
     await vote.save(null, {useMasterKey: true});
+
     return {status: "Success", description: "Your vote has been added"};
   });
 
-  api<VoteRequest, boolean>("removeVote", async (user, request) => {
+  api<VoteRequest, {status: string; description: string}>("removeVote", async (user, request) => {
     const note = Parse.Object.extend("Note").createWithoutData(request.note);
+    const board = await new Parse.Query("Board").get(request.board, {useMasterKey: true});
 
     const voteQuery = new Parse.Query("Vote");
     voteQuery.equalTo("note", note);
     voteQuery.equalTo("user", user);
     const vote = await voteQuery.limit(1).first({useMasterKey: true});
-    if (vote) {
-      await vote.destroy({useMasterKey: true});
-      return true;
-    }
 
-    throw new Error(`No votes for user '${user.id}' exist on note '${request.note}'`);
+    // Check if vote exists
+    if (!vote) {
+      return {status: "Error", description: `No votes for user '${user.id}' exist on note '${request.note}'`};
+    }
+    // Check if 'voting' is active
+    if (board.get("voting") === "disabled") {
+      return {status: "Error", description: "You cannot withdraw a vote while voting is disabled"};
+    }
+    // Delete corresponding vote
+    await vote.destroy({useMasterKey: true});
+
+    return {status: "Success", description: "Your vote was withdrawn"};
   });
 };
