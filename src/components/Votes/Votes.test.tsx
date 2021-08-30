ecfc4aac562b85146aa51cadc37106c46c0cc86c
Manuel <accounts@manuelbrandstetter.de>
2021-08-30 09:20:31 +0200

    Add utility function to Votes.test.tsx to reduce code duplicates

--- src/components/Votes/Votes.test.tsx
+++ src/components/Votes/Votes.test.tsx
@@ -3,52 +3,46 @@ import store from "store";
 import {ActionFactory} from "store/action";
 import {Votes} from "./Votes";
 
+const createVotes = (withVotes: boolean, activeVoting: boolean, className?: string) => (
+  <Votes
+    noteId="test-id"
+    className={className}
+    votes={
+      withVotes
+        ? [
+            {
+              id: "test-id",
+              board: "test-board",
+              note: "test-note",
+              user: "test-user",
+            },
+          ]
+        : []
+    }
+    activeVoting={activeVoting}
+  />
+);
+
 describe("Votes", () => {
   describe("should render correctly", () => {
     test("with no votes and disabled voting", () => {
-      const votes = render(<Votes noteId="test-id" votes={[]} activeVoting={false} />);
+      const votes = render(createVotes(false, false));
       expect(votes.container).toMatchSnapshot();
     });
     test("with no votes and active voting", () => {
-      const votes = render(<Votes noteId="test-id" votes={[]} activeVoting />);
+      const votes = render(createVotes(false, true));
       expect(votes.container).toMatchSnapshot();
     });
     test("with votes and disabled voting", () => {
-      const votes = render(
-        <Votes
-          noteId="test-id"
-          votes={[
-            {
-              id: "test-id",
-              board: "test-board",
-              note: "test-note",
-              user: "test-user",
-            },
-          ]}
-          activeVoting={false}
-        />
-      );
+      const votes = render(createVotes(true, false));
       expect(votes.container).toMatchSnapshot();
     });
     test("with votes and active voting", () => {
-      const votes = render(
-        <Votes
-          noteId="test-id"
-          votes={[
-            {
-              id: "test-id",
-              board: "test-board",
-              note: "test-note",
-              user: "test-user",
-            },
-          ]}
-          activeVoting
-        />
-      );
+      const votes = render(createVotes(true, true));
       expect(votes.container).toMatchSnapshot();
     });
     test("with additional classname", () => {
-      const votes = render(<Votes className="test-classname" noteId="test-id" votes={[]} activeVoting={false} />);
+      const votes = render(createVotes(false, false, "test-classname"));
       expect(votes.container).toMatchSnapshot();
     });
   });
@@ -57,39 +51,13 @@ describe("Votes", () => {
     const storeDispatchSpy = jest.spyOn(store, "dispatch");
 
     test("addVote", () => {
-      const votes = render(
-        <Votes
-          noteId="test-id"
-          votes={[
-            {
-              id: "test-id",
-              board: "test-board",
-              note: "test-note",
-              user: "test-user",
-            },
-          ]}
-          activeVoting
-        />
-      );
+      const votes = render(createVotes(true, true));
       fireEvent.click(votes.container.getElementsByClassName("dot-button")[1]);
       expect(storeDispatchSpy).toHaveBeenCalledWith(ActionFactory.addVote("test-id"));
     });
 
     test("deleteVote", () => {
-      const votes = render(
-        <Votes
-          noteId="test-id"
-          votes={[
-            {
-              id: "test-id",
-              board: "test-board",
-              note: "test-note",
-              user: "test-user",
-            },
-          ]}
-          activeVoting
-        />
-      );
+      const votes = render(createVotes(true, true));
       fireEvent.click(votes.container.getElementsByClassName("dot-button")[0]);
       expect(storeDispatchSpy).toHaveBeenCalledWith(ActionFactory.deleteVote("test-id"));
     });
