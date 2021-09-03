b3bf513e482207e7db13fb0b33072a6ff52f7173
Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>
2021-09-03 09:23:29 +0200

    Disable unimplemented menu buttons (#655)

--- src/components/MenuBars/MenuBars.tsx
+++ src/components/MenuBars/MenuBars.tsx
@@ -34,19 +34,19 @@ function MenuBars() {
     <div className={classNames("menu-bars", {"menu-bars--admin": showAdminMenu, "menu-bars--user": !showAdminMenu}, {"menu-bars--isAdmin": isAdmin})}>
       <div className="menu user-menu">
         <div className="menu__items">
-          <MenuToggle direction="right" toggleStartLabel="Mark me as done" toggleStopLabel="Unmark me as done" icon={CheckIcon} onToggle={() => null} />
-          <MenuButton direction="right" label="Add image or giphy" icon={AddImageIcon} onClick={() => null} />
-          <MenuButton direction="right" label="Add sticker" icon={AddStickerIcon} onClick={() => null} />
-          <MenuButton direction="right" label="Settings" icon={SettingsIcon} onClick={() => null} />
+          <MenuToggle disabled direction="right" toggleStartLabel="Mark me as done" toggleStopLabel="Unmark me as done" icon={CheckIcon} onToggle={() => null} />
+          <MenuButton disabled direction="right" label="Add image or giphy" icon={AddImageIcon} onClick={() => null} />
+          <MenuButton disabled direction="right" label="Add sticker" icon={AddStickerIcon} onClick={() => null} />
+          <MenuButton disabled direction="right" label="Settings" icon={SettingsIcon} onClick={() => null} />
         </div>
       </div>
       {isAdmin && (
         <div className="menu admin-menu">
           <div className="menu__items">
-            <MenuToggle direction="left" toggleStartLabel="Start column mode" toggleStopLabel="End column mode" icon={ColumnIcon} onToggle={() => null} />
-            <MenuToggle direction="left" toggleStartLabel="Start timer" toggleStopLabel="Stop timer" icon={TimerIcon} onToggle={() => null} />
+            <MenuToggle disabled direction="left" toggleStartLabel="Start column mode" toggleStopLabel="End column mode" icon={ColumnIcon} onToggle={() => null} />
+            <MenuToggle disabled direction="left" toggleStartLabel="Start timer" toggleStopLabel="Stop timer" icon={TimerIcon} onToggle={() => null} />
             <MenuToggle direction="left" toggleStartLabel="Start voting phase" toggleStopLabel="End voting phase" icon={VoteIcon} onToggle={toggleVoting} />
-            <MenuToggle direction="left" toggleStartLabel="Start focused mode" toggleStopLabel="End focused mode" icon={FocusIcon} onToggle={() => null} />
+            <MenuToggle disabled direction="left" toggleStartLabel="Start focused mode" toggleStopLabel="End focused mode" icon={FocusIcon} onToggle={() => null} />
           </div>
         </div>
       )}
