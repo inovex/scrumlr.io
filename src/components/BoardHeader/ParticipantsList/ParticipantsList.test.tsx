15c887ff3e6828e0b98c91b1f6604e7163c2bbe2
Manuel <accounts@manuelbrandstetter.de>
2021-09-09 12:29:33 +0200

    Add one further snapshot test for filtered participant lists

--- src/components/BoardHeader/ParticipantsList/ParticipantsList.test.tsx
+++ src/components/BoardHeader/ParticipantsList/ParticipantsList.test.tsx
@@ -1,18 +1,29 @@
-import {render} from "@testing-library/react";
+import {render, fireEvent} from "@testing-library/react";
 import {ParticipantsList} from ".";
 
 describe("ParticipantsList", () => {
   describe("should render correctly", () => {
-    const portal = global.document.createElement("div");
-    portal.setAttribute("id", "portal");
-    global.document.querySelector("body")!.appendChild(portal);
+    test("on filtered participants", () => {
+      const portal = global.document.createElement("div");
+      portal.setAttribute("id", "portal");
+      global.document.querySelector("body")!.appendChild(portal);
+      const {container} = render(<ParticipantsList open onClose={() => null} participants={[...Array(3).keys()].map((n) => ({id: n, displayName: `Participant ${n}`}))} />, {
+        container: global.document.querySelector("#portal")!,
+      });
+      fireEvent.change(container.querySelector("input")!, {target: {value: "Participant 0"}});
+      expect(container).toMatchSnapshot();
+    });
 
     test("on open", () => {
-      const {container} = render(<ParticipantsList open onClose={() => null} participants={[1, 2, 3].map((_, i) => ({id: i, displayName: `Participant ${i}`}))} />, {
+      const portal = global.document.createElement("div");
+      portal.setAttribute("id", "portal");
+      global.document.querySelector("body")!.appendChild(portal);
+      const {container} = render(<ParticipantsList open onClose={() => null} participants={[...Array(3).keys()].map((n) => ({id: n, displayName: `Participant ${n}`}))} />, {
         container: global.document.querySelector("#portal")!,
       });
       expect(container).toMatchSnapshot();
     });
+
     test("on close", () => {
       const {container} = render(<ParticipantsList open={false} onClose={() => null} participants={[]} />, {container: global.document.querySelector("#portal")!});
       expect(container).toMatchSnapshot();
