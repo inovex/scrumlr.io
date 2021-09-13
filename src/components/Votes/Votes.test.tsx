0bfb25a5f477dcad1bff821129a375644004e6d2
Dominik Weinzierl <dominik.weinzierl@mailbox.org>
2021-09-13 11:07:13 +0200

    Revert changes

--- src/components/Votes/Votes.test.tsx
+++ src/components/Votes/Votes.test.tsx
@@ -1,78 +1,7 @@
 import {fireEvent, render} from "@testing-library/react";
-import {wrapWithTestBackend} from "react-dnd-test-utils";
 import store from "store";
 import {ActionFactory} from "store/action";
-import configureStore from "redux-mock-store";
-import {Provider} from "react-redux";
-import Parse from "parse";
-import Note from "components/Note/Note";
 import {Votes} from "./Votes";
-import Board from "../Board/Board";
-import Column from "../Column/Column";
-
-const mockStore = configureStore();
-const createNote = () => {
-  const initialState = {
-    board: {
-      data: {
-        columns: [{id: "test_column", name: "test_header", hidden: false}],
-        votingIteration: 0,
-      },
-    },
-    notes: [{}],
-    users: {
-      admins: [],
-      basic: ["test-user-1", "test-user-2", "test-user-3"],
-      all: [],
-    },
-  };
-
-  const store = mockStore(initialState);
-
-  const [NoteContext] = wrapWithTestBackend(Note);
-
-  return (
-    <Provider store={store}>
-      <NoteContext
-        key=""
-        noteId="test-id"
-        text=""
-        authorId=""
-        columnId="test_column"
-        columnName=""
-        columnColor=""
-        isAdmin
-        activeVoting
-        showAuthors
-        votes={[
-          {
-            id: "0",
-            board: "test-board",
-            note: "test-id",
-            user: "test-user-1",
-            votingIteration: 0,
-          },
-          {
-            id: "1",
-            board: "test-board",
-            note: "test-id",
-            user: "test-user-2",
-            votingIteration: 0,
-          },
-          {
-            id: "2",
-            board: "test-board",
-            note: "test-id",
-            user: "test-user-2",
-            votingIteration: 0,
-          },
-        ]}
-        childrenNotes={[]}
-        authorName=""
-      />
-    </Provider>
-  );
-};
 
 const createVotes = (withVotes: boolean, activeVoting: boolean, className?: string) => (
   <Votes
@@ -94,32 +23,6 @@ const createVotes = (withVotes: boolean, activeVoting: boolean, className?: stri
     activeVoting={activeVoting}
   />
 );
-
-const createBoardWithColumns = () => {
-  const initialState = {
-    board: {
-      data: {
-        columns: [{id: "test_column", name: "Positive", hidden: false}],
-      },
-    },
-    notes: [],
-    users: {
-      admins: [],
-      basic: [],
-      all: [],
-    },
-  };
-  const store = mockStore(initialState);
-  const [BoardContext] = wrapWithTestBackend(Board);
-
-  return (
-    <Provider store={store}>
-      <BoardContext name="" boardstatus="" currentUserIsModerator>
-        <Column key="backlog-blue" id="test_column" color="backlog-blue" name="Positive" />
-      </BoardContext>
-    </Provider>
-  );
-};
 
 describe("Votes", () => {
   describe("should render correctly", () => {
@@ -174,26 +77,6 @@ describe("Votes", () => {
       const votes = render(createVotes(true, true));
       fireEvent.click(votes.container.getElementsByClassName("dot-button")[0]);
       expect(storeDispatchSpy).toHaveBeenCalledWith(ActionFactory.deleteVote("test-id"));
-    });
-  });
-
-  describe("Test amount of visible votes", () => {
-    test("test-user-1 has one vote during vote phase", () => {
-      Parse.User.current = jest.fn(() => ({id: "test-user-1"}));
-      const {container} = render(createNote());
-      expect((container.querySelector(".dot-button")?.lastChild as HTMLSpanElement).innerHTML).toEqual("1");
-    });
-
-    test("test-user-2 hast two votes during vote phase", () => {
-      Parse.User.current = jest.fn(() => ({id: "test-user-2"}));
-      const {container} = render(createNote());
-      expect((container.querySelector(".dot-button")?.lastChild as HTMLSpanElement).innerHTML).toEqual("2");
-    });
-
-    test("test-user-3 hast zero votes during vote phase and has only the add vote button", () => {
-      Parse.User.current = jest.fn(() => ({id: "test-user-3"}));
-      const {container} = render(createNote());
-      expect(container.querySelector(".note__votes")?.childElementCount).toEqual(1);
     });
   });
 });
