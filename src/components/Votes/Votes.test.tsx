3ca0ae9bd63344c4dae7c4fd18e3f2fb0d4608ba
Dominik Weinzierl <dominik.weinzierl@mailbox.org>
2021-09-13 11:16:48 +0200

    Update start of votingIteration in tests

--- src/components/Votes/Votes.test.tsx
+++ src/components/Votes/Votes.test.tsx
@@ -15,7 +15,7 @@ const createVotes = (withVotes: boolean, activeVoting: boolean, className?: stri
               board: "test-board",
               note: "test-note",
               user: "test-user",
-              votingIteration: 0,
+              votingIteration: 1,
             },
           ]
         : []
