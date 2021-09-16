bb5ba333e7cf49ae27bb4af12926dc0496ff9df2
Dominik Weinzierl <dominik.weinzierl@mailbox.org>
2021-09-16 14:50:09 +0200

    Add comments

--- src/store/middleware/voteConfiguration.ts
+++ src/store/middleware/voteConfiguration.ts
@@ -5,6 +5,9 @@ import {Toast} from "utils/Toast";
 import {ActionType, ReduxAction} from "../action";
 
 export const passVoteConfigurationMiddlware = async (stateAPI: MiddlewareAPI<Dispatch<AnyAction>, ApplicationState>, dispatch: Dispatch, action: ReduxAction) => {
+  /**
+   * New vote configuration added by the moderator/admin
+   */
   if (action.type === ActionType.AddVoteConfiguration) {
     const response = (await API.addVoteConfiguration(action.voteConfiguration)) as {status: string; description: string};
     if (response.status === "Error") {
@@ -12,6 +15,9 @@ export const passVoteConfigurationMiddlware = async (stateAPI: MiddlewareAPI<Dis
     }
   }
 
+  /**
+   * Remove current vote configuration from the database (if we cancel a voting iteraion, we don't need the entry in the backend anymore)
+   */
   if (action.type === ActionType.RemoveVoteConfiguration) {
     const boardId = stateAPI.getState().board.data!.id;
     API.removeVoteConfiguration(boardId);
--- src/store/reducer/voteConfiguration.ts
+++ src/store/reducer/voteConfiguration.ts
@@ -1,15 +1,25 @@
 import {VoteConfigurationClientModel} from "types/voteConfiguration";
 import {ActionType, ReduxAction} from "store/action";
 
+/**
+ * Default ovte configuration if the state is undefined (e.g. no default vote configuration available in the database)
+ */
 const defaultVoteConfiguration = {votingIteration: 0, voteLimit: 10, showVotesOfOtherUsers: false, allowMultipleVotesPerNote: true};
 
 export const voteConfigurationReducer = (state: VoteConfigurationClientModel = defaultVoteConfiguration, action: ReduxAction): VoteConfigurationClientModel => {
   switch (action.type) {
+    /**
+     * If we receive a new vote configuration (e.g. we update the vote configuraton or we receive the intitial vote configuration), we need to update our state too.
+     */
     case ActionType.InitializeVoteConfiguration:
-    case ActionType.RemovedVoteConfiguration:
     case ActionType.AddedVoteConfiguration: {
       return action.voteConfiguration;
     }
+    /**
+     * If we remove the vote configuration form our datebase, we don't need to remove it from the state too. The user can change the vote configuraton
+     * and can reuse the old state for the new vote configuration.
+     */
+    case ActionType.RemovedVoteConfiguration:
     default: {
       return state;
     }
