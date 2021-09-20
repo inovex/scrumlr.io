d37aac0ba301a7bb61807eaa88a2f261a64b7a0c
Dominik Weinzierl <dominik.weinzierl@mailbox.org>
2021-09-20 14:11:58 +0200

    Update column reducer tests

---
+++ src/store/reducer/__tests__/board.test.ts
@@ -0,0 +1,48 @@
+import {BoardState} from "types/store";
+import {AddColumnRequest, EditColumnRequest} from "types/column";
+import {boardReducer} from "../board";
+import {ActionFactory} from "../../action";
+
+describe("board reducer", () => {
+  let initialState: BoardState;
+
+  beforeEach(() => {
+    initialState = {
+      data: {
+        id: "test_board",
+        name: "test_board",
+        columns: [
+          {
+            id: "test_column_1",
+            name: "test_column_1",
+          },
+        ],
+      },
+    };
+  });
+
+  test("add column", () => {
+    const test_column: AddColumnRequest = {name: "test_column_2", hidden: false};
+    const newState = boardReducer(initialState, ActionFactory.addColumn(test_column));
+
+    expect(newState.data.columns.length).toEqual(2);
+    expect(newState.data.columns[1]).toEqual(test_column);
+  });
+
+  test("remove column", () => {
+    const newState = boardReducer(initialState, ActionFactory.deleteColumn("test_column_1"));
+    expect(newState.data.columns.length).toEqual(0);
+  });
+
+  test("edit column (full)", () => {
+    const editRequest: EditColumnRequest = {id: "test_column_1", name: "New name", color: "planning-pink", hidden: true};
+    const newState = boardReducer(initialState, ActionFactory.editColumn({id: "test_column_1", name: "New name", color: "planning-pink", hidden: true}));
+    expect(newState.data.columns[0]).toEqual(editRequest);
+  });
+
+  test("edit column (partial)", () => {
+    const editRequest: EditColumnRequest = {id: "test_column_1", name: "New name"};
+    const newState = boardReducer(initialState, ActionFactory.editColumn({id: "test_column_1", name: "New name"}));
+    expect(newState.data.columns[0].name).toEqual("New name");
+  });
+});
--- src/store/reducer/__tests__/users.test.ts
+++ src/store/reducer/__tests__/users.test.ts
@@ -4,67 +4,67 @@ import {ActionFactory} from "../../action";
 import {UserClientModel} from "../../../types/user";
 
 const createUser = (id: string, name: string, admin: boolean): UserClientModel => ({
-    id,
-    displayName: name,
-    admin,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    online: true
-})
+  id,
+  displayName: name,
+  admin,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  online: true,
+  showHiddenColumns: true,
+});
 
-describe('users reducer', () => {
+describe("users reducer", () => {
+  let initialState: UsersState;
 
-    let initialState: UsersState;
+  beforeEach(() => {
+    initialState = {
+      admins: [],
+      basic: [],
+      all: [],
+    };
+  });
 
-    beforeEach(() => {
-        initialState = {
-            admins: [],
-            basic: [],
-            all: []
-        }
-    });
+  test("add admins", () => {
+    const admin = createUser("1", "John Doe", true);
+    const newState = usersReducer(initialState, ActionFactory.setUsers([admin], true));
 
-    test('add admins', () => {
-        const admin = createUser('1', 'John Doe', true);
-        const newState = usersReducer(initialState, ActionFactory.setUsers([ admin ], true));
+    expect(newState.admins).toEqual([admin]);
+    expect(newState.all).toEqual([admin]);
+    expect(newState.basic).toEqual([]);
+  });
 
-        expect(newState.admins).toEqual([ admin ]);
-        expect(newState.all).toEqual([ admin ]);
-        expect(newState.basic).toEqual([]);
-    });
+  test("add basic", () => {
+    const user = createUser("2", "Jane Doe", false);
+    const newState = usersReducer(initialState, ActionFactory.setUsers([user], false));
 
-    test('add basic', () => {
-        const user = createUser('2', 'Jane Doe', false);
-        const newState = usersReducer(initialState, ActionFactory.setUsers([ user ], false));
+    expect(newState.basic).toEqual([user]);
+    expect(newState.all).toEqual([user]);
+    expect(newState.admins).toEqual([]);
+  });
 
-        expect(newState.basic).toEqual([ user ]);
-        expect(newState.all).toEqual([ user ]);
-        expect(newState.admins).toEqual([]);
-    });
+  test("merge admins and basic users", () => {
+    const admin = createUser("1", "John Doe", true);
+    const user = createUser("2", "Jane Doe", false);
 
-    test('merge admins and basic users', () => {
-        const admin = createUser('1', 'John Doe', true);
-        const user = createUser('2', 'Jane Doe', false);
+    const state1 = usersReducer(initialState, ActionFactory.setUsers([user], false));
+    const state2 = usersReducer(state1, ActionFactory.setUsers([admin], true));
 
-        const state1 = usersReducer(initialState, ActionFactory.setUsers([ user ], false));
-        const state2 = usersReducer(state1, ActionFactory.setUsers([ admin ], true));
+    expect(state2.admins).toEqual([admin]);
+    expect(state2.basic).toEqual([user]);
+    expect(state2.all).toContain(admin);
+    expect(state2.all).toContain(user);
+  });
 
-        expect(state2.admins).toEqual([ admin ]);
-        expect(state2.basic).toEqual([ user ]);
-        expect(state2.all).toContain(admin);
-        expect(state2.all).toContain(user);
-    });
+  test("set user status correctly offline/online", () => {
+    const user = createUser("1", "Jane Doe", false);
 
-    test('set user status correctly offline/online', () => {
-      const user = createUser('1', 'Jane Doe', false);
+    const state1 = usersReducer(initialState, ActionFactory.setUsers([user], false));
+    expect(state1.all.find((u) => u.id === user.id)?.online).toBe(true);
 
-      const state1 = usersReducer(initialState, ActionFactory.setUsers([ user ], false));
-      expect(state1.all.find(u=>u.id === user.id)?.online).toBe(true);
+    const state2 = usersReducer(state1, ActionFactory.setUserStatus(user.id, false));
+    expect(state2.all.find((u) => u.id === user.id)?.online).toBe(false);
 
-      const state2 = usersReducer(state1, ActionFactory.setUserStatus(user.id, false));
-      expect(state2.all.find(u=>u.id === user.id)?.online).toBe(false);
-
-      const state3 = usersReducer(state2, ActionFactory.setUserStatus(user.id, true));
-      expect(state3.all.find(u=>u.id === user.id)?.online).toBe(true);
+    const state3 = usersReducer(state2, ActionFactory.setUserStatus(user.id, true));
+    expect(state3.all.find((u) => u.id === user.id)?.online).toBe(true);
   });
 });
