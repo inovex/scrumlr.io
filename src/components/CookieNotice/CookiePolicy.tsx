1ff0423b98bc01022e01bad63942e6e541a92b94
andiKandi <86951527+andiKandi@users.noreply.github.com>
2021-09-20 14:20:27 +0200

    Closes #165: Implement Cookie Consent (#602)
    * first draft for the cookie notice. Tests still missing, style questionable.
    
    * minor optical changes
    
    * initial cookie value changed to null
    
    * first tests for cookie notice and cookie policy
    
    * Paths are now correct and absolute.
    
    * Deleted not unnecessary cookieServerModel
    
    * Switched from Material UI to Portal in CookiePolicy.tsx
    
    * Test are now successful
    
    * Scrollable Content and fixed header/footer
    
    * Tests updated to new design and elements
    
    * Update CookiePolicy.scss
    
    * after review: more explicit and simpler
    
    * after review: css changes
    
    * after review: notice toggle via null & not css; COOKIE_CONSENT_NAME not exported because of circular dependency
    
    * cookie policy content is stored externally in .md file
    
    * removed cookie from redux state
    
    * open removed
    
    * sticky to fixed
    
    * merged main into cookie branch
    
    * tests updated
    
    * yarn update before merge into main
    
    * yarn update before merge into main
    
    * package-lock.json deleted
    
    * some changes after review
    
    * some changes after review p2
    
    * some changes after review p3
    
    * some changes after review p4: no material ui
    
    * some changes after review p5: tests updated
    
    * minor change
    
    * yarn.lock
    
    * tests updated
    
    Co-authored-by: Andreas Kraus <akraus931@google.com>
    Co-authored-by: Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>

---
+++ src/components/CookieNotice/CookiePolicy.tsx
@@ -0,0 +1,56 @@
+import "./CookiePolicy.scss";
+import {useEffect, useState} from "react";
+import Portal from "components/Portal/Portal";
+import marked from "marked";
+import policyText from "./CookiePolicyText.md";
+
+interface CookiePolicyProps {
+  decline: () => void;
+  accept: () => void;
+  onClose: () => void;
+  show: boolean;
+  darkBackground: boolean;
+}
+
+const CookiePolicy = ({decline, accept, onClose, show, darkBackground}: CookiePolicyProps) => {
+  const [policy, setPolicy] = useState({markdown: ""});
+
+  useEffect(() => {
+    fetch(policyText)
+      .then((response) => response.text())
+      .then((text) => setPolicy({markdown: marked(text)}));
+  });
+
+  if (!show) {
+    return null;
+  }
+
+  const onAccept = () => {
+    accept();
+  };
+
+  const onDecline = () => {
+    decline();
+  };
+
+  return (
+    <Portal onClose={onClose} darkBackground={darkBackground}>
+      <div className="cookie-policy">
+        <div className="cookie-policy__title">
+          <h1>Cookie Policy of scrumlr.io</h1>
+        </div>
+        <div className="cookie-policy__body" dangerouslySetInnerHTML={{__html: policy.markdown}} />
+        <div className="cookie-policy__footer">
+          <button className="cookie-policy__button-decline" type="button" onClick={onDecline}>
+            Decline
+          </button>
+          <button className="cookie-policy__button-accept" type="button" onClick={onAccept}>
+            Accept
+          </button>
+        </div>
+      </div>
+    </Portal>
+  );
+};
+
+export default CookiePolicy;
