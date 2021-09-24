fff51b08bfb861f4d624bbc404935b5eb7af9431
Andreas Kraus <akraus931@google.com>
2021-09-24 10:38:21 +0200

    comments | microsoft photo retrieval failure

--- server/src/cloud/auth.ts
+++ server/src/cloud/auth.ts
@@ -19,22 +19,25 @@ export interface UserInformation {
 }
 
 export const initializeAuthFunctions = (): void => {
-  /**
-   * GOOGLE OAuth
-   * */
   if (GOOGLE_CLIENT_ID && GOOGLE_CLIENT_SECRET && AUTH_REDIRECT_URI) {
-    // https://github.com/autodidaktum/google-oauth2-parse-react/blob/master/deploy/cloud/main.js
+    /**
+     * Function generates consent page URL where the code is retrieved
+     * github.com/autodidaktum/google-oauth2-parse-react/blob/master/deploy/cloud/main.js
+     * https://www.npmjs.com/package/googleapis#oauth2-client
+     */
     publicApi<{state: string}, string>("GoogleSignIn", async ({state}) => {
       const oauth2Client = getGoogleOAuth2Client();
-
-      // returns the login link
       return oauth2Client.generateAuthUrl({
-        access_type: "offline",
+        access_type: "offline", // gets refresh_token
         scope: ["openid", "profile"],
         state,
       });
     });
 
+    /**
+     * 1) Function exchanges the code for access token.
+     * 2) Access token is used to retrieve user information.
+     */
     publicApi<{code: string}, UserInformation>("GoogleVerifySignIn", async ({code}) => {
       const oauth2Client = getGoogleOAuth2Client();
       const {tokens} = await oauth2Client.getToken(code);
@@ -54,15 +57,20 @@ export const initializeAuthFunctions = (): void => {
     });
   }
 
-  /**
-   * GITHUB OAuth
-   * */
   if (GITHUB_CLIENT_ID && GITHUB_CLIENT_SECRET && AUTH_REDIRECT_URI) {
+    /**
+     * Function generates consent page URL where the code is retrieved
+     * https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps
+     */
     publicApi<{state: string}, string>(
       "GithubSignIn",
       async ({state}) => `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=user&state=${state}&redirect_uri=${encodeURI(AUTH_REDIRECT_URI)}`
     );
 
+    /**
+     * 1) Function exchanges code for access token.
+     * 2) Access token is used to retrieve user information.
+     */
     publicApi<{code: string}, UserInformation>("GithubVerifySignIn", async ({code}) => {
       const accessTokenRequest = await axios.post(
         "https://github.com/login/oauth/access_token",
@@ -84,10 +92,11 @@ export const initializeAuthFunctions = (): void => {
     });
   }
 
-  /**
-   * MICROSOFT OAuth
-   * */
   if (MICROSOFT_CLIENT_ID && MICROSOFT_CLIENT_SECRET && AUTH_REDIRECT_URI) {
+    /**
+     * Function generates consent page URL where the code is retrieved
+     * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow
+     */
     publicApi<{state: string}, string>(
       "MicrosoftSignIn",
       async ({state}) =>
@@ -96,6 +105,10 @@ export const initializeAuthFunctions = (): void => {
         )}&response_mode=query&scope=User.Read&state=${state}`
     );
 
+    /**
+     * 1) Function exchanges code for access token.
+     * 2) Access token is used to retrieve user information.
+     */
     publicApi<{code: string}, UserInformation>("MicrosoftVerifySignIn", async ({code}) => {
       const postData = {
         client_id: MICROSOFT_CLIENT_ID,
@@ -113,6 +126,7 @@ export const initializeAuthFunctions = (): void => {
       const accessTokenRequest = await axios.post(`https://login.microsoftonline.com/common/oauth2/v2.0/token`, qs.stringify(postData), config);
       const accessToken = accessTokenRequest.data.access_token;
       const user = await axios.get("https://graph.microsoft.com/v1.0/me", {headers: {Authorization: `Bearer ${accessToken}`, Accept: "application/json"}});
+      // https://docs.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0 -> photo retrieval not supported for personal account | otherwise admin rigths needed
       return {
         id: user.data.id,
         name: user.data.displayName,
