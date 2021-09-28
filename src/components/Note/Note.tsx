e201bce300602bc9eb837855208bc51b3b882cc8
Dominik Weinzierl <dominik.weinzierl@mailbox.org>
2021-09-28 12:43:34 +0200

    Replaced with useEffect to react on updates

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -4,7 +4,7 @@ import classNames from "classnames";
 import Parse from "parse";
 import store from "store";
 import {ActionFactory} from "store/action";
-import React, {useRef} from "react";
+import React, {useEffect, useRef} from "react";
 import NoteDialog from "components/NoteDialog/NoteDialog";
 import {ReactComponent as EditIcon} from "assets/icon-edit.svg";
 import {Votes} from "components/Votes";
@@ -33,19 +33,31 @@ interface NoteProps {
 const Note = (props: NoteProps) => {
   const noteRef = useRef<HTMLLIElement>(null);
 
-  const [showDialog, setShowDialog] = React.useState(false);
+  const [showDialog, setShowDialog] = React.useState(props.focus && props.activeModeration);
+
   const handleShowDialog = () => {
     // Toggle cards for all users (note dialogs that were visible before the moderation phase are also visible after the moderation phase)
     if (props.activeModeration) {
       // Only moderators can show cards for all users (other users should not interact with the cards)
       if (props.noteId && props.currentUserIsModerator) {
         store.dispatch(ActionFactory.editNote({id: props.noteId, focus: !props.focus}));
-        setShowDialog(!props.focus);
       }
     } else {
       setShowDialog(!showDialog);
     }
   };
+
+  useEffect(() => {
+    setShowDialog(props.focus);
+  }, [props.focus]);
+
+  useEffect(() => {
+    setShowDialog(false);
+    // Clean up
+    if (!props.activeModeration && props.focus && props.noteId) {
+      store.dispatch(ActionFactory.editNote({id: props.noteId, focus: false}));
+    }
+  }, [props.activeModeration]);
 
   const [{isDragging}, drag] = useDrag({
     type: props.childrenNotes.length > 0 ? "STACK" : "NOTE",
@@ -85,7 +97,7 @@ const Note = (props: NoteProps) => {
           )}
           <Votes className="note__votes" noteId={props.noteId!} votes={props.votes.concat(props.childrenNotes.flatMap((n) => n.votes))} activeVoting={props.activeVoting} />
         </footer>
-        <NoteDialog {...props} onClose={handleShowDialog} show={(showDialog && !props.activeModeration) || (props.activeModeration && props.focus)} />
+        <NoteDialog {...props} onClose={handleShowDialog} show={showDialog} />
       </div>
       {props.childrenNotes.length > 0 && <div className="note__in-stack" />}
     </li>
