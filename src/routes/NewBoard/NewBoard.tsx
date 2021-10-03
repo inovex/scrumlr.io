dcf93e12a509034d373c714c648c63cda8e1c8d2
Johann Böhler <bitionaire@users.noreply.github.com>
2021-10-03 17:05:22 +0200

    Added key attributes and removed console.warnings (#794)

--- src/routes/NewBoard/NewBoard.tsx
+++ src/routes/NewBoard/NewBoard.tsx
@@ -61,9 +61,12 @@ function NewBoard(props: NewBoardProps) {
         <input type="checkbox" checked={joinConfirmationRequired} onChange={(e) => setJoinConfirmationRequired(e.target.checked)} />
         JoinConfirmationRequired
       </label>
+
       <select onChange={(e) => setColumnTemplate(e.target.value)} defaultValue={columnTemplate}>
         {Object.keys(columnTemplates).map((key) => (
-          <option value={key}>{key}</option>
+          <option key={key} value={key}>
+            {key}
+          </option>
         ))}
       </select>
 
