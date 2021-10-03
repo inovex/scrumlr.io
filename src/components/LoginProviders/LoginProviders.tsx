4fae915fc0d5dd831ab5b6d59a6819adc0ad85da
bitionaire <johann.boehler@scrumlr.io>
2021-10-03 14:38:31 +0200

    Added click handler tests for login providers

--- src/components/LoginProviders/LoginProviders.tsx
+++ src/components/LoginProviders/LoginProviders.tsx
@@ -3,9 +3,15 @@ import {AuthenticationManager} from "../../utils/authentication/AuthenticationMa
 // TODO: Include after deployment: <button onClick={() => AuthenticationManager.signInWithAuthProvider("apple")}>Sign in with Apple</button>
 const LoginProviders = () => (
   <div className="login-control">
-    <button onClick={() => AuthenticationManager.signInWithAuthProvider("google")}>Sign in with Google</button>
-    <button onClick={() => AuthenticationManager.signInWithAuthProvider("github")}>Sign in with Github</button>
-    <button onClick={() => AuthenticationManager.signInWithAuthProvider("microsoft")}>Sign in with Microsoft</button>
+    <button id="google" onClick={() => AuthenticationManager.signInWithAuthProvider("google")}>
+      Sign in with Google
+    </button>
+    <button id="github" onClick={() => AuthenticationManager.signInWithAuthProvider("github")}>
+      Sign in with Github
+    </button>
+    <button id="microsoft" onClick={() => AuthenticationManager.signInWithAuthProvider("microsoft")}>
+      Sign in with Microsoft
+    </button>
   </div>
 );
 export default LoginProviders;
