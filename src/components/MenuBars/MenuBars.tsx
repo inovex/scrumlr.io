9a7530a1345e40c7585a97e1eccd766dcc2d3c48
Manuel Brandstetter <accounts@manuelbrandstetter.de>
2021-10-11 11:35:19 +0200

    Revert MenuBars test settings

--- src/components/MenuBars/MenuBars.tsx
+++ src/components/MenuBars/MenuBars.tsx
@@ -1,22 +1,21 @@
 import {useState} from "react";
-// import {ActionFactory} from "store/action";
-// import store, {useAppSelector} from "store";
+import {ActionFactory} from "store/action";
+import store, {useAppSelector} from "store";
 import Parse from "parse";
 import classNames from "classnames";
 
 import MenuButton from "components/MenuBars/MenuItem/MenuButton";
-// import MenuToggle from "components/MenuBars/MenuItem/MenuToggle";
+import MenuToggle from "components/MenuBars/MenuItem/MenuToggle";
 
 import {ReactComponent as AddImageIcon} from "assets/icon-addimage.svg";
 import {ReactComponent as AddStickerIcon} from "assets/icon-addsticker.svg";
-// import {ReactComponent as CheckIcon} from "assets/icon-check.svg";
-// import {ReactComponent as SettingsIcon} from "assets/icon-settings.svg";
-// import {ReactComponent as ColumnIcon} from "assets/icon-column.svg";
-// import {ReactComponent as FocusIcon} from "assets/icon-focus.svg";
-// import {ReactComponent as VoteIcon} from "assets/icon-vote.svg";
+import {ReactComponent as CheckIcon} from "assets/icon-check.svg";
+import {ReactComponent as SettingsIcon} from "assets/icon-settings.svg";
+import {ReactComponent as ColumnIcon} from "assets/icon-column.svg";
+import {ReactComponent as FocusIcon} from "assets/icon-focus.svg";
+import {ReactComponent as VoteIcon} from "assets/icon-vote.svg";
 import {ReactComponent as ToggleSettingsMenuIcon} from "assets/icon-toggle-settings-menu.svg";
 import {ReactComponent as ToggleAddMenuIcon} from "assets/icon-toggle-add-menu.svg";
-import {useAppSelector} from "../../store";
 import {TimerButton} from "./MenuItem/variants/TimerButton";
 
 import "./MenuBars.scss";
@@ -34,12 +33,12 @@ function MenuBars() {
 
   const isAdmin = state.admins.map((admin) => admin.id).indexOf(currentUser!.id) !== -1;
 
-  // const toggleVoting = (active: boolean) => {
-  //   if (active) {
-  //     store.dispatch(ActionFactory.addVoteConfiguration({boardId: state.boardId, voteLimit: 5, showVotesOfOtherUsers: false, allowMultipleVotesPerNote: true}));
-  //   }
-  //   store.dispatch(ActionFactory.editBoard({id: state.boardId, voting: active ? "active" : "disabled"}));
-  // };
+  const toggleVoting = (active: boolean) => {
+    if (active) {
+      store.dispatch(ActionFactory.addVoteConfiguration({boardId: state.boardId, voteLimit: 5, showVotesOfOtherUsers: false, allowMultipleVotesPerNote: true}));
+    }
+    store.dispatch(ActionFactory.editBoard({id: state.boardId, voting: active ? "active" : "disabled"}));
+  };
 
   // const toggleTimer = (active: boolean) => {
   //   if (active) {
@@ -67,24 +66,19 @@ function MenuBars() {
     >
       <section className={classNames("menu", "user-menu", {"menu-animation": animate})} onTransitionEnd={(event) => handleAnimate(event)}>
         <div className="menu__items">
-          {/* <MenuToggle disabled direction="right" toggleStartLabel="Mark me as done" toggleStopLabel="Unmark me as done" icon={CheckIcon} onToggle={() => null} /> */}
-          <TimerButton />
+          <MenuToggle disabled direction="right" toggleStartLabel="Mark me as done" toggleStopLabel="Unmark me as done" icon={CheckIcon} onToggle={() => null} />
           <MenuButton disabled direction="right" label="Add image or giphy" icon={AddImageIcon} onClick={() => null} />
           <MenuButton disabled direction="right" label="Add sticker" icon={AddStickerIcon} onClick={() => null} />
-          {/* <MenuButton disabled direction="right" label="Settings" icon={SettingsIcon} onClick={() => null} /> */}
-          <TimerButton />
+          <MenuButton disabled direction="right" label="Settings" icon={SettingsIcon} onClick={() => null} />
         </div>
       </section>
       {isAdmin && (
         <section className={classNames("menu", "admin-menu", {"menu-animation": animate})} onTransitionEnd={(event) => handleAnimate(event)}>
           <div className="menu__items">
-            {/* <MenuToggle disabled direction="left" toggleStartLabel="Start column mode" toggleStopLabel="End column mode" icon={ColumnIcon} onToggle={() => null} /> */}
+            <MenuToggle disabled direction="left" toggleStartLabel="Start column mode" toggleStopLabel="End column mode" icon={ColumnIcon} onToggle={() => null} />
             <TimerButton />
-            <TimerButton />
-            <TimerButton />
-            <TimerButton />
-            {/* <MenuToggle direction="left" toggleStartLabel="Start voting phase" toggleStopLabel="End voting phase" icon={VoteIcon} onToggle={toggleVoting} /> */}
-            {/* <MenuToggle disabled direction="left" toggleStartLabel="Start focused mode" toggleStopLabel="End focused mode" icon={FocusIcon} onToggle={() => null} /> */}
+            <MenuToggle direction="left" toggleStartLabel="Start voting phase" toggleStopLabel="End voting phase" icon={VoteIcon} onToggle={toggleVoting} />
+            <MenuToggle disabled direction="left" toggleStartLabel="Start focused mode" toggleStopLabel="End focused mode" icon={FocusIcon} onToggle={() => null} />
           </div>
         </section>
       )}
