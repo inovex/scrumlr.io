354eab7a776b0407eb050de247ab1687676d1968
bitionaire <johann.boehler@scrumlr.io>
2021-10-12 21:04:29 +0200

    Removed unused item

--- src/components/BoardHeader/HeaderMenu/BoardSettings/BoardSettings.tsx
+++ src/components/BoardHeader/HeaderMenu/BoardSettings/BoardSettings.tsx
@@ -28,38 +28,36 @@ export const BoardSettings = (props: BoardSettingsProps) => {
   };
 
   return (
-    <div className="header-menu__item-button">
-      <li className="header-menu__info">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            onSubmit();
+    <li className="header-menu__info">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit();
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") onSubmit();
+        }}
+      >
+        <input
+          className="info__board-name"
+          value={props.boardName}
+          disabled={!props.activeEditMode}
+          onChange={(e) => props.setBoardName(e.target.value)}
+          ref={(input) => {
+            if (!input?.disabled) input?.focus();
           }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") onSubmit();
-          }}
-        >
-          <input
-            className="info__board-name"
-            value={props.boardName}
-            disabled={!props.activeEditMode}
-            onChange={(e) => props.setBoardName(e.target.value)}
-            ref={(input) => {
-              if (!input?.disabled) input?.focus();
-            }}
-            onFocus={(e) => e.target.select()}
-          />
-          <button type="button" className="info__access-mode" disabled={!props.activeEditMode} onClick={() => props.setJoinConfirmationRequired(!props.joinConfirmationRequired)}>
-            <div className={classNames("info__access-mode-lock", {"info__access-mode-lock--unlocked": !props.joinConfirmationRequired})} />
-            {props.joinConfirmationRequired ? "Private Session" : "Public Session"}
+          onFocus={(e) => e.target.select()}
+        />
+        <button type="button" className="info__access-mode" disabled={!props.activeEditMode} onClick={() => props.setJoinConfirmationRequired(!props.joinConfirmationRequired)}>
+          <div className={classNames("info__access-mode-lock", {"info__access-mode-lock--unlocked": !props.joinConfirmationRequired})} />
+          {props.joinConfirmationRequired ? "Private Session" : "Public Session"}
+        </button>
+        {props.currentUserIsModerator && (
+          <button type="submit" className="info__edit-button">
+            {props.activeEditMode ? "save" : "edit"}
           </button>
-          {props.currentUserIsModerator && (
-            <button type="submit" className="info__edit-button">
-              {props.activeEditMode ? "save" : "edit"}
-            </button>
-          )}
-        </form>
-      </li>
-    </div>
+        )}
+      </form>
+    </li>
   );
 };
