ea5332f3fa1754dd4ff18f06dbb824d068427ec1
Dominik Weinzierl <dominik.weinzierl@mailbox.org>
2021-10-18 11:55:00 +0200

    Implement requested changes

--- src/components/Votes/VoteButtons/AddVoteButton.tsx
+++ src/components/Votes/VoteButtons/AddVoteButton.tsx
@@ -3,19 +3,20 @@ import {ActionFactory} from "store/action";
 import {DotButton} from "components/DotButton";
 import "./AddVoteButton.scss";
 import {ReactComponent as PlusIcon} from "assets/icon-add.svg";
+import {FC} from "react";
 
 type AddVoteProps = {
   noteId: string;
 };
 
-export const AddVoteButton = ({noteId}: AddVoteProps) => {
+export const AddVoteButton: FC<AddVoteProps> = ({noteId}: AddVoteProps) => {
   const addVote = () => {
     store.dispatch(ActionFactory.addVote(noteId));
   };
 
   return (
     <DotButton className="vote-button-add" onClick={addVote}>
-      <PlusIcon className="vote-button-add-icon" />
+      <PlusIcon className="vote-button-add__icon" />
     </DotButton>
   );
 };
