ebda7e2b60c86d51e70076ef9c032e29234e596b
Dominik Weinzierl <dominik.weinzierl@mailbox.org>
2021-10-20 12:38:47 +0200

    Implement fade-out/fade-in effect

--- src/components/NoteDialog/NoteDialogComponents/NoteDialogNoteWrapper.tsx
+++ src/components/NoteDialog/NoteDialogComponents/NoteDialogNoteWrapper.tsx
@@ -1,11 +1,39 @@
 import classNames from "classnames";
-import {FC} from "react";
+import {FC, useEffect} from "react";
 import "./NoteDialogNoteWrapper.scss";
 
 type NoteDialogNoteWrapperProps = {};
 
-export const NoteDialogNoteWrapper: FC<NoteDialogNoteWrapperProps> = ({children}) => (
+export const NoteDialogNoteWrapper: FC<NoteDialogNoteWrapperProps> = ({children}) => {
+  const handleScroll = (scrollbar: Element) => {
+    const notes = scrollbar.querySelectorAll(".note-dialog__note");
+    const scrollbarHeight = scrollbar.clientHeight;
+    notes.forEach((note) => {
+      const distanceToTop = note.getBoundingClientRect().top - scrollbar.getBoundingClientRect().top;
+      const noteHeight = note.scrollHeight;
+
+      let opacity = 1;
+      if (distanceToTop < 0) {
+        opacity = (noteHeight + distanceToTop) / noteHeight;
+      }
+      if (scrollbarHeight - (distanceToTop + noteHeight) < 0) {
+        opacity = (scrollbarHeight - distanceToTop) / noteHeight;
+      }
+
+      if (opacity >= 0) {
+        (note as HTMLElement).style.opacity = opacity.toString();
+      }
+    });
+  };
+
+  useEffect(() => {
+    const scrollbar = document.querySelector(".note-dialog__scrollbar")!;
+    scrollbar.removeEventListener("scroll", (e) => handleScroll(scrollbar));
+    scrollbar.addEventListener("scroll", (e) => handleScroll(scrollbar));
+  }, [children]);
+  return (
     <div className={classNames("note-dialog__scrollbar")}>
       <div className={classNames("note-dialog__note-wrapper")}>{children}</div>
     </div>
   );
+};
