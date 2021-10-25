838d54a93717eff2829af7dc08663aefa866bfd8
Dominik Weinzierl <dominik.weinzierl@mailbox.org>
2021-10-25 10:19:04 +0200

    Update API comments

--- server/src/cloud/board.ts
+++ server/src/cloud/board.ts
@@ -105,7 +105,7 @@ export type EditableBoardAttributes = {
 
 export type EditBoardRequest = {id: string} & Partial<EditableBoardAttributes>;
 
-export type DeleteBoardRequest = {id: string};
+export type DeleteBoardRequest = {boardId: string};
 
 export interface JoinRequestResponse {
   board: string;
@@ -327,9 +327,9 @@ export const initializeBoardFunctions = () => {
   });
 
   api<DeleteBoardRequest, boolean>("deleteBoard", async (user, request) => {
-    await requireValidBoardAdmin(user, request.id);
+    await requireValidBoardAdmin(user, request.boardId);
     const boardQuery = new Parse.Query(Parse.Object.extend("Board"));
-    const board = await boardQuery.get(request.id, {useMasterKey: true});
+    const board = await boardQuery.get(request.boardId, {useMasterKey: true});
 
     const noteQuery = new Parse.Query(Parse.Object.extend("Note"));
     noteQuery.equalTo("board", board);
@@ -352,12 +352,12 @@ export const initializeBoardFunctions = () => {
     await Parse.Object.destroyAll(voteConfigurations, {useMasterKey: true});
 
     const adminRoleQuery = new Parse.Query(Parse.Role);
-    adminRoleQuery.equalTo("name", `admin_of_${request.id}`);
+    adminRoleQuery.equalTo("name", `admin_of_${request.boardId}`);
     const adminRoles = await adminRoleQuery.findAll({useMasterKey: true});
     await Parse.Object.destroyAll(adminRoles, {useMasterKey: true});
 
     const memberRoleQuery = new Parse.Query(Parse.Role);
-    memberRoleQuery.equalTo("name", `member_of_${request.id}`);
+    memberRoleQuery.equalTo("name", `member_of_${request.boardId}`);
     const memberRoles = await memberRoleQuery.findAll({useMasterKey: true});
     await Parse.Object.destroyAll(memberRoles, {useMasterKey: true});
 
--- src/api/vote.ts
+++ src/api/vote.ts
@@ -1,6 +1,20 @@
 import {callAPI} from "api/callApi";
 
 export const VoteAPI = {
-  addVote: (board: string, note: string) => callAPI("addVote", {board, note}),
-  deleteVote: (board: string, note: string) => callAPI("removeVote", {board, note}),
+  /**
+   * Changes the permissions of a participant.
+   *
+   * @param boardId the identifier of the board
+   * @param noteId the note id
+   * @returns a {status, description} object
+   */
+  addVote: (boardId: string, noteId: string) => callAPI("addVote", {boardId, noteId}),
+  /**
+   * Changes the permissions of a participant.
+   *
+   * @param boardId the identifier of the board
+   * @param noteId the note id
+   * @returns a {status, description} object
+   */
+  deleteVote: (boardId: string, noteId: string) => callAPI("removeVote", {boardId, noteId}),
 };
