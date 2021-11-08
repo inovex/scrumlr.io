0e5e27da9c5e80d78760d91e52ec6d99bb966eb5
Johann Böhler <bitionaire@users.noreply.github.com>
2021-11-08 09:51:56 +0100

    Fixed user online status (#997)

---
+++ server/src/cloud/userOnlineStatus.ts
@@ -0,0 +1,16 @@
+export const initializeUserOnlineStatus = () => {
+  (Parse.Cloud as any).onLiveQueryEvent(({event, sessionToken}) => {
+    if (event === "ws_disconnect") {
+      const query = new Parse.Query<Parse.Object>("_Session");
+
+      query.equalTo("sessionToken", sessionToken);
+      query.first({useMasterKey: true}).then((session) => {
+        if (session) {
+          const user = session.get("user");
+          user.unset("boards");
+          user.save(null, {useMasterKey: true});
+        }
+      });
+    }
+  });
+};
