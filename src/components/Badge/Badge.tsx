a027b1fd6ac58eac776c16e07b6acca5f5566d6f
Dominik Weinzierl <60005702+Dominik-Weinzierl@users.noreply.github.com>
2021-11-22 15:48:33 +0100

    Closes #1042: Badge (#1080)

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,13 @@
+import "./Badge.scss";
+
+type BadgeProps = {
+  text: string;
+};
+
+export var Badge = function({text}: BadgeProps) {
+  return (
+    <div className="badge">
+      <span>{text}</span>
+    </div>
+  );
+}
