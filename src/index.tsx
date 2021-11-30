814b279951aadaee0301a292febfdbee8895b7b3
Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>
2021-11-30 09:58:16 +0100

    Move ToastContainer into the Board route (#1153)

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,13 @@ import {Provider} from "react-redux";
 import {DndProvider} from "react-dnd";
 import {HTML5Backend} from "react-dnd-html5-backend";
 import "index.scss";
-import {ToastContainer} from "react-toastify";
 import Parse from "parse";
 import {CookieNotice} from "components/CookieNotice";
 import store from "store";
 import Router from "routes/Router";
 import {I18nextProvider} from "react-i18next";
 import i18n from "./i18n";
+import {ToastContainer} from "react-toastify";
 
 Parse.initialize("Scrumlr");
 
@@ -30,8 +30,8 @@ ReactDOM.render(
     <I18nextProvider i18n={i18n}>
       <Provider store={store}>
         <DndProvider backend={HTML5Backend}>
-          <Router />
           <ToastContainer className="toast-container__container" toastClassName="toast-container__toast" bodyClassName="toast-container__body" limit={2} />
+          <Router />
           <CookieNotice />
         </DndProvider>
       </Provider>
