9b5f1e574c9287b2e0f854569ae8d4909f2cbe3d
bitionaire <johann.boehler@scrumlr.io>
2021-12-04 22:06:48 +0100

    Verify user session in `<RequireAuthentication />`

--- src/routes/RequireAuthentication.tsx
+++ src/routes/RequireAuthentication.tsx
@@ -1,12 +1,30 @@
 import {Navigate} from "react-router-dom";
 import Parse from "parse";
 import {useLocation} from "react-router";
-import {ReactNode} from "react";
+import {ReactNode, useEffect, useState} from "react";
+import {LoadingScreen} from "../components/LoadingScreen";
 
-function RequireAuthentication({children}: {children: ReactNode}) {
+const RequireAuthentication = function({children}: {children: ReactNode}) {
   const location = useLocation();
-  const currentUser = Parse.User.current();
-  if (currentUser) {
+
+  const [verifiedSession, setVerifiedSession] = useState<boolean | undefined>(undefined);
+
+  useEffect(() => {
+    Parse.Session.current()
+      .then(() => {
+        setVerifiedSession(true);
+      })
+      .catch(() => {
+        setVerifiedSession(false);
+      });
+  }, []);
+
+  if (verifiedSession === undefined) {
+    // wait while session needs to be verified
+    return <LoadingScreen />;
+  }
+
+  if (verifiedSession) {
     return <>{children}</>;
   }
   return <Navigate to="/login" state={{from: location}} />;
