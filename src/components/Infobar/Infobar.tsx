d78cde15440822e47ee26182d90f56e05c75fa47
Manuel Brandstetter <accounts@manuelbrandstetter.de>
2021-12-08 08:42:50 +0100

    Add equality check for the redux selector in the infobar component to avoid unnecessary re-rendering

--- src/components/Infobar/Infobar.tsx
+++ src/components/Infobar/Infobar.tsx
@@ -2,16 +2,20 @@ import {Timer} from "components/Timer";
 import {VoteDisplay} from "components/Votes/VoteDisplay";
 import ReactDOM from "react-dom";
 import "./Infobar.scss";
-import {useAppSelector} from "../../store";
+import _ from "underscore";
+import {useAppSelector} from "store";
 
 export const Infobar = () => {
-  const state = useAppSelector((applicationState) => ({
-    endTime: applicationState.board.data?.timerUTCEndTime,
-    activeVoting: applicationState.board.data?.voting === "active",
-    possibleVotes: applicationState.voteConfiguration.voteLimit,
-    usedVotes: applicationState.votes.filter((vote) => vote.votingIteration === applicationState.voteConfiguration.votingIteration && vote.user === Parse.User.current()?.id)
-      .length,
-  }));
+  const state = useAppSelector(
+    (applicationState) => ({
+      endTime: applicationState.board.data?.timerUTCEndTime,
+      activeVoting: applicationState.board.data?.voting === "active",
+      possibleVotes: applicationState.voteConfiguration.voteLimit,
+      usedVotes: applicationState.votes.filter((vote) => vote.votingIteration === applicationState.voteConfiguration.votingIteration && vote.user === Parse.User.current()?.id)
+        .length,
+    }),
+    _.isEqual
+  );
 
   return ReactDOM.createPortal(
     <aside className="infobar">
