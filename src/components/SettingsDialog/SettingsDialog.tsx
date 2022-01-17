0d9fe27df265770ca6c04b009cd7fe4abc4877a0
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-01-17 14:03:43 +0100

    Add basic styling for the SettingsDialog.tsx

--- src/components/SettingsDialog/SettingsDialog.tsx
+++ src/components/SettingsDialog/SettingsDialog.tsx
@@ -1,27 +1,46 @@
-import React, {useEffect} from "react";
-import ReactDOM from "react-dom";
+import React from "react";
 import {Outlet} from "react-router";
 import {Link} from "react-router-dom";
+import {useAppSelector} from "store";
 import "./SettingsDialog.scss";
+import {Portal} from "components/Portal";
 
 export const SettingsDialog: React.VFC = () => {
-  useEffect(() => {
-    console.log("SettingsDialog");
-  });
+  const boardId = useAppSelector((applicationState) => applicationState.board.data!.id);
 
-  return ReactDOM.createPortal(
-    <aside className="settings-dialog">
-      <nav>
-        <Link to="">Board Settings</Link>
-        <Link to="participants">Participants</Link>
-        <Link to="appearance">Appearance</Link>
-        <Link to="share">Share Session</Link>
-        <Link to="export">Export Board</Link>
-        <Link to="feedback">Feedback</Link>
-        <Link to="profile">Change your profile</Link>
-      </nav>
-      <Outlet />
-    </aside>,
-    document.body
+  return (
+    <Portal darkBackground onClose={() => undefined}>
+      <aside className="settings-dialog">
+        <nav className="settings-dialog__navigation">
+          <Link to="">
+            <div className="navigation__item">Board Settings</div>
+          </Link>
+          <Link to="participants">
+            <div className="navigation__item">Participants</div>
+          </Link>
+          <Link to="appearance">
+            <div className="navigation__item">Appearance</div>
+          </Link>
+          <Link to="share">
+            <div className="navigation__item">Share Session</div>
+          </Link>
+          <Link to="export">
+            <div className="navigation__item">Export Board</div>
+          </Link>
+          <Link to="feedback">
+            <div className="navigation__item">Feedback</div>
+          </Link>
+          <Link to="profile">
+            <div className="navigation__item">Change your profile</div>
+          </Link>
+        </nav>
+        <article className="settings-dialog__content">
+          <Outlet />
+        </article>
+        <Link to={`/board/${boardId}`} className="settings-dialog__close-button">
+          X
+        </Link>
+      </aside>
+    </Portal>
   );
 };
