806d31379081771520c2f355ac6454ee7ed448ad
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-01-17 22:02:19 +0100

    Add active classname for links to show the current location

--- src/components/SettingsDialog/SettingsDialog.tsx
+++ src/components/SettingsDialog/SettingsDialog.tsx
@@ -7,47 +7,49 @@ import {Portal} from "components/Portal";
 import {ReactComponent as CloseIcon} from "assets/icon-close.svg";
 import {ReactComponent as SettingsIcon} from "assets/icon-settings.svg";
 import {ScrumlrLogo} from "components/ScrumlrLogo";
+import classNames from "classnames";
 
 export const SettingsDialog: React.VFC = () => {
   const navigate = useNavigate();
   const boardId = useAppSelector((applicationState) => applicationState.board.data!.id);
+  console.log(window.location.pathname);
 
   return (
     <Portal darkBackground onClose={() => navigate(`/board/${boardId}`)}>
       <aside className="settings-dialog">
         <nav className="settings-dialog__navigation">
           <ScrumlrLogo />
-          <Link to="" className="navigation__item">
+          <Link to="" className={classNames("navigation__item", {"navigation__item--active": window.location.pathname.endsWith("/settings")})}>
             <p>Board Settings</p>
             <p>Name, Access Policy</p>
             <SettingsIcon className="navigation-item__icon" />
           </Link>
-          <Link to="participants" className="navigation__item">
+          <Link to="participants" className={classNames("navigation__item", {"navigation__item--active": window.location.pathname.endsWith("/settings/participants")})}>
             <p>Participants</p>
             <p>User List, User Rrole</p>
             <SettingsIcon className="navigation-item__icon" />
           </Link>
-          <Link to="appearance" className="navigation__item">
+          <Link to="appearance" className={classNames("navigation__item", {"navigation__item--active": window.location.pathname.endsWith("/appearance")})}>
             <p>Appearance</p>
             <p>Notifications, Language, UI</p>
             <SettingsIcon className="navigation-item__icon" />
           </Link>
-          <Link to="share" className="navigation__item">
+          <Link to="share" className={classNames("navigation__item", {"navigation__item--active": window.location.pathname.endsWith("/share")})}>
             <p>Share Session</p>
             <p>QR Code, URL</p>
             <SettingsIcon className="navigation-item__icon" />
           </Link>
-          <Link to="export" className="navigation__item">
+          <Link to="export" className={classNames("navigation__item", {"navigation__item--active": window.location.pathname.endsWith("/export")})}>
             <p>Export Board</p>
             <p>Share the board</p>
             <SettingsIcon className="navigation-item__icon" />
           </Link>
-          <Link to="feedback" className="navigation__item">
+          <Link to="feedback" className={classNames("navigation__item", {"navigation__item--active": window.location.pathname.endsWith("/feedback")})}>
             <p>Feedback</p>
             <p>We love to hear from you</p>
             <SettingsIcon className="navigation-item__icon" />
           </Link>
-          <Link to="profile" className="navigation__item">
+          <Link to="profile" className={classNames("navigation__item", {"navigation__item--active": window.location.pathname.endsWith("/profile")})}>
             <p>TODO Displayname</p>
             <p>Edit Profile</p>
             <SettingsIcon className="navigation-item__icon" />
