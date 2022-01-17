bcd271dc8021bd197a91392c97f0b1507ea74cc8
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-01-17 19:16:50 +0100

    Add styling for navigation items

--- src/components/SettingsDialog/SettingsDialog.tsx
+++ src/components/SettingsDialog/SettingsDialog.tsx
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 import {useAppSelector} from "store";
 import "./SettingsDialog.scss";
 import {Portal} from "components/Portal";
+import {ReactComponent as CloseIcon} from "assets/icon-close.svg";
 
 export const SettingsDialog: React.VFC = () => {
   const boardId = useAppSelector((applicationState) => applicationState.board.data!.id);
@@ -12,33 +13,40 @@ export const SettingsDialog: React.VFC = () => {
     <Portal darkBackground onClose={() => undefined}>
       <aside className="settings-dialog">
         <nav className="settings-dialog__navigation">
-          <Link to="">
-            <div className="navigation__item">Board Settings</div>
+          <Link to="" className="navigation__item">
+            <p>Board Settings</p>
+            <p>Name, Access Policy</p>
           </Link>
-          <Link to="participants">
-            <div className="navigation__item">Participants</div>
+          <Link to="participants" className="navigation__item">
+            <p>Participants</p>
+            <p>User List, User Rrole</p>
           </Link>
-          <Link to="appearance">
-            <div className="navigation__item">Appearance</div>
+          <Link to="appearance" className="navigation__item">
+            <p>Appearance</p>
+            <p>Notifications, Language, UI</p>
           </Link>
-          <Link to="share">
-            <div className="navigation__item">Share Session</div>
+          <Link to="share" className="navigation__item">
+            <p>Share Session</p>
+            <p>QR Code, URL</p>
           </Link>
-          <Link to="export">
-            <div className="navigation__item">Export Board</div>
+          <Link to="export" className="navigation__item">
+            <p>Export Board</p>
+            <p>Share the board</p>
           </Link>
-          <Link to="feedback">
-            <div className="navigation__item">Feedback</div>
+          <Link to="feedback" className="navigation__item">
+            <p>Feedback</p>
+            <p>We love to hear from you</p>
           </Link>
-          <Link to="profile">
-            <div className="navigation__item">Change your profile</div>
+          <Link to="profile" className="navigation__item">
+            <p>TODO Displayname</p>
+            <p>Edit Profile</p>
           </Link>
         </nav>
         <article className="settings-dialog__content">
           <Outlet />
         </article>
         <Link to={`/board/${boardId}`} className="settings-dialog__close-button">
-          X
+          <CloseIcon className="close-button__icon" />
         </Link>
       </aside>
     </Portal>
