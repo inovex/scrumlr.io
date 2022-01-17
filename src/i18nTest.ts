f0b96786860e36120379c365bc6cabeec00b8b3a
Johanna Dolinga <jdolinga@inovex.de>
2022-01-17 13:32:11 +0100

    load path

--- src/i18nTest.ts
+++ src/i18nTest.ts
@@ -1,11 +1,12 @@
 import i18n from "i18next";
 import Backend from "i18next-fs-backend";
+import {join} from "path";
 import {initReactI18next} from "react-i18next";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const options: any = {
   backend: {
-    loadPath: "locales/en/translation.json",
+    loadPath: join(__dirname, "../public/locales/en/translation.json"),
   },
   initImmediate: false,
   lng: "en",
