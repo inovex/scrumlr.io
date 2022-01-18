ecb9ea162558c79ed620b9b33083460784386324
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-01-18 09:17:28 +0100

    Add redundant components and tests which will be replaced in the new settings dialog

--- src/components/BoardHeader/HeaderMenu/BoardOptions/index.ts
+++ src/components/BoardHeader/HeaderMenu/BoardOptions/index.ts
@@ -1,15 +1,9 @@
 import {ShowAuthorOption} from "./ShowAuthorOption";
 import {ShowHiddenColumnsOption} from "./ShowHiddenColumnsOption";
-import {DeleteBoardOption} from "./DeleteBoardOption";
-import {ExportBoardOption} from "./ExportBoardOption";
 import {ShowOtherUsersNotesOption} from "./ShowOtherUsersNotesOption";
-import {ShareQrCodeOption} from "./ShareQrCodeOption";
 
 export const BoardOption = {
   ShowAuthorOption,
   ShowHiddenColumnsOption,
-  DeleteBoardOption,
-  ExportBoardOption,
   ShowOtherUsersNotesOption,
-  ShareQrCodeOption,
 };
