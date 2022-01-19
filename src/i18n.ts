c9fcf3858801b18b18c2c616ab26a6afd1acfe12
Johanna Dolinga <jdolinga@inovex.de>
2022-01-19 16:25:40 +0100

    package.json updates

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,32 +1,24 @@
 import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
-import ChainedBackend from "i18next-chained-backend";
-import HttpBackend from "i18next-http-backend";
-import LocalStorageBackend from "i18next-localstorage-backend";
+import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
-  .use(ChainedBackend)
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "en",
-    // ... your i18next config
     backend: {
-      backends: [LocalStorageBackend, HttpBackend],
-      backendOptions: [
-        {
-          expirationTime: 7 * 24 * 60 * 60 * 1000, // 7 days
-        },
-        {
-          loadPath: "public/locales/{{lng}}/{{ns}}.json",
-        },
-      ],
+      queryStringParams: {v: "1.0.0"},
     },
     detection: {
       lookupLocalStorage: "Scrumlr/locale",
       order: ["localStorage"],
       caches: ["localStorage"],
+    },
+    fallbackLng: "en",
+    react: {
+      useSuspense: false,
     },
   });
 
--- src/i18nTest.ts
+++ src/i18nTest.ts
@@ -1,21 +1,21 @@
-import i18n from "i18next";
-import Backend from "i18next-fs-backend";
 import {join} from "path";
+import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
+import Backend from "i18next-fs-backend";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const options: any = {
-  backend: {
-    loadPath: join(__dirname, "../public/locales/en/translation.json"),
-  },
-  initImmediate: false,
-  lng: "en",
-  debug: false,
-  react: {
-    useSuspense: false,
-  },
-};
-
-i18n.use(Backend).use(initReactI18next).init(options);
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    backend: {
+      loadPath: join(__dirname, "../public/locales/en/translation.json"),
+    },
+    initImmediate: false,
+    lng: "en",
+    debug: false,
+    react: {
+      useSuspense: false,
+    },
+  });
 
 export default i18n;
