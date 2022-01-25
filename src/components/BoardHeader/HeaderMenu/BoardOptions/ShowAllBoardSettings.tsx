9a2511a6a0d19aae2c7d4f55e9b621c169a33174
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-01-25 20:13:34 +0100

    Add localization to board header option

--- src/components/BoardHeader/HeaderMenu/BoardOptions/ShowAllBoardSettings.tsx
+++ src/components/BoardHeader/HeaderMenu/BoardOptions/ShowAllBoardSettings.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useTranslation} from "react-i18next";
 import {BoardOption} from "./BoardOption";
 import {BoardOptionLink} from "./BoardOptionLink";
 
@@ -6,8 +7,11 @@ type ShowAllBoardSettingsProps = {
   onClose?: () => void;
 };
 
-export const ShowAllBoardSettings: React.VFC<ShowAllBoardSettingsProps> = (props) => (
-  <BoardOption data-testid="allSettings">
-    <BoardOptionLink to="settings" label="Show all board settings" onClick={props.onClose} />
-  </BoardOption>
-);
+export const ShowAllBoardSettings: React.VFC<ShowAllBoardSettingsProps> = (props) => {
+  const {t} = useTranslation();
+  return (
+    <BoardOption data-testid="allSettings">
+      <BoardOptionLink to="settings" label={t("BoardHeader.showAllBoardSettings")} onClick={props.onClose} />
+    </BoardOption>
+  );
+};
