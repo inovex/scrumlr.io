47fdc65922ab66598c754049805ac59b80380a5d
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-02-07 11:45:52 +0100

    Add generic dialog component for the voting- and timer- dialog

---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,25 @@
+import {FC} from "react";
+import className from "classnames";
+import {Portal} from "components/Portal";
+import {ReactComponent as CloseIcon} from "assets/icon-close.svg";
+import "./Dialog.scss";
+
+type DialogProps = {
+  title: string;
+  className?: string;
+  onClose?: () => void;
+};
+
+export const Dialog: FC<DialogProps> = (props) => (
+  <Portal darkBackground onClose={() => props.onClose?.()}>
+    <aside className={className("dialog", props.className)}>
+      <article className="dialog__content">
+        <h2 className="dialog__header-text">Voting</h2>
+        {props.children}
+        <button onClick={() => props.onClose?.()} className="dialog__close-button">
+          <CloseIcon className="close-button__icon" />
+        </button>
+      </article>
+    </aside>
+  </Portal>
+);
--- src/components/VotingDialog/VotingDialog.tsx
+++ src/components/VotingDialog/VotingDialog.tsx
@@ -1,12 +1,10 @@
 import {VFC, useState} from "react";
 import {useNavigate} from "react-router";
-import {Portal} from "components/Portal";
 import {Toggle} from "components/Toggle";
 import store, {useAppSelector} from "store";
-import {Link} from "react-router-dom";
 import {ActionFactory} from "store/action";
-import {ReactComponent as CloseIcon} from "assets/icon-close.svg";
 import "./VotingDialog.scss";
+import {Dialog} from "components/Dialog";
 
 export const VotingDialog: VFC = () => {
   const navigate = useNavigate();
@@ -28,6 +26,7 @@ export const VotingDialog: VFC = () => {
     store.dispatch(ActionFactory.editBoard({id: boardId, voting: "active"}));
     navigate(`/board/${boardId}`);
   };
+
   const stopVoting = () => {
     store.dispatch(ActionFactory.editBoard({id: boardId, voting: "disabled"}));
     navigate(`/board/${boardId}`);
@@ -38,48 +37,41 @@ export const VotingDialog: VFC = () => {
   };
 
   return (
-    <Portal darkBackground onClose={() => navigate(`/board/${boardId}`)}>
-      <aside className="voting-dialog">
-        {!activeVoting ? (
-          <article className="voting-dialog__content">
-            <h2 className="voting-dialog__header-text">Voting</h2>
-            <button className="voting-dialog__button" onClick={() => setAllowCumulativeVoting((state) => !state)}>
-              <label>Allow cumulative voting</label>
-              <Toggle active={allowCumulativeVoting} className="voting-dialog__toggle" />
+    <Dialog title="Voting" onClose={() => navigate(`/board/${boardId}`)}>
+      {activeVoting ? (
+        <>
+          <button className="voting-dialog__start-button" onClick={() => cancelVoting()}>
+            <label>Cancel voting phase</label>
+          </button>
+          <button className="voting-dialog__start-button" onClick={() => stopVoting()}>
+            <label>End voting phase</label>
+          </button>
+        </>
+      ) : (
+        <>
+          <button className="voting-dialog__button" onClick={() => setAllowCumulativeVoting((state) => !state)}>
+            <label>Allow cumulative voting</label>
+            <Toggle active={allowCumulativeVoting} className="voting-dialog__toggle" />
+          </button>
+          <button className="voting-dialog__button" onClick={() => setAnonymousVoting((state) => !state)}>
+            <label>Anonymous voting</label>
+            <Toggle active={anonymousVoting} className="voting-dialog__toggle" />
+          </button>
+          <div className="voting-dialog__button">
+            <label>Number of votes</label>
+            <button onClick={() => setNumberOfVotes((prev) => Math.min(++prev, 99))} className="voting-dialog__vote-button">
+              +
             </button>
-            <button className="voting-dialog__button" onClick={() => setAnonymousVoting((state) => !state)}>
-              <label>Anonymous voting</label>
-              <Toggle active={anonymousVoting} className="voting-dialog__toggle" />
+            <label className="voting-dialog__vote-label">{numberOfVotes}</label>
+            <button onClick={() => setNumberOfVotes((prev) => Math.max(--prev, 0))} className="voting-dialog__vote-button">
+              -
             </button>
-            <div className="voting-dialog__button">
-              <label>Number of votes</label>
-              <button onClick={() => setNumberOfVotes((prev) => Math.min(++prev, 99))} className="voting-dialog__vote-button">
-                +
-              </button>
-              <label className="voting-dialog__vote-label">{numberOfVotes}</label>
-              <button onClick={() => setNumberOfVotes((prev) => Math.max(--prev, 0))} className="voting-dialog__vote-button">
-                -
-              </button>
-            </div>
-            <button className="voting-dialog__start-button" onClick={() => startVoting()}>
-              <label>Start voting phase</label>
-            </button>
-          </article>
-        ) : (
-          <article className="voting-dialog__content">
-            <h2 className="voting-dialog__header-text">Voting</h2>
-            <button className="voting-dialog__start-button" onClick={() => cancelVoting()}>
-              <label>Cancel voting phase</label>
-            </button>
-            <button className="voting-dialog__start-button" onClick={() => stopVoting()}>
-              <label>End voting phase</label>
-            </button>
-          </article>
-        )}
-        <Link to={`/board/${boardId}`} className="voting-dialog__close-button">
-          <CloseIcon className="close-button__icon" />
-        </Link>
-      </aside>
-    </Portal>
+          </div>
+          <button className="voting-dialog__start-button" onClick={() => startVoting()}>
+            <label>Start voting phase</label>
+          </button>
+        </>
+      )}
+    </Dialog>
   );
 };
