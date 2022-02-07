841ffb9fd6c6681c9972caa5ae1b8c562e753e0f
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-02-07 10:17:16 +0100

    Add vote limit buttons, end voting button and cancel voting button

--- src/components/VotingDialog/VotingDialog.tsx
+++ src/components/VotingDialog/VotingDialog.tsx
@@ -11,41 +11,71 @@ import "./VotingDialog.scss";
 export const VotingDialog: VFC = () => {
   const navigate = useNavigate();
   const boardId = useAppSelector((state) => state.board.data!.id);
+  const activeVoting = useAppSelector((state) => state.board.data!.voting === "active");
   const [allowCumulativeVoting, setAllowCumulativeVoting] = useState(false);
   const [anonymousVoting, setAnonymousVoting] = useState(false);
+  const [numberOfVotes, setNumberOfVotes] = useState(5);
 
   const startVoting = () => {
     store.dispatch(
       ActionFactory.addVoteConfiguration({
         boardId,
-        voteLimit: 5,
+        voteLimit: numberOfVotes,
         showVotesOfOtherUsers: !anonymousVoting,
         allowMultipleVotesPerNote: allowCumulativeVoting,
       })
     );
     store.dispatch(ActionFactory.editBoard({id: boardId, voting: "active"}));
+    navigate(`/board/${boardId}`);
+  };
+  const stopVoting = () => {
+    store.dispatch(ActionFactory.editBoard({id: boardId, voting: "disabled"}));
+    navigate(`/board/${boardId}`);
+  };
+  const cancelVoting = () => {
+    store.dispatch(ActionFactory.cancelVoting(boardId));
+    navigate(`/board/${boardId}`);
   };
 
   return (
     <Portal darkBackground onClose={() => navigate(`/board/${boardId}`)}>
       <aside className="voting-dialog">
-        <article className="voting-dialog__content">
-          <h2 className="voting-dialog__header-text">Voting</h2>
-          <button className="voting-dialog__button" onClick={() => setAllowCumulativeVoting((state) => !state)}>
-            <label>Allow cumulative voting</label>
-            <Toggle active={allowCumulativeVoting} className="voting-dialog__toggle" />
-          </button>
-          <button className="voting-dialog__button" onClick={() => setAnonymousVoting((state) => !state)}>
-            <label>Anonymous voting</label>
-            <Toggle active={anonymousVoting} className="voting-dialog__toggle" />
-          </button>
-          <button className="voting-dialog__button">
-            <label>Number of votes</label>
-          </button>
-          <button className="voting-dialog__start-button" onClick={() => startVoting()}>
-            <label>Start voting phase</label>
-          </button>
-        </article>
+        {!activeVoting ? (
+          <article className="voting-dialog__content">
+            <h2 className="voting-dialog__header-text">Voting</h2>
+            <button className="voting-dialog__button" onClick={() => setAllowCumulativeVoting((state) => !state)}>
+              <label>Allow cumulative voting</label>
+              <Toggle active={allowCumulativeVoting} className="voting-dialog__toggle" />
+            </button>
+            <button className="voting-dialog__button" onClick={() => setAnonymousVoting((state) => !state)}>
+              <label>Anonymous voting</label>
+              <Toggle active={anonymousVoting} className="voting-dialog__toggle" />
+            </button>
+            <div className="voting-dialog__button">
+              <label>Number of votes</label>
+              <button onClick={() => setNumberOfVotes((prev) => Math.min(++prev, 99))} className="voting-dialog__vote-button">
+                +
+              </button>
+              <label className="voting-dialog__vote-label">{numberOfVotes}</label>
+              <button onClick={() => setNumberOfVotes((prev) => Math.max(--prev, 0))} className="voting-dialog__vote-button">
+                -
+              </button>
+            </div>
+            <button className="voting-dialog__start-button" onClick={() => startVoting()}>
+              <label>Start voting phase</label>
+            </button>
+          </article>
+        ) : (
+          <article className="voting-dialog__content">
+            <h2 className="voting-dialog__header-text">Voting</h2>
+            <button className="voting-dialog__start-button" onClick={() => cancelVoting()}>
+              <label>Cancel voting phase</label>
+            </button>
+            <button className="voting-dialog__start-button" onClick={() => stopVoting()}>
+              <label>End voting phase</label>
+            </button>
+          </article>
+        )}
         <Link to={`/board/${boardId}`} className="voting-dialog__close-button">
           <CloseIcon className="close-button__icon" />
         </Link>
