d375afbd39e20b0d78d802c7ee6480993a94f4f8
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-02-13 11:34:00 +0100

    Ongoing portal refactor

---
+++ src/utils/hooks/useWindowEvent.ts
@@ -0,0 +1,19 @@
+import {useEffect, useRef} from "react";
+
+export function useWindowEvent<TType extends keyof WindowEventMap>(
+  type: TType,
+  listener: (this: Window, ev: WindowEventMap[TType]) => any,
+  options?: boolean | AddEventListenerOptions
+) {
+  const listenerRef = useRef(listener);
+  listenerRef.current = listener;
+
+  useEffect(() => {
+    function handler(event: WindowEventMap[TType]) {
+      listenerRef.current.call(window, event);
+    }
+
+    window.addEventListener(type, handler, options);
+    return () => window.removeEventListener(type, handler, options);
+  }, [type, options]);
+}
