64375a29eaec84cb0a3b42f93868f5ae7d2d1c52
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-02-27 13:20:39 +0100

    Change generic type name

--- src/utils/hooks/useWindowEvent.ts
+++ src/utils/hooks/useWindowEvent.ts
@@ -1,15 +1,11 @@
 import {useEffect, useRef} from "react";
 
-export function useWindowEvent<TType extends keyof WindowEventMap>(
-  type: TType,
-  listener: (this: Window, ev: WindowEventMap[TType]) => unknown,
-  options?: boolean | AddEventListenerOptions
-) {
+export function useWindowEvent<T extends keyof WindowEventMap>(type: T, listener: (this: Window, ev: WindowEventMap[T]) => unknown, options?: boolean | AddEventListenerOptions) {
   const listenerRef = useRef(listener);
   listenerRef.current = listener;
 
   useEffect(() => {
-    function handler(event: WindowEventMap[TType]) {
+    function handler(event: WindowEventMap[T]) {
       listenerRef.current.call(window, event);
     }
 
