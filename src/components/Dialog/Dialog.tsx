3f073b4c5993c1686e0a2629ed3635ccf1dedb7e
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-03-08 12:09:56 +0100

    Remove old attribute of portal

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -13,7 +13,7 @@ type DialogProps = {
 };
 
 export const Dialog: FC<DialogProps> = ({title, className, onClose, children, ...other}) => (
-  <Portal darkBackground={false} onClose={() => onClose?.()}>
+  <Portal onClose={() => onClose?.()}>
     <aside className={classNames("dialog", className)} {...other}>
       <article className="dialog__content">
         <h2 className="dialog__header-text">{title}</h2>
