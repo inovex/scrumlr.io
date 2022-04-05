33bf4d7ed6acb4e4b56a8cce9c98e3ee2e2eb7da
Resaki1 <christian.reski@t-online.de>
2022-04-05 10:41:54 +0200

    added input functionality

--- src/components/SettingsDialog/ProfileSettings/ProfileSettings.tsx
+++ src/components/SettingsDialog/ProfileSettings/ProfileSettings.tsx
@@ -1,12 +1,21 @@
 import classNames from "classnames";
-import {useRef} from "react";
-import {useAppSelector} from "store";
+import {useEffect, useRef, useState} from "react";
 import Parse from "parse";
+import store, {useAppSelector} from "store";
+import {ActionFactory} from "store/action";
+import {UserServerModel} from "types/user";
+import {ApplicationState} from "types/store";
 import {SettingsButton} from "../Components/SettingsButton";
 import "./ProfileSettings.scss";
 
 export const ProfileSettings = () => {
-  const userName = useAppSelector((applicationState) => applicationState.users.all.find((user) => user.id === Parse.User.current()?.id)?.displayName);
+  const state = useAppSelector((applicationState: ApplicationState) => ({
+    user: applicationState.users.all.find((user) => user.id === Parse.User.current()?.id),
+  }));
+
+  const [userName, setUserName] = useState<string | undefined>();
+
+  useEffect(() => setUserName(state.user?.displayName), [state.user?.displayName]);
 
   const nameInputRef = useRef<HTMLInputElement>(null);
 
@@ -16,9 +25,20 @@ export const ProfileSettings = () => {
         <h2 className="settings-dialog__header-text">Profile</h2>
       </header>
       <div className="profile-settings__container">
-        <SettingsButton className="profile-settings__user-name-button" label="User Name" onClick={() => nameInputRef.current?.focus()}>
-          <input ref={nameInputRef} className="profile-settings__user-name-button_input" value={userName} /* onChange={(e) => setUserName(e.target.value)} */ />
-        </SettingsButton>
+        {userName && (
+          <SettingsButton className="profile-settings__user-name-button" label="User Name" onClick={() => nameInputRef.current?.focus()}>
+            <input
+              ref={nameInputRef}
+              className="profile-settings__user-name-button_input"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              onKeyPress={(e) =>
+                e.key === "Enter" && state.user && userName && store.dispatch(ActionFactory.updateUser({...(state.user as unknown as UserServerModel), displayName: userName}))
+              }
+              onBlur={() => state.user && userName && store.dispatch(ActionFactory.updateUser({...(state.user as unknown as UserServerModel), displayName: userName}))}
+            />
+          </SettingsButton>
+        )}
       </div>
     </div>
   );
