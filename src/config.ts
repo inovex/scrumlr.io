b66ad617b43011adb23ec203080b30b02fbc4a54
bitionaire <johann.boehler@scrumlr.io>
2022-04-15 12:47:55 +0200

    Use window location for url configuration

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const SERVER_HTTP_URL = process.env.REACT_APP_SERVER_HTTP_URL || `/api`;
-export const SERVER_WEBSOCKET_URL = process.env.REACT_APP_SERVER_WEBSOCKET_URL || "/api";
+export const SERVER_HTTP_URL = process.env.REACT_APP_SERVER_HTTP_URL || `${window.location.origin.replace(window.location.protocol, "https:")}/api`;
+export const SERVER_WEBSOCKET_URL = process.env.REACT_APP_SERVER_WEBSOCKET_URL || `${window.location.origin.replace(window.location.protocol, "wss:")}/api`;
