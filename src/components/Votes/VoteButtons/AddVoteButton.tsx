1d206f523918a82f10703492830db59c4ff68f9e
Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>
2022-04-27 09:19:21 +0200

    Various changes regarding styling, appearance and voting (#1621)

--- src/components/Votes/VoteButtons/AddVoteButton.tsx
+++ src/components/Votes/VoteButtons/AddVoteButton.tsx
@@ -1,11 +1,9 @@
-import {useAppSelector} from "store";
 import {Actions} from "store/action";
 import {DotButton} from "components/DotButton";
 import "./AddVoteButton.scss";
 import "./VoteButton.scss";
 import {ReactComponent as PlusIcon} from "assets/icon-add.svg";
 import {FC} from "react";
-import {ApplicationState} from "types";
 import {useDispatch} from "react-redux";
 
 type AddVoteProps = {
@@ -16,14 +14,9 @@ type AddVoteProps = {
 
 export const AddVoteButton: FC<AddVoteProps> = ({noteId, tabIndex, disabled}) => {
   const dispatch = useDispatch();
-  const state = useAppSelector((applicationState: ApplicationState) => ({
-    votingEnabled: !!applicationState.votings.open,
-  }));
 
   const addVote = () => {
-    if (state.votingEnabled) {
-      dispatch(Actions.addVote(noteId));
-    }
+    dispatch(Actions.addVote(noteId));
   };
 
   return (
