b886b3f5d65ab2939dfedd1092bd1fb69eb27c6b
Benjamin Fürtsch <Joshua.Fuertsch@web.de>
2022-05-06 12:23:39 +0200

    add surrounding green border

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -123,7 +123,7 @@ export const Column = ({id, name, color, visible, index, tabIndex}: ColumnProps)
   );
 
   return (
-    <section className={`column ${getColorClassName(color)}`} ref={columnRef}>
+    <section className={`column ${state.moderating && "column__moderation-isActive"} ${getColorClassName(color)}`} ref={columnRef}>
       <div className="column__content">
         <div className="column__header">
           <div className="column__header-title">
