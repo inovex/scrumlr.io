b3fc879daf8ca43a66825361f2727a679136c7d8
Benjamin Fürtsch <49522775+Benjosh95@users.noreply.github.com>
2022-05-06 19:07:07 +0200

    Add highlighting of active moderation mode(#1641)
    * extend moderation notification text
    
    * add surrounding green border
    
    * add isModerator condition
    
    * fix className snapshot error

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -123,7 +123,7 @@ export const Column = ({id, name, color, visible, index, tabIndex}: ColumnProps)
   );
 
   return (
-    <section className={`column ${getColorClassName(color)}`} ref={columnRef}>
+    <section className={classNames("column", {"column__moderation-isActive": isModerator && state.moderating}, getColorClassName(color))} ref={columnRef}>
       <div className="column__content">
         <div className="column__header">
           <div className="column__header-title">
