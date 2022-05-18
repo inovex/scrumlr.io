f09e03336d6a2f41ea1cbcd667d674402559ffa8
Sebastian Schwarzer <sebastian.schwarzer@inovex.de>
2022-05-18 17:43:20 +0200

    Add scrollbar

--- src/components/SettingsDialog/ProfileSettings/ProfileSettings.tsx
+++ src/components/SettingsDialog/ProfileSettings/ProfileSettings.tsx
@@ -23,19 +23,21 @@ export const ProfileSettings = () => {
       <header className="settings-dialog__header">
         <h2 className="settings-dialog__header-text">{t("ProfileSettings.Profile")}</h2>
       </header>
-      <div className="profile-settings__container">
-        <SettingsButton className="profile-settings__user-name-button" label={t("ProfileSettings.UserName")} onClick={() => nameInputRef.current?.focus()}>
-          <input
-            ref={nameInputRef}
-            className="profile-settings__user-name-button_input"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            onKeyPress={(e) => e.key === "Enter" && state.participant && userName && store.dispatch(Actions.editSelf({...state.participant.user, name: userName}))}
-            onBlur={() => state.participant && userName && store.dispatch(Actions.editSelf({...state.participant.user, name: userName}))}
-          />
-        </SettingsButton>
+      <div className="profile-settings__container-wrapper">
+        <div className="profile-settings__container">
+          <SettingsButton className="profile-settings__user-name-button" label={t("ProfileSettings.UserName")} onClick={() => nameInputRef.current?.focus()}>
+            <input
+              ref={nameInputRef}
+              className="profile-settings__user-name-button_input"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              onKeyPress={(e) => e.key === "Enter" && state.participant && userName && store.dispatch(Actions.editSelf({...state.participant.user, name: userName}))}
+              onBlur={() => state.participant && userName && store.dispatch(Actions.editSelf({...state.participant.user, name: userName}))}
+            />
+          </SettingsButton>
 
-        <AvatarSettings id={id} />
+          <AvatarSettings id={id} />
+        </div>
       </div>
     </div>
   );
