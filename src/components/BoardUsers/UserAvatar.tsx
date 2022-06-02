33b90cd422aefde4603aace88e04384acf4cb276
Resaki1 <christian.reski@t-online.de>
2022-06-02 17:53:24 +0200

    briefly displaying ready checkmark when users set themselves ready while raising their hand

--- src/components/BoardUsers/UserAvatar.tsx
+++ src/components/BoardUsers/UserAvatar.tsx
@@ -1,3 +1,4 @@
+import {useEffect, useRef, useState} from "react";
 import "./BoardUser.scss";
 import classNames from "classnames";
 import {ReactComponent as IconCheck} from "assets/icon-check.svg";
@@ -16,11 +17,31 @@ export interface UserAvatarProps {
   badgeText?: string;
 }
 
-export const UserAvatar = ({name, badgeText, id, ready, raisedHand, avatar, className, avatarClassName}: UserAvatarProps) => (
-  <div className={classNames("user-avatar", className, ready && "user-ready")} title={name}>
-    {ready && !raisedHand && <IconCheck className="user-avatar__ready" />}
-    {raisedHand && <RaisedHand className="user-avatar__raised-hand" />}
-    {avatar ? <img src={avatar} className={avatarClassName} alt={name} /> : <Avatar seed={id} className={avatarClassName} />}
-    {badgeText && <Badge text={badgeText} />}
-  </div>
-);
+export const UserAvatar = ({name, badgeText, id, ready, raisedHand, avatar, className, avatarClassName}: UserAvatarProps) => {
+  const [showHand, setShowHand] = useState<boolean | undefined>(raisedHand);
+  const prevReadyRef = useRef<boolean | undefined>(ready);
+  const handRef = useRef<boolean | undefined>(raisedHand);
+
+  useEffect(() => {
+    if (prevReadyRef.current === false && ready === true && raisedHand === true) {
+      setShowHand(false);
+      setTimeout(() => {
+        setShowHand(handRef.current);
+      }, 2000);
+    } else {
+      setShowHand(raisedHand);
+    }
+
+    prevReadyRef.current = ready;
+    handRef.current = raisedHand;
+  }, [ready, raisedHand]);
+
+  return (
+    <div className={classNames("user-avatar", className, ready && "user-ready")} title={name}>
+      {ready && !showHand && <IconCheck className="user-avatar__ready" />}
+      {showHand && <RaisedHand className="user-avatar__raised-hand" />}
+      {avatar ? <img src={avatar} className={avatarClassName} alt={name} /> : <Avatar seed={id} className={avatarClassName} />}
+      {badgeText && <Badge text={badgeText} />}
+    </div>
+  );
+};
