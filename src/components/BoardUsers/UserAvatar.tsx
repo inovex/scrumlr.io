2620ac0e588c0fcbf33b9d5b9406c1ed5876a5e6
Resaki1 <christian.reski@t-online.de>
2022-06-02 12:35:24 +0200

    displaying raised hand icon in users list

--- src/components/BoardUsers/UserAvatar.tsx
+++ src/components/BoardUsers/UserAvatar.tsx
@@ -1,6 +1,7 @@
 import "./BoardUser.scss";
 import classNames from "classnames";
 import {ReactComponent as IconCheck} from "assets/icon-check.svg";
+import {ReactComponent as RaisedHand} from "assets/icon-hand.svg";
 import {Avatar} from "../Avatar";
 import {Badge} from "../Badge";
 
@@ -11,12 +12,14 @@ export interface UserAvatarProps {
   name: string;
   avatar?: string;
   ready?: boolean;
+  raisedHand?: boolean;
   badgeText?: string;
 }
 
-export const UserAvatar = ({name, badgeText, id, ready, avatar, className, avatarClassName}: UserAvatarProps) => (
-  <div className={classNames("user-avatar", className)} title={name}>
-    {ready && <IconCheck className="user-avatar__ready" />}
+export const UserAvatar = ({name, badgeText, id, ready, raisedHand, avatar, className, avatarClassName}: UserAvatarProps) => (
+  <div className={classNames("user-avatar", className, ready && "user-ready")} title={name}>
+    {ready && !raisedHand && <IconCheck className="user-avatar__ready" />}
+    {raisedHand && <RaisedHand className="user-avatar__raised-hand" />}
     {avatar ? <img src={avatar} className={avatarClassName} alt={name} /> : <Avatar seed={id} className={avatarClassName} />}
     {badgeText && <Badge text={badgeText} />}
   </div>
