880265e1af04cdffe1da5a3864287e5258c7b3b5
Christian Oehne <info@oehne.de>
2022-06-10 21:43:12 +0200

    remove useless string concatination

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -94,7 +94,7 @@ export function register(config?: Config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.log("This web app is being served cache-first by a service " + "worker. To learn more, visit https://cra.link/PWA (you only see this message on localhost");
+          console.log("This web app is being served cache-first by a service worker. To learn more, visit https://cra.link/PWA (you only see this message on localhost");
         });
       } else {
         // Is not localhost. Just register service worker
