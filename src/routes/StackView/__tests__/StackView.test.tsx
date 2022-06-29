b305cb4f0b51747db0849b1c718828cfa6211c04
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-06-29 19:34:07 +0200

    Add empty test cases for StackView

---
+++ src/routes/StackView/__tests__/StackView.test.tsx
@@ -0,0 +1,11 @@
+describe("StackView", () => {
+  describe("display", () => {
+    it("should display the column name", () => {});
+    it("should display the note itself", () => {});
+    it("should display the notes that are stacked", () => {});
+  });
+  describe("side effects", () => {
+    it("should navigate to board route on close", () => {});
+    it("should unshare note during active moderation on close", () => {});
+  });
+});
