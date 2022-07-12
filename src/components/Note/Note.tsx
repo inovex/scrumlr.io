84ec23eef6efe5adbd29ba1120ec30c0bb057b0c
Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>
2022-07-12 14:15:03 +0200

    Preparations for the new StackView (#1888)

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,96 +1,59 @@
-import "./Note.scss";
 import classNames from "classnames";
-import {Actions} from "store/action";
-import React, {useEffect, useRef} from "react";
-import {NoteDialog} from "components/NoteDialog";
-import {Votes} from "components/Votes";
+import React, {useRef, useEffect} from "react";
 import {useDrag, useDrop} from "react-dnd";
-import {UserAvatar} from "components/BoardUsers";
-import {TabIndex} from "constants/tabIndex";
 import {useDispatch} from "react-redux";
-import _ from "underscore";
-import {Participant} from "types/participant";
-import {useAppSelector} from "store";
+import {useNavigate} from "react-router";
 import {useTranslation} from "react-i18next";
+import _ from "underscore";
+import {TabIndex} from "constants/tabIndex";
+import {UserAvatar} from "components/BoardUsers";
+import {Votes} from "components/Votes";
+import {useAppSelector} from "store";
+import {Actions} from "store/action";
+import {Participant} from "types/participant";
+import "./Note.scss";
 
 interface NoteProps {
   noteId: string;
-  columnId: string;
-  columnName: string;
-  columnColor: string;
-  columnVisible: boolean;
-  showAuthors: boolean;
-  moderating: boolean;
   viewer: Participant;
   tabIndex?: number;
 }
 
 export const Note = (props: NoteProps) => {
   const {t} = useTranslation();
-  const noteRef = useRef<HTMLLIElement>(null);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const noteRef = useRef<HTMLLIElement>(null);
 
   const note = useAppSelector((state) => state.notes.find((n) => n.id === props.noteId), _.isEqual);
-  const noteIsShared = useAppSelector((state) => state.board.data?.sharedNote === props.noteId);
-  const childrenNotes = useAppSelector(
-    (state) =>
-      state.notes
-        .filter((n) => n.position.stack === note?.id)
-        .map((n) => ({
-          ...n,
-          authorName: state.participants?.others.find((p) => p.user.id === n.author)?.user.name ?? t("Note.me")!,
-          avatar: (state.participants?.others.find((p) => p.user.id === n.author) ?? state.participants?.self)!.user.avatar,
-        })),
-    _.isEqual
-  );
+  const isStack = useAppSelector((state) => state.notes.filter((n) => n.position.stack === props.noteId).length > 0);
+  const isShared = useAppSelector((state) => state.board.data?.sharedNote === props.noteId);
   const author = useAppSelector((state) => state.participants?.others.find((p) => p.user.id === note!.author) ?? state.participants?.self);
   const authorName = useAppSelector((state) => (author?.user.id === state.participants?.self.user.id ? t("Note.me") : author!.user.name));
+  const showAuthors = useAppSelector((state) => !!state.board.data?.showAuthors);
+  const moderating = useAppSelector((state) => state.view.moderating);
 
-  const [showDialog, setShowDialog] = React.useState(noteIsShared);
-
-  const handleShowDialog = () => {
-    if (props.moderating && props.noteId) {
-      if (noteIsShared) {
-        dispatch(Actions.stopSharing());
-      } else {
-        dispatch(Actions.shareNote(props.noteId));
-      }
-      setShowDialog(!noteIsShared);
-    } else {
-      if ((props.viewer.role === "OWNER" || props.viewer.role === "MODERATOR") && noteIsShared) {
-        dispatch(Actions.stopSharing());
-      }
-      setShowDialog(!showDialog);
+  /* eslint-disable */
+  useEffect(() => {
+    if (isShared && !document.location.pathname.endsWith(props.noteId)) {
+      navigate(`stack/${note!.id}`);
     }
-  };
+  }, []);
 
   useEffect(() => {
-    if (props.moderating) {
-      // Nothing to update
-      if (showDialog === noteIsShared) {
-        return;
+    if (isShared) {
+      if (!document.location.pathname.endsWith(props.noteId)) {
+        navigate(`stack/${note!.id}`);
       }
-
-      // Moderator has already one dialog open
-      if (showDialog && !noteIsShared && props.noteId) {
-        dispatch(Actions.shareNote(props.noteId));
-      } else {
-        setShowDialog(false);
-      }
+    } else if (document.location.pathname.endsWith(props.noteId)) {
+      navigate(`.`);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.moderating]);
-
-  useEffect(() => {
-    if (showDialog !== noteIsShared) {
-      setShowDialog(noteIsShared);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [noteIsShared]);
+  }, [isShared]);
+  /* eslint-enable */
 
   const [{isDragging}, drag] = useDrag({
-    type: childrenNotes.length > 0 ? "STACK" : "NOTE",
-    item: {id: props.noteId, columnId: props.columnId},
+    type: isStack ? "STACK" : "NOTE",
+    item: {id: props.noteId, columnId: note!.position.column},
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
@@ -100,16 +63,23 @@ export const Note = (props: NoteProps) => {
     accept: ["NOTE", "STACK"],
     drop: (item: {id: string}, monitor) => {
       if (!monitor.didDrop()) {
-        dispatch(Actions.editNote(item.id, {position: {stack: props.noteId!, column: props.columnId, rank: 0}}));
+        dispatch(Actions.editNote(item.id, {position: {stack: props.noteId!, column: note!.position.column, rank: 0}}));
       }
     },
     collect: (monitor) => ({isOver: monitor.isOver({shallow: true})}),
     canDrop: (item: {id: string}) => item.id !== props.noteId,
   }));
 
+  const handleClick = () => {
+    if (moderating && (props.viewer.role === "MODERATOR" || props.viewer.role === "OWNER")) {
+      dispatch(Actions.shareNote(props.noteId));
+    }
+    navigate(`stack/${props.noteId}`);
+  };
+
   const handleKeyPress = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter" && !showDialog) {
-      handleShowDialog();
+    if (e.key === "Enter") {
+      navigate(`stack/${props.noteId}`);
     }
   };
 
@@ -117,14 +87,14 @@ export const Note = (props: NoteProps) => {
   drop(noteRef);
 
   return (
-    <li className={classNames("note__root")} onClick={handleShowDialog} onKeyPress={handleKeyPress} ref={noteRef}>
+    <li className={classNames("note__root")} onClick={handleClick} onKeyPress={handleKeyPress} ref={noteRef}>
       <div
         className={classNames("note", {"note--own-card": props.viewer.user.id === note?.author}, {"note--isDragging": isDragging}, {"note--isOver": isOver})}
         tabIndex={props.tabIndex ?? TabIndex.default}
       >
         <p className="note__text">{note!.text}</p>
         <div className="note__footer">
-          {(props.showAuthors || props.viewer.user.id === author!.user.id) && (
+          {(showAuthors || props.viewer.user.id === author!.user.id) && (
             <figure className="note__author" aria-roledescription="author">
               <UserAvatar id={note!.author} avatar={author!.user.avatar} name={authorName} className="note__user-avatar" avatarClassName="note__user-avatar" />
               <figcaption className="note__author-name">{authorName}</figcaption>
@@ -132,22 +102,8 @@ export const Note = (props: NoteProps) => {
           )}
           <Votes tabIndex={props.tabIndex} noteId={props.noteId!} aggregateVotes />
         </div>
-        {showDialog && (
-          <NoteDialog
-            {...props}
-            text={note!.text}
-            authorId={note!.author}
-            authorName={authorName}
-            avatar={author!.user.avatar}
-            childrenNotes={childrenNotes}
-            onClose={handleShowDialog}
-            onDeleteOfParent={() => setShowDialog(false)}
-            moderating={props.moderating}
-            columnVisible={props.columnVisible}
-          />
-        )}
       </div>
-      {childrenNotes.length > 0 && <div className="note__in-stack" />}
+      {isStack && <div className="note__in-stack" />}
     </li>
   );
 };
