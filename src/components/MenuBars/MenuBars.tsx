cbe3d5486d9981ce425685bc232e217873e3b01c
Tim Engel <tim.engel.lp@gmail.com>
2022-07-13 16:45:40 +0200

    add hotkeys

--- src/components/MenuBars/MenuBars.tsx
+++ src/components/MenuBars/MenuBars.tsx
@@ -17,12 +17,28 @@ import {TabIndex} from "constants/tabIndex";
 import {useTranslation} from "react-i18next";
 import {useDispatch} from "react-redux";
 import "./MenuBars.scss";
+import {useHotkeys} from "react-hotkeys-hook";
+import {hotkeyMap} from "constants/hotkeys";
+import {Toast} from "../../utils/Toast";
 
 export const MenuBars = () => {
   const {t} = useTranslation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const {
+    TOGGLE_MODERATION,
+    TOGGLE_RAISED_HAND,
+    TOGGLE_READY_STATE,
+    SHOW_TIMER_MENU,
+    SHOW_VOTING_MENU,
+    SHOW_SETTINGS,
+    TOGGLE_HOTKEYS,
+    TOGGLE_SHOW_AUTHORS,
+    TOGGLE_SHOW_OTHER_USERS_NOTES,
+    TOGGLE_COLUMN_VISIBILITY,
+  } = hotkeyMap;
 
+  const [hotkeysEnabled, setHotkeysEnabled] = useState(true);
   const [showAdminMenu, toggleMenus] = useState(false);
   const [animate, setAnimate] = useState(false);
 
@@ -30,6 +46,9 @@ export const MenuBars = () => {
     (rootState) => ({
       currentUser: rootState.participants!.self,
       moderation: rootState.view.moderating,
+      showAuthors: rootState.board.data?.showAuthors,
+      showNotesOfOtherUsers: rootState.board.data?.showNotesOfOtherUsers,
+      showHiddenColumns: rootState.participants!.self.showHiddenColumns,
     }),
     _.isEqual
   );
@@ -56,6 +75,85 @@ export const MenuBars = () => {
     dispatch(Actions.setRaisedHand(state.currentUser.user.id, !raisedHand));
   };
 
+  const showTimerMenu = () => navigate("timer");
+  const showVotingMenu = () => navigate("voting");
+  const showSettings = () => navigate("settings");
+
+  const toggleAdminMenu = () => {
+    setAnimate(true);
+    toggleMenus((prevState) => !prevState);
+  };
+  const toggleHotkeys = () => {
+    if (hotkeysEnabled) {
+      Toast.info(t("Hotkeys.hotkeysDisabled"), 1500);
+    } else {
+      Toast.info(t("Hotkeys.hotkeysEnabled"), 1500);
+    }
+    setHotkeysEnabled(!hotkeysEnabled);
+  };
+
+  const hotkeyOptions = {
+    enabled: hotkeysEnabled,
+  };
+  const hotkeyOptionsAdmin = {
+    enabled: hotkeysEnabled && isAdmin,
+  };
+
+  useHotkeys(TOGGLE_HOTKEYS, toggleHotkeys, [hotkeysEnabled]);
+  useHotkeys(TOGGLE_MODERATION, toggleModeration, hotkeyOptions, [state.moderation]);
+  useHotkeys(TOGGLE_READY_STATE, toggleReadyState, hotkeyOptions, [isReady]);
+  useHotkeys(TOGGLE_RAISED_HAND, toggleRaiseHand, hotkeyOptions, [raisedHand]);
+  useHotkeys(
+    SHOW_TIMER_MENU,
+    () => {
+      if (!showAdminMenu) {
+        toggleAdminMenu();
+      }
+      showTimerMenu();
+    },
+    hotkeyOptionsAdmin,
+    [showAdminMenu]
+  );
+  useHotkeys(
+    SHOW_VOTING_MENU,
+    () => {
+      if (!showAdminMenu) {
+        toggleAdminMenu();
+      }
+      showVotingMenu();
+    },
+    hotkeyOptionsAdmin,
+    [showAdminMenu]
+  );
+  useHotkeys(SHOW_SETTINGS, showSettings, hotkeyOptions);
+  useHotkeys(
+    TOGGLE_SHOW_AUTHORS,
+    (e: KeyboardEvent) => {
+      e.preventDefault();
+      dispatch(Actions.editBoard({showAuthors: !state.showAuthors}));
+    },
+    hotkeyOptionsAdmin,
+    [state.showAuthors]
+  );
+  useHotkeys(
+    TOGGLE_SHOW_OTHER_USERS_NOTES,
+    (e: KeyboardEvent) => {
+      e.preventDefault();
+      dispatch(Actions.editBoard({showNotesOfOtherUsers: !state.showNotesOfOtherUsers}));
+    },
+    hotkeyOptionsAdmin,
+    [state.showNotesOfOtherUsers]
+  );
+  useHotkeys(
+    TOGGLE_COLUMN_VISIBILITY,
+    (e: KeyboardEvent) => {
+      e.preventDefault();
+      dispatch(Actions.setShowHiddenColumns(!state.showHiddenColumns));
+    },
+    hotkeyOptionsAdmin,
+    [state.showHiddenColumns]
+  );
+
   return (
     <aside id="menu-bars" className={classNames("menu-bars", {"menu-bars--admin": showAdminMenu, "menu-bars--user": !showAdminMenu}, {"menu-bars--isAdmin": isAdmin})}>
       <section className={classNames("menu", "user-menu", {"menu-animation": animate})} onTransitionEnd={(event) => handleAnimate(event)}>
@@ -78,14 +176,14 @@ export const MenuBars = () => {
             onToggle={toggleRaiseHand}
             value={raisedHand}
           />
-          <MenuButton tabIndex={TabIndex.UserMenu + 2} direction="right" label={t("MenuBars.settings")} onClick={() => navigate("settings")} icon={SettingsIcon} />
+          <MenuButton tabIndex={TabIndex.UserMenu + 2} direction="right" label={t("MenuBars.settings")} onClick={showSettings} icon={SettingsIcon} />
         </div>
       </section>
       {isAdmin && (
         <section className={classNames("menu", "admin-menu", {"menu-animation": animate})} onTransitionEnd={(event) => handleAnimate(event)}>
           <div className="menu__items">
-            <MenuButton tabIndex={TabIndex.AdminMenu} direction="left" label="Timer" onClick={() => navigate("timer")} icon={TimerIcon} />
-            <MenuButton tabIndex={TabIndex.AdminMenu + 1} direction="left" label="Voting" onClick={() => navigate("voting")} icon={VoteIcon} />
+            <MenuButton tabIndex={TabIndex.AdminMenu} direction="left" label="Timer" onClick={showTimerMenu} icon={TimerIcon} />
+            <MenuButton tabIndex={TabIndex.AdminMenu + 1} direction="left" label="Voting" onClick={showVotingMenu} icon={VoteIcon} />
             <MenuToggle
               value={state.moderation}
               direction="left"
@@ -100,14 +198,7 @@ export const MenuBars = () => {
         </section>
       )}
       {isAdmin && (
-        <button
-          className="menu-bars__switch"
-          onClick={() => {
-            setAnimate(true);
-            toggleMenus((prevState) => !prevState);
-          }}
-          aria-label={showAdminMenu ? t("MenuBars.switchToUserMenu") : t("MenuBars.switchToAdminMenu")}
-        >
+        <button className="menu-bars__switch" onClick={toggleAdminMenu} aria-label={showAdminMenu ? t("MenuBars.switchToUserMenu") : t("MenuBars.switchToAdminMenu")}>
           <ToggleAddMenuIcon className="switch__icon switch__icon--add" aria-hidden />
           <ToggleSettingsMenuIcon className="switch__icon switch__icon--settings" aria-hidden />
         </button>
---
+++ src/constants/hotkeys.ts
@@ -0,0 +1,12 @@
+export const hotkeyMap = {
+  TOGGLE_HOTKEYS: "shift+?, shift+ß",
+  TOGGLE_MODERATION: "f",
+  TOGGLE_READY_STATE: "d",
+  TOGGLE_RAISED_HAND: "h",
+  SHOW_TIMER_MENU: "t",
+  SHOW_VOTING_MENU: "v",
+  SHOW_SETTINGS: "s",
+  TOGGLE_SHOW_AUTHORS: "cmd+a, ctrl+a",
+  TOGGLE_SHOW_OTHER_USERS_NOTES: "cmd+u, ctrl+u",
+  TOGGLE_COLUMN_VISIBILITY: "cmd+j, ctrl+j",
+};
