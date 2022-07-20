61aef785efc349af465bd5bf34eb0b4b61510d4c
bitionaire <johann.boehler@scrumlr.io>
2022-07-20 19:37:59 +0200

    Fixed test

--- src/utils/test/export.test.ts
+++ src/utils/test/export.test.ts
@@ -5,25 +5,26 @@ import dummyBoardData from "./dummy-board-data.json";
 
 describe("the board export functions", () => {
   it("returns the board data in markdown format", async () => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const boardData: BoardDataType = dummyBoardData;
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    delete window.location;
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    window.location = new URL(`https://scrumlr.io/board/${boardData.board.id}/settings/export`);
+    import("i18nTest").then(async () => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const boardData: BoardDataType = dummyBoardData;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      delete window.location;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      window.location = new URL(`https://scrumlr.io/board/${boardData.board.id}/settings/export`);
 
-    API.exportBoard = jest.fn((): Promise<Response> => Promise.resolve(new Response(JSON.stringify(boardData))));
+      API.exportBoard = jest.fn((): Promise<Response> => Promise.resolve(new Response(JSON.stringify(boardData))));
 
-    const mdExport = await getMarkdownExport(boardData.board.id);
-    const toBeTranslated = "undefined";
+      const mdExport = await getMarkdownExport(boardData.board.id);
 
-    expect(API.exportBoard).toHaveBeenCalledTimes(1);
-    expect(mdExport).toBe(
-      // eslint-disable-next-line max-len
-      `# Test Board\n\n- ${toBeTranslated}: 1\n- [${toBeTranslated}](https://scrumlr.io/board/4af0854f-2cf1-4607-b1a6-6f96cd88a000)\n\n## Positive (2 ${toBeTranslated})\n\n- qwert _(Happy Hornet, 2 ${toBeTranslated})_\n    - asdf _(Happy Hornet, 1 ${toBeTranslated})_\n\n## Negative (2 ${toBeTranslated})\n\n- asdf _(Happy Hornet, 2 ${toBeTranslated})_\n- poiuz _(Happy Hornet)_\n\n> ${toBeTranslated} [scrumlr.io](https://scrumlr.io)  \n${toBeTranslated} [inovex](https://www.inovex.de)  \n\n![Scrumlr Logo](//scrumlr-logo-light.svg)`
-    );
+      expect(API.exportBoard).toHaveBeenCalledTimes(1);
+      expect(mdExport).toBe(
+        // eslint-disable-next-line max-len
+        `# Test Board\n\n- Participants: 1\n- [Link to board](https://scrumlr.io/board/4af0854f-2cf1-4607-b1a6-6f96cd88a000)\n\n## Positive (2 Notes)\n\n- qwert _(Happy Hornet, 2 Votes)_\n    - asdf _(Happy Hornet, 1 Vote)_\n\n## Negative (2 Notes)\n\n- asdf _(Happy Hornet, 2 Votes)_\n- poiuz _(Happy Hornet)_\n\n> Generated on [scrumlr.io](https://scrumlr.io)  \nProvided with ♥️ by [inovex](https://www.inovex.de)  \n\n![Scrumlr Logo](//scrumlr-logo-light.svg)`
+      );
+    });
   });
 });
