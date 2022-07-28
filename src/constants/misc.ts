a4d52e19801fde4193994575993e1c14a0ccdbcc
Tim Engel <tim.engel.lp@gmail.com>
2022-07-28 18:27:28 +0900

    Add export option copy to clipboard in Markdown format (#1983)
    * add export option: markdown to clipboard
    
    * refactor translation
    
    * add default url
    
    * refactor, use default board name constant
    
    * Apply suggestions from code review
    
    Co-authored-by: Johann Böhler <bitionaire@users.noreply.github.com>
    
    * rename mdBrackets
    
    * add markdown export test
    
    * fix eslint error
    
    Co-authored-by: Johann Böhler <bitionaire@users.noreply.github.com>
    Co-authored-by: bitionaire <johann.boehler@scrumlr.io>

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -2,3 +2,7 @@ export const DEFAULT_BOARD_NAME = "scrumlr.io";
 export const MIN_PASSWORD_LENGTH = 1;
 export const PLACEHOLDER_PASSWORD = "••••••••••";
 export const TEMPORARY_COLUMN_ID = "TEMP_ID";
+export const DEFAULT_URL = "https://scrumlr.io";
+
+export const TOAST_TIMER_DEFAULT = 3000;
+export const TOAST_TIMER_SHORT = 1500;
