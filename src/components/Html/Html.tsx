6cc20242c731dd02b50cc114a939eab23b19a7e2
Resaki1 <christian.reski@t-online.de>
2022-08-16 19:30:25 +0200

    automatically sync theme with system settings if theme isn't set in application

--- src/components/Html/Html.tsx
+++ src/components/Html/Html.tsx
@@ -1,10 +1,18 @@
-import {FC} from "react";
+import {FC, useState} from "react";
 import {Helmet} from "react-helmet";
 import {useAppSelector} from "store";
 
 export const Html: FC = () => {
   const lang = useAppSelector((state) => state.view.language);
-  const theme = localStorage.getItem("theme") ?? (!window.matchMedia || window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? (!window.matchMedia || window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"));
+
+  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+    const colorScheme = e.matches ? "dark" : "light";
+
+    if (!localStorage.getItem("theme")) {
+      setTheme(colorScheme);
+    }
+  });
 
   return <Helmet htmlAttributes={{lang, theme}} />;
 };
