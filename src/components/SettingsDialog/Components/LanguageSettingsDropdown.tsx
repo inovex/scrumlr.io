36828f6afe177e614e46c816ea6a319604a6bc66
Resaki1 <christian.reski@t-online.de>
2022-08-18 10:51:07 +0200

    removed unnecessary page reload after setting the language

--- src/components/SettingsDialog/Components/LanguageSettingsDropdown.tsx
+++ src/components/SettingsDialog/Components/LanguageSettingsDropdown.tsx
@@ -18,8 +18,6 @@ export const LanguageSettingsDropdown: VFC<LanguageSettingsDropdownProps> = ({sh
   const changeLanguage = (language: string) => {
     i18n.changeLanguage(language).then(() => {
       document.documentElement.lang = i18n.language;
-      // eslint-disable-next-line no-restricted-globals
-      location.reload();
     });
   };
 
