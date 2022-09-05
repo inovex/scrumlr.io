34354029df556fc4ee74e007e48b8067f9726942
Sebastian Schwarzer <sebastian.schwarzer@inovex.de>
2022-09-05 16:32:24 +0200

    Use settingsDropdown in languageSettingsDropdown
    Replace the static two language selection with a dynamic approach for
    multiple languages.
    Move the required css to the settingsDropdown component.
    If languageSettingsDropdown specific css is required, the respective css
    file has to be created again.
    The dropdown uses buttons, therefore no keyboard handler is needed.
    
    Use i18n hook in Appearance settings to update the language.

--- src/components/SettingsDialog/Appearance/Appearance.tsx
+++ src/components/SettingsDialog/Appearance/Appearance.tsx
@@ -1,18 +1,12 @@
 import classNames from "classnames";
-import {t} from "i18next";
-import {useState, FocusEvent} from "react";
-import {SettingsButton} from "../Components/SettingsButton";
+import {useTranslation} from "react-i18next";
 import {LanguageSettingsDropdown} from "../Components/LanguageSettingsDropdown";
 import "../SettingsDialog.scss";
 import "./Appearance.scss";
 import {ThemeSettings} from "../Components/ThemeSettings";
 
 export const Appearance = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const handleBlur = (e: FocusEvent<HTMLButtonElement>, callback: () => void) => {
-    if (!e.currentTarget.contains(e.relatedTarget)) callback();
-  };
+  const {t} = useTranslation();
 
   return (
     <div className={classNames("settings-dialog__container", "accent-color__lean-lilac")}>
@@ -21,16 +15,7 @@ export const Appearance = () => {
       </header>
       <div className="appearance-container">
         <ThemeSettings />
-        <SettingsButton
-          className="appearance-settings_language-dropdown"
-          label={t("Appearance.Language")}
-          aria-haspopup="listbox"
-          aria-expanded={showDropdown}
-          onClick={() => setShowDropdown((prevVal) => !prevVal)}
-          onBlur={(e) => handleBlur(e, () => setShowDropdown(false))}
-        >
-          <LanguageSettingsDropdown showDropdown={showDropdown} setShowDropdown={setShowDropdown} />
-        </SettingsButton>
+        <LanguageSettingsDropdown />
       </div>
     </div>
   );
