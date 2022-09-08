ca1be538fda419cf954b863828de3be26ef9f598
schwehn42 <schwehn42@gmail.com>
2022-09-08 14:37:56 +0200

    adjust style, add warning icon

--- src/components/SettingsDialog/ExportBoard/ExportHintHiddenColumns/ExportHintHiddenColumns.tsx
+++ src/components/SettingsDialog/ExportBoard/ExportHintHiddenColumns/ExportHintHiddenColumns.tsx
@@ -1,5 +1,6 @@
 import {FC} from "react";
 import {useTranslation} from "react-i18next";
+import {ReactComponent as InfoIcon} from "assets/icon-warning.svg";
 import {Column} from "../../../../types/column";
 import "./ExportHintHiddenColumns.scss";
 
@@ -15,12 +16,17 @@ export const ExportHintHiddenColumns: FC<ExportHintHiddenColumnsProps> = ({colum
   if (hiddenColumns.length > 0) {
     return (
       <div className={className}>
-        {t("ExportBoardOption.hintHiddenColumns")}
-        <ul>
-          {hiddenColumns.map((hiddenCol) => (
-            <li>{hiddenCol.name}</li>
-          ))}
-        </ul>
+        <div className="hint-hidden-columns__info-container">
+          <InfoIcon className="hint-hidden-columns__info-icon" />
+          <span className="hint-hidden-columns__info-text">{t("ExportBoardOption.hintHiddenColumns")}</span>
+        </div>
+        <div>
+          <ul className="hint-hidden-columns__columns-list">
+            {hiddenColumns.map((hiddenCol) => (
+              <li>{hiddenCol.name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
