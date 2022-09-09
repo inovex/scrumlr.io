0c116c3a664e3ceea1a4929115c385f337ef380f
Sebastian Schwarzer <sebastian.schwarzer@inovex.de>
2022-09-09 14:36:44 +0200

    Rollback due to tests keep failing

--- src/components/ConfirmationDialog/ConfirmationDialog.tsx
+++ src/components/ConfirmationDialog/ConfirmationDialog.tsx
@@ -1,6 +1,5 @@
 import classNames from "classnames";
 import {Button} from "components/Button";
-import {Portal} from "components/Portal";
 import {ScrumlrLogo} from "components/ScrumlrLogo";
 import "./ConfirmationDialog.scss";
 
@@ -14,20 +13,18 @@ export interface ConfirmationDialogProps {
 }
 
 export const ConfirmationDialog = ({headline, acceptMessage, onAccept, declineMessage, onDecline, className}: ConfirmationDialogProps) => (
-  <Portal>
-    <div className={classNames("confirmation-dialog__wrapper", className)}>
-      <div className="confirmation-dialog">
-        <ScrumlrLogo accentColorClassNames={["accent-color--blue", "accent-color--purple", "accent-color--lilac", "accent-color--pink"]} />
-        <h2>{headline}</h2>
-        <Button type="submit" color="primary" className="confirmation-dialog__button" onClick={() => onAccept()}>
-          {acceptMessage}
-        </Button>
-        <Button type="submit" color="primary" className="confirmation-dialog__button" variant="outlined" onClick={() => onDecline()} autoFocus>
-          {declineMessage}
-        </Button>
-      </div>
+  <div className={classNames("confirmation-dialog__wrapper", className)}>
+    <div className="confirmation-dialog">
+      <ScrumlrLogo accentColorClassNames={["accent-color--blue", "accent-color--purple", "accent-color--lilac", "accent-color--pink"]} />
+      <h2>{headline}</h2>
+      <Button type="submit" color="primary" className="confirmation-dialog__button" onClick={() => onAccept()}>
+        {acceptMessage}
+      </Button>
+      <Button type="submit" color="primary" className="confirmation-dialog__button" variant="outlined" onClick={() => onDecline()} autoFocus>
+        {declineMessage}
+      </Button>
     </div>
-  </Portal>
+  </div>
 );
 
 export default ConfirmationDialog;
