e54587a81f1664d6b2f42eaaa4ee2849cc46384d
Sebastian Schwarzer <sebastian.schwarzer@inovex.de>
2022-09-19 14:59:09 +0200

    Add some spacing

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -16,6 +16,7 @@ export type BoardState = {
   showNextButton: boolean;
   showPreviousButton: boolean;
 };
+
 type ColumnState = {
   firstVisibleColumnIndex: number;
   lastVisibleColumnIndex: number;
