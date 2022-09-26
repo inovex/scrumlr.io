855591482bb372423428eb2b1fca32349a228c34
Jakob Schwehn <schwehn42@gmail.com>
2022-09-26 16:34:44 +0200

    put content and style

--- src/components/Requests/Request/Request.tsx
+++ src/components/Requests/Request/Request.tsx
@@ -1,14 +1,25 @@
 import "./Request.scss";
+import {ReactComponent as HandIcon} from "assets/icon-hand.svg";
 import {Participant} from "../../../types/participant";
+import {UserAvatar} from "../../BoardUsers";
 
 export type RequestProps = {
   participant: Participant;
 };
 
 export const Request = ({participant}: RequestProps) => (
-    <div className="request__container">
-      <div>(Avatar)</div>
-      <div>({participant.user.name})</div>
-      <div>(Click)</div>
+  <div className="request__container">
+    <figure className="request__request-figure">
+      <UserAvatar id={participant.user.id} avatar={participant.user.avatar} name={participant.user.name} />
+    </figure>
+
+    <div className="request__info-container">
+      <span className="request__participant-name">{participant.user.name}</span>
+      <span className="request__info-text">Hat sich gemeldet</span>
     </div>
-  );
+
+    <div className="request__icon-container">
+      <HandIcon className="request-icon" />
+    </div>
+  </div>
+);
