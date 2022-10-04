8ad2d2489f5ff68b9e90e7947910d2f7f5823840
Jakob Schwehn <schwehn42@gmail.com>
2022-10-04 14:25:20 +0200

    position button on top of info container

--- src/components/Requests/Request/Request.tsx
+++ src/components/Requests/Request/Request.tsx
@@ -13,9 +13,13 @@ export const Request = ({participant}: RequestProps) => (
       <UserAvatar id={participant.user.id} avatar={participant.user.avatar} name={participant.user.name} />
     </figure>
 
-    <div className="request__info-container">
-      <span className="request__participant-name">{participant.user.name}</span>
-      <span className="request__info-text">Hat sich gemeldet</span>
+    {/* either `request__text-container` or `request__button` is displayed */}
+    <div className="request__middle-wrapper">
+      <div className="request__info-container">
+        <span className="request__participant-name">{participant.user.name}</span>
+        <span className="request__info-text">Hat sich gemeldet</span>
+      </div>
+      <button className="request__button">Hand senken</button>
     </div>
 
     <div className="request__icon-container">
