6d3ea98577f9ebcc62f6523340800f14580b07f8
Resaki1 <christian.reski@t-online.de>
2022-10-08 16:39:34 +0200

    fixed position indicator

--- src/components/StackNavigation/Dots/StackNavigationDots.tsx
+++ src/components/StackNavigation/Dots/StackNavigationDots.tsx
@@ -8,6 +8,16 @@ type StackNavigationDotsProps = {
   currentIndex: number;
 };
 
+const getCenterDot = (length: number, currentIndex: number) => {
+  if (currentIndex < 3) {
+    return 3;
+  }
+  if (length - currentIndex < 4) {
+    return length - 4;
+  }
+  return currentIndex;
+};
+
 export const StackNavigationDots = ({stacks, currentIndex}: StackNavigationDotsProps) => {
   const navigate = useNavigate();
 
@@ -15,7 +25,7 @@ export const StackNavigationDots = ({stacks, currentIndex}: StackNavigationDotsP
     navigate(`../note/${stacks[index].id}/stack`);
   };
 
-  const centerDot = currentIndex < 3 ? 3 : stacks.length - currentIndex < 3 ? stacks.length - 3 : currentIndex;
+  const centerDot = getCenterDot(stacks.length, currentIndex);
 
   return (
     <div className="stack-view__navigation-dots">
@@ -28,7 +38,7 @@ export const StackNavigationDots = ({stacks, currentIndex}: StackNavigationDotsP
             className={classNames(
               "stack-view__navigation-dot",
               index === currentIndex && "stack-view__navigation-dot--active",
-              distance === 3 && index !== 0 && "stack-view__navigation-dot--small",
+              distance === 3 && index !== 0 && index !== stacks.length - 1 && "stack-view__navigation-dot--small",
               distance > 3 && "stack-view__navigation-dot--hidden"
             )}
             aria-label={`Go to note #${index + 1}`}
