d22db7a68310f1921043eedefd79a01f670aea60
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-10-25 11:36:56 +0200

    Implement new design of vote counter

--- src/components/Votes/VoteDisplay/VoteDisplay.tsx
+++ src/components/Votes/VoteDisplay/VoteDisplay.tsx
@@ -1,17 +1,19 @@
 import "./VoteDisplay.scss";
 import {useTranslation} from "react-i18next";
-import {FC} from "react";
+import {ReactComponent as VoteIcon} from "assets/icon-vote.svg";
 
 type VoteDisplayProps = {
   usedVotes: number;
   possibleVotes: number;
 };
 
-export const VoteDisplay: FC<VoteDisplayProps> = ({usedVotes, possibleVotes}) => {
+export const VoteDisplay = ({usedVotes, possibleVotes}: VoteDisplayProps) => {
   const {t} = useTranslation();
 
   return (
     <div className="vote-display">
+      {usedVotes > 0 && <div className="vote-display__progress-bar" style={{right: `calc(72px - (${usedVotes} / ${possibleVotes}) * 72px)`}} />}
+      <VoteIcon />
       <span title={t("VoteDisplay.tooltip", {remaining: possibleVotes - usedVotes, total: possibleVotes})}>
         {usedVotes} / {possibleVotes}
       </span>
