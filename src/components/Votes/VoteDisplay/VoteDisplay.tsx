f6e484dd2480fc33b8f0698d2fc76f15cc99d32a
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-10-26 09:23:22 +0200

    Store temporary flag & cancel icons in svg files

--- src/components/Votes/VoteDisplay/VoteDisplay.tsx
+++ src/components/Votes/VoteDisplay/VoteDisplay.tsx
@@ -1,6 +1,8 @@
 import "./VoteDisplay.scss";
 import {useTranslation} from "react-i18next";
 import {ReactComponent as VoteIcon} from "assets/icon-vote.svg";
+import {ReactComponent as CancelIcon} from "assets/icon-cancel.svg";
+import {ReactComponent as FlagIcon} from "assets/icon-flag.svg";
 import store, {useAppSelector} from "store";
 import {Actions} from "store/action";
 
@@ -24,17 +26,13 @@ export const VoteDisplay = ({usedVotes, possibleVotes}: VoteDisplayProps) => {
         <div className="vote-display__short-actions">
           <div className="short-actions__button-wrapper">
             <button onClick={() => store.dispatch(Actions.closeVoting(voting!))}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M3.5 2.75a.75.75 0 00-1.5 0v14.5a.75.75 0 001.5 0v-4.392l1.657-.348a6.449 6.449 0 014.271.572 7.948 7.948 0 005.965.524l2.078-.64A.75.75 0 0018 12.25v-8.5a.75.75 0 00-.904-.734l-2.38.501a7.25 7.25 0 01-4.186-.363l-.502-.2a8.75 8.75 0 00-5.053-.439l-1.475.31V2.75z" />
-              </svg>
+              <FlagIcon />
             </button>
             <span>Finish</span>
           </div>
           <div className="short-actions__button-wrapper">
             <button onClick={() => store.dispatch(Actions.abortVoting(voting!))}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-              </svg>
+              <CancelIcon />
             </button>
             <span>Abort</span>
           </div>
