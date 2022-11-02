2503b88e907e685f91f6f1c69c925a3381aecc72
NNKKOO <58441634+NNKKOO@users.noreply.github.com>
2022-11-02 13:28:21 +0100

    Update Hotkeys
    replaced shift with alt/option

--- src/constants/hotkeys.ts
+++ src/constants/hotkeys.ts
@@ -6,9 +6,9 @@ export const hotkeyMap = {
   SHOW_TIMER_MENU: "t",
   SHOW_VOTING_MENU: "v",
   SHOW_SETTINGS: "s",
-  TOGGLE_SHOW_AUTHORS: "shift+a",
+  TOGGLE_SHOW_AUTHORS: "option+a, alt+a",
   TOGGLE_SHOW_OTHER_USERS_NOTES: "cmd+u, ctrl+u",
   TOGGLE_COLUMN_VISIBILITY: "cmd+j, ctrl+j",
-  SELECT_NOTE_INPUT_FIRST_KEY: ["shift"],
+  SELECT_NOTE_INPUT_FIRST_KEY: ["option", "alt"],
   SET_TIMER_FIRST_KEY: ["ctrl+t"],
 };
