8d461b24f6643de5c68d44247a86edb23e7b3c5b
NNKKOO <58441634+NNKKOO@users.noreply.github.com>
2022-11-10 14:34:06 +0100

    Add a bump effect to the vote-counter

--- src/components/Votes/VoteButtons/RemoveVoteButton.tsx
+++ src/components/Votes/VoteButtons/RemoveVoteButton.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
 import {useDispatch} from "react-redux";
 import {Actions} from "store/action";
 import {DotButton} from "components/DotButton";
@@ -16,8 +16,21 @@ export const RemoveVoteButton: FC<RemoveVoteProps> = ({noteId, disabled, childre
     dispatch(Actions.deleteVote(noteId));
   };
 
+  const [doBump, setDoBump] = useState(false);
+
+  useEffect(() => {
+    setDoBump(true);
+  }, [children]);
+
   return (
-    <DotButton className="vote-button-remove" disabled={disabled} onClick={deleteVote}>
+    <DotButton
+      className={doBump ? "vote-button-remove bump" : "vote-button-remove"}
+      disabled={disabled}
+      onClick={deleteVote}
+      onAnimationEnd={() => {
+        setDoBump(false);
+      }}
+    >
       <span className="vote-button-remove__folded-corner" />
       <span>{children}</span>
     </DotButton>
