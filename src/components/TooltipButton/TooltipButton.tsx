61dcf71245fe8ea9c68a5d3ce03e72253660ef6a
Resaki1 <christian.reski@t-online.de>
2022-11-11 10:20:13 +0100

    fixed tooltip button showing undefined if no hotkey is passed as prop

--- src/components/TooltipButton/TooltipButton.tsx
+++ src/components/TooltipButton/TooltipButton.tsx
@@ -26,7 +26,7 @@ export const TooltipButton = (props: TooltipButtonProps) => {
       <div className="tooltip-button__tooltip" aria-hidden>
         <span className="tooltip-button__tooltip-text">
           {props.label}
-          <span className="tooltip-button__hotkey">{` [${props.hotkeyKey}]`}</span>
+          {props.hotkeyKey && <span className="tooltip-button__hotkey">{` [${props.hotkeyKey}]`}</span>}
         </span>
       </div>
       <Icon className="tooltip-button__icon" aria-hidden />
