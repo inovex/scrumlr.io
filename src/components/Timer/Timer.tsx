6cd4c48688f99fdd8db39802e120143e91b9a414
Sebastian Schwarzer <44020029+CronJorian@users.noreply.github.com>
2022-12-08 18:47:53 +0100

    Check if any user is done (#2479)

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -41,6 +41,7 @@ export const Timer = (props: TimerProps) => {
       state.participants!.others.filter((p) => p.connected && p.role === "PARTICIPANT").length &&
       state.participants!.others.filter((p) => p.connected && p.role === "PARTICIPANT").every((participant) => participant.ready)
   );
+  const anyReady = useAppSelector((state) => state.participants!.others.filter((p) => p.connected).some((participant) => participant.ready));
   const isModerator = useAppSelector((state) => state.participants?.self.role === "OWNER" || state.participants?.self.role === "MODERATOR");
 
   const boardId = useAppSelector((state) => state.board.data!.id);
@@ -62,7 +63,7 @@ export const Timer = (props: TimerProps) => {
     if (!previousPlayTimesUpState && playTimesUp) {
       timesUpSoundObject.on("end", () => setPlayTimesUp(false));
       playTimesUpSound();
-      if (isModerator) {
+      if (isModerator && anyReady) {
         Toast.info(
           <div>
             {t("Toast.moderatorResetReadyStates")}
