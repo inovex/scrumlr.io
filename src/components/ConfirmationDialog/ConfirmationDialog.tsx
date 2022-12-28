6f5454e641b9916883763351e5c258facd73724c
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-12-28 16:48:30 +0100

    Add animations, own styled buttons

--- src/components/ConfirmationDialog/ConfirmationDialog.tsx
+++ src/components/ConfirmationDialog/ConfirmationDialog.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {Button} from "components/Button";
+import {ReactComponent as CloseIcon} from "assets/icon-close.svg";
 import {Portal} from "components/Portal";
+import {animated, Transition} from "react-spring";
 import "./ConfirmationDialog.scss";
 
 type ConfirmationDialogProps = {
@@ -10,23 +11,36 @@ type ConfirmationDialogProps = {
   onDecline: () => void;
 };
 
-export const ConfirmationDialog: React.FC<ConfirmationDialogProps> = (props) => (
+export const ConfirmationDialog: React.FC<ConfirmationDialogProps> = (props) => {
+  const transitionConfig = {
+    from: {opacity: 0, transform: "translateY(-20px)"},
+    enter: {opacity: 1, transform: "translateY(0px)"},
+    items: true,
+  };
+
+  return (
     <Portal onClose={props.onDecline}>
       <div className="confirmation-dialog__background" />
       <div className="confirmation-dialog__wrapper">
-        <aside className="confirmation-dialog">
-          <h2 className="confirmation-dialog__title">{props.title}</h2>
-          <div className="confirmation-dialog__buttons">
-            <Button type="submit" color="secondary" className="confirmation-dialog__button" onClick={() => props.onAccept()}>
-              Yes
-            </Button>
-            <Button type="submit" color="secondary" className="confirmation-dialog__button" variant="outlined" onClick={() => props.onDecline()} autoFocus>
-              No
-            </Button>
-          </div>
-        </aside>
+        <Transition {...transitionConfig}>
+          {(styles) => (
+            <animated.aside style={styles} className="confirmation-dialog">
+              <button onClick={() => props.onDecline()} className="confirmation-dialog__close-button">
+                <CloseIcon className="dialog__close-icon" />
+              </button>
+              <h2 className="confirmation-dialog__title">{props.title}</h2>
+              <div className="confirmation-dialog__buttons">
+                <button className="confirmation-dialog__button confirmation-dialog__button--accept" onClick={() => props.onAccept()}>
+                  Yes
+                </button>
+                <button className="confirmation-dialog__button confirmation-dialog__button--decline" onClick={() => props.onDecline()}>
+                  No
+                </button>
+              </div>
+            </animated.aside>
+          )}
+        </Transition>
       </div>
     </Portal>
   );
-
-export default ConfirmationDialog;
+};
