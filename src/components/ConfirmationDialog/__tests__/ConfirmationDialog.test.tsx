0a5a50bb797bb95bdcf5566f06271227c7a09143
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2022-12-28 23:28:43 +0100

    Update tests and snapshots

--- src/components/ConfirmationDialog/__tests__/ConfirmationDialog.test.tsx
+++ src/components/ConfirmationDialog/__tests__/ConfirmationDialog.test.tsx
@@ -2,8 +2,16 @@ import {render} from "testUtils";
 import {ConfirmationDialog} from "../ConfirmationDialog";
 
 describe("<ConfirmationDialog />", () => {
+  beforeEach(() => {
+    const portal = global.document.createElement("div");
+    portal.setAttribute("id", "portal");
+    global.document.querySelector("body")!.appendChild(portal);
+  });
+
   test("snapshot test", () => {
-    const {container} = render(<ConfirmationDialog headline="headline" acceptMessage="accept" declineMessage="decline" onAccept={jest.fn()} onDecline={jest.fn()} />);
+    const {container} = render(<ConfirmationDialog title="headline" onAcceptLabel="accept" onDeclineLabel="decline" onAccept={jest.fn()} onDecline={jest.fn()} />, {
+      container: global.document.querySelector("#portal")!,
+    });
     expect(container.firstChild).toMatchSnapshot();
   });
 });
