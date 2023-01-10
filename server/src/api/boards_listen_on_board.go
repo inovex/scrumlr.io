d4fb45447c6a75420ec9aaa26338c7d7baae4c56
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-01-10 13:31:15 +0100

    Retrieve assignments with initial websocket messag

--- server/src/api/boards_listen_on_board.go
+++ server/src/api/boards_listen_on_board.go
@@ -28,7 +28,7 @@ func (s *Server) openBoardSocket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	board, requests, sessions, columns, notes, votings, votes, err := s.boards.FullBoard(r.Context(), id)
+	board, requests, sessions, columns, notes, votings, votes, assignments, err := s.boards.FullBoard(r.Context(), id)
 	if err != nil {
 		logger.Get().Errorw("failed to prepare init message", "board", id, "user", userID, "err", err)
 		s.closeBoardSocket(id, userID, conn)
@@ -48,6 +48,7 @@ func (s *Server) openBoardSocket(w http.ResponseWriter, r *http.Request) {
 			Votes      []*dto2.Vote                `json:"votes"`
 			Sessions   []*dto2.BoardSession        `json:"participants"`
 			Requests   []*dto2.BoardSessionRequest `json:"requests"`
+      Assignments []*dto2.Assignment          `json:"assignments"`
 		}{
 			Board:    board,
 			Columns:  columns,
@@ -56,6 +57,7 @@ func (s *Server) openBoardSocket(w http.ResponseWriter, r *http.Request) {
 			Votes:    votes,
 			Sessions: sessions,
 			Requests: requests,
+      Assignments: assignments,
 		},
 	})
 	if err != nil {
--- server/src/services/services.go
+++ server/src/services/services.go
@@ -38,7 +38,7 @@ type Boards interface {
 	GetColumn(ctx context.Context, boardID, columnID uuid.UUID) (*dto.Column, error)
 	ListColumns(ctx context.Context, boardID uuid.UUID) ([]*dto.Column, error)
 
-	FullBoard(ctx context.Context, boardID uuid.UUID) (*dto.Board, []*dto.BoardSessionRequest, []*dto.BoardSession, []*dto.Column, []*dto.Note, []*dto.Voting, []*dto.Vote, error)
+	FullBoard(ctx context.Context, boardID uuid.UUID) (*dto.Board, []*dto.BoardSessionRequest, []*dto.BoardSession, []*dto.Column, []*dto.Note, []*dto.Voting, []*dto.Vote, []*dto.Assignment, error)
 }
 
 type BoardSessions interface {
