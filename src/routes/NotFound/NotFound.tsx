7c2d3fcd7eb15e7f713f102d1417220cf421cc98
Jakob Schwehn <schwehn42@gmail.com>
2023-03-25 11:02:46 +0100

    first layout

--- src/routes/NotFound/NotFound.tsx
+++ src/routes/NotFound/NotFound.tsx
@@ -1,10 +1,22 @@
-import {VFC} from "react";
-import {ScrumlrLogo} from "components/ScrumlrLogo";
 import "./NotFound.scss";
+import {ReactComponent as StanToilet} from "assets/stan/stan-toilet.svg";
+import {ReactComponent as Logo404} from "assets/pages/error/404.svg";
 
-export const NotFound: VFC = () => (
-  <div className="not-found">
-    <ScrumlrLogo className="not-found__logo" accentColorClassNames={["accent-color--blue", "accent-color--purple", "accent-color--lilac", "accent-color--pink"]} />
-    <p className="not-found__text">404 | Page not found</p>
+export const NotFound = () => (
+  <div className="not-found__root">
+    <header className="not-found__header">
+      <div className="scrumlr-logo">logo</div>
+    </header>
+    <main className="not-found__main">
+      <div className="not-found__content">
+        <div className="not-found__title">Oh no! Error</div>
+        <div className="not-found__description">Maybe Stan has broken this page.</div>
+        <button className="not-found__return-button">Back to Homepage</button>
+      </div>
+      <div className="not-found__background">
+        <Logo404 className="not-found__logo404" />
+        <StanToilet className="not-found__logo-stan" />
+      </div>
+    </main>
   </div>
 );
