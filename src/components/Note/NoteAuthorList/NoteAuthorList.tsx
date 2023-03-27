1ad0a2c5b906ffcca68805cb4847432701110d37
Jakob Schwehn <schwehn42@gmail.com>
2023-03-27 16:18:43 +0200

    css changes

--- src/components/Note/NoteAuthorList/NoteAuthorList.tsx
+++ src/components/Note/NoteAuthorList/NoteAuthorList.tsx
@@ -30,14 +30,8 @@ export const NoteAuthorList = (props: Props) => {
         (
           a // iterate over authors, order is changed in CSS
         ) => (
-          <figure
-            className={classNames("note__author", {"note__author--self": stackAuthor.isSelf /* , "note__author--with-rest": restUsersExist */})}
-            aria-roledescription="author"
-          >
+          <figure className={classNames("note__author", {"note__author--self": stackAuthor.isSelf, "note__author--with-rest": restUsersExist})} aria-roledescription="author">
             <UserAvatar id={a.user!.id} avatar={a.user!.avatar} title={a.displayName} className="note__user-avatar" avatarClassName="note__user-avatar" />
-            {/* index === self.length - 1 && !restUsersExist ? ( // only the stack authors name is displayed after the last figure
-              <figcaption className="note__author-name">{stackAuthor.displayName}</figcaption>
-            ) : null */}
           </figure>
         )
       )}
