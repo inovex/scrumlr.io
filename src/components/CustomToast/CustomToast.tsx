20513d63d32ca387b920a0ddc1938fc9dc0b44bf
BenJosh95 <Joshua.Fuetsch@web.de>
2023-04-03 20:06:25 +0200

    progress

--- src/components/CustomToast/CustomToast.tsx
+++ src/components/CustomToast/CustomToast.tsx
@@ -1,6 +1,8 @@
 import "./CustomToast.scss";
 import {ReactElement} from "react";
 import classNames from "classnames";
+import {ReactComponent as IconDelete} from "assets/icon-delete.svg";
+import {ReactComponent as IconHand} from "assets/icon-hand.svg";
 
 export interface CustomToastProps {
   icon?: ReactElement;
@@ -15,23 +17,29 @@ export interface CustomToastProps {
 const buttons = ["yes", "no", "cancel"];
 
 export const CustomToast = () => (
-    <div className={classNames("toast", {"toast__single-button-container": buttons.length === 1})}>
-      <div className="content">
-        <div className="icon">IC</div>
-        <div className="info">
-          <div className="info-text">Karte wurde gelöscht bre was loooosddddd</div>
-          <div className="info-hint">
-            <div className="info-hint-text">Don't show this anymore</div>
-            <button className="info-hint-button" />
-          </div>
+  <div className={classNames("toast", {"toast__single-button-container": buttons.length === 1})}>
+    <div className="content">
+      <div className="icon">
+        <IconDelete />
+      </div>
+      <div className="info">
+        <div className="info-text">Du hast den Präsentationsmodus gestartet. Ausgewählte Karten werden mit allen Teilnehmern</div>
+        <div className="info-hint">
+          <div className="info-hint-text">Don't show this anymore</div>
+          <button className="info-hint-button" />
         </div>
       </div>
-      {buttons.length === 1 && <button className="actions-single-button">X</button>}
-      {buttons.length > 1 && (
-        <div className="actions">
-          <button className="actions-button">YES</button>
-          <button className="actions-button">CANCEL</button>
-        </div>
-      )}
     </div>
-  );
+    {buttons.length === 1 && (
+      <button className="actions-single-button">
+        <IconHand />
+      </button>
+    )}
+    {buttons.length > 1 && (
+      <div className="actions">
+        <button className="actions-button">YES</button>
+        <button className="actions-button">CANCEL</button>
+      </div>
+    )}
+  </div>
+);
