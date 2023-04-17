19ee952b44f12a7e771ce82275febe894442233f
Jakob Schwehn <schwehn42@gmail.com>
2023-04-17 11:57:42 +0200

    stan mobile light

--- src/routes/NotFound/NotFound.tsx
+++ src/routes/NotFound/NotFound.tsx
@@ -1,5 +1,6 @@
 import "./NotFound.scss";
 import {ReactComponent as StanWebLight} from "assets/stan/Stan_404_Web_Light.svg";
+import {ReactComponent as StanMobileLight} from "assets/stan/Stan_404_Mobile_Light.svg";
 import {ReactComponent as ScrumlrLogoLight} from "assets/scrumlr-logo-light.svg";
 
 export const NotFound = () => (
@@ -20,6 +21,7 @@ export const NotFound = () => (
       </div>
       <div className="not-found__image-wrapper">
         <StanWebLight className="not-found__logo-stan--web-light" />
+        <StanMobileLight className="not-found__logo-stan--mobile-light" />
       </div>
     </main>
   </div>
