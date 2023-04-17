3e589b761d73ef5a0b4cc00b8be241500662b492
Jakob Schwehn <schwehn42@gmail.com>
2023-04-17 15:42:15 +0200

    dark mode v1

--- src/routes/NotFound/NotFound.tsx
+++ src/routes/NotFound/NotFound.tsx
@@ -1,17 +1,26 @@
 import "./NotFound.scss";
 import {ReactComponent as StanWebLight} from "assets/stan/Stan_404_Web_Light.svg";
+import {ReactComponent as StanWebDark} from "assets/stan/Stan_404_Web_Dark.svg";
+
 import {ReactComponent as StanMobileLight} from "assets/stan/Stan_404_Mobile_Light.svg";
+import {ReactComponent as StanMobileDark} from "assets/stan/Stan_404_Mobile_Dark.svg";
+
 import {ReactComponent as ScrumlrLogoLight} from "assets/scrumlr-logo-light.svg";
+import {ReactComponent as ScrumlrLogoDark} from "assets/scrumlr-logo-dark.svg";
+
 import {ReactComponent as BackgroundFreeFormLight} from "assets/pages/404/404_Background_light.svg";
+import {ReactComponent as BackgroundFreeFormDark} from "assets/pages/404/404_Background_dark.svg";
 
 export const NotFound = () => (
   <div className="not-found__root">
     <div className="not-found__background">
-      <BackgroundFreeFormLight />
+      <BackgroundFreeFormLight className="not-found_background-form--light" />
+      <BackgroundFreeFormDark className="not-found_background-form--dark" />
     </div>
     <header className="not-found__header">
       <div className="scrumlr-logo">
-        <ScrumlrLogoLight className="not-found__scrumlr-logo" />
+        <ScrumlrLogoLight className="not-found__scrumlr-logo--light" />
+        <ScrumlrLogoDark className="not-found__scrumlr-logo--dark" />
       </div>
     </header>
     <main className="not-found__main">
@@ -25,7 +34,10 @@ export const NotFound = () => (
       </div>
       <div className="not-found__image-wrapper">
         <StanWebLight className="not-found__logo-stan--web-light" />
+        <StanWebDark className="not-found__logo-stan--web-dark" />
+
         <StanMobileLight className="not-found__logo-stan--mobile-light" />
+        <StanMobileDark className="not-found__logo-stan--mobile-dark" />
       </div>
     </main>
   </div>
