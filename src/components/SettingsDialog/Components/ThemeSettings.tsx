2c186114ccf091d80fa717aae8d05d0331660730
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-04-20 07:08:47 +0200

    Reuse useLocalStorage hook for theme settings

--- src/components/SettingsDialog/Components/ThemeSettings.tsx
+++ src/components/SettingsDialog/Components/ThemeSettings.tsx
@@ -1,14 +1,15 @@
+import {useEffect} from "react";
+import {t} from "i18next";
 import {ReactComponent as DarkIcon} from "assets/icon-darkmode.svg";
 import {ReactComponent as LightIcon} from "assets/icon-lightmode.svg";
 import {ReactComponent as AutoIcon} from "assets/icon-settings.svg";
 import ThemePreviewDark from "assets/themes/theme-preview-dark.svg";
 import ThemePreviewLight from "assets/themes/theme-preview-light.svg";
-import {t} from "i18next";
-import {useEffect, useState} from "react";
+import {useLocalStorage} from "utils/hooks/useLocalStorage";
 import "./ThemeSettings.scss";
 
 export const ThemeSettings = () => {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "auto");
+  const [theme, setTheme] = useLocalStorage("theme", "auto");
   useEffect(() => {
     if (theme === "auto") {
       const autoTheme = window.matchMedia("(prefers-color-scheme: dark)")?.matches ? "dark" : "light";
