4f8b6519b131520df9e4252a2af90364637dfa57
Jakob Schwehn <schwehn42@gmail.com>
2023-05-11 14:30:26 +0200

    fe: add api remove reaction

--- src/api/reaction.ts
+++ src/api/reaction.ts
@@ -16,9 +16,26 @@ export const ReactionAPI = {
         return (await response.json()) as Reaction;
       }
 
-      throw new Error(`create note request resulted in status ${response.status}`);
+      throw new Error(`create reaction request resulted in status ${response.status}`);
     } catch (error) {
       throw new Error(`unable to add reaction with error: ${error}`);
+    }
+  },
+
+  removeReaction: async (board: string, note: string, reaction: string) => {
+    try {
+      const response = await fetch(`${SERVER_HTTP_URL}/boards/${board}/notes/${note}/reactions/${reaction}`, {
+        method: "DELETE",
+        credentials: "include",
+      });
+
+      if (response.status === 204) {
+        return;
+      }
+
+      throw new Error(`create reaction request resulted in status ${response.status}`);
+    } catch (error) {
+      throw new Error(`unable to remove reaction with error: ${error}`);
     }
   },
 };
