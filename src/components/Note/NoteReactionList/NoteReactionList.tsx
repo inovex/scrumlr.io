5917db6e18a6420eab258a01c8f938dbd923d0d1
Jakob Schwehn <schwehn42@gmail.com>
2023-05-22 15:11:01 +0200

    ajdust NoteReactionList

--- src/components/Note/NoteReactionList/NoteReactionList.tsx
+++ src/components/Note/NoteReactionList/NoteReactionList.tsx
@@ -1,23 +1,28 @@
 import {isEqual} from "underscore";
 import "./NoteReactionList.scss";
 import {useAppSelector} from "../../../store";
-import {Reaction} from "../../../types/reaction";
+import {Reaction, ReactionType} from "../../../types/reaction";
 import {Participant} from "../../../types/participant";
 
 interface NoteReactionListProps {
   noteId: string;
 }
 
-interface ReactionModeled {
-  reactionType: string;
+export interface ReactionModeled {
+  reactionType: ReactionType;
   amount: number;
   users: Participant[];
+  reactedSelf: boolean;
 }
 
 export const NoteReactionList = (props: NoteReactionListProps) => {
   const me = useAppSelector((state) => state.participants?.self);
   const others = useAppSelector((state) => state.participants?.others) ?? [];
   const participants = [me, ...others];
+  /*
+   * a filtered reactions list, where reactions of the same type are combined into the ReactionModeled interface.
+   * that way we have easy access to the amount of the same reactions and the users who made them
+   */
   const reactions = useAppSelector(
     (state) =>
       state.reactions
@@ -25,17 +30,22 @@ export const NoteReactionList = (props: NoteReactionListProps) => {
         .reduce((acc: ReactionModeled[], reaction: Reaction) => {
           const existingReaction = acc.find((r) => r.reactionType === reaction.reactionType);
           const participant = participants.find((p) => p?.user.id === reaction.user);
+          const existingParticipant = existingReaction?.users.find((p) => p.user.id === participant?.user.id);
+          const reactedSelf = participant?.user.id === me?.user.id;
 
           if (!participant) throw new Error("participant must exist");
 
           if (existingReaction) {
             existingReaction.amount++;
-            existingReaction.users.push(participant);
+            if (!existingParticipant) {
+              existingReaction.users.push(participant);
+            }
           } else {
             acc.push({
               reactionType: reaction.reactionType,
               amount: 1,
               users: [participant],
+              reactedSelf,
             });
           }
 
