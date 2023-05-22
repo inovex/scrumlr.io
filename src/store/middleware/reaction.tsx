32287e7b1f538021df3c1e2d197c2defcae782ac
Jakob Schwehn <schwehn42@gmail.com>
2023-05-22 16:08:20 +0200

    add reaction middleware

--- src/store/middleware/reaction.tsx
+++ src/store/middleware/reaction.tsx
@@ -1,7 +1,13 @@
 import {Dispatch, MiddlewareAPI} from "@reduxjs/toolkit";
 import {ApplicationState} from "../../types";
-import {ReduxAction} from "../action";
+import {Action, ReduxAction} from "../action";
+import {API} from "../../api";
 
 export const passReactionMiddleware = (stateAPI: MiddlewareAPI<Dispatch, ApplicationState>, dispatch: Dispatch, action: ReduxAction) => {
-  // TODO
+  if (action.type === Action.AddReaction) {
+    API.addReaction(action.context.board!, action.noteId, action.reactionType);
+  }
+  if (action.type === Action.DeleteReaction) {
+    API.deleteReaction(action.context.board!, action.reactionId);
+  }
 };
