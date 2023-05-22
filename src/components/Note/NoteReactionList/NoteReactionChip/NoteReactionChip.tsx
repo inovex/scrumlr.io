32e9b25f3e3d15d9338d4c8aeed0e476ea923f9f
Jakob Schwehn <schwehn42@gmail.com>
2023-05-22 15:26:15 +0200

    make chips clickable

--- src/components/Note/NoteReactionList/NoteReactionChip/NoteReactionChip.tsx
+++ src/components/Note/NoteReactionList/NoteReactionChip/NoteReactionChip.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import React from "react";
 import {ReactionModeled} from "../NoteReactionList";
 import {ReactionImageMap} from "../../../../types/reaction";
 
@@ -11,10 +12,26 @@ interface NoteReactionChipProps {
 export const NoteReactionChip = (props: NoteReactionChipProps) => {
   const reactionImage = ReactionImageMap.get(props.reaction.reactionType);
   const reactionUsers = props.reaction.users.map((u) => u.user.name).join(",");
+
+  const addReaction = () => {};
+  const removeReaction = () => {};
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation(); // in board overview, prevent note from opening stack view
+    if (props.reaction.reactedSelf) {
+      removeReaction();
+    } else {
+      addReaction();
+    }
+  };
   return (
-    <div className={classNames("note-reaction-chip__root", {"note-reaction-chip__root--self": props.reaction.reactedSelf})} title={reactionUsers}>
+    <button
+      className={classNames("note-reaction-chip__root", {"note-reaction-chip__root--self": props.reaction.reactedSelf})}
+      title={reactionUsers}
+      onClick={(e) => handleClick(e)}
+    >
       <div>{reactionImage}</div>
       <div>{props.reaction.amount}</div>
-    </div>
+    </button>
   );
 };
