32bc51004b0477a52c92575c37d6af4119054e2f
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-05-23 22:28:30 +0200

    Use translation for hotkey notification settings

--- src/components/SettingsDialog/Components/NotificationSettings.tsx
+++ src/components/SettingsDialog/Components/NotificationSettings.tsx
@@ -2,15 +2,17 @@ import {FC} from "react";
 import {Toggle} from "components/Toggle";
 import store, {useAppSelector} from "store";
 import {Actions} from "store/action";
+import {useTranslation} from "react-i18next";
 import {SettingsButton} from "./SettingsButton";
 
 export const NotificationSettings: FC = () => {
+  const {t} = useTranslation();
   const hotkeyNotificationsEnabled = useAppSelector((state) => state.view.hotkeyNotificationsEnabled);
 
   return (
     <section>
       <SettingsButton
-        label="Show hotkey notifications"
+        label={t("Appearance.showHotkeyNotifications")}
         onClick={() => (hotkeyNotificationsEnabled ? store.dispatch(Actions.disableHotkeyNotifications()) : store.dispatch(Actions.enableHotkeyNotifications()))}
       >
         <Toggle active={hotkeyNotificationsEnabled} />
