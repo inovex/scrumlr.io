9ce90f6d07a0f5cdb2bb2293b9059beff2134d90
Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>
2023-05-24 15:57:51 +0200

    feat: toggle hotkey notifications (#2940)

--- src/components/SettingsDialog/Components/SettingsButton.tsx
+++ src/components/SettingsDialog/Components/SettingsButton.tsx
@@ -18,11 +18,12 @@ export const SettingsButton: FC<PropsWithChildren<SettingsButtonProps>> = ({labe
 
   return (
     <button
-      type="button"
-      disabled={disabled}
+      aria-label={label}
       className={classNames("settings-option-button", {"settings-option-button--disabled": disabled}, className)}
+      disabled={disabled}
       onClick={onClick}
       onBlur={onBlur}
+      type="button"
       {...other}
     >
       {label && <span className="settings-option-button__label">{label}</span>}
