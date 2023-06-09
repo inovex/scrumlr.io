90f0a981a149e0fad3f064c561ae03e44e037b01
Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>
2023-06-09 11:24:32 +0200

    feat: "Mark me as done" for participants in timer/voting pill short actions (#2965)
    * Changes
    
    * Fix icon size of check icon
    
    * Changes
    
    * Changes
    
    * Update snapshot
    
    * Change Infobar from portal element to normal element
    
    * Add aria-label to short action buttons
    
    * Update Timer snapshot
    
    * Remove unused constant
    
    * Add --votes-depleted state to vote display component
    
    * Change background color of short action buttons
    
    * Design changes to timer/voting pill
    
    * Remove duplicate
    
    * Add comment to explain tooltip z-index:

--- src/components/Votes/VoteDisplay/VoteDisplay.tsx
+++ src/components/Votes/VoteDisplay/VoteDisplay.tsx
@@ -1,9 +1,11 @@
 import {useTranslation} from "react-i18next";
+import classNames from "classnames";
 import store, {useAppSelector} from "store";
 import {Actions} from "store/action";
 import {ReactComponent as VoteIcon} from "assets/icon-vote.svg";
 import {ReactComponent as CancelIcon} from "assets/icon-cancel.svg";
 import {ReactComponent as FlagIcon} from "assets/icon-flag.svg";
+import {ReactComponent as CheckIcon} from "assets/icon-check.svg";
 import "./VoteDisplay.scss";
 
 type VoteDisplayProps = {
@@ -13,31 +15,48 @@ type VoteDisplayProps = {
 
 export const VoteDisplay = ({usedVotes, possibleVotes}: VoteDisplayProps) => {
   const {t} = useTranslation();
+  const me = useAppSelector((state) => state.participants!.self);
   const voting = useAppSelector((state) => state.votings.open?.id);
-  const isModerator = useAppSelector((state) => state.participants?.self.role === "OWNER" || state.participants?.self.role === "MODERATOR");
+  const isAdmin = me.role === "OWNER" || me.role === "MODERATOR";
+  const isReady = me.ready;
 
   return (
-    <div className="vote-display">
+    <div className={classNames("vote-display", {"vote-display--votes-depleted": usedVotes === possibleVotes})}>
       {usedVotes > 0 && <div className="vote-display__progress-bar" style={{right: `calc(72px - (${usedVotes} / ${possibleVotes}) * 72px)`}} />}
       <span title={t("VoteDisplay.tooltip", {remaining: possibleVotes - usedVotes, total: possibleVotes})}>
         {usedVotes} / {possibleVotes}
       </span>
-      {isModerator && (
-        <div className="vote-display__short-actions">
-          <div className="short-actions__button-wrapper">
-            <button onClick={() => store.dispatch(Actions.closeVoting(voting!))}>
-              <FlagIcon />
+      <div className="vote-display__short-actions">
+        {isAdmin && (
+          <>
+            <div className="short-actions__short-action">
+              <button aria-label={t("VoteDisplay.finishActionTooltip")} className="short-action__button" onClick={() => store.dispatch(Actions.closeVoting(voting!))}>
+                <FlagIcon />
+              </button>
+              <span className="short-action__tooltip">{t("VoteDisplay.finishActionTooltip")}</span>
+            </div>
+            <div className="short-actions__short-action">
+              <button aria-label={t("VoteDisplay.abortActionTooltip")} className="short-action__button" onClick={() => store.dispatch(Actions.abortVoting(voting!))}>
+                <CancelIcon />
+              </button>
+              <span className="short-action__tooltip">{t("VoteDisplay.abortActionTooltip")}</span>
+            </div>
+          </>
+        )}
+        {!isAdmin && (
+          <div className="short-actions__short-action">
+            <button
+              aria-label={isReady ? t("MenuBars.unmarkAsDone") : t("MenuBars.markAsDone")}
+              className={classNames("short-action__button", {"short-action__button--ready": isReady}, {"short-action__button--unready": !isReady})}
+              onClick={() => store.dispatch(Actions.setUserReadyStatus(me!.user.id, !isReady))}
+            >
+              <CheckIcon className="short-action__check-icon" />
+              <CancelIcon className="short-action__cancel-icon" />
             </button>
-            <span>{t("VoteDisplay.finishActionTooltip")}</span>
+            <span className="short-action__tooltip">{isReady ? t("MenuBars.unmarkAsDone") : t("MenuBars.markAsDone")}</span>
           </div>
-          <div className="short-actions__button-wrapper">
-            <button onClick={() => store.dispatch(Actions.abortVoting(voting!))}>
-              <CancelIcon />
-            </button>
-            <span>{t("VoteDisplay.abortActionTooltip")}</span>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
       <VoteIcon />
     </div>
   );
