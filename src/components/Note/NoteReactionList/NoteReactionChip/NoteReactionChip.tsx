aa70c3a4da19411a646aa492ae7d0d77992315e8
Jakob Schwehn <schwehn42@gmail.com>
2023-06-13 11:36:13 +0200

    feat(fe/NoteReactionChip): users tooltip

--- src/components/Note/NoteReactionList/NoteReactionChip/NoteReactionChip.tsx
+++ src/components/Note/NoteReactionList/NoteReactionChip/NoteReactionChip.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import React from "react";
+import {Tooltip} from "react-tooltip";
 import {ReactionModeled} from "../NoteReactionList";
 import {ReactionImageMap, ReactionType} from "../../../../types/reaction";
 
@@ -12,16 +13,24 @@ interface NoteReactionChipProps {
 
 export const NoteReactionChip = (props: NoteReactionChipProps) => {
   const reactionImage = ReactionImageMap.get(props.reaction.reactionType);
-  const reactionUsers = props.reaction.users.map((u) => u.user.name).join(",");
+  const reactionUsers = props.reaction.users.map((u) => u.user.name).join(", ");
 
   return (
-    <button
-      className={classNames("note-reaction-chip__root", {"note-reaction-chip__root--self": props.reaction.myReactionId})} // highlight chips that yourself reacted to
-      title={reactionUsers}
-      onClick={(e) => props.handleClickReaction(e, props.reaction.reactionType)}
-    >
-      <div className="note-reaction-chip__reaction">{reactionImage}</div>
-      <div className="note-reaction-chip__amount">{props.reaction.amount}</div>
-    </button>
+    <>
+      <button
+        id={`reaction-${props.reaction.noteId}-${props.reaction.reactionType}`} // unique identifier
+        className={classNames("note-reaction-chip__root", {"note-reaction-chip__root--self": props.reaction.myReactionId})} // highlight chips that yourself reacted to
+        onClick={(e) => props.handleClickReaction(e, props.reaction.reactionType)}
+      >
+        <div className="note-reaction-chip__reaction">{reactionImage}</div>
+        <div className="note-reaction-chip__amount">{props.reaction.amount}</div>
+      </button>
+      <Tooltip
+        anchorSelect={`#reaction-${props.reaction.noteId}-${props.reaction.reactionType}`}
+        place="top"
+        variant={document.documentElement.getAttribute("theme") === "dark" ? "dark" : "light"}
+        content={reactionUsers}
+      />
+    </>
   );
 };
