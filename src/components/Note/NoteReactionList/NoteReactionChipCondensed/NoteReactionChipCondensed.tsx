de5f9613b03ef0f3cad38485259bcc944f55bde4
Jakob Schwehn <schwehn42@gmail.com>
2023-06-13 15:40:07 +0200

    feat(fe/NoteReactionChipCondensed): multiline tooltip

--- src/components/Note/NoteReactionList/NoteReactionChipCondensed/NoteReactionChipCondensed.tsx
+++ src/components/Note/NoteReactionList/NoteReactionChipCondensed/NoteReactionChipCondensed.tsx
@@ -9,12 +9,14 @@ interface NoteReactionChipPropsCondensed {
 }
 
 export const NoteReactionChipCondensed = (props: NoteReactionChipPropsCondensed) => {
-  const {noteId} = props.reactions[0]; // assertion: props.reactions > ACTIVATE_CONDENSED_VIEW_MIN_USER_AMOUNT
+  // assertion: props.reactions > ACTIVATE_CONDENSED_VIEW_MIN_USER_AMOUNT > 0
+  const {noteId} = props.reactions[0];
   // filter out own reaction if exists.
   const reactionsFiltered = props.reactions.filter((r) => !r.myReactionId);
   const reactionImages = reactionsFiltered.map((r) => ReactionImageMap.get(r.reactionType));
-  // result: "User 1, User 2: laughingEmoji / User 3: heartEmoji"
-  const title = reactionsFiltered.map((r) => `${r.users.map((u) => u.user.name).join(", ")}: ${ReactionImageMap.get(r.reactionType)}`).join(" / ");
+  // result example: [0]: "User 1, User 2: laughingEmoji"
+  //                 [1]: "User 3: heartEmoji"
+  const reactionUsersTitle = reactionsFiltered.map((r) => `${r.users.map((u) => u.user.name).join(", ")}: ${ReactionImageMap.get(r.reactionType)}`);
   const totalAmount = reactionsFiltered.reduce((sum, reactionModeled) => sum + reactionModeled.amount, 0);
 
   return (
@@ -25,7 +27,13 @@ export const NoteReactionChipCondensed = (props: NoteReactionChipPropsCondensed)
         ))}
         <div className="note-reaction-chip-condensed__amount">{totalAmount}</div>
       </div>
-      <Tooltip anchorSelect={`#reactions-condensed-${noteId}`} place="top" variant={document.documentElement.getAttribute("theme") === "dark" ? "dark" : "light"} content={title} />
+      <Tooltip anchorSelect={`#reactions-condensed-${noteId}`} place="top" variant={document.documentElement.getAttribute("theme") === "dark" ? "dark" : "light"}>
+        <div className="note-reaction-chip-condensed__tooltip">
+          {reactionUsersTitle.map((t) => (
+            <span>{t}</span>
+          ))}
+        </div>
+      </Tooltip>
     </>
   );
 };
