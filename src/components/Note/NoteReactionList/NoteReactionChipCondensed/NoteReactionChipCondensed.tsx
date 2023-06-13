979559efa48007ef3dd20277a510dc3ac8ec40e3
Jakob Schwehn <schwehn42@gmail.com>
2023-06-13 13:13:44 +0200

    feat(fe/NoteReactionChipCondensed): add tooltip

--- src/components/Note/NoteReactionList/NoteReactionChipCondensed/NoteReactionChipCondensed.tsx
+++ src/components/Note/NoteReactionList/NoteReactionChipCondensed/NoteReactionChipCondensed.tsx
@@ -1,3 +1,4 @@
+import {Tooltip} from "react-tooltip";
 import {ReactionModeled} from "../NoteReactionList";
 import {ReactionImageMap} from "../../../../types/reaction";
 
@@ -8,6 +9,7 @@ interface NoteReactionChipPropsCondensed {
 }
 
 export const NoteReactionChipCondensed = (props: NoteReactionChipPropsCondensed) => {
+  const {noteId} = props.reactions[0]; // assertion: props.reactions > ACTIVATE_CONDENSED_VIEW_MIN_USER_AMOUNT
   // filter out own reaction if exists.
   const reactionsFiltered = props.reactions.filter((r) => !r.myReactionId);
   const reactionImages = reactionsFiltered.map((r) => ReactionImageMap.get(r.reactionType));
@@ -16,11 +18,14 @@ export const NoteReactionChipCondensed = (props: NoteReactionChipPropsCondensed)
   const totalAmount = reactionsFiltered.reduce((sum, reactionModeled) => sum + reactionModeled.amount, 0);
 
   return (
-    <div className="note-reaction-chip-condensed__root" title={title}>
-      {reactionImages.map((emoji) => (
-        <div className="note-reaction-chip-condensed__reaction">{emoji}</div>
-      ))}
-      <div className="note-reaction-chip-condensed__amount">{totalAmount}</div>
-    </div>
+    <>
+      <div id={`reactions-condensed-${noteId}`} className="note-reaction-chip-condensed__root">
+        {reactionImages.map((emoji) => (
+          <div className="note-reaction-chip-condensed__reaction">{emoji}</div>
+        ))}
+        <div className="note-reaction-chip-condensed__amount">{totalAmount}</div>
+      </div>
+      <Tooltip anchorSelect={`#reactions-condensed-${noteId}`} place="top" variant={document.documentElement.getAttribute("theme") === "dark" ? "dark" : "light"} content={title} />
+    </>
   );
 };
