07607717a89a9d6052f0f02f49194bf33d4dd023
Benedict Homuth <bhomuth@inovex.de>
2023-06-16 12:53:38 +0200

    added backend support for syncing notes after boardsettings changed

--- server/src/services/boards/boards.go
+++ server/src/services/boards/boards.go
@@ -104,7 +104,7 @@ func (s *BoardService) Update(ctx context.Context, body dto.BoardUpdateRequest) 
 		ShowAuthors:           body.ShowAuthors,
 		ShowNotesOfOtherUsers: body.ShowNotesOfOtherUsers,
 		AllowStacking:         body.AllowStacking,
-    TimerStart:            body.TimerStart,
+		TimerStart:            body.TimerStart,
 		TimerEnd:              body.TimerEnd,
 		SharedNote:            body.SharedNote,
 	}
@@ -135,12 +135,12 @@ func (s *BoardService) Update(ctx context.Context, body dto.BoardUpdateRequest) 
 }
 
 func (s *BoardService) SetTimer(_ context.Context, id uuid.UUID, minutes uint8) (*dto.Board, error) {
-  timerStart := time.Now().Local();
+	timerStart := time.Now().Local()
 	timerEnd := timerStart.Add(time.Minute * time.Duration(minutes))
 	update := database.BoardTimerUpdate{
-		ID:       id,
-    TimerStart: &timerStart,
-		TimerEnd: &timerEnd,
+		ID:         id,
+		TimerStart: &timerStart,
+		TimerEnd:   &timerEnd,
 	}
 	board, err := s.database.UpdateBoardTimer(update)
 	if err != nil {
@@ -151,9 +151,9 @@ func (s *BoardService) SetTimer(_ context.Context, id uuid.UUID, minutes uint8) 
 
 func (s *BoardService) DeleteTimer(_ context.Context, id uuid.UUID) (*dto.Board, error) {
 	update := database.BoardTimerUpdate{
-		ID:       id,
-    TimerStart: nil,
-		TimerEnd: nil,
+		ID:         id,
+		TimerStart: nil,
+		TimerEnd:   nil,
 	}
 	board, err := s.database.UpdateBoardTimer(update)
 	if err != nil {
@@ -180,6 +180,34 @@ func (s *BoardService) UpdatedBoard(board database.Board) {
 	if err != nil {
 		logger.Get().Errorw("unable to broadcast updated board", "err", err)
 	}
+
+	err = s.SyncBoardSettingChange(board.ID)
+	if err != nil {
+		logger.Get().Errorw("unable to broadcast notes, following a updated board call", "err", err)
+	}
+}
+
+func (s *BoardService) SyncBoardSettingChange(boardID uuid.UUID) error {
+	columns, err := s.database.GetColumns(boardID)
+	if err != nil {
+		return err
+	}
+
+	var columnsID []uuid.UUID
+	for _, column := range columns {
+		columnsID = append(columnsID, column.ID)
+	}
+	notes, err := s.database.GetNotes(boardID, columnsID...)
+	if err != nil {
+		return err
+	}
+
+	err = s.realtime.BroadcastToBoard(boardID, realtime.BoardEvent{
+		Type: realtime.BoardEventNotesSync,
+		Data: dto.Notes(notes),
+	})
+
+	return err
 }
 
 func (s *BoardService) DeletedBoard(board uuid.UUID) {
