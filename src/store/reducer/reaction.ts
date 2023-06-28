01e47ad0884cf1b2ee622dad1d467428555a415a
Jakob Schwehn <schwehn42@gmail.com>
2023-06-28 18:32:06 +0200

    feat(fe/reducer): reduce updated reaction into store

--- src/store/reducer/reaction.ts
+++ src/store/reducer/reaction.ts
@@ -1,5 +1,5 @@
+import {ReactionState} from "types/reaction";
 import {Action, ReduxAction} from "../action";
-import {ReactionState} from "../../types/reaction";
 
 // eslint-disable-next-line @typescript-eslint/default-param-last
 export const reactionReducer = (state: ReactionState = [], action: ReduxAction): ReactionState => {
@@ -12,6 +12,14 @@ export const reactionReducer = (state: ReactionState = [], action: ReduxAction):
 
     case Action.DeletedReaction:
       return state.filter((r) => r.id !== action.reactionId);
+
+    case Action.UpdatedReaction: {
+      const updatedReaction = action.reaction;
+      // since we don't mutate our original state we first filter out the old reaction ...
+      const filteredState = state.filter((r) => r.id !== updatedReaction.id);
+      // ... then add the reaction with the new reactionType
+      return [...filteredState, updatedReaction];
+    }
 
     default:
       return state;
