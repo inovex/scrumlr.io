d226cb7d41e864865cffd32964c10afc72904ca0
Jakob Schwehn <schwehn42@gmail.com>
2023-06-29 14:33:12 +0200

    feat(fe/NoteReactionChip): make click handlers optional

--- src/components/Note/NoteReactionList/NoteReactionChip/NoteReactionChip.tsx
+++ src/components/Note/NoteReactionList/NoteReactionChip/NoteReactionChip.tsx
@@ -9,22 +9,30 @@ import "./NoteReactionChip.scss";
 
 interface NoteReactionChipProps {
   reaction: ReactionModeled;
-  handleClickReaction: (e: React.MouseEvent<HTMLButtonElement>, reactionType: ReactionType) => void;
-  handleLongPressReaction: (e: LongPressReactEvents) => void;
+  handleClickReaction?: (e: React.MouseEvent<HTMLButtonElement>, reactionType: ReactionType) => void;
+  handleLongPressReaction?: (e: LongPressReactEvents) => void;
 }
 
 export const NoteReactionChip = (props: NoteReactionChipProps) => {
   const reactionImage = ReactionImageMap.get(props.reaction.reactionType);
   const reactionUsers = props.reaction.users.map((u) => u.user.name).join(", ");
 
-  const bindLongPress = useLongPress((e) => props.handleLongPressReaction(e));
+  const bindLongPress = useLongPress((e) => {
+    if (props.handleLongPressReaction) {
+      props.handleLongPressReaction(e);
+    }
+  });
 
   return (
     <>
       <button
         id={`reaction-${props.reaction.noteId}-${props.reaction.reactionType}`} // unique identifier
         className={classNames("note-reaction-chip__root", {"note-reaction-chip__root--self": props.reaction.myReactionId})} // highlight chips that yourself reacted to
-        onClick={(e) => props.handleClickReaction(e, props.reaction.reactionType)} // bind short press
+        onClick={(e) => {
+          if (props.handleClickReaction) {
+            props.handleClickReaction(e, props.reaction.reactionType);
+          }
+        }} // bind short press
         onTouchStart={(e) => e.stopPropagation()} // prevent note dragging from here
         {...bindLongPress()} // bind long press
       >
