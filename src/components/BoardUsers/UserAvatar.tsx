765e27cfa9d3f590fd86e854fb31945d40380dfc
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-07-11 21:36:35 +0200

    Changes

--- src/components/BoardUsers/UserAvatar.tsx
+++ src/components/BoardUsers/UserAvatar.tsx
@@ -1,9 +1,10 @@
-import "./UserAvatar.scss";
+import {useEffect, useState, useRef} from "react";
 import classNames from "classnames";
 import {ReactComponent as IconCheck} from "assets/icon-ready.svg";
 import {ReactComponent as RaisedHand} from "assets/icon-hand.svg";
 import {AvataaarProps, Avatar} from "../Avatar";
 import {Badge} from "../Badge";
+import "./UserAvatar.scss";
 
 export interface UserAvatarProps {
   className?: string;
@@ -16,11 +17,25 @@ export interface UserAvatarProps {
   badgeText?: string;
 }
 
-export const UserAvatar = ({title, badgeText, id, ready, raisedHand, avatar, className, avatarClassName}: UserAvatarProps) => (
-  <div className={classNames("user-avatar", className, ready && "user-ready")} title={title}>
-    {ready && <IconCheck className="user-avatar__ready" />}
-    {raisedHand && <RaisedHand className="user-avatar__raised-hand" />}
-    <Avatar seed={id} avatar={avatar} className={avatarClassName} />
-    {badgeText && <Badge text={badgeText} />}
-  </div>
-);
+export const UserAvatar = ({title, badgeText, id, ready, raisedHand, avatar, className, avatarClassName}: UserAvatarProps) => {
+  const [readyAnimation, setReadyAnimation] = useState<boolean>(false);
+  const prevReadyRef = useRef<boolean>();
+
+  useEffect(() => {
+    if (ready && prevReadyRef.current === false) {
+      setReadyAnimation(true);
+      setTimeout(() => setReadyAnimation(false), 600);
+    }
+
+    prevReadyRef.current = ready;
+  }, [ready]);
+
+  return (
+    <div className={classNames("user-avatar", className, ready && "user-ready", {"user-avatar--ready-animated": readyAnimation})} title={title}>
+      {ready && <IconCheck className="user-avatar__ready" />}
+      {raisedHand && <RaisedHand className="user-avatar__raised-hand" />}
+      <Avatar seed={id} avatar={avatar} className={avatarClassName} />
+      {badgeText && <Badge text={badgeText} />}
+    </div>
+  );
+};
