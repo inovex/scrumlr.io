2be2a3be07f7cd30a3d80f53eb202a067037f043
Christian Reski <35272402+Resaki1@users.noreply.github.com>
2023-07-14 11:18:40 +0200

    fix: caching problem with PWA and login providers (#2873)
    Co-authored-by: Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -21,20 +21,17 @@ registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({request, url}: {request: Request; url: URL}) => {
     // If this isn't a navigation, skip.
-    if (request.mode !== "navigate") {
-      return false;
-    }
+    if (request.mode !== "navigate") return false;
 
     // If this is a URL that starts with /_, skip.
-    if (url.pathname.startsWith("/_")) {
-      return false;
-    }
+    if (url.pathname.startsWith("/_")) return false;
 
     // If this looks like a URL for a resource, because it contains
     // a file extension, skip.
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    }
+    if (url.pathname.match(fileExtensionRegexp)) return false;
+
+    // if this URL is a login provider URL, skip
+    if (url.pathname.includes("/api/login/")) return false;
 
     // Return true to signal that we want to use the handler.
     return true;
