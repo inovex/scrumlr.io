da9a200b2c1751c1f39a7a212a426393a76bdc0c
Jakob Schwehn <schwehn42@gmail.com>
2023-07-24 18:44:35 +0200

    feat(util): move delta hook

---
+++ src/utils/hooks/useMoveDelta.ts
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from "react";
+
+/** used to get a updated delta where you are dragging an element
+ * right now only y-axis is supported
+ * @param movableRef reference to element that is draggable (where the touch event is called)
+ * @param parentRef reference to element which is moved
+ * @returns delta y which the element was moved from original position
+ * */
+export const useMoveDelta = (movableRef: React.RefObject<HTMLDivElement>, parentRef: React.RefObject<HTMLDivElement>) => {
+  const [deltaY, setDeltaY] = useState(0);
+
+  useEffect(() => {
+    const movableElement = movableRef.current!;
+    const parentElement = parentRef.current!;
+    const refY = movableElement.getBoundingClientRect().y;
+    const offsetY = parentElement.offsetHeight;
+
+    const handleTouchMove = (e: globalThis.TouchEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+      const touch = e.changedTouches[0];
+      setDeltaY(touch.clientY - refY + offsetY);
+    };
+    movableElement.addEventListener("touchmove", handleTouchMove);
+
+    return () => {
+      movableElement.removeEventListener("touchmove", handleTouchMove);
+    };
+  }, [movableRef, parentRef]);
+
+  return deltaY;
+};
