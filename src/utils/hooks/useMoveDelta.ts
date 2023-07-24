7ffcc3bd8c566ddba93d46f28bc5a7d0c6bb4d50
Jakob Schwehn <schwehn42@gmail.com>
2023-07-24 18:46:44 +0200

    change(NoteReactionPopup): useMoveDelta hook second param optional

--- src/utils/hooks/useMoveDelta.ts
+++ src/utils/hooks/useMoveDelta.ts
@@ -6,14 +6,14 @@ import React, {useEffect, useState} from "react";
  * @param parentRef reference to element which is moved
  * @returns delta y which the element was moved from original position
  * */
-export const useMoveDelta = (movableRef: React.RefObject<HTMLDivElement>, parentRef: React.RefObject<HTMLDivElement>) => {
+export const useMoveDelta = (movableRef: React.RefObject<HTMLDivElement>, parentRef?: React.RefObject<HTMLDivElement>) => {
   const [deltaY, setDeltaY] = useState(0);
 
   useEffect(() => {
     const movableElement = movableRef.current!;
-    const parentElement = parentRef.current!;
+    const parentElement = parentRef?.current;
     const refY = movableElement.getBoundingClientRect().y;
-    const offsetY = parentElement.offsetHeight;
+    const offsetY = parentElement?.offsetHeight ?? 0;
 
     const handleTouchMove = (e: globalThis.TouchEvent) => {
       e.preventDefault();
