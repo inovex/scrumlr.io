d631d83b1dc3eb2e592366c4f3ede74414d3b7fb
Jakob Schwehn <schwehn42@gmail.com>
2023-07-24 14:31:25 +0200

    feat(NoteAuthorList): separate stack author from rest and display in row

--- src/components/Note/NoteAuthorList/NoteAuthorList.tsx
+++ src/components/Note/NoteAuthorList/NoteAuthorList.tsx
@@ -52,32 +52,53 @@ export const NoteAuthorList = (props: NoteAuthorListProps) => {
   // where (n) displays a number and p displays an avatar
   const SHOW_MAX_AUTHORS = 3;
   const stackAuthor = authorExtendedInfo[0];
-  const restAuthors = authorExtendedInfo.slice();
+  const restAuthors = authorExtendedInfo.slice(1);
   const slicedAuthors = restAuthors.splice(0, authorExtendedInfo.length > SHOW_MAX_AUTHORS ? SHOW_MAX_AUTHORS - 1 : SHOW_MAX_AUTHORS); // max first n authors
   const restUsersExist = restAuthors.length > 0;
   const restUsersTitle = restAuthors.map((a) => a.displayName).join("\x0A"); // join names with line breaks
   return props.showAuthors || props.viewer.user.id === stackAuthor.user!.id ? (
-    <div className={classNames("note-author-list", {"note-author-list--self": stackAuthor.isSelf})}>
-      {slicedAuthors.map((a) => (
+    <div className="note-author-list">
+      <div className={classNames("note-author__container", {"note-author__container--self": stackAuthor.isSelf})}>
         <figure
           className={classNames("note__author", {
             "note__author--self": stackAuthor.isSelf,
             "note__author--with-rest": restUsersExist,
           })}
           aria-roledescription="author"
-          key={a.user!.id}
+          key={stackAuthor.user!.id}
         >
-          <UserAvatar id={a.user!.id} avatar={a.user!.avatar} title={a.displayName} className="note__user-avatar" avatarClassName="note__user-avatar" />
+          <UserAvatar
+            id={stackAuthor.user!.id}
+            avatar={stackAuthor.user!.avatar}
+            title={stackAuthor.displayName}
+            className="note__user-avatar"
+            avatarClassName="note__user-avatar"
+          />
         </figure>
-      ))}
 
-      {restUsersExist && (
-        <figure className={classNames("note-author-rest", {"note-author-rest--self": stackAuthor.isSelf})} title={restUsersTitle}>
-          {restAuthors.length}
-        </figure>
-      )}
+        <div className={classNames("note__author-name", {"note__author-name--self": stackAuthor.isSelf})}>{stackAuthor.displayName}</div>
+      </div>
 
-      <div className={classNames("note__author-name", {"note__author-name--self": stackAuthor.isSelf})}>{stackAuthor.displayName}</div>
+      <div className="note-rest-authors__container">
+        {slicedAuthors.map((a) => (
+          <figure
+            className={classNames("note__author", {
+              "note__author--self": stackAuthor.isSelf,
+              "note__author--with-rest": restUsersExist,
+            })}
+            aria-roledescription="author"
+            key={a.user!.id}
+          >
+            <UserAvatar id={a.user!.id} avatar={a.user!.avatar} title={a.displayName} className="note__user-avatar" avatarClassName="note__user-avatar" />
+          </figure>
+        ))}
+
+        {restUsersExist && (
+          <figure className={classNames("note-author-rest", {"note-author-rest--self": stackAuthor.isSelf})} title={restUsersTitle}>
+            {restAuthors.length}
+          </figure>
+        )}
+      </div>
     </div>
   ) : null;
 };
