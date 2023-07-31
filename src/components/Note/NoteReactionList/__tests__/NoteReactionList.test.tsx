6c4bbaa6fddf26c464c0de5a018a73bf068843d4
Jakob Schwehn <schwehn42@gmail.com>
2023-07-31 15:16:24 +0200

    test(NoteReactionList): one more test

--- src/components/Note/NoteReactionList/__tests__/NoteReactionList.test.tsx
+++ src/components/Note/NoteReactionList/__tests__/NoteReactionList.test.tsx
@@ -2,6 +2,7 @@ import {Provider} from "react-redux";
 import getTestStore from "utils/test/getTestStore";
 import {NoteReactionList} from "../NoteReactionList";
 import {render} from "testUtils";
+import {fireEvent} from "@testing-library/react";
 
 const NOTE_ID = "test-notes-id-1";
 
@@ -12,11 +13,17 @@ const createNoteReactionList = (note: string) => (
 );
 
 describe("NoteReactionList", () => {
-  describe("data", () => {
-    it("should have two reactions", () => {
-      const {container} = render(createNoteReactionList(NOTE_ID));
-      const reactionContainer = container.getElementsByClassName("note-reaction-list__reaction-chips-container")[0];
-      expect(reactionContainer.children.length).toBe(2);
-    });
+  it("should have two reactions", () => {
+    const {container} = render(createNoteReactionList(NOTE_ID));
+    const reactionContainer = container.getElementsByClassName("note-reaction-list__reaction-chips-container")[0];
+    expect(reactionContainer.children.length).toBe(2);
+  });
+
+  it("should show no reactions on clicking sticker", () => {
+    const {container} = render(createNoteReactionList(NOTE_ID));
+    const stickerContainer = container.getElementsByClassName("note-reaction-list__add-reaction-sticker-container")[0];
+    fireEvent.click(stickerContainer);
+    const reactionContainer = container.getElementsByClassName("note-reaction-list__reaction-chips-container")[0];
+    expect(reactionContainer.children.length).toBe(0); // no reaction are rendered because bar is visible instead.
   });
 });
