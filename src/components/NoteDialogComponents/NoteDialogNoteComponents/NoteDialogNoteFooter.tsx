35e2eb26019ee3783fb90c1e56e47e7250fc6c56
Jakob Schwehn <schwehn42@gmail.com>
2023-08-03 18:57:21 +0200

    feat(NoteDialogNoteFooter): remove author, add reactions list to footer

--- src/components/NoteDialogComponents/NoteDialogNoteComponents/NoteDialogNoteFooter.tsx
+++ src/components/NoteDialogComponents/NoteDialogNoteComponents/NoteDialogNoteFooter.tsx
@@ -1,13 +1,9 @@
-import {UserAvatar} from "components/BoardUsers";
 import {FC} from "react";
-import classNames from "classnames";
-import {useAppSelector} from "store";
-import {useTranslation} from "react-i18next";
 import {Participant} from "types/participant";
 import {AvataaarProps} from "components/Avatar";
-import _ from "underscore";
-import "./NoteDialogNoteFooter.scss";
 import {Votes} from "components/Votes";
+import {NoteReactionList} from "components/Note/NoteReactionList/NoteReactionList";
+import "./NoteDialogNoteFooter.scss";
 
 type NoteDialogNoteFooterProps = {
   showAuthors: boolean;
@@ -18,37 +14,9 @@ type NoteDialogNoteFooterProps = {
   viewer: Participant;
 };
 
-export const NoteDialogNoteFooter: FC<NoteDialogNoteFooterProps> = (props: NoteDialogNoteFooterProps) => {
-  const {t} = useTranslation();
-
-  const note = useAppSelector((state) => state.notes.find((n) => n.id === props.noteId), _.isEqual);
-  const author = useAppSelector((state) => {
-    const noteAuthor = state.participants?.others.find((p) => p.user.id === note?.author) ?? state.participants?.self;
-    const isSelf = noteAuthor?.user.id === state.participants?.self.user.id;
-    const displayName = isSelf ? t("Note.me") : noteAuthor!.user.name;
-    return {
-      ...noteAuthor,
-      displayName,
-      isSelf,
-    };
-  }, _.isEqual);
-  return (
-    <div className="note-dialog__note-footer">
-      <div className="note-dialog__author-container">
-        {(props.showAuthors || props.viewer.user.id === props.authorId) && (
-          <figure className={classNames("note-dialog__note-author", {"note-dialog__note-author--self": author.isSelf})}>
-            <UserAvatar
-              id={props.authorId}
-              avatar={props.avatar}
-              title={props.authorName}
-              className="note-dialog__note-user-avatar"
-              avatarClassName="note-dialog__note-user-avatar"
-            />
-            <figcaption className="note-dialog__note-author-name">{props.authorName}</figcaption>
-          </figure>
-        )}
-      </div>
-      <Votes {...props} className="note-dialog__note-votes" />
-    </div>
-  );
-};
+export const NoteDialogNoteFooter: FC<NoteDialogNoteFooterProps> = (props: NoteDialogNoteFooterProps) => (
+  <div className="note-dialog__note-footer">
+    <NoteReactionList noteId={props.noteId} parentFocus />
+    <Votes {...props} className="note-dialog__note-votes" />
+  </div>
+);
