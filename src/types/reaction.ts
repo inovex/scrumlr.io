bdfe25f81b559454dee752647658939a74f1dbf4
Jakob Schwehn <schwehn42@gmail.com>
2023-08-16 14:58:04 +0200

    feat: Board Reaction types

---
+++ src/types/reaction.ts
@@ -0,0 +1,14 @@
+export interface BoardReaction {
+  user: string; // participant uuid
+  reactionType: ReactionType;
+}
+
+export type ReactionType = "thinking" | "heart" | "like" | "dislike" | "joy" | "celebration" | "poop" | "applause" | "tada";
+
+export const BoardReactionImageMap = new Map<ReactionType, string>([
+  ["tada", "🎉"],
+  ["applause", "👏"],
+  ["heart", "💖"],
+  ["like", "👍"],
+  ["dislike", "👎"],
+]);
