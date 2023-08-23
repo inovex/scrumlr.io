2acf0453ad331043c8c390c80e5b19edb7b490b8
Jakob Schwehn <schwehn42@gmail.com>
2023-08-23 11:47:54 +0200

    fix: remove unnecessary eslint-disable

--- src/utils/hooks/useSize.ts
+++ src/utils/hooks/useSize.ts
@@ -1,5 +1,4 @@
 import {RefObject, useLayoutEffect, useState} from "react";
-// eslint-disable-next-line import/no-extraneous-dependencies
 import useResizeObserver from "@react-hook/resize-observer";
 
 // this hook returns the current dimensions of a reference html element
