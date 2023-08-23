1da5a811cea1604f8ca8fb035455347a292e0c11
Jakob Schwehn <schwehn42@gmail.com>
2023-08-23 11:59:51 +0200

    refactor: reaction type instead of interface

--- src/types/reaction.ts
+++ src/types/reaction.ts
@@ -1,12 +1,12 @@
 /**
  * representation of a reaction on the server side
  */
-export interface Reaction {
+export type Reaction = {
   id: string;
   note: string;
   user: string;
   reactionType: ReactionType;
-}
+};
 
 export type ReactionState = Reaction[];
 
