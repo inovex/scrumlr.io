29136acc31d16048357c23bcf4433470f3eda13d
Jakob Schwehn <schwehn42@gmail.com>
2023-08-24 11:48:15 +0200

    feat: add reaction to state

--- src/components/BoardReactionContainer/BoardReactionContainer.tsx
+++ src/components/BoardReactionContainer/BoardReactionContainer.tsx
@@ -1,15 +1,22 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {BoardReaction} from "types/reaction";
 
 export const BoardReactionContainer = () => {
+  const [reactions, setReactions] = useState<BoardReaction[]>([]);
   useEffect(() => {
     const handle = (e: CustomEvent<BoardReaction>) => {
-      console.log("evnt", e.detail);
+      setReactions([...reactions, e.detail]);
     };
     document.addEventListener("BoardReactionEvent", handle as EventListener);
 
     return () => document.removeEventListener("BoardReactionEvent", handle as EventListener);
-  }, []);
+  }, [reactions]);
 
-  return <div className="board-reaction-container__root" />;
+  return (
+    <div className="board-reaction-container__root">
+      {reactions.map((r) => (
+        <div>{r.reactionType}</div>
+      ))}
+    </div>
+  );
 };
