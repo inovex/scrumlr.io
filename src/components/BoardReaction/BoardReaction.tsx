f11a1d2dafdb95298ffdaa74aef1ff756c2a24cd
Jakob Schwehn <schwehn42@gmail.com>
2023-08-25 14:00:57 +0200

    refactor: move method to utils

--- src/components/BoardReaction/BoardReaction.tsx
+++ src/components/BoardReaction/BoardReaction.tsx
@@ -1,6 +1,7 @@
 import {BoardReactionEventType, BoardReactionImageMap} from "types/reaction";
-import "./BoardReaction.scss";
 import {memo} from "react";
+import {getRandomNumberInRange} from "utils/random";
+import "./BoardReaction.scss";
 
 type BoardReactionProps = {
   reaction: BoardReactionEventType;
@@ -12,12 +13,7 @@ export const BoardReaction = memo((props: BoardReactionProps) => {
   const emoji = BoardReactionImageMap.get(props.reaction.reactionType);
   const {name} = props.reaction.user.user;
 
-  const getRandomNumber = (min: number, max: number): number => {
-    const range = max - min + 1;
-    return Math.floor(Math.random() * range) + min;
-  };
-
-  const leftOffset = getRandomNumber(25, 75);
+  const leftOffset = getRandomNumberInRange(25, 75);
 
   return (
     <div className="board-reaction__root" style={{left: `${leftOffset}vw`}}>
