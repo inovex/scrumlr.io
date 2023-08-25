214423f6ab21fcb4011f39243e3c7543c0a9e997
Jakob Schwehn <schwehn42@gmail.com>
2023-08-25 12:11:43 +0200

    feat: random offset

--- src/components/BoardReaction/BoardReaction.tsx
+++ src/components/BoardReaction/BoardReaction.tsx
@@ -9,8 +9,15 @@ export const BoardReaction = (props: BoardReactionProps) => {
   const emoji = BoardReactionImageMap.get(props.reaction.reactionType);
   const {name} = props.reaction.user.user;
 
+  const getRandomNumber = (min: number, max: number): number => {
+    const range = max - min + 1;
+    return Math.floor(Math.random() * range) + min;
+  };
+
+  const leftOffset = getRandomNumber(25, 75);
+
   return (
-    <div className="board-reaction__root">
+    <div className="board-reaction__root" style={{left: `${leftOffset}vw`}}>
       <div className="board-reaction__emoji-container">{emoji}</div>
       <div className="board-reaction__name-container">{name}</div>
     </div>
