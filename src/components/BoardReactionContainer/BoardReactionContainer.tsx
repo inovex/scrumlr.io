fbea52e297220f389527349607de95c0b145aaa6
Jakob Schwehn <schwehn42@gmail.com>
2023-09-01 14:15:29 +0200

    dispatch actions in menu

--- src/components/BoardReactionContainer/BoardReactionContainer.tsx
+++ src/components/BoardReactionContainer/BoardReactionContainer.tsx
@@ -1,29 +1,13 @@
-import {useEffect, useState} from "react";
-import {BoardReactionEventType} from "types/reaction";
+import {useAppSelector} from "store";
 import {BoardReaction} from "../BoardReaction/BoardReaction";
 import "./BoardReactionContainer.scss";
 
 export const BoardReactionContainer = () => {
-  const [reactions, setReactions] = useState<BoardReactionEventType[]>([]);
-
-  useEffect(() => {
-    const removeAfterDelay = (id: string, delay: number) => {
-      setTimeout(() => {
-        setReactions((prevState) => prevState.filter((r) => r.id !== id));
-      }, delay);
-    };
-    const handle = (e: CustomEvent<BoardReactionEventType>) => {
-      setReactions((prevState) => [...prevState, e.detail]);
-      removeAfterDelay(e.detail.id, 5_000);
-    };
-    document.addEventListener("BoardReactionEvent", handle as EventListener);
-
-    return () => document.removeEventListener("BoardReactionEvent", handle as EventListener);
-  }, [reactions]);
+  const boardReactions = useAppSelector((state) => state.boardReactions);
 
   return (
     <div className="board-reaction-container__root">
-      {reactions.map((r) => (
+      {boardReactions.map((r) => (
         <BoardReaction key={r.id} reaction={r} />
       ))}
     </div>
--- src/components/BoardReactionMenu/BoardReactionMenu.tsx
+++ src/components/BoardReactionMenu/BoardReactionMenu.tsx
@@ -3,15 +3,28 @@ import {ReactComponent as CloseIcon} from "assets/icon-close.svg";
 import {BoardReactionEventType, BoardReactionImageMap, ReactionType} from "types/reaction";
 import {useAppSelector} from "store";
 import {uniqueId} from "underscore";
+import {useDispatch} from "react-redux";
+import {Actions} from "store/action";
 import "./BoardReactionMenu.scss";
 
+const REMOVAL_DELAY_MS = 5_000;
+
 export const BoardReactionMenu = () => {
+  const dispatch = useDispatch();
   const me = useAppSelector((state) => state.participants?.self);
+
+  const removeAfterDelay = (id: string, delay: number) => {
+    setTimeout(() => {
+      dispatch(Actions.removeBoardReaction(id));
+    }, delay);
+  };
 
   const onClickReaction = (e: React.MouseEvent<HTMLButtonElement>, reaction: ReactionType) => {
     e.stopPropagation();
     const id = uniqueId();
-    document.dispatchEvent(new CustomEvent<BoardReactionEventType>("BoardReactionEvent", {detail: {id, reactionType: reaction, user: me!}}));
+    const boardReaction: BoardReactionEventType = {id, reactionType: reaction, user: me!};
+    dispatch(Actions.addBoardReaction(boardReaction));
+    removeAfterDelay(id, REMOVAL_DELAY_MS);
   };
 
   return (
