8a4fca56749e7fa2d43e93fc20fdfd1a6b0e06cf
Jakob Schwehn <schwehn42@gmail.com>
2023-09-11 12:40:26 +0200

    feat: use hotkeys 1-5 to make a board reaction

--- src/components/BoardReactionMenu/BoardReactionMenu.tsx
+++ src/components/BoardReactionMenu/BoardReactionMenu.tsx
@@ -3,20 +3,26 @@ import {ReactComponent as CloseIcon} from "assets/icon-close.svg";
 import {BoardReactionImageMap, ReactionType} from "types/reaction";
 import {Actions} from "store/action";
 import {useDispatch} from "react-redux";
+import {useHotkeys} from "react-hotkeys-hook";
 import "./BoardReactionMenu.scss";
 
 export const BoardReactionMenu = () => {
   const dispatch = useDispatch();
 
-  const onClickReaction = (e: React.MouseEvent<HTMLButtonElement>, reaction: ReactionType) => {
+  const boardReactions = [...BoardReactionImageMap];
+
+  const onClickReaction = (e: React.MouseEvent<HTMLButtonElement> | KeyboardEvent, reaction: ReactionType) => {
     e.stopPropagation();
     dispatch(Actions.addBoardReaction(reaction));
   };
 
+  // hotkey is converted to a number which is then used as the index for the reaction type
+  useHotkeys(["1", "2", "3", "4", "5"], (e, k) => onClickReaction(e, boardReactions[+k.keys![0]][0]));
+
   return (
     <div className="board-reactions__root">
       <div className="board-reactions__container">
-        {[...BoardReactionImageMap].map(([reactionType, emoji]) => (
+        {boardReactions.map(([reactionType, emoji]) => (
           <button key={reactionType} className="board-reactions__item board-reactions__reaction" onClick={(e) => onClickReaction(e, reactionType)}>
             <span>{emoji}</span>
           </button>
