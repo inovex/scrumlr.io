619ebd590b5d1afee5b1a83fb1d583ee145ce1d8
Jakob Schwehn <schwehn42@gmail.com>
2023-09-12 15:20:00 +0200

    fix: note dialog header note author list container size

--- src/components/NoteDialogComponents/NoteDialogNoteComponents/NoteDialogNoteHeader.tsx
+++ src/components/NoteDialogComponents/NoteDialogNoteComponents/NoteDialogNoteHeader.tsx
@@ -22,7 +22,9 @@ export const NoteDialogNoteHeader = (props: NoteDialogNoteHeaderProps) => {
   const author = participants.find((p) => p?.user.id === props.authorId)!;
   return (
     <div className="note-dialog-note-header__root">
-      <NoteAuthorList authors={[author]} showAuthors={props.showAuthors} viewer={props.viewer} />
+      <div className="note-dialog-note-header__author-list-container">
+        <NoteAuthorList authors={[author]} showAuthors={props.showAuthors} viewer={props.viewer} />
+      </div>
       <Votes {...props} className="note-dialog__note-votes" />
     </div>
   );
