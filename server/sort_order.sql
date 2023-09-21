8ce5f90a3d4e5a3036babfa80dc709237b7153ce
bitionaire <johann.boehler@scrumlr.io>
2023-09-21 20:57:58 +0200

    Added few queries related to notes

--- server/sort_order.sql
+++ server/sort_order.sql
@@ -6,7 +6,7 @@ ALTER TABLE columns ADD notes_order uuid ARRAY;
 ALTER TABLE notes ADD stack_order uuid ARRAY;
 
 -- MIGRATE columns
-UPDATE boards AS b SET columns_order=(SELECT array_agg(id::uuid ORDER BY index ASC) FROM columns WHERE board=b.id) WHERE id=b.id;
+UPDATE boards AS b SET columns_order=(SELECT array_agg(id::uuid ORDER BY index) FROM columns WHERE board=b.id) WHERE id=b.id;
 
 -- MIGRATE notes
 UPDATE columns AS c SET notes_order=(SELECT array_agg(id::uuid ORDER BY rank DESC) FROM notes WHERE "column"=c.id AND stack IS null) WHERE id=c.id;
@@ -16,9 +16,9 @@ UPDATE notes AS n SET stack_order=(SELECT array_agg(id::uuid ORDER BY rank DESC)
 
 -- INSERT COLUMN AT THE END
 WITH insert_column AS (
-  INSERT INTO columns (board,name,color,visible) VALUES ('faa7a927-23e3-4072-b75c-8843a3a3fcd4', 'Test', 'backlog-blue', true) RETURNING *
+  INSERT INTO columns (board,name,color,visible) VALUES ('${board_id}', 'Test', 'backlog-blue', true) RETURNING *
 )
-UPDATE boards SET columns_order=array_append(boards.columns_order, (SElECT id FROM insert_column));
+UPDATE boards SET columns_order=array_append(boards.columns_order, (SElECT id FROM insert_column)) WHERE id=(SELECT board FROM insert_column);
 
 -- INSERT COLUMN ANYWHERE
 WITH insert_column AS (
@@ -42,22 +42,37 @@ UPDATE boards SET columns_order=array_remove(boards.columns_order, (SELECT id FR
 
 
 -- INSERT NOTE
--- TODO
+WITH insert_note AS (
+  INSERT INTO notes (author, board, "column", text) VALUES ('b2459d9e-daad-433b-b323-eb834debea55', 'faa7a927-23e3-4072-b75c-8843a3a3fcd4', '4e4f730a-3af6-4fe4-afc6-431963abf0e0', 'TestPaste') RETURNING *
+)
+UPDATE columns AS c SET notes_order=array_append(c.notes_order, (SELECT id FROM insert_note)) WHERE id=(SELECT "column" FROM insert_note) AND board=(SELECT board FROM insert_note);
 
 -- INSERT NOTE WITHIN STACK
--- TODO
+WITH insert_note AS (
+  INSERT INTO notes (author, board, "column", stack, text) VALUES ('b2459d9e-daad-433b-b323-eb834debea55', 'faa7a927-23e3-4072-b75c-8843a3a3fcd4', '4e4f730a-3af6-4fe4-afc6-431963abf0e0', 'd7e13a47-d9a7-46bb-b646-96391b080e15', 'TestPasteStack') RETURNING *
+)
+UPDATE notes AS n SET stack_order=array_append(n.stack_order, (SELECT id FROM insert_note)) WHERE id=(SELECT stack FROM insert_note);
 
 -- MOVE NOTE WITHIN COLUMN
--- TODO
+BEGIN;
+UPDATE columns SET notes_order=array_remove(columns.notes_order, '${note_id}') WHERE id='${column_id}';
+UPDATE columns AS c SET notes_order=c.notes_order[:1]||'${note_id}'||c.notes_order[2:] WHERE c.id='${column_id}';
+COMMIT;
 
 -- MOVE NOTE TO ANOTHER COLUMN
--- TODO
+BEGIN;
+UPDATE columns SET notes_order=array_remove(columns.notes_order, '${note_id}') WHERE id='${old_column_id}';
+UPDATE columns AS c SET notes_order=c.notes_order[:1]||'${note_id}'||c.notes_order[2:] WHERE c.id='${new_column_id}';
+COMMIT;
 
 -- MOVE NOTE TO STACK
 -- TODO
 
 -- MOVE NOTE WITHIN STACK
--- TODO
+BEGIN;
+UPDATE notes SET stack_order=array_remove(notes.stack_order, '${note_id}') WHERE id='${stack_parent_id}';
+UPDATE notes AS n SET stack_order=n.stack_order[:1]||'${note_id}'||n.stack_order[2:] WHERE n.id='${stack_parent_id}';
+COMMIT;
 
 -- CALCULATE NEW ORDER BY VOTING
 -- TODO
