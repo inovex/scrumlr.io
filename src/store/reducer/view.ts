a78aa3fd7f6e4540b3bbac6df5da7aec8eceefe6
Jakob Schwehn <schwehn42@gmail.com>
2023-09-22 13:33:30 +0200

    save setting in local storage

--- src/store/reducer/view.ts
+++ src/store/reducer/view.ts
@@ -1,7 +1,7 @@
 import {ViewState} from "types/view";
 import {Action, ReduxAction} from "store/action";
 import {getFromStorage} from "utils/storage";
-import {HOTKEY_NOTIFICATIONS_ENABLE_STORAGE_KEY} from "constants/storage";
+import {BOARD_REACTIONS_ENABLE_STORAGE_KEY, HOTKEY_NOTIFICATIONS_ENABLE_STORAGE_KEY} from "constants/storage";
 
 const INITIAL_VIEW_STATE: ViewState = {
   moderating: false,
@@ -11,7 +11,7 @@ const INITIAL_VIEW_STATE: ViewState = {
   hotkeysAreActive: true,
   noteFocused: false,
   hotkeyNotificationsEnabled: typeof window !== "undefined" && getFromStorage(HOTKEY_NOTIFICATIONS_ENABLE_STORAGE_KEY) !== "false",
-  showBoardReactions: true,
+  showBoardReactions: typeof window !== "undefined" && getFromStorage(BOARD_REACTIONS_ENABLE_STORAGE_KEY) !== "false",
 };
 
 // eslint-disable-next-line @typescript-eslint/default-param-last
