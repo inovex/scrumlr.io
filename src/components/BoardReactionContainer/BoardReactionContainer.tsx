0b3026e03cc6fe4bc5f18a4cb1625f22421cc928
Jakob Schwehn <schwehn42@gmail.com>
2023-09-22 12:02:22 +0200

    feat(BoardReactionContainer): don't display board reaction if disabled

--- src/components/BoardReactionContainer/BoardReactionContainer.tsx
+++ src/components/BoardReactionContainer/BoardReactionContainer.tsx
@@ -4,12 +4,7 @@ import "./BoardReactionContainer.scss";
 
 export const BoardReactionContainer = () => {
   const boardReactions = useAppSelector((state) => state.boardReactions);
+  const showBoardReactions = useAppSelector((state) => state.view.showBoardReactions);
 
-  return (
-    <div className="board-reaction-container__root">
-      {boardReactions.map((r) => (
-        <BoardReaction key={r.id} reaction={r} />
-      ))}
-    </div>
-  );
+  return <div className="board-reaction-container__root">{showBoardReactions && boardReactions.map((r) => <BoardReaction key={r.id} reaction={r} />)}</div>;
 };
