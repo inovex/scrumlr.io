0329996dc57809752031cd1d536c46949b9aba13
bitionaire <johann.boehler@googlemail.com>
2023-10-02 12:34:18 +0200

    Refactored column creation & deletion

--- server/src/database/columns.go
+++ server/src/database/columns.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/uptrace/bun"
-	"math"
 	"scrumlr.io/server/common"
 	"scrumlr.io/server/database/types"
 )
@@ -45,30 +44,33 @@ type ColumnUpdate struct {
 // CreateColumn creates a new column. The index will be set to the highest available or the specified one. All other
 // indices will be adopted (increased by 1) to the new index.
 func (d *Database) CreateColumn(column ColumnInsert) (Column, error) {
-	maxIndexSelect := d.db.NewSelect().Model((*Column)(nil)).ColumnExpr("COUNT(*) as index").Where("board = ?", column.Board)
 
-	newIndex := math.MaxInt
-	if column.Index != nil {
-		if *column.Index < 0 {
-			newIndex = 0
-		} else {
-			newIndex = *column.Index
-		}
-	}
+	// insert column
+	insertColumn := d.db.NewInsert().
+		Model(&column).
+		Returning("*")
 
-	query := d.db.NewInsert()
-	if column.Index != nil {
-		indexUpdate := d.db.NewUpdate().Model((*Column)(nil)).Set("index = index+1").Where("index >= ?", newIndex).Where("board = ?", column.Board)
-		query = query.With("indexUpdate", indexUpdate)
-	}
+	// insert column into the order
+	updateBoardColumns := d.db.NewUpdate().
+		Model((*BoardColumns)(nil)).
+		ModelTableExpr("board_columns AS b").
+		Set("columns=b.columns[:?]||(SELECT id FROM \"insertColumn\")||b.columns[?:]", *column.Index-2, *column.Index-1).
+		Where("\"board\" = ?", column.Board).
+		Returning("columns")
+
+	// select the results of the insertion as return result
+	selectColumn := d.db.NewSelect().
+		Model((*Column)(nil)).
+		Table("insertColumn").
+		ColumnExpr("*")
 
 	var c Column
-	_, err := query.
-		With("maxIndexSelect", maxIndexSelect).
-		Model(&column).
-		Value("index", fmt.Sprintf("LEAST(coalesce((SELECT index FROM \"maxIndexSelect\"),0), %d)", newIndex)).
-		Returning("*").
-		Exec(common.ContextWithValues(context.Background(), "Database", d, "Board", column.Board), &c)
+	err := selectColumn.
+		With("insertColumn", insertColumn).
+		With("updateBoardColumns", updateBoardColumns).
+		Scan(context.Background(), &c)
+
+	// FIXME ORDER: check effect on realtime server
 
 	return c, err
 }
@@ -116,24 +118,23 @@ func (d *Database) UpdateColumn(column ColumnUpdate) (Column, error) {
 
 // DeleteColumn deletes a column and adapts all indices of the other columns.
 func (d *Database) DeleteColumn(board, column, user uuid.UUID) error {
-	var columns []Column
-	selectPreviousIndex := d.db.NewSelect().Model((*Column)(nil)).Column("index", "board").Where("id = ?", column)
-	indexUpdate := d.db.NewUpdate().
-		With("selectPreviousIndex", selectPreviousIndex).
-		Model((*Column)(nil)).Set("index = index-1").
-		Where("board = (SELECT board from \"selectPreviousIndex\")").
-		Where("index >= (SELECT index from \"selectPreviousIndex\")")
-	boardUpdate := d.db.NewUpdate().
-		Model((*Board)(nil)).
-		Set("shared_note = null").
-		Where("id = ? AND (SELECT \"column\" FROM notes WHERE id = (SELECT shared_note FROM boards WHERE id = ?)) = ?", board, board, column)
-	_, err := d.db.NewDelete().
-		With("boardUpdate", boardUpdate).
-		With("indexUpdate", indexUpdate).
+
+	deleteColumn := d.db.NewDelete().
 		Model((*Column)(nil)).
 		Where("id = ?", column).
-		Returning("*").
-		Exec(common.ContextWithValues(context.Background(), "Database", d, "Board", board, "Column", column, "User", user, "Result", &columns), &columns)
+		Where("board = ?", board)
+
+	updateColumnOrder := d.db.NewUpdate().
+		Model((*BoardColumns)(nil)).
+		ModelTableExpr("board_columns AS c").
+		Set("columns=array_remove(c.columns, ?)", column).
+		Where("board = ?", board)
+
+	_, err := updateColumnOrder.
+		With("deleteColumn", deleteColumn).
+		Exec(context.Background())
+
+	// FIXME ORDER: check effect on realtime server
 
 	return err
 }
