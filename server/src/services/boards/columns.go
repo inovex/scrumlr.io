3925d8046f4ed5dda52b34caa1efa4c8c3551926
bitionaire <johann.boehler@scrumlr.io>
2023-10-02 19:04:17 +0200

    Fixed notifications and logging

--- server/src/services/boards/columns.go
+++ server/src/services/boards/columns.go
@@ -18,17 +18,21 @@ import (
 func (s *BoardService) CreateColumn(_ context.Context, body dto.ColumnRequest) (*dto.WrappedColumn, error) {
 	result, err := s.database.CreateColumn(database.ColumnInsert{Board: body.Board, Name: body.Name, Color: body.Color, Visible: body.Visible}, body.Index)
 	var column *dto.WrappedColumn
-	if err != nil {
+	if err == nil {
 		column = new(dto.WrappedColumn).From(result)
 		defer s.CreatedColumn(body.Board, *column)
+	} else {
+		logger.Get().Warnw("failed to create column", "request", body, "error", err)
 	}
 	return column, err
 }
 
 func (s *BoardService) DeleteColumn(_ context.Context, board, column uuid.UUID) error {
 	err := s.database.DeleteColumn(board, column)
-	if err != nil {
+	if err == nil {
 		defer s.DeletedColumn(board, column)
+	} else {
+		logger.Get().Warnw("failed to delete column", "board", board, "column", column, "error", err)
 	}
 	return err
 }
@@ -36,9 +40,11 @@ func (s *BoardService) DeleteColumn(_ context.Context, board, column uuid.UUID) 
 func (s *BoardService) UpdateColumn(_ context.Context, body dto.ColumnUpdateRequest) (*dto.WrappedColumn, error) {
 	result, err := s.database.UpdateColumn(database.ColumnUpdate{ID: body.ID, Board: body.Board, Name: body.Name, Color: body.Color, Visible: body.Visible}, body.Index)
 	var column *dto.WrappedColumn
-	if err != nil {
+	if err == nil {
 		column = new(dto.WrappedColumn).From(result)
 		defer s.UpdatedColumn(body.Board, *column)
+	} else {
+		logger.Get().Warnw("failed to update column", "request", body, "error", err)
 	}
 	return column, err
 }
