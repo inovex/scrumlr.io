ca99e6b1449b2ef06f4d1eed23f970f820c7e056
Jakob Schwehn <schwehn42@gmail.com>
2023-10-04 16:09:42 +0200

    improvement: display self with 'me'

--- src/components/BoardReaction/BoardReaction.tsx
+++ src/components/BoardReaction/BoardReaction.tsx
@@ -3,6 +3,7 @@ import {memo} from "react";
 import {getRandomNumberInRange} from "utils/random";
 import {useAppSelector} from "store";
 import classNames from "classnames";
+import {useTranslation} from "react-i18next";
 import "./BoardReaction.scss";
 
 type BoardReactionProps = {
@@ -12,6 +13,7 @@ type BoardReactionProps = {
 // component that displays a board reaction (emoji + name)
 // memo prevents re-rendering even when parent state changes
 export const BoardReaction = memo((props: BoardReactionProps) => {
+  const {t} = useTranslation();
   const emoji = BoardReactionImageMap.get(props.reaction.reactionType);
   const me = useAppSelector((state) => state.participants!.self);
   const others = useAppSelector((state) => state.participants!.others);
@@ -19,13 +21,14 @@ export const BoardReaction = memo((props: BoardReactionProps) => {
   const reactionUser = all.find((p) => p.user.id === props.reaction.user)!;
   const {name} = reactionUser.user;
   const reactedSelf = reactionUser.user.id === me.user.id;
+  const displayName = reactedSelf ? t("BoardReaction.me") : name;
 
   const leftOffset = getRandomNumberInRange(5, 85);
 
   return (
     <div className="board-reaction__root" style={{left: `${leftOffset}vw`}}>
       <div className="board-reaction__emoji-container">{emoji}</div>
-      <div className={classNames("board-reaction__name-container", {"board-reaction__name-container--self": reactedSelf})}>{name}</div>
+      <div className={classNames("board-reaction__name-container", {"board-reaction__name-container--self": reactedSelf})}>{displayName}</div>
     </div>
   );
 });
