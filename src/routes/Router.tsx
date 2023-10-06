174cc028e8d9f5589ed5665ba5defbbfd7df82af
Lennart Krauch <lennart.krauch@inovex.de>
2023-10-06 11:16:38 +0200

    migrated BrowserRouter to createBrowserRouter
    Signed-off-by: Lennart Krauch <lennart.krauch@inovex.de>

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes} from "react-router-dom";
+import {Routes, createBrowserRouter, RouterProvider} from "react-router-dom";
 import {LoginBoard} from "routes/LoginBoard";
 import {NewBoard} from "routes/NewBoard";
 import {BoardGuard} from "routes/Board";
@@ -18,57 +18,59 @@ import {ProfileSettings} from "components/SettingsDialog/ProfileSettings";
 import {Homepage} from "./Homepage";
 import {Legal} from "./Legal";
 import {StackView} from "./StackView";
-import RouteChangeObserver from "./RouteChangeObserver";
+// import RouteChangeObserver from "./RouteChangeObserver";
+// import path from "path";
 
-const Router = () => (
-  <BrowserRouter>
-    <RouteChangeObserver />
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/legal/termsAndConditions" element={<Legal document="termsAndConditions" />} />
-      <Route path="/legal/privacyPolicy" element={<Legal document="privacyPolicy" />} />
-      <Route path="/legal/cookiePolicy" element={<Legal document="cookiePolicy" />} />
-      <Route
-        path="/new"
-        element={
-          <RequireAuthentication>
-            <NewBoard />
-          </RequireAuthentication>
-        }
-      />
-      <Route path="/login" element={<LoginBoard />} />
-      <Route
-        path="/board/:boardId/print"
-        element={
-          <RequireAuthentication>
-            <BoardGuard printViewEnabled />
-          </RequireAuthentication>
-        }
-      />
-      <Route
-        path="/board/:boardId"
-        element={
-          <RequireAuthentication>
-            <BoardGuard printViewEnabled={false} />
-          </RequireAuthentication>
-        }
-      >
-        <Route path="settings" element={<SettingsDialog />}>
-          <Route path="board" element={<BoardSettings />} />
-          <Route path="participants" element={<Participants />} />
-          <Route path="appearance" element={<Appearance />} />
-          <Route path="share" element={<ShareSession />} />
-          <Route path="export" element={<ExportBoard />} />
-          <Route path="feedback" element={<Feedback />} />
-          <Route path="profile" element={<ProfileSettings />} />
-        </Route>
-        <Route path="voting" element={<VotingDialog />} />
-        <Route path="timer" element={<TimerDialog />} />
-        <Route path="note/:noteId/stack" element={<StackView />} />
+const Root = () => (
+  <Routes>
+    <Route path="/" element={<Homepage />} />
+    <Route path="/legal/termsAndConditions" element={<Legal document="termsAndConditions" />} />
+    <Route path="/legal/privacyPolicy" element={<Legal document="privacyPolicy" />} />
+    <Route path="/legal/cookiePolicy" element={<Legal document="cookiePolicy" />} />
+    <Route
+      path="/new"
+      element={
+        <RequireAuthentication>
+          <NewBoard />
+        </RequireAuthentication>
+      }
+    />
+    <Route path="/login" element={<LoginBoard />} />
+    <Route
+      path="/board/:boardId/print"
+      element={
+        <RequireAuthentication>
+          <BoardGuard printViewEnabled />
+        </RequireAuthentication>
+      }
+    />
+    <Route
+      path="/board/:boardId"
+      element={
+        <RequireAuthentication>
+          <BoardGuard printViewEnabled={false} />
+        </RequireAuthentication>
+      }
+    >
+      <Route path="settings" element={<SettingsDialog />}>
+        <Route path="board" element={<BoardSettings />} />
+        <Route path="participants" element={<Participants />} />
+        <Route path="appearance" element={<Appearance />} />
+        <Route path="share" element={<ShareSession />} />
+        <Route path="export" element={<ExportBoard />} />
+        <Route path="feedback" element={<Feedback />} />
+        <Route path="profile" element={<ProfileSettings />} />
       </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
+      <Route path="voting" element={<VotingDialog />} />
+      <Route path="timer" element={<TimerDialog />} />
+      <Route path="note/:noteId/stack" element={<StackView />} />
+    </Route>
+    <Route path="*" element={<NotFound />} />
+  </Routes>
 );
+
+const router = createBrowserRouter([{path: "*", Component: Root}]);
+
+const Router = () => <RouterProvider router={router} />;
 
 export default Router;
