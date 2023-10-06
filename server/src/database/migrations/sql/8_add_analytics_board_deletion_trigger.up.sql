49d90657cd4e0db56389eafb467457e3287649d3
Benedict Homuth <bhomuth@inovex.de>
2023-10-06 15:48:14 +0200

    implemented review feedback

--- server/src/database/migrations/sql/8_add_analytics_board_deletion_trigger.up.sql
+++ server/src/database/migrations/sql/8_add_analytics_board_deletion_trigger.up.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS deleted_boards(
     first_note_created timestamptz,
     last_note_created timestamptz,
     avg_chars_per_note int,
-    total_users int,
-    total_admins int,
+    total_participants int,
+    total_moderators int,
     total_votes int, 
     total_votings int, 
     created_at timestamptz,
@@ -26,8 +26,8 @@ CREATE OR REPLACE FUNCTION record_deleted_board_statistics()
         first_note_created timestamptz;
         last_note_created timestamptz;
         avg_chars_per_note int;
-        total_users int;
-        total_admins int;
+        total_participants int;
+        total_moderators int;
         total_votes int;
         total_votings int;
 
@@ -38,19 +38,19 @@ CREATE OR REPLACE FUNCTION record_deleted_board_statistics()
 
             SELECT count(id) INTO total_notes FROM notes WHERE board = OLD.id;
             SELECT count(n.id) INTO hidden_notes FROM notes n INNER JOIN columns c ON n."column" = c.id WHERE c."visible" = false AND n."board" = OLD.id;
-            SELECT created_at INTO first_note_created FROM notes WHERE board = OLD.id ORDER BY created_at LIMIT 1;
-            SELECT created_at INTO last_note_created FROM notes WHERE board = OLD.id ORDER BY created_at DESC LIMIT 1;
+            SELECT min(created_at) INTO first_note_created FROM notes WHERE board = OLD.id;
+            SELECT max(created_at) INTO last_note_created FROM notes WHERE board = OLD.id;
             SELECT AVG(LENGTH(text)) INTO avg_chars_per_note FROM notes WHERE board = OLD.id;
 
-            SELECT count(user) INTO total_users FROM board_sessions WHERE board = OLD.id;
-            SELECT count(user) INTO total_admins FROM board_sessions s WHERE s.board = OLD.id AND s.role = 'OWNER' OR s.role = 'MODERATOR';
+            SELECT count(user) INTO total_participants FROM board_sessions s WHERE board = OLD.id AND s.role = 'PARTICIPANT';
+            SELECT count(user) INTO total_moderators FROM board_sessions s WHERE s.board = OLD.id AND s.role = 'OWNER' OR s.role = 'MODERATOR';
 
             SELECT count(board) INTO total_votes FROM votes WHERE board = OLD.id;
             SELECT count(id) INTO total_votings FROM votings WHERE board = OLD.id;
 
             --  Insert data
-            INSERT INTO deleted_boards (id, access_policy, total_columns, hidden_columns, total_notes, hidden_notes, avg_chars_per_note, first_note_created, last_note_created, total_users, total_admins, total_votes, total_votings, created_at) 
-                VALUES (OLD.id, OLD.access_policy, total_columns, hidden_columns, total_notes, hidden_notes, avg_chars_per_note, first_note_created, last_note_created, total_users, total_admins, total_votes, total_votings, OLD.created_at);
+            INSERT INTO deleted_boards (id, access_policy, total_columns, hidden_columns, total_notes, hidden_notes, avg_chars_per_note, first_note_created, last_note_created, total_participants, total_moderators, total_votes, total_votings, created_at) 
+                VALUES (OLD.id, OLD.access_policy, total_columns, hidden_columns, total_notes, hidden_notes, avg_chars_per_note, first_note_created, last_note_created, total_participants, total_moderators, total_votes, total_votings, OLD.created_at);
             RETURN OLD;
         END;
     $$ LANGUAGE plpgsql;
