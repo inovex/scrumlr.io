c85fb733a366bed018450c65c8a28619c906d894
Benedict Homuth <bhomuth@inovex.de>
2023-10-10 18:31:08 +0200

    removed board observer

--- server/src/services/boards/boards.go
+++ server/src/services/boards/boards.go
@@ -27,7 +27,6 @@ func NewBoardService(db *database.Database, rt *realtime.Broker) services.Boards
 	b := new(BoardService)
 	b.database = db
 	b.realtime = rt
-	b.database.AttachObserver((database.BoardObserver)(b))
 	return b
 }
 
@@ -93,7 +92,12 @@ func (s *BoardService) FullBoard(ctx context.Context, boardID uuid.UUID) (*dto.B
 }
 
 func (s *BoardService) Delete(_ context.Context, id uuid.UUID) error {
-	return s.database.DeleteBoard(id)
+	err := s.database.DeleteBoard(id)
+	if err != nil {
+		return err
+	}
+	go s.DeletedBoard(id)
+	return err
 }
 
 func (s *BoardService) Update(ctx context.Context, body dto.BoardUpdateRequest) (*dto.Board, error) {
@@ -104,7 +108,7 @@ func (s *BoardService) Update(ctx context.Context, body dto.BoardUpdateRequest) 
 		ShowAuthors:           body.ShowAuthors,
 		ShowNotesOfOtherUsers: body.ShowNotesOfOtherUsers,
 		AllowStacking:         body.AllowStacking,
-    TimerStart:            body.TimerStart,
+		TimerStart:            body.TimerStart,
 		TimerEnd:              body.TimerEnd,
 		SharedNote:            body.SharedNote,
 	}
@@ -131,34 +135,37 @@ func (s *BoardService) Update(ctx context.Context, body dto.BoardUpdateRequest) 
 	if err != nil {
 		return nil, err
 	}
+	go s.UpdatedBoard(board)
 	return new(dto.Board).From(board), err
 }
 
 func (s *BoardService) SetTimer(_ context.Context, id uuid.UUID, minutes uint8) (*dto.Board, error) {
-  timerStart := time.Now().Local();
+	timerStart := time.Now().Local()
 	timerEnd := timerStart.Add(time.Minute * time.Duration(minutes))
 	update := database.BoardTimerUpdate{
-		ID:       id,
-    TimerStart: &timerStart,
-		TimerEnd: &timerEnd,
+		ID:         id,
+		TimerStart: &timerStart,
+		TimerEnd:   &timerEnd,
 	}
 	board, err := s.database.UpdateBoardTimer(update)
 	if err != nil {
 		return nil, err
 	}
+	go s.UpdatedBoardTimer(board)
 	return new(dto.Board).From(board), err
 }
 
 func (s *BoardService) DeleteTimer(_ context.Context, id uuid.UUID) (*dto.Board, error) {
 	update := database.BoardTimerUpdate{
-		ID:       id,
-    TimerStart: nil,
-		TimerEnd: nil,
+		ID:         id,
+		TimerStart: nil,
+		TimerEnd:   nil,
 	}
 	board, err := s.database.UpdateBoardTimer(update)
 	if err != nil {
 		return nil, err
 	}
+	go s.UpdatedBoardTimer(board)
 	return new(dto.Board).From(board), err
 }
 
