efa1f035070c822291c8b49e9e7ea9a94fe13597
Jakob Schwehn <schwehn42@gmail.com>
2023-10-25 13:13:18 +0200

    linter fix

--- src/store/reducer/boardReaction.ts
+++ src/store/reducer/boardReaction.ts
@@ -1,6 +1,7 @@
 import {Action, ReduxAction} from "store/action";
 import {BoardReactionState} from "types/boardReaction";
 
+// eslint-disable-next-line @typescript-eslint/default-param-last
 export const boardReactionReducer = (state: BoardReactionState = [], action: ReduxAction): BoardReactionState => {
   switch (action.type) {
     case Action.AddedBoardReaction:
