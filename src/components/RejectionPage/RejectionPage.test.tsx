8bb7f7c69c1d24c6b63704fe2e10906bfa432ee1
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-01-10 14:38:49 +0100

    Add snapshot test

--- src/components/RejectionPage/RejectionPage.test.tsx
+++ src/components/RejectionPage/RejectionPage.test.tsx
@@ -1,5 +1,11 @@
+import {render} from "testUtils";
+import {RejectionPage} from "./RejectionPage";
+
 describe("RejectionPage", () => {
-  it.todo("should match snapshot");
+  it("should match snapshot", () => {
+    const {container} = render(<RejectionPage />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
 
   it.todo("should redirect to homepage on button click");
 });
