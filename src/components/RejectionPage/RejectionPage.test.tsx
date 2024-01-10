d5b12579094465b8304e2d5f710ad6abedbc3b46
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-01-10 14:55:37 +0100

    Test redirect to homepage from rejection page

--- src/components/RejectionPage/RejectionPage.test.tsx
+++ src/components/RejectionPage/RejectionPage.test.tsx
@@ -1,11 +1,30 @@
-import {render} from "testUtils";
+import {screen, fireEvent, render} from "@testing-library/react";
+import {BrowserRouter, MemoryRouter, Routes, Route} from "react-router-dom";
 import {RejectionPage} from "./RejectionPage";
 
 describe("RejectionPage", () => {
   it("should match snapshot", () => {
-    const {container} = render(<RejectionPage />);
+    const {container} = render(
+      <BrowserRouter>
+        <RejectionPage />
+      </BrowserRouter>
+    );
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  it.todo("should redirect to homepage on button click");
+  it("should redirect to homepage on button click", () => {
+    const {getByText} = render(
+      <MemoryRouter initialEntries={["/board/test-uuid"]}>
+        <Routes>
+          <Route path="/board/test-uuid" element={<RejectionPage />} />
+          <Route path="/" element={<div>Homepage</div>} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const button = getByText("RejectionPage.button");
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+    expect(screen.getByText("Homepage")).toBeInTheDocument();
+  });
 });
