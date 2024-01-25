536fac069037a83e9a61e64fa67fd042d84d0cd8
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-01-25 11:45:45 +0100

    Test

--- src/index.tsx
+++ src/index.tsx
@@ -64,4 +64,15 @@ root.render(
 );
 store.dispatch(Actions.initApplication());
 
-unregister();
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.unregister();
+
+    if (caches) {
+      // Service worker cache should be cleared with caches.delete()
+      caches.keys().then(async (names) => {
+        await Promise.all(names.map((name) => caches.delete(name)));
+      });
+    }
+  });
+}
