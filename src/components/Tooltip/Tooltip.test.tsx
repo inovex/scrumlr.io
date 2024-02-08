622a4b0fbff6661ad81c50d7f58b3c3a21322e56
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-02-08 11:36:42 +0100

    Add some tests to test both properties and data attributes

--- src/components/Tooltip/Tooltip.test.tsx
+++ src/components/Tooltip/Tooltip.test.tsx
@@ -12,7 +12,7 @@ describe("Tooltip", () => {
     expect(queryByText("Hello world")).toBeNull();
   });
 
-  it("should be visible on mouseenter", async () => {
+  it("should be visible on mouseenter using props", async () => {
     render(
       <div>
         <button id="test-button">Test Button</button>
@@ -24,11 +24,39 @@ describe("Tooltip", () => {
     expect(await screen.findByText("Hello world")).toBeInTheDocument();
   });
 
-  it("should be visible on focus", async () => {
+  it("should be visible on focus using props", async () => {
     render(
       <>
         <button id="test-button">Test Button</button>
         <Tooltip anchorSelect="#test-button" content="Hello world" />
+      </>
+    );
+    const button = screen.getByText("Test Button");
+    fireEvent.focus(button);
+    expect(await screen.findByText("Hello world")).toBeInTheDocument();
+  });
+
+  it("should be visible on mouseenter using data attributes", async () => {
+    render(
+      <div>
+        <button data-tooltip-id="test-tooltip" data-tooltip-content="Hello world">
+          Test Button
+        </button>
+        <Tooltip id="test-tooltip" />
+      </div>
+    );
+    const button = screen.getByText("Test Button");
+    fireEvent.mouseEnter(button);
+    expect(await screen.findByText("Hello world")).toBeInTheDocument();
+  });
+
+  it("should be visible on focus using data attributes", async () => {
+    render(
+      <>
+        <button data-tooltip-id="test-tooltip" data-tooltip-content="Hello world">
+          Test Button
+        </button>
+        <Tooltip id="test-tooltip" />
       </>
     );
     const button = screen.getByText("Test Button");
