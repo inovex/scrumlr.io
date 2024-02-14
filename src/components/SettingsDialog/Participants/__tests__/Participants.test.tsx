914937972a75ed525b5e71bdca565876f1ab9162
Sebastian Schwarzer <sschwarzer@outlook.de>
2024-02-14 13:18:13 +0100

    Test whether ban button is available

--- src/components/SettingsDialog/Participants/__tests__/Participants.test.tsx
+++ src/components/SettingsDialog/Participants/__tests__/Participants.test.tsx
@@ -1,8 +1,8 @@
 import {Provider} from "react-redux";
 import {MockStoreEnhanced} from "redux-mock-store";
 import {render} from "testUtils";
-import getTestStore from "utils/test/getTestStore";
 import {ApplicationState} from "types";
+import getTestStore from "utils/test/getTestStore";
 import {Participants} from "../Participants";
 
 const createParticipants = (store: MockStoreEnhanced<ApplicationState>) => (
@@ -15,5 +15,19 @@ describe("Participants", () => {
   test("should render filter and participants correctly", () => {
     const {container} = render(createParticipants(getTestStore()));
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  describe("Ban Feature", () => {
+    test("ban button not available on self", () => {
+      const {container} = render(createParticipants(getTestStore()));
+      const participants = container.getElementsByClassName("participants__list")[0].childNodes;
+      expect(participants[0].lastChild).not.toHaveClass("participant__kick-icon");
+    });
+
+    test("ban button available on moderator", () => {
+      const {container} = render(createParticipants(getTestStore()));
+      const participants = container.getElementsByClassName("participants__list")[0].childNodes;
+      expect(participants[1].lastChild).toHaveClass("participant__kick-icon");
+    });
   });
 });
