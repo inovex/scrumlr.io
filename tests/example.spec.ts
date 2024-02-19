1fe1524bff12a5ddba2455bc9740868578071820
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-02-19 14:25:52 +0100

    Changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("test", async ({page}) => {
+  await page.goto("localhost:3000");
+  await expect(page).toHaveTitle("scrumlr.io - Online collaboration");
 });
