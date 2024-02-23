7bcc0db430cf91bfeff71565fadf437cf4272982
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-02-23 09:30:52 +0100

    Replace localhost with root slash

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test("test", async ({page}) => {
-  await page.goto("localhost:3000");
+  await page.goto("/");
   await expect(page).toHaveTitle("scrumlr.io - Online collaboration");
 });
