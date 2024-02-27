d99428c7e553ec5be32c7b4bfd2139c480539819
Sebastian Schwarzer <44020029+CronJorian@users.noreply.github.com>
2024-02-27 16:23:44 +0100

    feat: (Un)ban participants (Frontend Implementation) (#3864)
    Co-authored-by: Jakob Schwehn <schwehn42@gmail.com>
    Co-authored-by: Sebastian Schwarzer <sebastian.schwarzer@inovex.de>

--- src/components/SettingsDialog/Participants/__tests__/Participants.test.tsx
+++ src/components/SettingsDialog/Participants/__tests__/Participants.test.tsx
@@ -1,8 +1,10 @@
+import {within} from "@testing-library/react";
+import {t} from "i18next";
 import {Provider} from "react-redux";
 import {MockStoreEnhanced} from "redux-mock-store";
 import {render} from "testUtils";
-import getTestStore from "utils/test/getTestStore";
 import {ApplicationState} from "types";
+import getTestStore from "utils/test/getTestStore";
 import {Participants} from "../Participants";
 
 const createParticipants = (store: MockStoreEnhanced<ApplicationState>) => (
@@ -15,5 +17,19 @@ describe("Participants", () => {
   test("should render filter and participants correctly", () => {
     const {container} = render(createParticipants(getTestStore()));
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  describe("Ban Feature", () => {
+    test("ban button not available on self", () => {
+      const {container} = render(createParticipants(getTestStore()));
+      const participants = container.getElementsByClassName("participants__list-item");
+      expect(within(participants[0]).queryByLabelText(t("Participants.BanParticipantTooltip"))).not.toBeInTheDocument();
+    });
+
+    test("ban button available on moderator", () => {
+      const {container} = render(createParticipants(getTestStore()));
+      const participants = container.getElementsByClassName("participants__list-item");
+      expect(within(participants[1]).getByLabelText(t("Participants.BanParticipantTooltip"))).toBeInTheDocument();
+    });
   });
 });
