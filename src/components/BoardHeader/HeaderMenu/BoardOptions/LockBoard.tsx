7196dc128b638e19d6d6607a2fc9b988fe9cca99
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-03-07 17:12:32 +0100

    Set toggle value of lock board component to current value of allow editing from redux store

--- src/components/BoardHeader/HeaderMenu/BoardOptions/LockBoard.tsx
+++ src/components/BoardHeader/HeaderMenu/BoardOptions/LockBoard.tsx
@@ -2,15 +2,17 @@ import {BoardOption} from "./BoardOption";
 import {BoardOptionButton} from "./BoardOptionButton";
 import {BoardOptionToggle} from "./BoardOptionToggle";
 import "../BoardSettings/BoardSettings.scss";
+import {useAppSelector} from "store";
 
-export const LockBoard = () => 
+export const LockBoard = () => {
   // const {t} = useTranslation();
+  const allowEditing = useAppSelector((state) => state.board.data!.allowEditing);
 
-   (
+  return (
     <BoardOption>
       <BoardOptionButton label="Allow changes" onClick={() => {}}>
-        <BoardOptionToggle active />
+        <BoardOptionToggle active={allowEditing} />
       </BoardOptionButton>
     </BoardOption>
-  )
-;
+  );
+};
