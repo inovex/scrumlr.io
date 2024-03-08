b80522c92dd993c4e9959d70b6bdd48330769196
Mateo <mateo.ivankovic@inovex.de>
2024-03-08 16:14:01 +0100

    Remove unnecessary backend code for abborting a vote

--- server/src/database/types/voting_status.go
+++ server/src/database/types/voting_status.go
@@ -1,36 +1,33 @@
 package types
 
 import (
-	"encoding/json"
-	"errors"
+  "encoding/json"
+  "errors"
+  "fmt"
 )
 
 // VotingStatus is the state of a voting session and can be one of open, aborted or closed.
 type VotingStatus string
 
 const (
-	// VotingStatusOpen is the state for an open voting session, meaning that votes are allowed.
-	VotingStatusOpen VotingStatus = "OPEN"
+  // VotingStatusOpen is the state for an open voting session, meaning that votes are allowed.
+  VotingStatusOpen VotingStatus = "OPEN"
 
-	// VotingStatusAborted is the state for an aborted voting session.
-	//
-	// All votes will be deleted if a voting session is aborted.
-	VotingStatusAborted VotingStatus = "ABORTED"
-
-	// VotingStatusClosed is the state for a closed voting session.
-	//
-	// The results of the voting session are available to all participants of a board.
-	VotingStatusClosed VotingStatus = "CLOSED"
+  // VotingStatusClosed is the state for a closed voting session.
+  //
+  // The results of the voting session are available to all participants of a board.
+  VotingStatusClosed VotingStatus = "CLOSED"
 )
 
 func (votingStatus *VotingStatus) UnmarshalJSON(b []byte) error {
-	var s string
-	json.Unmarshal(b, &s)
-	unmarshalledVotingStatus := VotingStatus(s)
-	switch unmarshalledVotingStatus {
-	case VotingStatusOpen, VotingStatusAborted, VotingStatusClosed:
-		*votingStatus = unmarshalledVotingStatus
-		return nil
-	}
-	return errors.New("invalid session role")
+  var s string
+  json.Unmarshal(b, &s)
+  unmarshalledVotingStatus := VotingStatus(s)
+  fmt.Println(unmarshalledVotingStatus)
+  switch unmarshalledVotingStatus {
+  case VotingStatusOpen, VotingStatusClosed:
+    *votingStatus = unmarshalledVotingStatus
+    return nil
+  }
+  return errors.New("invalid session role")
 }
