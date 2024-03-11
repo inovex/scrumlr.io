c65dab18d6daf42e938096c9289e1477e6bf898a
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-03-11 16:27:21 +0100

    Changes

--- src/@types/i18next.d.ts
+++ src/@types/i18next.d.ts
@@ -1,11 +1,15 @@
 import "i18next";
-import translation from "../i18n/en/translation.json";
+import translationEn from "../i18n/en/translation.json";
+import translationDe from "../i18n/de/translation.json";
+
+type GermanTranslation = typeof translationDe;
+type EnglishTranslation = typeof translationEn;
 
 declare module "i18next" {
   interface CustomTypeOptions {
-    defaultNS: "translation";
     resources: {
-      translation: typeof translation;
+      translation: GermanTranslation & EnglishTranslation;
     };
+    returnNull: false;
   }
 }
