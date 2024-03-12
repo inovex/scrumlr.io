54fe658a6ffb0b18d9b1abe61563ccc400a732f1
Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>
2024-03-12 13:39:51 +0100

    localization: add type-safety (#3962)

--- src/i18nTest.ts
+++ src/i18nTest.ts
@@ -1,21 +1,26 @@
-import {join} from "path";
 import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
-import Backend from "i18next-fs-backend";
 
-i18n
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    backend: {
-      loadPath: join(__dirname, "../public/locales/en/translation.json"),
-    },
-    initImmediate: false,
-    lng: "en",
-    debug: false,
-    react: {
-      useSuspense: false,
-    },
-  });
+import translationDe from "./i18n/de/translation.json";
+import translationEn from "./i18n/en/translation.json";
+
+export const resources = {
+  en: {
+    translation: translationEn,
+  },
+  de: {
+    translation: translationDe,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  initImmediate: false,
+  lng: "en",
+  debug: true,
+  react: {
+    useSuspense: false,
+  },
+});
 
 export default i18n;
