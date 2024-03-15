bfcf3525ba1616c1ed96d0b74623f8a875293e0d
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-03-15 09:55:59 +0100

    Update tests of votes component

--- src/components/Votes/__tests__/Votes.test.tsx
+++ src/components/Votes/__tests__/Votes.test.tsx
@@ -4,12 +4,18 @@ import {render} from "testUtils";
 import getTestStore from "utils/test/getTestStore";
 import {ApplicationState} from "types";
 import getTestVoting from "utils/test/getTestVoting";
+import getTestParticipant from "utils/test/getTestParticipant";
+import i18n from "i18nTest";
+import {I18nextProvider} from "react-i18next";
+import {screen} from "@testing-library/dom";
 
 const createVotes = (overwrite?: Partial<ApplicationState>) => {
   return (
-    <Provider store={getTestStore(overwrite)}>
-      <Votes noteId="test-notes-id-1" />
-    </Provider>
+    <I18nextProvider i18n={i18n}>
+      <Provider store={getTestStore(overwrite)}>
+        <Votes noteId="test-notes-id-1" />
+      </Provider>
+    </I18nextProvider>
   );
 };
 
@@ -56,5 +62,61 @@ describe("Votes", () => {
       const votes = render(createVotes());
       expect(votes.container).toMatchSnapshot();
     });
+  });
+
+  it("add vote button should be hidden and remove vote button disabled on locked board", () => {
+    render(
+      createVotes({
+        board: {
+          status: "ready",
+          data: {
+            id: "test-board-id",
+            name: "test-board-name",
+            accessPolicy: "PUBLIC",
+            showAuthors: true,
+            showNotesOfOtherUsers: true,
+            showNoteReactions: true,
+            allowStacking: true,
+            allowEditing: false,
+          },
+        },
+        participants: {
+          self: getTestParticipant({role: "PARTICIPANT"}),
+          others: [],
+          focusInitiator: null,
+        },
+      })
+    );
+
+    expect(screen.queryByTitle(i18n.t("Votes.AddVote"))).not.toBeInTheDocument();
+    expect(screen.queryByTitle(i18n.t("Votes.RemoveVote"))).toBeDisabled();
+  });
+
+  it("add vote button should be visiable and remove vote button not disabled on locked board if participant is moderator", () => {
+    render(
+      createVotes({
+        board: {
+          status: "ready",
+          data: {
+            id: "test-board-id",
+            name: "test-board-name",
+            accessPolicy: "PUBLIC",
+            showAuthors: true,
+            showNotesOfOtherUsers: true,
+            showNoteReactions: true,
+            allowStacking: true,
+            allowEditing: false,
+          },
+        },
+        participants: {
+          self: getTestParticipant({role: "MODERATOR"}),
+          others: [],
+          focusInitiator: null,
+        },
+      })
+    );
+
+    expect(screen.getByTitle(i18n.t("Votes.AddVote"))).toBeInTheDocument();
+    expect(screen.queryByTitle(i18n.t("Votes.RemoveVote"))).not.toBeDisabled();
   });
 });
