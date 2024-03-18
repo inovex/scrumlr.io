7b3e282a004db1d2bdf762efd88e3b216f316c0f
Manuel Brandstetter <36969812+brandstetterm@users.noreply.github.com>
2024-03-18 10:16:16 +0100

    fix: add missing title to voting buttons  (#3980)

--- src/components/Votes/VoteButtons/RemoveVoteButton.tsx
+++ src/components/Votes/VoteButtons/RemoveVoteButton.tsx
@@ -1,18 +1,21 @@
-import {FC, PropsWithChildren, useEffect, useRef, useState} from "react";
+import {FC, useEffect, useRef, useState} from "react";
 import {useDispatch} from "react-redux";
+import {useTranslation} from "react-i18next";
+import classNames from "classnames";
 import {Actions} from "store/action";
 import {DotButton} from "components/DotButton";
-import "./RemoveVoteButton.scss";
-import classNames from "classnames";
 import {ReactComponent as RemoveIcon} from "assets/icon-remove.svg";
+import "./RemoveVoteButton.scss";
 
 type RemoveVoteProps = {
   noteId: string;
   disabled?: boolean;
+  numberOfVotes: number;
 };
 
-export const RemoveVoteButton: FC<PropsWithChildren<RemoveVoteProps>> = ({noteId, disabled, children}) => {
+export const RemoveVoteButton: FC<RemoveVoteProps> = ({noteId, disabled, numberOfVotes}) => {
   const dispatch = useDispatch();
+  const {t} = useTranslation();
 
   const deleteVote = () => {
     dispatch(Actions.deleteVote(noteId));
@@ -27,10 +30,11 @@ export const RemoveVoteButton: FC<PropsWithChildren<RemoveVoteProps>> = ({noteId
       return;
     }
     setDoBump(true);
-  }, [children]);
+  }, [numberOfVotes]);
 
   return (
     <DotButton
+      title={disabled ? t("Votes.VotesOnNote", {votes: numberOfVotes}) : t("Votes.RemoveVote")}
       className={classNames("vote-button-remove", {bump: doBump})}
       disabled={disabled}
       onClick={deleteVote}
@@ -40,7 +44,7 @@ export const RemoveVoteButton: FC<PropsWithChildren<RemoveVoteProps>> = ({noteId
     >
       <span className="vote-button-remove__folded-corner" />
       <RemoveIcon className="vote-button-remove__icon" />
-      <span className="vote-button-remove__count">{children}</span>
+      <span className="vote-button-remove__count">{numberOfVotes}</span>
     </DotButton>
   );
 };
