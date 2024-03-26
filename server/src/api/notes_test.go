a47a6f653fa49077b31f076778f9734ca09bd403
Mateo <mateo.ivankovic@inovex.de>
2024-03-26 11:22:30 +0100

    uncommented test

--- server/src/api/notes_test.go
+++ server/src/api/notes_test.go
@@ -135,69 +135,69 @@ func TestNotesTestSuite(t *testing.T) {
 	suite.Run(t, new(NotesTestSuite))
 }
 
-// func (suite *NotesTestSuite) TestCreateNote() {
-//
-//	tests := []struct {
-//		name         string
-//		expectedCode int
-//		err          error
-//	}{
-//		{
-//			name:         "all ok",
-//			expectedCode: http.StatusCreated,
-//		},
-//		{
-//			name:         "api err",
-//			expectedCode: http.StatusConflict,
-//			err: &common.APIError{
-//				Err:        errors.New("test"),
-//				StatusCode: http.StatusConflict,
-//				StatusText: "no",
-//				ErrorText:  "way",
-//			},
-//		},
-//		{
-//			name:         "unexpected err",
-//			expectedCode: http.StatusInternalServerError,
-//			err:          errors.New("oops"),
-//		},
-//	}
-//	for _, tt := range tests {
-//		suite.Run(tt.name, func() {
-//			s := new(Server)
-//			mock := new(NotesMock)
-//			testText := "asdf"
-//
-//			boardId, _ := uuid.NewRandom()
-//			userId, _ := uuid.NewRandom()
-//			colId, _ := uuid.NewRandom()
-//
-//			mock.On("Create", dto.NoteCreateRequest{
-//				Board:  boardId,
-//				User:   userId,
-//				Text:   testText,
-//				Column: colId,
-//			}).Return(&dto.Note{
-//				Text: testText,
-//			}, tt.err)
-//
-//			s.notes = mock
-//
-//			req := NewTestRequestBuilder("POST", "/", strings.NewReader(fmt.Sprintf(`{
-//				"column": "%s",
-//				"text" : "%s"
-//				}`, colId.String(), testText))).
-//				AddToContext("Board", boardId).
-//				AddToContext("User", userId)
-//			rr := httptest.NewRecorder()
-//
-//			s.createNote(rr, req.Request())
-//			suite.Equal(tt.expectedCode, rr.Result().StatusCode)
-//			mock.AssertExpectations(suite.T())
-//		})
-//	}
-//
-// }
+func (suite *NotesTestSuite) TestCreateNote() {
+
+	tests := []struct {
+		name         string
+		expectedCode int
+		err          error
+	}{
+		{
+			name:         "all ok",
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "api err",
+			expectedCode: http.StatusConflict,
+			err: &common.APIError{
+				Err:        errors.New("test"),
+				StatusCode: http.StatusConflict,
+				StatusText: "no",
+				ErrorText:  "way",
+			},
+		},
+		{
+			name:         "unexpected err",
+			expectedCode: http.StatusInternalServerError,
+			err:          errors.New("oops"),
+		},
+	}
+	for _, tt := range tests {
+		suite.Run(tt.name, func() {
+			s := new(Server)
+			mock := new(NotesMock)
+			testText := "asdf"
+
+			boardId, _ := uuid.NewRandom()
+			userId, _ := uuid.NewRandom()
+			colId, _ := uuid.NewRandom()
+
+			mock.On("Create", dto.NoteCreateRequest{
+				Board:  boardId,
+				User:   userId,
+				Text:   testText,
+				Column: colId,
+			}).Return(&dto.Note{
+				Text: testText,
+			}, tt.err)
+
+			s.notes = mock
+
+			req := NewTestRequestBuilder("POST", "/", strings.NewReader(fmt.Sprintf(`{
+				"column": "%s",
+				"text" : "%s"
+				}`, colId.String(), testText))).
+				AddToContext("Board", boardId).
+				AddToContext("User", userId)
+			rr := httptest.NewRecorder()
+
+			s.createNote(rr, req.Request())
+			suite.Equal(tt.expectedCode, rr.Result().StatusCode)
+			mock.AssertExpectations(suite.T())
+		})
+	}
+
+}
 func (suite *NotesTestSuite) TestGetNote() {
 
 	tests := []struct {
