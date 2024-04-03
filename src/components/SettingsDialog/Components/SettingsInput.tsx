07e006c6dd574bd5ed62f671ccee27a42d6ebca1
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-04-03 09:18:01 +0200

    refactor: settings input

--- src/components/SettingsDialog/Components/SettingsInput.tsx
+++ src/components/SettingsDialog/Components/SettingsInput.tsx
@@ -1,4 +1,7 @@
-import {ChangeEvent, FC, PropsWithChildren} from "react";
+import classNames from "classnames";
+import React, {ChangeEvent, FC, useState} from "react";
+import {ReactComponent as VisibleIcon} from "assets/icon-visible.svg";
+import {ReactComponent as HiddenIcon} from "assets/icon-hidden.svg";
 import "./SettingsInput.scss";
 
 export interface SettingsInputProps {
@@ -11,33 +14,60 @@ export interface SettingsInputProps {
   type?: "text" | "password";
   placeholder?: string;
   maxLength?: number;
+  containerClassName?: string;
+  className?: string;
+  passwordToggle?: boolean;
 }
 
-export const SettingsInput: FC<PropsWithChildren<SettingsInputProps>> = ({label, id, value, onChange, submit, disabled, type, placeholder, children, maxLength}) => (
-  <div className="settings-input__container">
-    <input
-      className={!placeholder ? "settings-input__hidden-placeholder" : undefined}
-      placeholder={placeholder ?? label}
-      value={value}
-      onChange={onChange}
-      onBlur={() => value && submit()}
-      onKeyDown={(e) => e.key === "Enter" && value && submit()}
-      disabled={disabled}
-      type={type ?? "text"}
-      id={id}
-      autoComplete="off"
-      maxLength={maxLength}
-    />
-    {maxLength !== undefined && (
-      <small className="settings-input__length">
-        {value.length}/{maxLength}
-      </small>
-    )}
-    <label htmlFor={id}>{label}</label>
-    {children && (
-      <button className="settings-input__children" onMouseDown={(e) => e.preventDefault()}>
-        {children}
-      </button>
-    )}
-  </div>
-);
+export const SettingsInput: FC<SettingsInputProps> = ({
+  label,
+  id,
+  value,
+  onChange,
+  submit,
+  disabled,
+  type = "text",
+  placeholder,
+  maxLength,
+  containerClassName,
+  className,
+  passwordToggle = false,
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    setShowPassword((curr) => !curr);
+  };
+
+  console.log(value);
+
+  return (
+    <div className={classNames("settings-input__container", containerClassName)}>
+      <input
+        className={classNames("settings-input__input", className, {"settings-input__hidden-placeholder": !placeholder})}
+        placeholder={placeholder ?? label}
+        value={value}
+        onChange={onChange}
+        onBlur={() => value && submit()}
+        onKeyDown={(e) => e.key === "Enter" && value && submit()}
+        disabled={disabled}
+        type={type === "text" ? "text" : showPassword ? "text" : "password"}
+        id={id}
+        autoComplete="off"
+        maxLength={maxLength}
+      />
+      {maxLength !== undefined && (
+        <small className="settings-input__length">
+          {value.length}/{maxLength}
+        </small>
+      )}
+      <label htmlFor={id}>{label}</label>
+      {type === "password" && passwordToggle && value && (
+        <button className="settings-input__password-toggle" onClick={togglePasswordVisibility}>
+          {showPassword ? <VisibleIcon /> : <HiddenIcon />}
+        </button>
+      )}
+    </div>
+  );
+};
