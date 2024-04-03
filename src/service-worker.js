e1b62e7c4263d704361817eae5c4489458bbf1f7
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-04-03 11:35:26 +0200

    chore: try to remove service worker

---
+++ src/service-worker.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-restricted-globals
+const ignored = self.__WB_MANIFEST;
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  self.registration
+    .unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      clients.forEach((client) => {
+        if (client.url && "navigate" in client) {
+          client.navigate(client.url);
+        }
+      });
+    });
+});
