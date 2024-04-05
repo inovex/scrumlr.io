3e1e63e860fb0eaa6aacfff2e0452d311d348802
Benedict Homuth <bhomuth@inovex.de>
2024-04-05 21:23:32 +0200

    added deleteNote test to check if client can now get mocked

--- server/src/services/notes/notes_test.go
+++ server/src/services/notes/notes_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"scrumlr.io/server/common/dto"
+	"scrumlr.io/server/realtime"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
@@ -17,6 +18,29 @@ type NoteServiceTestSuite struct {
 	suite.Suite
 }
 
+type mockClient struct {
+	mock.Mock
+}
+
+func (mc *mockClient) Publish(subject string, event interface{}) error {
+	args := mc.Called(subject, event)
+	return args.Error(0)
+}
+
+func (mc *mockClient) SubscribeToBoardSessionEvents(subject string) (chan *realtime.BoardSessionRequestEventType, error) {
+	args := mc.Called(subject)
+	return args.Get(0).(chan *realtime.BoardSessionRequestEventType), args.Error(1)
+}
+
+func (mc *mockClient) SubscribeToBoardEvents(subject string) (chan *realtime.BoardEvent, error) {
+	args := mc.Called(subject)
+	return args.Get(0).(chan *realtime.BoardEvent), args.Error(1)
+}
+
+func newBroker(c realtime.Client) *realtime.Broker {
+	return &realtime.Broker{Con: c}
+}
+
 type DBMock struct {
 	DB
 	mock.Mock
@@ -27,21 +51,26 @@ func (m *DBMock) CreateNote(insert database.NoteInsert) (database.Note, error) {
 	return args.Get(0).(database.Note), args.Error(1)
 }
 
+func (m *DBMock) DeleteNote(caller uuid.UUID, board uuid.UUID, id uuid.UUID, deleteStack bool) error {
+	args := m.Called(caller, board, id, deleteStack)
+	return args.Error(0)
+}
+
 func TestNoteServiceTestSuite(t *testing.T) {
 	suite.Run(t, new(NoteServiceTestSuite))
 }
 
 func (suite *NoteServiceTestSuite) TestCreate() {
 	s := new(NoteService)
-	mock := new(DBMock)
-	s.database = mock
+	dbMock := new(DBMock)
+	s.database = dbMock
 
 	authorID, _ := uuid.NewRandom()
 	boardID, _ := uuid.NewRandom()
 	colID, _ := uuid.NewRandom()
 	txt := "aaaaaaaaaaaaaaaaaaaa"
 
-	mock.On("CreateNote", database.NoteInsert{
+	dbMock.On("CreateNote", database.NoteInsert{
 		Author: authorID,
 		Board:  boardID,
 		Column: colID,
@@ -55,6 +84,47 @@ func (suite *NoteServiceTestSuite) TestCreate() {
 		Text:   txt,
 	})
 	assert.NoError(suite.T(), err, "No error in create function.")
-	mock.AssertExpectations(suite.T())
+	dbMock.AssertExpectations(suite.T())
 
+}
+
+func (suite *NoteServiceTestSuite) TestDeleteNote() {
+	s := new(NoteService)
+
+	dbMock := new(DBMock)
+	s.database = dbMock
+
+	clientMock := &mockClient{}
+	rtMock := &realtime.Broker{
+		Con: clientMock,
+	}
+	s.realtime = rtMock
+
+	callerID, _ := uuid.NewRandom()
+	boardID, _ := uuid.NewRandom()
+	noteID, _ := uuid.NewRandom()
+	deleteStack := true
+	body := dto.NoteDeleteRequest{
+		DeleteStack: deleteStack,
+	}
+	publishSubject := "board." + boardID.String()
+	publishEvent := realtime.BoardEvent{
+		Type: realtime.BoardEventNoteDeleted,
+		Data: map[string]interface{}{
+			"deleteStack": deleteStack,
+			"note":        noteID,
+		},
+	}
+
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "User", callerID)
+	ctx = context.WithValue(ctx, "Board", boardID)
+
+	dbMock.On("DeleteNote", callerID, boardID, noteID, deleteStack).Return(nil)
+	clientMock.On("Publish", publishSubject, publishEvent).Return(nil)
+
+	s.Delete(ctx, body, noteID)
+
+	dbMock.AssertExpectations(suite.T())
+	clientMock.AssertExpectations(suite.T())
 }
