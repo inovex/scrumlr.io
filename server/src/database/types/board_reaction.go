493e519392dd06bb3da1306c9d946586dd30acc0
Mateo Ivankovic <mateo.ivankovic@inovex.de>
2024-04-12 09:08:11 +0200

    fix board reaction issue (#4063)

---
+++ server/src/database/types/board_reaction.go
@@ -0,0 +1,31 @@
+package types
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+type BoardReaction string
+
+const (
+	BoardReactionTada     BoardReaction = "tada"
+	BoardReactionApplause BoardReaction = "applause"
+	BoardReactionHeart    BoardReaction = "heart"
+	BoardReactionLike     BoardReaction = "like"
+	BoardReactionDislike  BoardReaction = "dislike"
+)
+
+func (reaction *BoardReaction) UnmarshalJSON(b []byte) error {
+	var s string
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
+	unmarshalledReaction := BoardReaction(s)
+	switch unmarshalledReaction {
+	case BoardReactionTada, BoardReactionApplause, BoardReactionHeart, BoardReactionLike, BoardReactionDislike:
+		*reaction = unmarshalledReaction
+		return nil
+	}
+	return errors.New("invalid board reaction")
+}
