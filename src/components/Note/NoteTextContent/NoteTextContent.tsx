4286bcc021eb10f4705c0d122ec86f2e8a4b3bdf
Jakob Schwehn <schwehn42@gmail.com>
2024-05-03 14:33:51 +0200

    use custom render func

--- src/components/Note/NoteTextContent/NoteTextContent.tsx
+++ src/components/Note/NoteTextContent/NoteTextContent.tsx
@@ -1,6 +1,6 @@
 import Linkify from "linkify-react";
 import classNames from "classnames";
-import React from "react";
+import {addProtocol} from "utils/images";
 import "./NoteTextContent.scss";
 
 type NoteTextContentProps = {
@@ -8,20 +8,33 @@ type NoteTextContentProps = {
   truncate: boolean;
 };
 
-export const NoteTextContent = (props: NoteTextContentProps) => (
-  <Linkify
-    options={{
-      target: "_blank",
-      className: classNames("note-text-content-url", {"note-text-content-url--truncate": props.truncate}),
-      defaultProtocol: "https",
-      attributes: {
-        onClick: (event: React.MouseEvent<HTMLAnchorElement>) => {
-          event.stopPropagation();
-        },
-      },
-      rel: "noopener noreferrer",
-    }}
-  >
-    {props.text}
-  </Linkify>
-);
+export const NoteTextContent = (props: NoteTextContentProps) => {
+  // render a link, opening in a new tab.
+  // this functionality could also be extended,
+  // e.g. you could add wrappers for other scrumlr links etc.
+  const renderLink = (content: {content: string}) => {
+    const url = addProtocol(content.content);
+    return (
+      <a
+        href={url}
+        title={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={classNames("note-text-content-url", {"note-text-content-url--truncate": props.truncate})}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {content.content}
+      </a>
+    );
+  };
+
+  return (
+    <Linkify
+      options={{
+        render: renderLink,
+      }}
+    >
+      {props.text}
+    </Linkify>
+  );
+};
