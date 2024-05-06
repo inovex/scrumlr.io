af164b51de2e66e1176df458334b7de17bddf237
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-05-06 14:49:42 +0200

    test: add tests for note reaction bar component

---
+++ src/components/Note/NoteReactionList/NoteReactionBar/NoteReactionBar.test.tsx
@@ -0,0 +1,40 @@
+import {render, fireEvent, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import {REACTION_EMOJI_MAP} from "types/reaction";
+import {NoteReactionBar} from "./NoteReactionBar";
+
+describe("NoteReactionBar", () => {
+  it("renders the reaction buttons correctly", () => {
+    render(<NoteReactionBar closeReactionBar={() => {}} reactions={[]} handleClickReaction={() => {}} />);
+
+    for (const [_, emoji] of REACTION_EMOJI_MAP.entries()) {
+      expect(screen.getByText(emoji)).toBeInTheDocument();
+    }
+  });
+
+  it("calls handleClickBar when a reaction button is clicked", () => {
+    const handleClickReaction = jest.fn();
+    render(<NoteReactionBar closeReactionBar={() => {}} reactions={[]} handleClickReaction={handleClickReaction} />);
+    fireEvent.click(screen.getByText("👍"));
+    expect(handleClickReaction).toHaveBeenCalled();
+  });
+
+  it("closes the reaction bar when Escape key is pressed", () => {
+    const closeReactionBar = jest.fn();
+    render(<NoteReactionBar closeReactionBar={closeReactionBar} reactions={[]} handleClickReaction={() => {}} />);
+    fireEvent.keyDown(document, {key: "Escape"});
+    expect(closeReactionBar).toHaveBeenCalled();
+  });
+
+  it("focuses the first emoji button after TAB is pressed on the last emoji button", async () => {
+    render(<NoteReactionBar closeReactionBar={() => {}} reactions={[]} handleClickReaction={() => {}} />);
+
+    const firstEmojiButton = screen.getByText([...REACTION_EMOJI_MAP.values()].at(0)!);
+    const lastEmojiButton = screen.getByText([...REACTION_EMOJI_MAP.values()].at(-1)!);
+
+    lastEmojiButton.focus();
+    expect(lastEmojiButton).toHaveFocus();
+    await userEvent.tab();
+    expect(firstEmojiButton).toHaveFocus();
+  });
+});
