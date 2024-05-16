097d29fcef4c114f952f459e77160e78966e02a0
Mateo <mateo.ivankovic@inovex.de>
2024-05-16 09:07:29 +0200

    updated initial_schema.down.sql

--- server/src/database/migrations/sql/1_initial_schema.down.sql
+++ server/src/database/migrations/sql/1_initial_schema.down.sql
@@ -5,6 +5,11 @@ drop type if exists session_role cascade;
 drop type if exists voting_status cascade;
 
 drop table if exists users cascade;
+drop table if exists github_users cascade;
+drop table if exists google_users cascade;
+drop table if exists microsoft_users cascade;
+drop table if exists azure_ad_users cascade;
+drop table if exists apple_users cascade;
 drop table if exists boards cascade;
 drop table if exists "columns" cascade;
 drop table if exists board_session_requests cascade;
@@ -12,3 +17,9 @@ drop table if exists board_sessions cascade;
 drop table if exists notes cascade;
 drop table if exists votings cascade;
 drop table if exists votes cascade;
+drop table if exists ractions cascade;
+drop table if exists deleted_boards cascade;
+
+DROP FUNCTION record_deleted_board_statistics();
+
+DROP TRIGGER before_delete_board ON boards;
