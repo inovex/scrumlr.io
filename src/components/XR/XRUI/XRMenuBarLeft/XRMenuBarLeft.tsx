83f480cfcc4596558f9f473f0b12ac7a02951649
Resaki1 <christian.reski@t-online.de>
2024-05-17 14:56:53 +0200

    fixed most svgs in xr

--- src/components/XR/XRUI/XRMenuBarLeft/XRMenuBarLeft.tsx
+++ src/components/XR/XRUI/XRMenuBarLeft/XRMenuBarLeft.tsx
@@ -1,4 +1,5 @@
-import {Svg, Text} from "@react-three/uikit";
+import {Content, Text} from "@react-three/uikit";
+import {Svg} from "@react-three/drei";
 import {TabBar, TabBarItem} from "components/apfel/tab-bar";
 import CheckIcon from "assets/icon-check.svg";
 import RaiseHand from "assets/icon-hand.svg";
@@ -9,7 +10,6 @@ import {useDispatch} from "react-redux";
 import {useAppSelector} from "store";
 import _ from "underscore";
 import {useState} from "react";
-import {FONT_COLOR, getColorFromName} from "components/XR/xr-constants";
 import XRReactionsMenu from "../XRReactionsMenu/XRReactionsMenu";
 
 const XRMenuBarLeft = () => {
@@ -45,20 +45,51 @@ const XRMenuBarLeft = () => {
   return (
     <>
       <TabBar positionType="absolute" positionLeft={-72} positionTop="25%" zIndexOffset={16} transformTranslateZ={16}>
-        <TabBarItem value="1" icon={<Svg src={CheckIcon} color={!isReady ? FONT_COLOR : getColorFromName("grooming-green")} />} onClick={toggleReadyState}>
+        <TabBarItem
+          value="1"
+          icon={
+            <Content>
+              <Svg src={CheckIcon} />
+            </Content>
+          }
+          onClick={toggleReadyState}
+        >
           <Text>{!isReady ? "Mark me as done" : "Unmark me as done"}</Text>
         </TabBarItem>
-        <TabBarItem value="2" icon={<Svg src={RaiseHand} color={!raisedHand ? FONT_COLOR : getColorFromName("poker-purple")} />} onClick={toggleRaiseHand}>
+        <TabBarItem
+          value="2"
+          icon={
+            <Content>
+              <Svg src={RaiseHand} />
+            </Content>
+          }
+          onClick={toggleRaiseHand}
+        >
           <Text>{!raisedHand ? "Raise your hand" : "Lower your hand"}</Text>
         </TabBarItem>
-        <TabBarItem value="3" icon={<Svg src={BoardReactionIcon} />} onClick={toggleBoardReactionsMenu}>
+        <TabBarItem
+          value="3"
+          icon={
+            <Content>
+              <Svg src={BoardReactionIcon} />
+            </Content>
+          }
+          onClick={toggleBoardReactionsMenu}
+        >
           <Text>React</Text>
         </TabBarItem>
-        <TabBarItem value="4" icon={<Svg src={SettingsIcon} />}>
+        <TabBarItem
+          value="4"
+          icon={
+            <Content>
+              <Svg src={SettingsIcon} />
+            </Content>
+          }
+        >
           <Text>Settings</Text>
         </TabBarItem>
       </TabBar>
-      {showBoardReactionsMenu && <XRReactionsMenu />}
+      {showBoardReactionsMenu && <XRReactionsMenu close={() => setShowBoardReactionsMenu(false)} />}
     </>
   );
 };
--- src/components/XR/XRUI/XRReactionsMenu/XRReactionsMenu.tsx
+++ src/components/XR/XRUI/XRReactionsMenu/XRReactionsMenu.tsx
@@ -1,5 +1,57 @@
+import {Content} from "@react-three/uikit";
+import {Svg} from "@react-three/drei";
+import {XRButton} from "components/apfel/XRButton";
 import {Card} from "components/apfel/card";
+import PartyPopper from "assets/emoji/emoji-party-popper.svg";
+import ClappingHands from "assets/emoji/emoji-clapping-hands.svg";
+import SparklingHeart from "assets/emoji/emoji-sparkling-heart.svg";
+import ThumbsUp from "assets/emoji/emoji-thumbs-up.svg";
+import ThumbsDown from "assets/emoji/emoji-thumbs-down.svg";
+import CloseIcon from "assets/icon-close.svg";
 
-const XRReactionsMenu = () => <Card positionType="absolute" height={64} width={256} positionBottom={-72} positionLeft={372} zIndexOffset={16} transformTranslateZ={16} />;
+const XRReactionsMenu = ({close}: {close: () => void}) => (
+  <Card
+    positionType="absolute"
+    height={64}
+    width={296}
+    positionBottom={-72}
+    positionLeft={352}
+    zIndexOffset={16}
+    transformTranslateZ={16}
+    justifyContent="space-evenly"
+    alignItems="center"
+  >
+    <XRButton variant="icon" size="md">
+      <Content>
+        <Svg src={PartyPopper} />
+      </Content>
+    </XRButton>
+    <XRButton variant="icon" size="md">
+      <Content>
+        <Svg src={ClappingHands} />
+      </Content>
+    </XRButton>
+    <XRButton variant="icon" size="md">
+      <Content>
+        <Svg src={SparklingHeart} />
+      </Content>
+    </XRButton>
+    <XRButton variant="icon" size="md">
+      <Content>
+        <Svg src={ThumbsUp} />
+      </Content>
+    </XRButton>
+    <XRButton variant="icon" size="md">
+      <Content>
+        <Svg src={ThumbsDown} />
+      </Content>
+    </XRButton>
+    <XRButton variant="icon" size="md" onClick={close}>
+      <Content>
+        <Svg src={CloseIcon} />
+      </Content>
+    </XRButton>
+  </Card>
+);
 
 export default XRReactionsMenu;
