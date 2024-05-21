8e3b4af3adb0b4e84c6c1a2fd49e0c73eadbf8dc
Jakob Schwehn <schwehn42@gmail.com>
2024-05-21 14:33:22 +0200

    Revert "hook v1.1"
    This reverts commit e2fccb7a1ae27c50d26e45420898d76bde2bf868.

--- src/utils/hooks/useTextAreaSize.ts
+++ src/utils/hooks/useTextAreaSize.ts
@@ -1,14 +1,17 @@
-import {MutableRefObject, useEffect} from "react";
+import React, {useEffect} from "react";
 
 // Updates the height of a <textarea> when the value changes.
-const useAutosizeTextArea = (textAreaRef: MutableRefObject<HTMLTextAreaElement | null>, value: string) => {
+const useAutosizeTextArea = (textAreaRef: React.MutableRefObject<HTMLTextAreaElement | null>, value: string) => {
   useEffect(() => {
     if (textAreaRef && textAreaRef.current) {
       const element = textAreaRef.current;
       // We need to reset the height momentarily to get the correct scrollHeight for the textarea
-      element.style.height = "auto";
+      element.style.height = "0px";
       const {scrollHeight} = element;
-      element.style.height = `${scrollHeight + 2}px`;
+
+      // We then set the height directly, outside the render loop
+      // Trying to set this with state or a ref will product an incorrect value.
+      element.style.height = `${scrollHeight  }px`;
     }
   }, [textAreaRef, value]);
 };
