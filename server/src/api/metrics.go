7945788e343fa793e5cef883421bc2c1bb84f744
Benedict Homuth <bhomuth@inovex.de>
2024-06-10 18:06:01 +0200

    whitespace

--- server/src/api/metrics.go
+++ server/src/api/metrics.go
@@ -52,6 +52,7 @@ func metricCounterMiddleware(next http.Handler) http.Handler {
 
 	})
 }
+
 func metricMeasureLatencyMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
