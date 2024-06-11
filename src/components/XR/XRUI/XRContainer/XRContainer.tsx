120e24f32e63fa72ba162e824730c1a73e431296
Resaki1 <christian.reski@t-online.de>
2024-06-11 16:27:18 +0200

    improved repositioning of spatial UI

--- src/components/XR/XRUI/XRContainer/XRContainer.tsx
+++ src/components/XR/XRUI/XRContainer/XRContainer.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unknown-property */
 import {Group, MeshPhongMaterial, Vector3} from "three";
-import {useRef, useState} from "react";
+import {useRef} from "react";
 import {Root} from "@react-three/uikit";
 import {Card} from "components/apfel/card";
 import {useFrame} from "@react-three/fiber";
@@ -14,25 +14,23 @@ export const GlassMaterial = MeshPhongMaterial;
 
 const cameraDirection = new Vector3();
 const WebXRExtensionCamPosition = new Vector3(0, 0, 5); // workaround to fix initial position set by the WebXR Browser Extension
-/* const currentLookAt = new Vector3(); */
-const targetLookAt = new Vector3();
 
 const XRContainer = () => {
   const handleBarRef = useRef<Group>(null!);
   const containerRef = useRef<Group>(null!);
-  const [isPositionSet, setIsPositionSet] = useState(false);
+  const isPositionSet = useRef(false);
 
   useFrame((state) => {
     if (handleBarRef.current) {
       // on session startup, set the initial position and rotation of the UI based on the device's position
-      if (!isPositionSet && !state.camera.position.equals(WebXRExtensionCamPosition)) {
+      if (!isPositionSet.current && !state.camera.position.equals(WebXRExtensionCamPosition)) {
         state.camera.getWorldDirection(cameraDirection);
         cameraDirection.multiplyScalar(1.4); // 1.4 meters in front of the camera
         const newPosition = state.camera.position.clone().add(cameraDirection);
         handleBarRef.current.position.copy(newPosition);
         handleBarRef.current.lookAt(state.camera.position);
-        targetLookAt.copy(state.camera.position);
-        setIsPositionSet(true);
+        containerRef.current.rotation.copy(handleBarRef.current.rotation);
+        isPositionSet.current = true;
       }
 
       /* const distance = containerRef.current.position.distanceTo(state.camera.position);
--- src/components/XR/XRUI/XRHandleBar/XRHandleBar.tsx
+++ src/components/XR/XRUI/XRHandleBar/XRHandleBar.tsx
@@ -1,28 +1,51 @@
 /* eslint-disable react/no-unknown-property */
 import {Grabbable} from "@coconut-xr/natuerlich/defaults";
-import {useFrame} from "@react-three/fiber";
-import {MutableRefObject, forwardRef, useImperativeHandle, useRef} from "react";
-import {Euler, Group, Object3DEventMap} from "three";
+import {useFrame, useThree} from "@react-three/fiber";
+import {MutableRefObject, forwardRef, useEffect, useImperativeHandle, useRef} from "react";
+import {Euler, Group, Object3DEventMap, Vector3} from "three";
 
 type XRHandleBarProps = {
   containerRef: MutableRefObject<Group<Object3DEventMap>>;
 };
 
+const currentLookAt = new Vector3();
+const targetLookAt = new Vector3();
+
 const XRHandleBar = forwardRef<Group, XRHandleBarProps>(({containerRef}: XRHandleBarProps, ref) => {
   const handleBarRef = useRef<Group>(null!);
+  const dragging = useRef(false);
+  const {camera} = useThree();
 
   useImperativeHandle(ref, () => handleBarRef.current, []);
 
-  useFrame(() => {
-    if (handleBarRef?.current) {
-      handleBarRef.current.getWorldPosition(containerRef.current.position);
-      containerRef.current.rotation.copy(handleBarRef.current.rotation);
-      containerRef.current.scale.copy(handleBarRef.current.scale);
+  useEffect(() => {
+    currentLookAt.copy(camera.position);
+  });
+
+  useFrame((state) => {
+    if (!containerRef.current || !handleBarRef.current) return;
+
+    handleBarRef.current.getWorldPosition(containerRef.current.position);
+    // containerRef.current.scale.copy(handleBarRef.current.scale);
+
+    if (dragging.current) {
+      targetLookAt.copy(state.camera.position);
+      containerRef.current.lookAt(currentLookAt.lerp(targetLookAt, 0.1));
+      handleBarRef.current.lookAt(currentLookAt.lerp(targetLookAt, 0.1));
     }
   });
 
   return (
-    <Grabbable ref={handleBarRef} rotation={new Euler(0, 0, 0.5 * Math.PI)}>
+    <Grabbable
+      ref={handleBarRef}
+      rotation={new Euler(0, 0, 0.5 * Math.PI)}
+      onGrabbed={() => {
+        dragging.current = true;
+      }}
+      onReleased={() => {
+        dragging.current = false;
+      }}
+    >
       <mesh position={[-0.6, 0, 0]}>
         <cylinderGeometry args={[0.01, 0.01, 0.3]} />
         <meshStandardMaterial transparent opacity={0.8} color="white" roughness={0.5} />
