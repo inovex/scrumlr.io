d976e605e7dff6a7aae7d9a1a1778aa175af76f9
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-06-16 10:18:52 +0200

    refactor: revise printer icon

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -69,3 +69,4 @@ export {ReactComponent as PlusOne} from "assets/icons/plus-one.svg";
 export {ReactComponent as Logout} from "assets/icons/logout.svg";
 export {ReactComponent as Warning} from "assets/icons/warning.svg";
 export {ReactComponent as Refresh} from "assets/icons/refresh.svg";
+export {ReactComponent as Printer} from "assets/icons/printer.svg";
