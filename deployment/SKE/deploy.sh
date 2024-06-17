8a4494fe9919843ec547b5a909267746e7094f28
Lennart Krauch <lennart.krauch@inovex.de>
2024-06-17 16:25:59 +0200

    updated install script

--- deployment/SKE/deploy.sh
+++ deployment/SKE/deploy.sh
@@ -50,14 +50,11 @@ check_k8s_connection
 
 # Input parameters
 DB_URL=$1
-DEPLOYMENT_DOMAIN=$2
 
-if [ -z "$DB_URL" ] || [ -z "$DEPLOYMENT_DOMAIN" ]; then
-  echo "Usage: $0 <DB_URL> <DEPLOYMENT_DOMAIN>"
+if [ -z "$DB_URL" ]; then
+  echo "Usage: $0 <DB_URL>"
   exit 1
 fi
-
-export SCRUMLR_DEPLOYMENT_DOMAIN="$DEPLOYMENT_DOMAIN"
 
 # Create namespace
 kubectl create namespace scrumlr || true
@@ -71,6 +68,17 @@ if ! helm_release_exists traefik scrumlr; then
 else
   echo "Traefik is already installed, skipping installation."
 fi
+
+# Prompt user to set up DNS records and input deployment domain
+echo "Please set up your DNS records to point to the Traefik load balancer."
+read -rp "Enter your deployment domain (e.g., scrumlr.stackit.rocks): " DEPLOYMENT_DOMAIN
+
+if [ -z "$DEPLOYMENT_DOMAIN" ]; then
+  echo "Error: Deployment domain is required."
+  exit 1
+fi
+
+export SCRUMLR_DEPLOYMENT_DOMAIN="$DEPLOYMENT_DOMAIN"
 
 # Install Cert-Manager
 if ! helm_release_exists cert-manager cert-manager; then
@@ -94,8 +102,8 @@ fi
 
 # Create secrets
 # replace /stackit with correct db path
-DB_UR_SCRUMLR=$(echo "$DB_URL" | sed 's/\/stackit/\/scrumlr/')
-kubectl -n scrumlr create secret generic postgresql-creds --from-literal=url="$DB_UR_SCRUMLR" --dry-run=client -o yaml | kubectl apply -f -
+DB_URL_SCRUMLR=$(echo "$DB_URL" | sed 's/\/stackit/\/scrumlr/')
+kubectl -n scrumlr create secret generic postgresql-creds --from-literal=url="$DB_URL_SCRUMLR" --dry-run=client -o yaml | kubectl apply -f -
 
 # Check if ECDSA key already exists
 if ! kubectl -n scrumlr get secret scrumlr-ecdsa-key >/dev/null 2>&1; then
@@ -105,12 +113,12 @@ else
   echo "ECDSA key already exists, skipping key generation."
 fi
 
-# check if DB exists
-if ! psql "$DB_URL" -lqt | cut -d \| -f 1 | grep -qw scrumlr; then
-  echo "Database does not exist, creating database."
-else
-  echo "Database already exists, skipping database creation."
-fi
+export DB_URL_SCRUMLR="$DB_URL_SCRUMLR"
+# Apply the database creation job
+envsubst < create_db_job.yaml | kubectl apply -f -
+
+# Wait for the job to complete
+kubectl -n scrumlr wait --for=condition=complete job/create-db-job
 
 # Apply cluster issuer
 kubectl apply -f cluster_issuer.yaml
--- docs/src/content/docs/self-hosting/ske.md
+++ docs/src/content/docs/self-hosting/ske.md
@@ -7,11 +7,45 @@ sidebar:
 
 ## Pre-requisites
 The following pre-requisites are required to deploy Scrumlr using SKE:
-- StackIT 
+- StackIT Project
+- Service Account with a valid Token for the StackIT API
 
-## Deploy SKE and Postgres Flex using Terraform
-In order to gurantee the correct deployment of SKE and Postgres Flex, you can use our premade Terraform code.
+## Deployment
 
+### Clone the Repository
 ```sh
-git clone (REPO)
+git clone https://github.com/inovex/scrumlr.io.git
+```
+```sh
+cd scrumlr.io/deployment/ske
+```
+
+### Deploy SKE and Postgres Flex using Terraform
+In order to gurantee the correct deployment of SKE and Postgres Flex, you can use our premade Terraform code.
+```sh
+cd terraform
+```
+You need to create a `terraform.tfvars` file with the following variables:
+```
+service_account_token = "YOUR_SERVICE_ACCOUNT_TOKEN
+project_id = "YOUR_PROJECT_ID"
+```
+Now you can deploy SKE and Postgres Flex using Terraform.
+```sh
+terraform init
+terraform apply
+```
+
+### Deploy Scrumlr
+Now that you have successfully deployed SKE and Postgres Flex, you can deploy Scrumlr.
+We have created a bash script which will bootstrap the deployment of Scrumlr.
+For this to work you need an active `kubectl` context.
+If you used the terraform code above, we recommend using `kubecm`for managing your kubectl contexts.
+You can install kubecm from [here](https://kubecm.cloud/en-us/install).
+```sh
+cd ..
+kubecm add  -f kubeconfig.yaml
+```
+```sh
+./deploy.sh
 ```
