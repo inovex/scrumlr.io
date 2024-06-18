757f60158382ac9e4f03e7d775dad316c6b0c937
Jakob Schwehn <schwehn42@gmail.com>
2024-06-18 17:01:04 +0200

    add className prop to UserPill

--- src/components/UserPill/UserPill.tsx
+++ src/components/UserPill/UserPill.tsx
@@ -1,12 +1,17 @@
 import {useAppSelector} from "store";
 import StanAvatar from "assets/stan/Stan_Avatar.png";
+import classNames from "classnames";
 import "./UserPill.scss";
 
-export const UserPill = () => {
+type UserPillProps = {
+  className: string;
+};
+
+export const UserPill = ({className}: UserPillProps) => {
   const myName = useAppSelector((state) => state.auth.user?.name!);
 
   return (
-    <div className="user-pill">
+    <div className={classNames("user-pill", className)}>
       <div className="user-pill__avatar-container">
         <img src={StanAvatar} className="user-pill__avatar" alt="Stan Avatar" />
       </div>
