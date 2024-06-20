9c209e8567efb50dc46a6531acdad791259cf244
Jakob Schwehn <schwehn42@gmail.com>
2024-06-20 14:50:40 +0200

    add stan to templates

--- src/routes/NewBoard/Templates/Templates.tsx
+++ src/routes/NewBoard/Templates/Templates.tsx
@@ -1,7 +1,11 @@
+import Stan from "assets/stan/Stan_Hanging_With_Coffee_Cropped.png";
 import "./Templates.scss";
 
 export const Templates = () => (
   <div className="templates">
+    <div className="templates__stan-container">
+      <img className="templates__stan" src={Stan} alt="Stan just hanging there with a coffee" />
+    </div>
     {/* TODO: display saved templates only when user isn't anonymous (blocked by #4229) */}
     <div className="templates__container templates__container--saved">
       <div className="templates__container-title">Your templates</div>
