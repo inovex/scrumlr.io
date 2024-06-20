5914db0a7fa68aa2a5916bc19a16558fa4ba4617
Jakob Schwehn <schwehn42@gmail.com>
2024-06-20 13:38:00 +0200

    container titles

--- src/routes/NewBoard/Templates/Templates.tsx
+++ src/routes/NewBoard/Templates/Templates.tsx
@@ -2,7 +2,12 @@ import "./Templates.scss";
 
 export const Templates = () => (
   <div className="templates">
-    <div className="templates__container templates__container--saved" />
-    <div className="templates__container templates__contaner--recommended" />
+    {/* TODO: display saved templates only when user isn't anonymous (blocked by #4229) */}
+    <div className="templates__container templates__container--saved">
+      <div className="templates__container-title">Your templates</div>
+    </div>
+    <div className="templates__container templates__contaner--recommended">
+      <div className="templates__container-title">Our Recommendations</div>
+    </div>
   </div>
 );
