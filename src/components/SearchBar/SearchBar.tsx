4327d010807ae8aa7100ffdfff7a5df8b1293d5f
Jakob Schwehn <schwehn42@gmail.com>
2024-06-21 14:14:39 +0200

    search bar disabled state

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -5,17 +5,18 @@ import "./SearchBar.scss";
 
 type SearchBarProps = {
   className?: string;
+  disabled?: boolean;
 };
 
 export const SearchBar = (props: SearchBarProps) => {
   const {t} = useTranslation();
 
   return (
-    <div className={classNames("search-bar", props.className)}>
+    <div className={classNames("search-bar", {"search-bar--disabled": props.disabled}, props.className)}>
       <div className="search-bar__logo-container">
         <SearchLogo className="search-bar__logo" aria-label="logo of magnifying glass" />
       </div>
-      <input className="search-bar__input" type="text" placeholder={t("SearchBar.placeholder")} />
+      <input className="search-bar__input" type="text" placeholder={t("SearchBar.placeholder")} disabled={props.disabled} />
     </div>
   );
 };
