0b4df662fa1443000da639c09be62910ffcc9cf4
Jakob Schwehn <schwehn42@gmail.com>
2024-06-21 15:12:20 +0200

    add disabled state to UserPill

--- src/components/UserPill/UserPill.tsx
+++ src/components/UserPill/UserPill.tsx
@@ -4,17 +4,19 @@ import classNames from "classnames";
 import "./UserPill.scss";
 
 type UserPillProps = {
-  className: string;
+  className?: string;
+  disabled?: boolean;
 };
 
-export const UserPill = ({className}: UserPillProps) => {
+export const UserPill = (props: UserPillProps) => {
   const myName = useAppSelector((state) => state.auth.user?.name!);
 
   return (
-    <button className={classNames("user-pill", className)} tabIndex={0}>
+    <button className={classNames("user-pill", props.className)} disabled={props.disabled} tabIndex={0}>
       <div className="user-pill__container">
         <div className="user-pill__avatar-container">
           <img src={StanAvatar} className="user-pill__avatar" alt="Stan Avatar" />
+          <div className="user-pill__avatar-overlay" />
         </div>
         <div className="user-pill__name">{myName}</div>
       </div>
