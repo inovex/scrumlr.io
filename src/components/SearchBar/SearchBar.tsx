0391fe715016f2bbf753959308fa1968d63622d6
Jakob Schwehn <schwehn42@gmail.com>
2024-06-22 15:37:07 +0200

    add close icon

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,22 +1,27 @@
 import {useTranslation} from "react-i18next";
 import classNames from "classnames";
-import {ReactComponent as SearchLogo} from "assets/icons/search.svg";
+import {ReactComponent as SearchIcon} from "assets/icons/search.svg";
+import {ReactComponent as CloseIcon} from "assets/icons/close.svg";
 import "./SearchBar.scss";
 
 type SearchBarProps = {
   className?: string;
   disabled?: boolean;
+  closable?: boolean;
 };
 
 export const SearchBar = (props: SearchBarProps) => {
   const {t} = useTranslation();
 
   return (
-    <div className={classNames("search-bar", {"search-bar--disabled": props.disabled}, props.className)}>
-      <div className="search-bar__logo-container">
-        <SearchLogo className="search-bar__logo" aria-label="logo of magnifying glass" />
+    <div className={classNames("search-bar", {"search-bar--disabled": props.disabled, "search-bar--closable": props.closable}, props.className)}>
+      <div className="search-bar__icon-container search-bar__icon-container--search-icon">
+        <SearchIcon className="search-bar__icon" aria-label="logo of magnifying glass" />
       </div>
       <input className="search-bar__input" type="text" placeholder={t("SearchBar.placeholder")} disabled={props.disabled} />
+      <div className="search-bar__icon-container search-bar__icon-container--close-icon" role="button">
+        <CloseIcon className="search-bar__icon" aria-label="close button" />
+      </div>
     </div>
   );
 };
