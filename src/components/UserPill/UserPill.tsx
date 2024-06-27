391035f766a69167b51a58b2175c2579b39d884f
Jakob Schwehn <schwehn42@gmail.com>
2024-06-27 09:29:54 +0200

    render custom avatar if available

--- src/components/UserPill/UserPill.tsx
+++ src/components/UserPill/UserPill.tsx
@@ -2,6 +2,7 @@ import {useAppSelector} from "store";
 import {useNavigate} from "react-router-dom";
 import StanAvatar from "assets/stan/Stan_Avatar.png";
 import classNames from "classnames";
+import {Avatar} from "components/Avatar";
 import "./UserPill.scss";
 
 type UserPillProps = {
@@ -12,17 +13,26 @@ type UserPillProps = {
 
 export const UserPill = (props: UserPillProps) => {
   const navigate = useNavigate();
+  const me = useAppSelector((state) => state.auth.user)!; // TODO omit this after #4270, since seed is optional then
   const myName = useAppSelector((state) => state.auth.user?.name);
+  const avatar = useAppSelector((state) => state.auth.user?.avatar);
 
   const openSettings = () => {
     navigate(`${props.locationPrefix ?? ""}/settings/profile`);
   };
 
+  const renderAvatar = () =>
+    avatar ? (
+      <Avatar seed={me.id} avatar={avatar} className={classNames("user-pill__avatar", "user-pill__avatar--own")} />
+    ) : (
+      <img src={StanAvatar} className={classNames("user-pill__avatar", "user-pill__avatar--stan")} alt="Stan Avatar" />
+    );
+
   return (
     <button className={classNames("user-pill", props.className)} disabled={props.disabled} tabIndex={0} onClick={openSettings}>
       <div className="user-pill__container">
         <div className="user-pill__avatar-container">
-          <img src={StanAvatar} className="user-pill__avatar" alt="Stan Avatar" />
+          {renderAvatar()}
           <div className="user-pill__avatar-overlay" />
         </div>
         <div className="user-pill__name">{myName}</div>
