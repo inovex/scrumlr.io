e83adb97e367cc3a59e9baf8004946ddd72dff29
Jakob Schwehn <schwehn42@gmail.com>
2024-06-28 11:14:18 +0200

    add arrows

--- src/routes/NewBoard/Templates/Templates.tsx
+++ src/routes/NewBoard/Templates/Templates.tsx
@@ -1,8 +1,11 @@
+import classNames from "classnames";
 import {Outlet} from "react-router-dom";
 import {useAppSelector} from "store";
 import {useTranslation} from "react-i18next";
 // using a png instead of svg for now. reason being problems with layering
 import Stan from "assets/stan/Stan_Hanging_With_Coffee_Cropped.png";
+import {ReactComponent as ArrowLeft} from "assets/icons/arrow-left.svg";
+import {ReactComponent as ArrowRight} from "assets/icons/arrow-right.svg";
 import "./Templates.scss";
 
 export const Templates = () => {
@@ -10,10 +13,12 @@ export const Templates = () => {
   const isAnonymous = useAppSelector((state) => state.auth.user?.isAnonymous) ?? true;
 
   const renderContainerHeader = (title: string) => (
-      <header className="templates__container-header">
-        <div className="templates__container-title">{title}</div>
-      </header>
-    );
+    <header className="templates__container-header">
+      <ArrowLeft className={classNames("templates__container-arrow", "templates__container-arrow--left")} />
+      <div className="templates__container-title">{title}</div>
+      <ArrowRight className={classNames("templates__container-arrow", "templates__container-arrow--right")} />
+    </header>
+  );
 
   return (
     <>
