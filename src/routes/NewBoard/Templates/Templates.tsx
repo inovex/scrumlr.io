05f3b9a1e446bb388849fa2b75712a3c5b2e6b2b
Jakob Schwehn <schwehn42@gmail.com>
2024-06-28 11:34:46 +0200

    disable arrow color

--- src/routes/NewBoard/Templates/Templates.tsx
+++ src/routes/NewBoard/Templates/Templates.tsx
@@ -8,15 +8,17 @@ import {ReactComponent as ArrowLeft} from "assets/icons/arrow-left.svg";
 import {ReactComponent as ArrowRight} from "assets/icons/arrow-right.svg";
 import "./Templates.scss";
 
+type Side = "left" | "right";
+
 export const Templates = () => {
   const {t} = useTranslation();
   const isAnonymous = useAppSelector((state) => state.auth.user?.isAnonymous) ?? true;
 
-  const renderContainerHeader = (title: string) => (
+  const renderContainerHeader = (side: Side, title: string) => (
     <header className="templates__container-header">
-      <ArrowLeft className={classNames("templates__container-arrow", "templates__container-arrow--left")} />
+      <ArrowLeft className={classNames("templates__container-arrow", "templates__container-arrow--left", {"templates__container-arrow--disabled": side === "left"})} />
       <div className="templates__container-title">{title}</div>
-      <ArrowRight className={classNames("templates__container-arrow", "templates__container-arrow--right")} />
+      <ArrowRight className={classNames("templates__container-arrow", "templates__container-arrow--right", {"templates__container-arrow--disabled": side === "right"})} />
     </header>
   );
 
@@ -27,8 +29,8 @@ export const Templates = () => {
         <div className="templates__stan-container">
           <img className="templates__stan" src={Stan} alt="Stan just hanging there with a coffee" />
         </div>
-        {!isAnonymous && <div className="templates__container templates__container--saved">{renderContainerHeader(t("Templates.savedTemplates"))}</div>}
-        <div className="templates__container templates__container--recommended">{renderContainerHeader(t("Templates.recommendedTemplates"))}</div>
+        {!isAnonymous && <div className="templates__container templates__container--saved">{renderContainerHeader("left", t("Templates.savedTemplates"))}</div>}
+        <div className="templates__container templates__container--recommended">{renderContainerHeader("right", t("Templates.recommendedTemplates"))}</div>
       </div>
     </>
   );
