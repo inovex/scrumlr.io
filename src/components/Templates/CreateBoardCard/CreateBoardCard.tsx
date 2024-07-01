e5958c1385857cc16651289cc46ef9f84b83ab18
Jakob Schwehn <schwehn42@gmail.com>
2024-07-01 15:28:28 +0200

    add content

--- src/components/Templates/CreateBoardCard/CreateBoardCard.tsx
+++ src/components/Templates/CreateBoardCard/CreateBoardCard.tsx
@@ -1,1 +1,9 @@
-export const CreateBoardCard = () => <div className="create-board-card" />;
+import {ReactComponent as PlusIcon} from "assets/icons/plus.svg";
+import "./CreateBoardCard.scss";
+
+export const CreateBoardCard = () => (
+  <div className="create-board-card">
+    <PlusIcon className="create-board-card__icon" />
+    <div className="create-board-card__title">Create new board</div>
+  </div>
+);
