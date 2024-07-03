0fc65dd1af33345ea28e089c810cca7fecc534f4
Oxymoron <71148001+blxckOxymoron@users.noreply.github.com>
2024-07-03 22:23:23 +0200

    only scroll container vertically
    - to fix horizontal scrolling on firefox

--- src/components/EmojiSuggestions/EmojiSuggestions.tsx
+++ src/components/EmojiSuggestions/EmojiSuggestions.tsx
@@ -15,17 +15,37 @@ type EmojiSuggestionsProps = {
   };
 };
 
+const PADDING = 8;
+
 export const EmojiSuggestions = ({autocomplete: {suggestions, keyboardFocusedIndex, acceptSuggestion}, approxTopDistance}: EmojiSuggestionsProps) => {
   // the refs of the elements to scroll into view
   const suggestionsRef = useRef<HTMLLIElement[]>([]);
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     // whenever the suggestions change, remove the old refs
     suggestionsRef.current = suggestionsRef.current.slice(0, suggestions.length);
 
     // scroll the currently selected element into view
-    suggestionsRef.current[keyboardFocusedIndex]?.scrollIntoView({behavior: "instant", block: "nearest", inline: "nearest"});
-    // suggestionsRef.current[keyboardFocusedIndex]?.focus();
+    // can't use scrollIntoView directly because it scrolls the whole container horizontally on firefox (see https://github.com/inovex/scrumlr.io/pull/4274)
+    const focusedElementRect = suggestionsRef.current[keyboardFocusedIndex]?.getBoundingClientRect();
+    const containerRect = containerRef.current?.getBoundingClientRect();
+    if (!focusedElementRect || !containerRect || !containerRef.current) return;
+
+    // a) is inside (doesn't care about padding) -> no need to scroll
+    if (focusedElementRect.top >= containerRect.top && focusedElementRect.bottom <= containerRect.bottom) return;
+
+    const scrollPosition =
+      focusedElementRect.top < containerRect.top
+        ? // b) is above
+          containerRef.current.scrollTop + focusedElementRect.top - containerRect.top - PADDING
+        : // c) is below
+          containerRef.current.scrollTop + focusedElementRect.bottom - containerRect.bottom + PADDING;
+
+    containerRef.current?.scrollTo({
+      top: scrollPosition,
+      behavior: "instant",
+    });
   }, [keyboardFocusedIndex, suggestions]);
 
   const skinToneComponent = useAppSelector((state) => state.skinTone.component);
@@ -33,7 +53,7 @@ export const EmojiSuggestions = ({autocomplete: {suggestions, keyboardFocusedInd
   if (suggestions.length === 0) return null;
 
   return (
-    <div className="emoji-suggestions emoji-suggestions__container" style={{"--approx-top-distance": approxTopDistance} as CSSProperties}>
+    <div className="emoji-suggestions emoji-suggestions__container" style={{"--approx-top-distance": approxTopDistance} as CSSProperties} ref={containerRef}>
       <ul className="emoji-suggestions__list">
         {suggestions.map(([slug, emoji, supportsSkintones], i) => {
           const actualEmoji = supportsSkintones ? emojiWithSkinTone(emoji, skinToneComponent) : emoji;
