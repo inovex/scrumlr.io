d277dfcecfc0a3f271ef7266460d3db719cb71eb
Jakob Schwehn <schwehn42@gmail.com>
2024-07-09 13:54:46 +0200

    add yielding yellow and info state colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,6 +1,6 @@
-export type Color = "backlog-blue" | "grooming-green" | "lean-lilac" | "online-orange" | "planning-pink" | "poker-purple" | "retro-red";
+export type Color = "backlog-blue" | "grooming-green" | "lean-lilac" | "online-orange" | "planning-pink" | "poker-purple" | "yielding-yellow";
 
-const COLOR_ORDER: Color[] = ["backlog-blue", "lean-lilac", "planning-pink", "retro-red", "grooming-green", "poker-purple", "online-orange"];
+const COLOR_ORDER: Color[] = ["backlog-blue", "lean-lilac", "planning-pink", "yielding-yellow", "grooming-green", "poker-purple", "online-orange"];
 export const getColorForIndex = (index: number) => COLOR_ORDER[index % COLOR_ORDER.length];
 
 export const getColorClassName = (color: Color | undefined) => `accent-color__${color ?? COLOR_ORDER[0]}`;
