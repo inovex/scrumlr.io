a696e17fe0e0cb8fe844272348291a8017fb0377
Jakob Schwehn <schwehn42@gmail.com>
2024-07-12 13:26:56 +0200

    add color prop

--- src/components/Button/PrimaryButton/PrimaryButton.tsx
+++ src/components/Button/PrimaryButton/PrimaryButton.tsx
@@ -5,6 +5,7 @@ import "./PrimaryButton.scss";
 type PrimaryButtonProps = {
   children?: React.ReactNode;
   className?: string;
+  color: "pink" | "blue";
   small?: boolean;
   icon?: React.ReactNode;
 
@@ -12,7 +13,18 @@ type PrimaryButtonProps = {
 };
 
 export const PrimaryButton = (props: PrimaryButtonProps) => (
-  <button className={classNames("primary-button", {"primary-button--small": props.small, "primary-button--with-icon": props.icon}, props.className)}>
+  <button
+    className={classNames(
+      "primary-button",
+      {
+        "primary-button--small": props.small,
+        "primary-button--with-icon": props.icon,
+        "primary-button--pink": props.color === "pink",
+        "primary-button--blue": props.color === "blue",
+      },
+      props.className
+    )}
+  >
     {props.children}
     {props.icon}
   </button>
