7709abd254274282aeeccff40454544f03a46a52
Jakob Schwehn <schwehn42@gmail.com>
2024-07-12 12:49:57 +0200

    add props and children

--- src/components/Button/PrimaryButton/PrimaryButton.tsx
+++ src/components/Button/PrimaryButton/PrimaryButton.tsx
@@ -1,3 +1,11 @@
+import React from "react";
+import classNames from "classnames";
 import "./PrimaryButton.scss";
 
-export const PrimaryButton = () => <div />;
+type PrimaryButtonProps = {
+  className?: string;
+  children?: React.ReactNode;
+  onClick?: () => void;
+};
+
+export const PrimaryButton = (props: PrimaryButtonProps) => <button className={classNames("primary-button", props.className)}>{props.children}</button>;
