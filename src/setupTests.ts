e8b0e932b949773169d974137284cde2eb553970
Jakob Schwehn <schwehn42@gmail.com>
2024-07-15 18:03:57 +0200

    use different workaround

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,23 +2,24 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-// eslint-disable-next-line import/no-extraneous-dependencies
-import MatchMediaMock from "jest-matchmedia-mock";
 import "@testing-library/jest-dom";
 
 global.ResizeObserver = require("resize-observer-polyfill");
 
 // jest can't handle match media, and the official workaround doesn't work (lol):
 // https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-// fortunately, there's a solution:
+// the package to mock it correctly almost works, but not quite:
 // https://yarnpkg.com/package?q=jest-matchmedia&name=jest-matchmedia-mock
+// fortunately, this seems to do the trick:
+// https://github.com/facebook/create-react-app/issues/10126#issuecomment-735272763
 
-let matchMedia: MatchMediaMock;
-
-beforeAll(() => {
-  matchMedia = new MatchMediaMock();
-});
-
-afterAll(() => {
-  matchMedia.clear();
+window.matchMedia = (query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(), // deprecated
+  removeListener: jest.fn(), // deprecated
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
 });
