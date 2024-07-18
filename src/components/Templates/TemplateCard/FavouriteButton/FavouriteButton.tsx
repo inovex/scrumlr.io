77bf08a80a377d929a7fbc3791c7919ba8a60e10
Jakob Schwehn <schwehn42@gmail.com>
2024-07-18 18:01:07 +0200

    basic layout and style

--- src/components/Templates/TemplateCard/FavouriteButton/FavouriteButton.tsx
+++ src/components/Templates/TemplateCard/FavouriteButton/FavouriteButton.tsx
@@ -1,3 +1,16 @@
+import classNames from "classnames";
+import {ReactComponent as FavouriteIcon} from "assets/icons/star.svg";
 import "./FavouriteButton.scss";
 
-export const FavouriteButton = () => <div>Favourite</div>;
+type FavouriteButtonProps = {
+  className?: string;
+  active: boolean;
+  onClick: () => void;
+  disabled?: boolean;
+};
+
+export const FavouriteButton = (props: FavouriteButtonProps) => (
+  <button className={classNames(props.className, "favourite-button", {"favourite-button--active": props.active})} onClick={props.onClick} disabled={props.disabled}>
+    <FavouriteIcon className="favourite-button__icon" />
+  </button>
+);
