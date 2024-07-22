53fc6db215009ac9d7a48ddb862133be7e6d78e4
Jakob Schwehn <schwehn42@gmail.com>
2024-07-22 16:14:54 +0200

    add tooltip to favourite button

--- src/components/Templates/TemplateCard/FavouriteButton/FavouriteButton.tsx
+++ src/components/Templates/TemplateCard/FavouriteButton/FavouriteButton.tsx
@@ -1,7 +1,10 @@
 import classNames from "classnames";
 import {getColorClassName} from "constants/colors";
 import {ReactComponent as FavouriteIcon} from "assets/icons/star.svg";
+import {Tooltip} from "components/Tooltip";
+import {uniqueId} from "underscore";
 import "./FavouriteButton.scss";
+import {useTranslation} from "react-i18next";
 
 type FavouriteButtonProps = {
   className?: string;
@@ -9,11 +12,20 @@ type FavouriteButtonProps = {
   onClick: () => void;
 };
 
-export const FavouriteButton = (props: FavouriteButtonProps) => (
-  <FavouriteIcon
-    className={classNames(props.className, "favourite-button", {"favourite-button--active": props.active}, getColorClassName("planning-pink"))}
-    role="button"
-    onClick={props.onClick}
-    tabIndex={0}
-  />
-);
+export const FavouriteButton = (props: FavouriteButtonProps) => {
+  const {t} = useTranslation();
+  const anchor = uniqueId("favourite-button-");
+
+  return (
+    <>
+      <FavouriteIcon
+        id={anchor}
+        className={classNames(props.className, "favourite-button", {"favourite-button--active": props.active}, getColorClassName("planning-pink"))}
+        role="button"
+        onClick={props.onClick}
+        tabIndex={0}
+      />
+      <Tooltip anchorSelect={`#${anchor}`} content={props.active ? t("Templates.TemplateCard.removeFavourite") : t("Templates.TemplateCard.addFavourite")} color="backlog-blue" />
+    </>
+  );
+};
