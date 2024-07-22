ff3f51e386ea2f392c0e82ef5d325e0d26244d5b
Jakob Schwehn <schwehn42@gmail.com>
2024-07-22 16:30:26 +0200

    create template translation

--- src/components/Templates/CreateTemplateCard/CreateTemplateCard.tsx
+++ src/components/Templates/CreateTemplateCard/CreateTemplateCard.tsx
@@ -1,4 +1,5 @@
 import {ReactComponent as PlusIcon} from "assets/icons/plus.svg";
+import {useTranslation} from "react-i18next";
 import "./CreateTemplateCard.scss";
 
 type CreateTemplateCardProps = {
@@ -6,9 +7,13 @@ type CreateTemplateCardProps = {
   disabled?: boolean;
 };
 
-export const CreateTemplateCard = (props: CreateTemplateCardProps) => (
-  <button className="create-template-card" disabled={props.disabled} onClick={props.onClick}>
-    <PlusIcon className="create-template-card__icon" />
-    <div className="create-template-card__title">Create new template</div>
-  </button>
-);
+export const CreateTemplateCard = (props: CreateTemplateCardProps) => {
+  const {t} = useTranslation();
+
+  return (
+    <button className="create-template-card" disabled={props.disabled} onClick={props.onClick}>
+      <PlusIcon className="create-template-card__icon" />
+      <div className="create-template-card__title">{t("Templates.CreateTemplateCard.create")}</div>
+    </button>
+  );
+};
