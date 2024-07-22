bb820b385e58414529d8eee6d2f678d8aa2586f3
Jakob Schwehn <schwehn42@gmail.com>
2024-07-22 16:24:34 +0200

    add column translation

--- src/components/Templates/TemplateCard/TemplateCard.tsx
+++ src/components/Templates/TemplateCard/TemplateCard.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import {useTranslation} from "react-i18next";
 import {Button} from "components/Button";
 import {FavouriteButton} from "components/Templates";
 import {ReactComponent as MenuIcon} from "assets/icons/three-dots.svg";
@@ -6,19 +7,23 @@ import {ReactComponent as ColumnsIcon} from "assets/icons/columns.svg";
 import {ReactComponent as NextIcon} from "assets/icons/next.svg";
 import "./TemplateCard.scss";
 
-export const TemplateCard = () => (
-  <div className="template-card">
-    <FavouriteButton className="template-card__favourite" active={false} onClick={() => {}} />
-    <div className="template-card__title">Title</div>
-    <MenuIcon className={classNames("template-card__icon", "template-card__icon--menu")} />
-    <div className="template-card__description">Lorem ipsum dolor sit amet, conse dolo sadipscing elitr vero eos et aquiteres.</div>
-    <ColumnsIcon className={classNames("template-card__icon", "template-card__icon--columns")} />
-    <div className="template-card__columns">
-      <div className="template-card__columns-title">3 Columns</div>
-      <div className="template-card__columns-subtitle">Ideas, Problems & Solutions</div>
+export const TemplateCard = () => {
+  const {t} = useTranslation();
+
+  return (
+    <div className="template-card">
+      <FavouriteButton className="template-card__favourite" active={false} onClick={() => {}} />
+      <div className="template-card__title">Title</div>
+      <MenuIcon className={classNames("template-card__icon", "template-card__icon--menu")} />
+      <div className="template-card__description">Lorem ipsum dolor sit amet, conse dolo sadipscing elitr vero eos et aquiteres.</div>
+      <ColumnsIcon className={classNames("template-card__icon", "template-card__icon--columns")} />
+      <div className="template-card__columns">
+        <div className="template-card__columns-title">{t("Templates.TemplateCard.column", {count: 3})}</div>
+        <div className="template-card__columns-subtitle">Ideas, Problems & Solutions</div>
+      </div>
+      <Button className="template-card__start-button" small icon={<NextIcon />}>
+        Start
+      </Button>
     </div>
-    <Button className="template-card__start-button" small icon={<NextIcon />}>
-      Start
-    </Button>
-  </div>
-);
+  );
+};
