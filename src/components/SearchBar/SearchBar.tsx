73cfa1b389553b43179094ae564af7545c4d327c
Jakob Schwehn <schwehn42@gmail.com>
2024-07-24 13:11:24 +0200

    make search bar stateless and clearable

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,28 +1,36 @@
+import {Dispatch, FormEvent, SetStateAction} from "react";
 import {useTranslation} from "react-i18next";
 import classNames from "classnames";
 import {ReactComponent as SearchIcon} from "assets/icons/search.svg";
-import {ReactComponent as CloseIcon} from "assets/icons/close.svg";
+import {ReactComponent as ClearIcon} from "assets/icons/close.svg";
 import "./SearchBar.scss";
 
 type SearchBarProps = {
   className?: string;
   disabled?: boolean;
-  closable?: boolean;
-  onClose?: () => void;
+  input: string;
+  setInput: Dispatch<SetStateAction<string>>;
 };
 
+/*
+ * stateless search bar component.
+ * if the input is not empty, it's clearable using the X button
+ */
 export const SearchBar = (props: SearchBarProps) => {
   const {t} = useTranslation();
 
+  const updateInput = (e: FormEvent<HTMLInputElement>) => props.setInput(e.currentTarget.value);
+  const clearInput = () => props.setInput("");
+
   return (
-    <div className={classNames("search-bar", {"search-bar--disabled": props.disabled, "search-bar--closable": props.closable}, props.className)}>
+    <div className={classNames(props.className, "search-bar", {"search-bar--disabled": props.disabled})}>
       <div className="search-bar__icon-container search-bar__icon-container--search-icon">
         <SearchIcon className="search-bar__icon" aria-label="logo of magnifying glass" />
       </div>
-      <input className="search-bar__input" type="text" placeholder={t("SearchBar.placeholder")} disabled={props.disabled} tabIndex={0} />
-      {props.closable && (
-        <div className="search-bar__icon-container search-bar__icon-container--close-icon" role="button" tabIndex={0} onClick={() => props.onClose?.()}>
-          <CloseIcon className="search-bar__icon" aria-label="close button" />
+      <input className="search-bar__input" type="text" placeholder={t("SearchBar.placeholder")} disabled={props.disabled} tabIndex={0} value={props.input} onInput={updateInput} />
+      {props.input && (
+        <div className="search-bar__icon-container search-bar__icon-container--clear-icon" role="button" tabIndex={0} onClick={clearInput}>
+          <ClearIcon className="search-bar__icon" aria-label="clear button" />
         </div>
       )}
     </div>
