ee69f59e991487c245fbbc2e184a38c0314b3cea
Benedict Homuth <bhomuth@inovex.de>
2024-07-25 09:57:35 +0200

    improved logging for column templates

--- server/src/services/board_templates/column_templates.go
+++ server/src/services/board_templates/column_templates.go
@@ -14,7 +14,7 @@ func (s *BoardTemplateService) CreateColumnTemplate(ctx context.Context, body dt
 	log := logger.FromContext(ctx)
 	tColumn, err := s.database.CreateColumnTemplate(database.ColumnTemplateInsert{BoardTemplate: body.BoardTemplate, Name: body.Name, Description: body.Description, Color: body.Color, Visible: body.Visible, Index: body.Index})
 	if err != nil {
-		log.Errorw("unable to create column template", "err", err)
+		log.Errorw("unable to create column template", "user", body.User, "err", err)
 		return nil, err
 	}
 	return new(dto.ColumnTemplate).From(tColumn), err
@@ -24,7 +24,7 @@ func (s *BoardTemplateService) GetColumnTemplate(ctx context.Context, boardTempl
 	log := logger.FromContext(ctx)
 	tColumn, err := s.database.GetColumnTemplate(boardTemplate, columnTemplate)
 	if err != nil {
-		log.Errorw("unable to get template column", "board", boardTemplate, "error", err)
+		log.Errorw("unable to get template column", "board", boardTemplate, "err", err)
 		return nil, fmt.Errorf("unable to get template column: %w", err)
 	}
 	return new(dto.ColumnTemplate).From(tColumn), err
@@ -34,7 +34,7 @@ func (s *BoardTemplateService) ListColumnTemplates(ctx context.Context, boardTem
 	log := logger.FromContext(ctx)
 	tColumns, err := s.database.ListColumnTemplates(boardTemplate)
 	if err != nil {
-		log.Errorw("unable to get template columns", "board", boardTemplate, "error", err)
+		log.Errorw("unable to get template columns", "board", boardTemplate, "err", err)
 		return nil, fmt.Errorf("unable to get template columns: %w", err)
 	}
 	return dto.ColumnTemplates(tColumns), err
@@ -44,7 +44,7 @@ func (s *BoardTemplateService) UpdateColumnTemplate(ctx context.Context, body dt
 	log := logger.FromContext(ctx)
 	tColumn, err := s.database.UpdateColumnTemplate(database.ColumnTemplateUpdate{ID: body.ID, BoardTemplate: body.BoardTemplate, Name: body.Name, Description: body.Description, Color: body.Color, Visible: body.Visible, Index: body.Index})
 	if err != nil {
-		log.Errorw("unable to update column template", "err", err)
+		log.Errorw("unable to update column template", "board", body.BoardTemplate, "column", body.ID, "err", err)
 		return nil, err
 	}
 	return new(dto.ColumnTemplate).From(tColumn), err
@@ -54,7 +54,7 @@ func (s *BoardTemplateService) DeleteColumnTemplate(ctx context.Context, board, 
 	log := logger.FromContext(ctx)
 	err := s.database.DeleteColumnTemplate(board, column, user)
 	if err != nil {
-		log.Errorw("unable to delete column template", "err", err)
+		log.Errorw("unable to delete column template", "board", board, "column", column, "err", err)
 		return err
 	}
 	return err
