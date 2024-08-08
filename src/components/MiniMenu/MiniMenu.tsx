71839c83897e9e508eb7dc2960d097a57ac7845b
Jakob Schwehn <schwehn42@gmail.com>
2024-08-08 11:49:06 +0200

    add MiniMenu props

--- src/components/MiniMenu/MiniMenu.tsx
+++ src/components/MiniMenu/MiniMenu.tsx
@@ -1,5 +1,16 @@
+import {ReactNode} from "react";
 import "./MiniMenu.scss";
 
-type MiniMenuProps = {};
+type MiniMenuItem = {
+  icon: ReactNode;
+  label: string;
+  active: boolean;
+  onClick: () => void;
+};
+
+type MiniMenuProps = {
+  open: boolean;
+  items: MiniMenuItem[];
+};
 
 export const MiniMenu = (props: MiniMenuProps) => <div>Hello MiniMenu</div>;
