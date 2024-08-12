4ca55deac77a01ee48f077189a3494fb656c2a4b
Jakob Schwehn <schwehn42@gmail.com>
2024-08-12 17:29:13 +0200

    make dropdown stateless

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import {ReactNode, useState} from "react";
+import {ReactNode} from "react";
 import classNames from "classnames";
 import {ReactComponent as ArrowIcon} from "assets/icons/arrow-down.svg";
 import "./Dropdown.scss";
@@ -10,30 +10,27 @@ type DropdownOption = {
 };
 
 type DropdownProps = {
+  open: boolean;
   options: DropdownOption[];
   activeKey: string;
+
+  onToggleMenu: () => void;
   onSelect: (id: string) => void;
 };
 
 export const Dropdown = (props: DropdownProps) => {
-  const [open, setOpen] = useState(false);
-
   const activeOption = props.options.find((option) => option.key === props.activeKey) ?? props.options[0];
 
-  const toggleMenu = () => {
-    setOpen((curr) => !curr);
-  };
-
   return (
-    <div className={classNames("dropdown", {"dropdown--open": open})}>
-      <div className={classNames("dropdown__option", "dropdown__option--active")} key={activeOption.key} role="button" onClick={toggleMenu}>
+    <div className={classNames("dropdown", {"dropdown--open": props.open})}>
+      <div className={classNames("dropdown__option", "dropdown__option--active")} key={activeOption.key} role="button" onClick={props.onToggleMenu}>
         <div className="dropdown__option-icon">{activeOption.icon}</div>
         <div className="dropdown__option-label">{activeOption.label}</div>
-        <div className={classNames("dropdown__option-arrow", {"dropdown__option-arrow--up": open, "dropdown__option-arrow--down": !open})}>
+        <div className={classNames("dropdown__option-arrow", {"dropdown__option-arrow--up": props.open, "dropdown__option-arrow--down": !props.open})}>
           <ArrowIcon />
         </div>
       </div>
-      {open &&
+      {props.open &&
         props.options
           .filter((option) => option.key !== props.activeKey)
           .map((option) => (
--- src/routes/Boards/CreateTemplate/CreateTemplate.tsx
+++ src/routes/Boards/CreateTemplate/CreateTemplate.tsx
@@ -1,23 +1,32 @@
+import {useState} from "react";
 import {Dropdown} from "components/Dropdown/Dropdown";
 import {ReactComponent as GlobeIcon} from "assets/icons/open.svg";
 import {ReactComponent as KeyIcon} from "assets/icons/key-protected.svg";
 import {ReactComponent as LockIcon} from "assets/icons/lock-closed.svg";
 import "./CreateTemplate.scss";
-import {useState} from "react";
 
 export const CreateTemplate = () => {
+  const [openDropdown, setOpenDropdown] = useState(false);
   const [activeOptionKey, setActiveOptionKey] = useState<string>("open");
+
+  const toggleDropDown = () => setOpenDropdown((curr) => !curr);
+  const selectDropdownOption = (key: string) => {
+    setActiveOptionKey(key);
+    setOpenDropdown(false);
+  };
 
   return (
     <div className="create-template">
       <Dropdown
+        open={openDropdown}
         options={[
           {key: "open", label: "Open", icon: <GlobeIcon />},
           {key: "password", label: "Password", icon: <KeyIcon />},
           {key: "approval", label: "Approval", icon: <LockIcon />},
         ]}
         activeKey={activeOptionKey}
-        onSelect={(key) => setActiveOptionKey(key)}
+        onToggleMenu={toggleDropDown}
+        onSelect={selectDropdownOption}
       />
     </div>
   );
