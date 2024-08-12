d1a42afc8f04e5bfb761053a89c22e1f6df9e068
Jakob Schwehn <schwehn42@gmail.com>
2024-08-12 14:22:26 +0200

    basic menu render

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,4 +1,6 @@
-import {ReactNode} from "react";
+import {ReactNode, useState} from "react";
+import classNames from "classnames";
+import {ReactComponent as ArrowIcon} from "assets/icons/arrow-down.svg";
 import "./Dropdown.scss";
 
 type DropdownOption = {
@@ -12,4 +14,24 @@ type DropdownProps = {
   onSelect?: (index: number) => void;
 };
 
-export const Dropdown = (props: DropdownProps) => <div>Hello Dropdown</div>;
+export const Dropdown = (props: DropdownProps) => {
+  const [open, setOpen] = useState(false);
+
+  const activeOption = props.options.find((_, index) => index === props.activeIndex)!;
+
+  const toggleMenu = () => {
+    setOpen((curr) => !curr);
+  };
+
+  return (
+    <div className={classNames("dropdown", {"dropdown--open": open})}>
+      <div className={classNames("dropdown__button", "dropdown__option")} role="button" onClick={toggleMenu}>
+        <div className="dropdown__button-icon">{activeOption.icon}</div>
+        <div className="dropdown__button-label">{activeOption.label}</div>
+        <div className="dropdown__button-arrow">
+          <ArrowIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/routes/Boards/CreateTemplate/CreateTemplate.tsx
+++ src/routes/Boards/CreateTemplate/CreateTemplate.tsx
@@ -1,3 +1,18 @@
+import {Dropdown} from "components/Dropdown/Dropdown";
+import {ReactComponent as GlobeIcon} from "assets/icons/open.svg";
+import {ReactComponent as KeyIcon} from "assets/icons/key-protected.svg";
+import {ReactComponent as LockIcon} from "assets/icons/lock-closed.svg";
 import "./CreateTemplate.scss";
 
-export const CreateTemplate = () => <div className="create-template">Hello CreateTemplate</div>;
+export const CreateTemplate = () => (
+  <div className="create-template">
+    <Dropdown
+      options={[
+        {label: "Open", icon: <GlobeIcon />},
+        {label: "Password", icon: <KeyIcon />},
+        {label: "Approval", icon: <LockIcon />},
+      ]}
+      activeIndex={0}
+    />
+  </div>
+);
