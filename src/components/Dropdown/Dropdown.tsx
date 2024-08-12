c4ea44693531384073fb6ad764c49cc3b4b0dc4c
Jakob Schwehn <schwehn42@gmail.com>
2024-08-12 17:41:13 +0200

    extract render dropdown option func

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -21,25 +21,35 @@ type DropdownProps = {
 export const Dropdown = (props: DropdownProps) => {
   const activeOption = props.options.find((option) => option.key === props.activeKey) ?? props.options[0];
 
-  return (
-    <div className={classNames("dropdown", {"dropdown--open": props.open})}>
-      <div className={classNames("dropdown__option", "dropdown__option--active")} key={activeOption.key} role="button" onClick={props.onToggleMenu}>
-        <div className="dropdown__option-icon">{activeOption.icon}</div>
-        <div className="dropdown__option-label">{activeOption.label}</div>
-        <div className={classNames("dropdown__option-arrow", {"dropdown__option-arrow--up": props.open, "dropdown__option-arrow--down": !props.open})}>
+  // active refers to the option on the very top
+  const renderDropdownOption = (option: DropdownOption, active: boolean) => (
+    <div
+      className={classNames("dropdown__option", "dropdown__option--active")}
+      key={option.key}
+      role="button"
+      onClick={active ? props.onToggleMenu : () => props.onSelect(option.key)}
+    >
+      <div className="dropdown__option-icon">{option.icon}</div>
+      <div className="dropdown__option-label">{option.label}</div>
+      {active ? (
+        <div
+          className={classNames("dropdown__option-arrow", {
+            "dropdown__option-arrow--up": props.open,
+            "dropdown__option-arrow--down": !props.open,
+          })}
+        >
           <ArrowIcon />
         </div>
-      </div>
-      {props.open &&
-        props.options
-          .filter((option) => option.key !== props.activeKey)
-          .map((option) => (
-            <div className={classNames("dropdown__option")} key={option.key} role="button" onClick={() => props.onSelect(option.key)}>
-              <div className="dropdown__option-icon">{option.icon}</div>
-              <div className="dropdown__option-label">{option.label}</div>
-              <div className="dropdown__option-arrow-placeholder" /> {/* placeholder for uniform width  */}
-            </div>
-          ))}
+      ) : (
+        <div className="dropdown__option-arrow-placeholder" />
+      )}
+    </div>
+  );
+
+  return (
+    <div className={classNames("dropdown", {"dropdown--open": props.open})}>
+      {renderDropdownOption(activeOption, true)}
+      {props.open && props.options.filter((option) => option.key !== props.activeKey).map((option) => renderDropdownOption(option, false))}
     </div>
   );
 };
