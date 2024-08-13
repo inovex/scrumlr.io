6e73d5a0834093ebe5af2c39c766792ad4b5b655
Jakob Schwehn <schwehn42@gmail.com>
2024-08-13 11:00:01 +0200

    change generic type to AccessPolicy

--- src/routes/Boards/CreateTemplate/CreateTemplate.tsx
+++ src/routes/Boards/CreateTemplate/CreateTemplate.tsx
@@ -1,33 +1,32 @@
 import {useState} from "react";
 import {useTranslation} from "react-i18next";
+import {AccessPolicy} from "types/board";
 import {Dropdown} from "components/Dropdown/Dropdown";
 import {ReactComponent as GlobeIcon} from "assets/icons/open.svg";
 import {ReactComponent as KeyIcon} from "assets/icons/key-protected.svg";
 import {ReactComponent as LockIcon} from "assets/icons/lock-closed.svg";
 import "./CreateTemplate.scss";
 
-type DropdownOption = "open" | "password" | "approval";
-
 export const CreateTemplate = () => {
   const {t} = useTranslation();
 
   const [openDropdown, setOpenDropdown] = useState(false);
-  const [activeOptionKey, setActiveOptionKey] = useState<DropdownOption>("open");
+  const [activeOptionKey, setActiveOptionKey] = useState<AccessPolicy>(AccessPolicy.PUBLIC);
 
   const toggleDropDown = () => setOpenDropdown((curr) => !curr);
-  const selectDropdownOption = (key: string) => {
+  const selectDropdownOption = (key: AccessPolicy) => {
     setActiveOptionKey(key);
     setOpenDropdown(false);
   };
 
   return (
     <div className="create-template">
-      <Dropdown<DropdownOption>
+      <Dropdown<AccessPolicy>
         open={openDropdown}
         options={[
-          {key: "open", label: t("CreateBoard.dropdown.open"), icon: <GlobeIcon />},
-          {key: "password", label: t("CreateBoard.dropdown.password"), icon: <KeyIcon />},
-          {key: "approval", label: t("CreateBoard.dropdown.approval"), icon: <LockIcon />},
+          {key: AccessPolicy.PUBLIC, label: t("CreateBoard.dropdown.open"), icon: <GlobeIcon />},
+          {key: AccessPolicy.BY_PASSPHRASE, label: t("CreateBoard.dropdown.password"), icon: <KeyIcon />},
+          {key: AccessPolicy.BY_INVITE, label: t("CreateBoard.dropdown.approval"), icon: <LockIcon />},
         ]}
         activeKey={activeOptionKey}
         onToggleMenu={toggleDropDown}
