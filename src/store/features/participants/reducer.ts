ad4c6cd27697226ed5806577aa9685e2c36c3d2e
Jakob Schwehn <schwehn42@gmail.com>
2024-08-29 14:50:25 +0200

    add other participants cases

--- src/store/features/participants/reducer.ts
+++ src/store/features/participants/reducer.ts
@@ -2,7 +2,7 @@ import {createReducer} from "@reduxjs/toolkit";
 import store from "store";
 import {Participant, ParticipantsState} from "./types";
 import {initializeBoard} from "../board";
-import {setParticipants} from "./actions";
+import {clearFocusInitiator, createdParticipant, editSelf, setFocusInitiator, setParticipants, updatedParticipant} from "./actions";
 
 const initialState: ParticipantsState = {};
 
@@ -20,4 +20,26 @@ export const participantsReducer = createReducer(initialState, (builder) =>
   builder
     .addCase(initializeBoard, (_state, action) => mapParticipantsToState(action.payload.participants))
     .addCase(setParticipants, (_state, action) => mapParticipantsToState(action.payload))
+    .addCase(createdParticipant, (state, action) => {
+      state.others?.push(action.payload);
+    })
+    .addCase(updatedParticipant, (state, action) => {
+      const isSelf = action.payload.user.id === store.getState().auth.user?.id;
+      if (isSelf) {
+        state.self = action.payload;
+      } else {
+        state.others = state.others?.map((p) => (p.user.id === action.payload.user.id ? action.payload : p));
+      }
+    })
+    .addCase(editSelf, (state, action) => {
+      if (state.self?.user) {
+        state.self.user = action.payload;
+      }
+    })
+    .addCase(setFocusInitiator, (state, action) => {
+      state.focusInitiator = action.payload;
+    })
+    .addCase(clearFocusInitiator, (state) => {
+      state.focusInitiator = undefined;
+    })
 );
