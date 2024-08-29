acea03ea85dee09baa7790e7fabcd0f9cee58954
jschlinger <johanna.schlinger@fau.de>
2024-08-29 17:25:43 +0200

    add jsx to auto focus first element

--- src/components/MiniMenu/MiniMenu.tsx
+++ src/components/MiniMenu/MiniMenu.tsx
@@ -1,4 +1,4 @@
-import {ReactNode} from "react";
+import {ReactNode, useEffect, useRef} from "react";
 import classNames from "classnames";
 import {uniqueId} from "underscore";
 import "./MiniMenu.scss";
@@ -15,12 +15,22 @@ type MiniMenuProps = {
   items: MiniMenuItem[];
 };
 
-export const MiniMenu = ({className, items}: MiniMenuProps) => (
-  <div className={classNames(className, "mini-menu")}>
-    {items.map((item) => {
-      const anchor = uniqueId(`mini-menu-${item.label}`);
-      return (
-        <button
+export const MiniMenu = ({className, items}: MiniMenuProps) => {
+  const firstButtonRef = useRef<HTMLButtonElement | null>(null);
+
+  useEffect(() => {
+    if (firstButtonRef.current) {
+      firstButtonRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <div className={classNames(className, "mini-menu")}>
+      {items.map((item, index) => {
+        const anchor = uniqueId(`mini-menu-${item.label}`);
+        return (
+          <button
+            ref={index === 0 ? firstButtonRef : null}
             data-tooltip-id="scrumlr-tooltip"
             data-tooltip-content={item.label}
             aria-label={item.label}
@@ -31,7 +41,8 @@ export const MiniMenu = ({className, items}: MiniMenuProps) => (
           >
             {item.icon}
           </button>
-      );
-    })}
-  </div>
-);
+        );
+      })}
+    </div>
+  );
+};
