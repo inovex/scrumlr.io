903fb30b2f5c40a8b4808a99e94f5ac800e32229
Jakob Schwehn <schwehn42@gmail.com>
2024-08-29 12:48:01 +0200

    columns reducer p1
    without initializeBoard

---
+++ src/store/features/columns/reducer.ts
@@ -0,0 +1,21 @@
+import {TEMPORARY_COLUMN_ID} from "constants/misc";
+import {createReducer} from "@reduxjs/toolkit";
+import {ColumnsState} from "./types";
+import {createColumnOptimistically, deleteColumnOptimistically, deletedColumn, editColumnOptimistically} from "./actions";
+
+const initialState: ColumnsState = [];
+
+export const columnsReducer = createReducer(
+  initialState,
+  (builder) =>
+    builder
+      .addCase(createColumnOptimistically, (state, action) => state.splice(action.payload.index, 0, action.payload))
+      .addCase(deleteColumnOptimistically, (state) => state.filter((c) => c.id !== TEMPORARY_COLUMN_ID))
+      .addCase(editColumnOptimistically, (state, action) => {
+        const col = state.find((c) => c.id === TEMPORARY_COLUMN_ID);
+        if (col) col.name = action.payload.column.name;
+      })
+      .addCase(deletedColumn, (state, action) => state.filter((c) => c.id !== action.payload))
+  // TODO initializeBoard type
+  // .addMatcher(isAnyOf(initializeBoard, updatedColumns), (_state, action) => action.payload)
+);
