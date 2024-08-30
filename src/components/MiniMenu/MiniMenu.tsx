644cf5d7740bcadef5c6af70a26d0d30946cbb51
jschlinger <johanna.schlinger@fau.de>
2024-08-30 12:09:12 +0200

    remove jsx to auto focus first element

--- src/components/MiniMenu/MiniMenu.tsx
+++ src/components/MiniMenu/MiniMenu.tsx
@@ -1,4 +1,4 @@
-import {ReactNode, useEffect, useRef} from "react";
+import {ReactNode} from "react";
 import classNames from "classnames";
 import {uniqueId} from "underscore";
 import "./MiniMenu.scss";
@@ -15,22 +15,12 @@ type MiniMenuProps = {
   items: MiniMenuItem[];
 };
 
-export const MiniMenu = ({className, items}: MiniMenuProps) => {
-  const firstButtonRef = useRef<HTMLButtonElement | null>(null);
-
-  useEffect(() => {
-    if (firstButtonRef.current) {
-      firstButtonRef.current.focus();
-    }
-  }, []);
-
-  return (
+export const MiniMenu = ({className, items}: MiniMenuProps) => (
     <div className={classNames(className, "mini-menu")}>
-      {items.map((item, index) => {
+      {items.map((item) => {
         const anchor = uniqueId(`mini-menu-${item.label}`);
         return (
           <button
-            ref={index === 0 ? firstButtonRef : null}
             data-tooltip-id="scrumlr-tooltip"
             data-tooltip-content={item.label}
             aria-label={item.label}
@@ -45,4 +35,3 @@ export const MiniMenu = ({className, items}: MiniMenuProps) => {
       })}
     </div>
   );
-};
