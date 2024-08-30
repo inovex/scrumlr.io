5f06b86cd17fdb376ea8426e795f93dd001ab770
Jakob Schwehn <schwehn42@gmail.com>
2024-08-30 11:22:54 +0200

    reaction actions, reducer, types

---
+++ src/store/features/reaction/actions.ts
@@ -0,0 +1,11 @@
+import {createAction} from "@reduxjs/toolkit";
+import {Reaction, ReactionType} from "./types";
+
+export const addReaction = createAction<{noteId: string; reactionType: ReactionType}>("scrumlr.io/addReaction");
+export const addedReaction = createAction<Reaction>("scrumlr.io/addedReaction");
+
+export const updateReaction = createAction<{noteId: string; reactionType: ReactionType}>("scrumlr.io/updateReaction");
+export const updatedReaction = createAction<Reaction>("scrumlr.io/updatedReaction");
+
+export const deleteReaction = createAction<string>("scrumlr.io/deleteReaction");
+export const deletedReaction = createAction<string>("scrumlr.io/deletedReaction");
