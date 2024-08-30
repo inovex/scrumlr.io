81dbc8cffc8c15f5bd2cb4e87a1d4ebfeaecd518
Jakob Schwehn <schwehn42@gmail.com>
2024-08-30 11:45:24 +0200

    requests actions, reducer, types

---
+++ src/store/features/requests/reducer.ts
@@ -0,0 +1,15 @@
+import {createReducer} from "@reduxjs/toolkit";
+import {RequestsState} from "./types";
+import {initializeBoard} from "../board";
+import {createJoinRequest, updateJoinRequest} from "./actions";
+
+const initialState: RequestsState = [];
+
+export const requestsReducer = createReducer(initialState, (builder) =>
+  builder
+    .addCase(initializeBoard, (_state, action) => action.payload.requests)
+    .addCase(createJoinRequest, (state, action) => {
+      state.push(action.payload);
+    })
+    .addCase(updateJoinRequest, (state, action) => state.map((r) => (r.user.id !== action.payload.user.id ? r : action.payload)))
+);
