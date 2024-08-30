cf6aa1027730cc9468acb6e3119e223f15744004
Jakob Schwehn <schwehn42@gmail.com>
2024-08-30 16:19:58 +0200

    remove tmp features

--- src/store/features-tmp/boardReactions/actions.ts
+++
@@ -1,7 +0,0 @@
-import {createAction} from "@reduxjs/toolkit";
-import {ReactionType} from "store/features/reactions/types";
-import {BoardReactionType} from "store/features/boardReactions/types";
-
-export const addBoardReaction = createAction<ReactionType>("scrumlr.io/addBoardReaction");
-export const addedBoardReaction = createAction<BoardReactionType>("scrumlr.io/addedBoardReaction");
-export const removeBoardReaction = createAction<string>("scrumlr.io/removeBoardReaction");
--- src/store/features-tmp/skinTone/actions.ts
+++
@@ -1,4 +0,0 @@
-import {createAction} from "@reduxjs/toolkit";
-import {SkinToneName} from "./types";
-
-export const setSkinTone = createAction<SkinToneName>("scrumlr.io/setSkinTone");
