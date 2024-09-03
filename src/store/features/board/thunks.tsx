44ae9a5115518866c2da121c7a9126e984d6da5e
Jakob Schwehn <schwehn42@gmail.com>
2024-09-03 17:00:24 +0200

    tmp -> thunks

--- src/store/features/board/thunks.tsx
+++ src/store/features/board/thunks.tsx
@@ -1,211 +1,201 @@
-import {Dispatch, MiddlewareAPI} from "@reduxjs/toolkit";
-import {ApplicationState} from "types";
-import {Actions, Action, ReduxAction} from "store/action";
 import Socket from "sockette";
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import {SERVER_WEBSOCKET_URL} from "config";
 import {ServerEvent} from "types/websocket";
-import store from "store/index";
+import {ApplicationState} from "types";
 import {API} from "api";
 import {Timer} from "utils/timer";
-import {Toast} from "../../../utils/Toast";
-import i18n from "../../../i18n";
-import {SERVER_WEBSOCKET_URL} from "../../../config";
+import store from "../../index";
+import {initializeBoard, updatedBoard, updatedBoardTimer} from "./actions";
+import {deletedColumn, updateColumns} from "../columns";
+import {deletedNote, syncNotes, updatedNotes} from "../notes";
+import {addedReaction, deletedReaction, updatedReaction} from "../reactions";
+import {createdParticipant, setParticipants, updatedParticipant} from "../participants";
+import {createdVoting, updatedVoting} from "../votings";
+import {updatedVotes} from "../votes";
+import {createJoinRequest, updateJoinRequest} from "../requests";
+import {addedBoardReaction, removeBoardReaction} from "../boardReactions";
+import {EditBoardRequest} from "./types";
 
-let socket: Socket | undefined;
+let socket: Socket | null = null;
 
-export const passBoardMiddleware = (stateAPI: MiddlewareAPI<Dispatch, ApplicationState>, dispatch: Dispatch, action: ReduxAction) => {
-  if (action.type === Action.LeaveBoard) {
-    socket?.close();
-  }
-
-  if (action.type === Action.PermittedBoardAccess) {
-    socket = new Socket(`${SERVER_WEBSOCKET_URL}/boards/${action.boardId}`, {
-      timeout: 5000,
-      maxAttempts: 0,
-      onmessage: async (evt: MessageEvent<string>) => {
-        const message: ServerEvent = JSON.parse(evt.data);
-
-        if (message.type === "INIT") {
-          const {board, columns, participants, notes, reactions, votes, votings, requests} = message.data;
-          store.dispatch(Actions.initializeBoard(board, participants, requests || [], columns, notes || [], reactions || [], votes || [], votings || []));
-        }
-
-        if (message.type === "BOARD_UPDATED") {
-          store.dispatch(Actions.updatedBoard(message.data));
-        }
-
-        if (message.type === "BOARD_TIMER_UPDATED") {
-          store.dispatch(Actions.updatedBoardTimer(message.data));
-        }
-
-        if (message.type === "BOARD_DELETED") {
-          store.dispatch(Actions.leaveBoard());
-          window.location.assign("/?boardDeleted=true");
-        }
-
-        if (message.type === "COLUMNS_UPDATED") {
-          const columns = message.data;
-          store.dispatch(Actions.updateColumns(columns));
-        }
-
-        if (message.type === "COLUMN_DELETED") {
-          const columnId = message.data;
-          store.dispatch(Actions.deletedColumn(columnId));
-        }
-
-        if (message.type === "NOTES_UPDATED") {
-          const notes = message.data;
-          store.dispatch(Actions.updatedNotes(notes));
-        }
-        if (message.type === "NOTE_DELETED") {
-          const noteId = message.data.note;
-          const {deleteStack} = message.data;
-          store.dispatch(Actions.deletedNote(noteId, deleteStack));
-        }
-        if (message.type === "REACTION_ADDED") {
-          const reaction = message.data;
-          store.dispatch(Actions.addedReaction(reaction));
-        }
-        if (message.type === "REACTION_DELETED") {
-          const reactionId = message.data;
-          store.dispatch(Actions.deletedReaction(reactionId));
-        }
-        if (message.type === "REACTION_UPDATED") {
-          const reaction = message.data;
-          store.dispatch(Actions.updatedReaction(reaction));
-        }
-        if (message.type === "NOTES_SYNC") {
-          const notes = message.data;
-          store.dispatch(Actions.syncNotes(notes ?? []));
-        }
-        if (message.type === "PARTICIPANT_CREATED") {
-          store.dispatch(Actions.createdParticipant(message.data));
-        }
-        if (message.type === "PARTICIPANT_UPDATED") {
-          store.dispatch(Actions.updatedParticipant(message.data));
-        }
-        if (message.type === "PARTICIPANTS_UPDATED") {
-          store.dispatch(Actions.setParticipants(message.data));
-        }
-
-        if (message.type === "VOTING_CREATED") {
-          store.dispatch(Actions.createdVoting(message.data));
-        }
-        if (message.type === "VOTING_UPDATED") {
-          store.dispatch(Actions.updatedVoting(message.data.voting, message.data.notes));
-        }
-
-        if (message.type === "VOTES_UPDATED") {
-          const votes = message.data;
-          store.dispatch(Actions.updatedVotes(votes));
-        }
-
-        if (message.type === "REQUEST_CREATED") {
-          store.dispatch(Actions.createJoinRequest(message.data));
-        }
-        if (message.type === "REQUEST_UPDATED") {
-          store.dispatch(Actions.updateJoinRequest(message.data));
-        }
-        if (message.type === "BOARD_REACTION_ADDED") {
-          store.dispatch(Actions.addedBoardReaction(message.data));
-          setTimeout(() => store.dispatch(Actions.removeBoardReaction(message.data.id)), 5000);
-        }
-      },
-    });
-  }
-
-  if (action.type === Action.EditBoard) {
-    const currentState = stateAPI.getState().board.data!;
-    API.editBoard(action.context.board!, {
-      sharedNote: currentState.sharedNote,
-      showVoting: currentState.showVoting,
-      timerStart: Timer.removeOffsetFromDate(currentState.timerStart, stateAPI.getState().view.serverTimeOffset),
-      timerEnd: Timer.removeOffsetFromDate(currentState.timerEnd, stateAPI.getState().view.serverTimeOffset),
-      accessPolicy: action.board.accessPolicy,
-      passphrase: action.board.passphrase,
-      allowStacking: action.board.allowStacking,
-      showAuthors: action.board.showAuthors,
-      showNotesOfOtherUsers: action.board.showNotesOfOtherUsers,
-      showNoteReactions: action.board.showNoteReactions,
-      name: action.board.name == null ? currentState.name : action.board.name,
-      isLocked: action.board.isLocked,
-    }).catch(() => {
-      i18n.on("loaded", () => {
-        Toast.error({
-          title: i18n.t("Error.editBoard"),
-          buttons: [i18n.t("Error.retry")],
-          firstButtonOnClick: () => store.dispatch(Actions.editBoard(action.board)),
-        });
-      });
-    });
-  }
-
-  if (action.type === Action.SetTimer) {
-    const currentState = stateAPI.getState().board.data!;
-    API.setTimer(currentState.id, action.minutes);
-  }
-
-  if (action.type === Action.CancelTimer) {
-    const currentState = stateAPI.getState().board.data!;
-    API.deleteTimer(currentState.id);
-  }
-
-  if (action.type === Action.IncrementTimer) {
-    const currentState = stateAPI.getState().board.data!;
-    API.incrementTimer(currentState.id);
-  }
-
-  if (action.type === Action.ShareNote) {
-    const currentState = stateAPI.getState().board.data!;
-    const note = stateAPI.getState().notes.find((n) => n.id === action.note);
-    const column = stateAPI.getState().columns.find((c) => c.id === note?.position.column);
-    if (!column?.visible) return; // Do not share notes in hidden columns
-    API.editBoard(action.context.board!, {
-      sharedNote: action.note,
-      showVoting: currentState.showVoting,
-      timerStart: Timer.removeOffsetFromDate(currentState.timerStart, stateAPI.getState().view.serverTimeOffset),
-      timerEnd: Timer.removeOffsetFromDate(currentState.timerEnd, stateAPI.getState().view.serverTimeOffset),
-    }).catch(() => {
-      i18n.on("loaded", () => {
-        Toast.error({
-          title: i18n.t("Error.shareNote"),
-          buttons: [i18n.t("Error.retry")],
-          firstButtonOnClick: () => store.dispatch(Actions.shareNote(action.note)),
-        });
-      });
-    });
-  }
-
-  if (action.type === Action.StopSharing) {
-    const currentState = stateAPI.getState().board.data!;
-    API.editBoard(action.context.board!, {
-      sharedNote: undefined,
-      showVoting: currentState.showVoting,
-      timerStart: Timer.removeOffsetFromDate(currentState.timerStart, stateAPI.getState().view.serverTimeOffset),
-      timerEnd: Timer.removeOffsetFromDate(currentState.timerEnd, stateAPI.getState().view.serverTimeOffset),
-    }).catch(() => {
-      i18n.on("loaded", () => {
-        Toast.error({
-          title: i18n.t("Error.unshareNote"),
-          buttons: [i18n.t("Error.retry")],
-          firstButtonOnClick: () => store.dispatch(Actions.stopSharing()),
-        });
-      });
-    });
-  }
-
-  if (action.type === Action.DeleteBoard) {
-    API.deleteBoard(action.context.board!)
-      .then(() => {
-        document.location.pathname = "/";
-      })
-      .catch(() => {
-        i18n.on("loaded", () => {
-          Toast.error({
-            title: i18n.t("Error.deleteBoard"),
-            buttons: [i18n.t("Error.retry")],
-            firstButtonOnClick: () => store.dispatch(Actions.deleteBoard()),
-          });
-        });
-      });
-  }
+export const leaveBoard = () => async () => {
+  socket?.close();
 };
+
+// generic args: <returnArg, payloadArg, otherArgs(like state type)
+export const permittedBoardAccess = createAsyncThunk<void, string, {state: ApplicationState}>("scrumlr.io/permittedBoardAccess", async (boardId: string, {dispatch}) => {
+  socket = new Socket(`${SERVER_WEBSOCKET_URL}/boards/${boardId}`, {
+    timeout: 5000,
+    maxAttempts: 0,
+    onmessage: async (evt: MessageEvent<string>) => {
+      const message: ServerEvent = JSON.parse(evt.data);
+
+      if (message.type === "INIT") {
+        const {board, columns, participants, notes, reactions, votes, votings, requests} = message.data;
+        dispatch(
+          initializeBoard({
+            board,
+            columns,
+            notes: notes ?? [],
+            participants,
+            reactions: reactions ?? [],
+            requests: requests ?? [],
+            votes: votes ?? [],
+            votings: votings ?? [],
+          })
+        );
+      }
+
+      if (message.type === "BOARD_UPDATED") {
+        dispatch(updatedBoard(message.data));
+      }
+
+      if (message.type === "BOARD_TIMER_UPDATED") {
+        dispatch(updatedBoardTimer(message.data));
+      }
+
+      if (message.type === "BOARD_DELETED") {
+        dispatch(leaveBoard());
+        window.location.assign("/?boardDeleted=true");
+      }
+
+      if (message.type === "COLUMNS_UPDATED") {
+        const columns = message.data;
+        dispatch(updateColumns(columns));
+      }
+
+      if (message.type === "COLUMN_DELETED") {
+        const columnId = message.data;
+        dispatch(deletedColumn(columnId));
+      }
+
+      if (message.type === "NOTES_UPDATED") {
+        const notes = message.data;
+        dispatch(updatedNotes(notes));
+      }
+      if (message.type === "NOTE_DELETED") {
+        const noteId = message.data.note;
+        const {deleteStack} = message.data;
+        dispatch(deletedNote({noteId, deleteStack}));
+      }
+      if (message.type === "REACTION_ADDED") {
+        const reaction = message.data;
+        dispatch(addedReaction(reaction));
+      }
+      if (message.type === "REACTION_DELETED") {
+        const reactionId = message.data;
+        dispatch(deletedReaction(reactionId));
+      }
+      if (message.type === "REACTION_UPDATED") {
+        const reaction = message.data;
+        dispatch(updatedReaction(reaction));
+      }
+      if (message.type === "NOTES_SYNC") {
+        const notes = message.data;
+        dispatch(syncNotes(notes ?? []));
+      }
+      if (message.type === "PARTICIPANT_CREATED") {
+        dispatch(createdParticipant(message.data));
+      }
+      if (message.type === "PARTICIPANT_UPDATED") {
+        dispatch(updatedParticipant(message.data));
+      }
+      if (message.type === "PARTICIPANTS_UPDATED") {
+        dispatch(setParticipants(message.data));
+      }
+
+      if (message.type === "VOTING_CREATED") {
+        dispatch(createdVoting(message.data));
+      }
+      if (message.type === "VOTING_UPDATED") {
+        dispatch(updatedVoting({voting: message.data.voting, notes: message.data.notes}));
+      }
+
+      if (message.type === "VOTES_UPDATED") {
+        const votes = message.data;
+        dispatch(updatedVotes(votes));
+      }
+
+      if (message.type === "REQUEST_CREATED") {
+        dispatch(createJoinRequest(message.data));
+      }
+      if (message.type === "REQUEST_UPDATED") {
+        dispatch(updateJoinRequest(message.data));
+      }
+      if (message.type === "BOARD_REACTION_ADDED") {
+        dispatch(addedBoardReaction(message.data));
+        setTimeout(() => store.dispatch(removeBoardReaction(message.data.id)), 5000);
+      }
+    },
+  });
+});
+
+export const editBoard = createAsyncThunk<void, EditBoardRequest, {state: ApplicationState}>("scrumlr.io/editBoard", async (payload, {getState}) => {
+  const board = getState().board.data!;
+  const {serverTimeOffset} = getState().view;
+  await API.editBoard(board.id, {
+    sharedNote: board.sharedNote,
+    showVoting: board.showVoting,
+    timerStart: Timer.removeOffsetFromDate(board.timerStart, serverTimeOffset),
+    timerEnd: Timer.removeOffsetFromDate(board.timerEnd, serverTimeOffset),
+    accessPolicy: payload.accessPolicy,
+    passphrase: payload.passphrase,
+    allowStacking: payload.allowStacking,
+    showAuthors: payload.showAuthors,
+    showNotesOfOtherUsers: payload.showNotesOfOtherUsers,
+    showNoteReactions: payload.showNoteReactions,
+    name: payload.name == null ? board.name : payload.name,
+    isLocked: payload.isLocked,
+  });
+});
+
+export const setTimer = createAsyncThunk<void, number, {state: ApplicationState}>("scrumlr.io/setTimer", async (payload: number, {getState}) => {
+  const {id} = getState().board.data!;
+  await API.setTimer(id, payload);
+});
+
+export const cancelTimer = createAsyncThunk<void, void, {state: ApplicationState}>("scrumlr.io/cancelTimer", async (_payload, {getState}) => {
+  const {id} = getState().board.data!;
+  await API.deleteTimer(id);
+});
+
+export const incrementTimer = createAsyncThunk<void, void, {state: ApplicationState}>("scrumlr.io/incrementTimer", async (_payload, {getState}) => {
+  const {id} = getState().board.data!;
+  await API.incrementTimer(id);
+});
+
+export const shareNote = createAsyncThunk<void, string, {state: ApplicationState}>("scrumlr.io/shareNote", async (payload, {getState}) => {
+  const board = getState().board.data!;
+  const {serverTimeOffset} = getState().view;
+  const note = getState().notes.find((n) => n.id === payload);
+  const column = getState().columns.find((c) => c.id === note?.position.column);
+
+  if (!column?.visible) return; // do not share notes in hidden columns
+
+  await API.editBoard(board.id, {
+    sharedNote: payload,
+    showVoting: board.showVoting,
+    timerStart: Timer.removeOffsetFromDate(board.timerStart, serverTimeOffset),
+    timerEnd: Timer.removeOffsetFromDate(board.timerEnd, serverTimeOffset),
+  });
+});
+
+export const stopSharing = createAsyncThunk<void, void, {state: ApplicationState}>("scrumlr.io/shareNote", async (_payload, {getState}) => {
+  const board = getState().board.data!;
+  const {serverTimeOffset} = getState().view;
+
+  await API.editBoard(board.id, {
+    sharedNote: undefined,
+    showVoting: board.showVoting,
+    timerStart: Timer.removeOffsetFromDate(board.timerStart, serverTimeOffset),
+    timerEnd: Timer.removeOffsetFromDate(board.timerEnd, serverTimeOffset),
+  });
+});
+
+export const deleteBoard = createAsyncThunk<void, void, {state: ApplicationState}>("scrumlr.io/deleteBoard", async (_payload, {getState}) => {
+  const {id} = getState().board.data!;
+  API.deleteBoard(id).then(() => {
+    document.location.pathname = "/";
+  });
+});
