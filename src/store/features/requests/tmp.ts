7ea92042bd23efcbb951e42c56239d695671f15e
Jakob Schwehn <schwehn42@gmail.com>
2024-09-04 13:58:47 +0200

    switch

--- src/store/features/requests/tmp.ts
+++ src/store/features/requests/tmp.ts
@@ -10,20 +10,30 @@ const socket: Socket | null = null;
 // was defined in board actions before
 export const joinBoard = createAsyncThunk<void, {boardId: string; passphrase?: string}, {state: ApplicationState}>("scrumlr.io/joinBoard", async (payload, {dispatch}) => {
   API.joinBoard(payload.boardId, payload.passphrase).then((r) => {
-    if (r.status === "ACCEPTED") {
-      dispatch(permittedBoardAccess(payload.boardId));
-    } else if (r.status === "REJECTED") {
-      dispatch(rejectedBoardAccess());
-    } else if (r.status === "PASSPHRASE_REQUIRED") {
-      dispatch(passphraseChallengeRequired());
-    } else if (r.status === "WRONG_PASSPHRASE") {
-      dispatch(incorrectPassphrase());
-    } else if (r.status === "PENDING") {
-      dispatch(pendingBoardAccessConfirmation({board: payload.boardId, requestReference: r.joinRequestReference!}));
-    } else if (r.status === "TOO_MANY_JOIN_REQUESTS") {
-      dispatch(tooManyJoinRequests());
-    } else if (r.status === "BANNED") {
-      dispatch(bannedFromBoard());
+    switch (r.status) {
+      case "ACCEPTED":
+        dispatch(permittedBoardAccess(payload.boardId));
+        break;
+      case "REJECTED":
+        dispatch(rejectedBoardAccess());
+        break;
+      case "PASSPHRASE_REQUIRED":
+        dispatch(passphraseChallengeRequired());
+        break;
+      case "WRONG_PASSPHRASE":
+        dispatch(incorrectPassphrase());
+        break;
+      case "PENDING":
+        dispatch(pendingBoardAccessConfirmation({board: payload.boardId, requestReference: r.joinRequestReference!}));
+        break;
+      case "TOO_MANY_JOIN_REQUESTS":
+        dispatch(tooManyJoinRequests());
+        break;
+      case "BANNED":
+        dispatch(bannedFromBoard());
+        break;
+      default:
+        break;
     }
   });
 });
