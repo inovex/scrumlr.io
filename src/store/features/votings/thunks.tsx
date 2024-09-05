77127d0294c5f77f32f79fb5a668d537f5581252
Jakob Schwehn <schwehn42@gmail.com>
2024-09-05 14:00:24 +0200

    votings thunks

--- src/store/features/votings/thunks.tsx
+++ src/store/features/votings/thunks.tsx
@@ -1,32 +1,16 @@
-import {Dispatch, MiddlewareAPI} from "@reduxjs/toolkit";
+import {createAsyncThunk} from "@reduxjs/toolkit";
 import {ApplicationState} from "types";
-import {Action, Actions, ReduxAction} from "store/action";
 import {API} from "api";
-import i18n from "i18next";
-import {Toast} from "../../../utils/Toast";
-import store from "../../index";
+import {CreateVotingRequest} from "./types";
 
-export const passVotingMiddleware = (stateAPI: MiddlewareAPI<Dispatch, ApplicationState>, dispatch: Dispatch, action: ReduxAction) => {
-  if (action.type === Action.CreateVoting) {
-    API.createVoting(action.context.board!, action.voting).catch(() => {
-      Toast.error({
-        title: i18n.t("Error.createVoting"),
-        buttons: [i18n.t("Error.retry")],
-        firstButtonOnClick: () => store.dispatch(Actions.createVoting(action.voting)),
-        autoClose: false,
-      });
-    });
-  }
+export const createVoting = createAsyncThunk<void, CreateVotingRequest, {state: ApplicationState}>("scrumlr.io/createVoting", async (payload, {getState}) => {
+  const boardId = getState().board.data!.id;
 
-  if (action.type === Action.CloseVoting) {
-    API.changeVotingStatus(action.context.board!, action.voting, "CLOSED").catch(() => {
-      Toast.error({
-        title: i18n.t("Error.closeVoting"),
-        buttons: [i18n.t("Error.retry")],
-        firstButtonOnClick: () => store.dispatch(Actions.closeVoting(action.voting)),
-        autoClose: false,
-      });
-    });
-    API.updateReadyStates(action.context.board!, false);
-  }
-};
+  await API.createVoting(boardId, payload);
+});
+
+export const closeVoting = createAsyncThunk<void, string, {state: ApplicationState}>("scrumlr.io/closeVoting", async (payload, {getState}) => {
+  const boardId = getState().board.data!.id;
+
+  await API.changeVotingStatus(boardId, payload, "CLOSED");
+});
