95ee428db429165588674aa103a7b3fe56c5fe7c
Jakob Schwehn <schwehn42@gmail.com>
2024-09-09 17:17:53 +0200

    apply retryable to auth thunks

--- src/store/features/auth/thunks.ts
+++ src/store/features/auth/thunks.ts
@@ -3,11 +3,11 @@ import {signIn, userCheckCompleted} from "store/features/auth/actions";
 import {ACCOUNT_TYPE_ANONYMOUS} from "store/features/auth/types";
 import {API} from "api";
 import {setServerInfo} from "store/features";
+import {retryable} from "store";
 
-// TODO Toasts
 export const initAuth = createAsyncThunk("auth/initAuth", async (_payload, {dispatch}) => {
   dispatch(setServerInfo());
-  API.getCurrentUser()
+  retryable(() => API.getCurrentUser(), dispatch, initAuth, "serverConnection")
     .then((user) => {
       if (user) {
         const isAnonymous = user.accountType === ACCOUNT_TYPE_ANONYMOUS;
@@ -21,53 +21,8 @@ export const initAuth = createAsyncThunk("auth/initAuth", async (_payload, {disp
 });
 
 // use createAsyncThunk, because the action also changes state in the reducer.
-export const signOut = createAsyncThunk("auth/signOut", async () => {
-  API.signOut().then(() => {
+export const signOut = createAsyncThunk("auth/signOut", async (_payload, {dispatch}) => {
+  retryable(() => API.signOut(), dispatch, signOut, "logout")
     // eslint-disable-next-line no-restricted-globals
-    location.reload();
-  });
+    .then(() => location.reload());
 });
-
-/*
-export const passAuthMiddleware = (stateAPI: MiddlewareAPI<Dispatch, ApplicationState>, dispatch: Dispatch, action: ReduxAction) => {
-  if (action.type === ViewAction.InitApplication) {
-    API.getCurrentUser()
-      .then((user) => {
-        if (user) {
-          const isAnonymous = user.accountType === ACCOUNT_TYPE_ANONYMOUS;
-          dispatch(Actions.signIn(user.id, user.name, isAnonymous, user.avatar));
-        }
-        dispatch(Actions.userCheckCompleted(true));
-      })
-      .catch(() => {
-        i18n.on("loaded", () => {
-          Toast.error({
-            title: i18n.t("Error.serverConnection"),
-            buttons: [i18n.t("Error.retry")],
-            firstButtonOnClick: () => store.dispatch(Actions.initApplication()),
-            autoClose: false,
-          });
-        });
-        dispatch(Actions.userCheckCompleted(false));
-      });
-  }
-
-  if (action.type === AuthAction.SignOut) {
-    API.signOut()
-      .then(() => {
-        // eslint-disable-next-line no-restricted-globals
-        location.reload();
-      })
-      .catch(() => {
-        i18n.on("loaded", () => {
-          Toast.error({
-            title: i18n.t("Error.logout"),
-            buttons: [i18n.t("Error.retry")],
-            firstButtonOnClick: () => store.dispatch(Actions.signOut()),
-            autoClose: false,
-          });
-        });
-      });
-  }
-};
-*/
