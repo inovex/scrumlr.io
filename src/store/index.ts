27b187094eee224499e83f65c556bf7faf4bb78a
Jakob Schwehn <schwehn42@gmail.com>
2024-09-09 17:12:43 +0200

    return promise

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
 export {store, useAppDispatch, useAppSelector} from "./store";
 export type {ApplicationState} from "./store";
 
-export {retryWithToast} from "./retryWithToast";
+export {retryable} from "./retryable";
--- src/store/retryable.ts
+++ src/store/retryable.ts
@@ -9,24 +9,25 @@ type ErrorKey = keyof (typeof resources.en.translation)["Error"];
 /**
  * function which tries the API call, and if it fails shows a toast,
  * which dispatches the action again, effectively retrying itself.
+ * @param promiseFn the API call function
  * @param dispatch the dispatch function of the store
  * @param action the dispatched action (makes sense to use the same as the one dispatched initially)
- * @param promiseFn the API call function
  * @param errorKey the error key to be used in the toast message.
  */
-export const retryWithToast = async <ResponseType, ActionReturnType, ActionPayload>(
+export const retryable = async <ResponseType, ActionReturnType, ActionPayload>(
+  promiseFn: () => Promise<ResponseType>,
   dispatch: Dispatch<AsyncThunkAction<ActionReturnType, ActionPayload, {state: ApplicationState}>>,
   action: () => AsyncThunkAction<ActionReturnType, ActionPayload, {state: ApplicationState}>,
-  promiseFn: () => Promise<ResponseType>,
   errorKey: ErrorKey
 ) => {
   try {
-    await promiseFn();
+    return await promiseFn();
   } catch (error) {
     Toast.error({
       title: i18n.t(`Error.${errorKey}`),
       buttons: [i18n.t("Error.retry")],
       firstButtonOnClick: () => dispatch(action()),
     });
+    throw error;
   }
 };
