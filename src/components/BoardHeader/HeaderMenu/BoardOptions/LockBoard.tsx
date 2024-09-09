631aa43307973aa4cd805bfdce6ee2b47ead0507
Jakob Schwehn <schwehn42@gmail.com>
2024-09-09 13:10:59 +0200

    var name fix

--- src/components/BoardHeader/HeaderMenu/BoardOptions/LockBoard.tsx
+++ src/components/BoardHeader/HeaderMenu/BoardOptions/LockBoard.tsx
@@ -9,12 +9,12 @@ import "../BoardSettings/BoardSettings.scss";
 export const LockBoard = () => {
   const dispatch = useAppDispatch();
   const {t} = useTranslation();
-  const allowEditing = useAppSelector((state) => state.board.data!.isLocked);
+  const isLocked = useAppSelector((state) => state.board.data!.isLocked);
 
   return (
     <BoardOption>
-      <BoardOptionButton aria-checked={allowEditing} role="switch" label={t("BoardSettings.IsLocked")} onClick={() => dispatch(editBoard({isLocked: !allowEditing}))}>
-        <BoardOptionToggle active={allowEditing} />
+      <BoardOptionButton aria-checked={isLocked} role="switch" label={t("BoardSettings.IsLocked")} onClick={() => dispatch(editBoard({isLocked: !isLocked}))}>
+        <BoardOptionToggle active={isLocked} />
       </BoardOptionButton>
     </BoardOption>
   );
