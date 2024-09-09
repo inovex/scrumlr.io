0a1ddc04fad82f87dbf3537442d949217b57995d
Jakob Schwehn <schwehn42@gmail.com>
2024-09-09 17:32:44 +0200

    apply retryable to columns thunks

--- src/store/features/columns/thunks.ts
+++ src/store/features/columns/thunks.ts
@@ -1,19 +1,34 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import {API} from "api";
-import {ApplicationState} from "store";
+import {ApplicationState, retryable} from "store";
 import {ColumnWithoutId} from "./types";
 
-export const editColumn = createAsyncThunk<void, {id: string; column: ColumnWithoutId}, {state: ApplicationState}>("columns/editColumn", async (payload, {getState}) => {
+export const editColumn = createAsyncThunk<void, {id: string; column: ColumnWithoutId}, {state: ApplicationState}>("columns/editColumn", async (payload, {dispatch, getState}) => {
   const boardId = getState().board.data!.id;
-  await API.editColumn(boardId, payload.id, payload.column);
+  await retryable(
+    () => API.editColumn(boardId, payload.id, payload.column),
+    dispatch,
+    () => editColumn({...payload}),
+    "editColumn"
+  );
 });
 
-export const createColumn = createAsyncThunk<void, ColumnWithoutId, {state: ApplicationState}>("columns/createColumn", async (payload, {getState}) => {
+export const createColumn = createAsyncThunk<void, ColumnWithoutId, {state: ApplicationState}>("columns/createColumn", async (payload, {dispatch, getState}) => {
   const boardId = getState().board.data!.id;
-  await API.createColumn(boardId, payload);
+  await retryable(
+    () => API.createColumn(boardId, payload),
+    dispatch,
+    () => createColumn({...payload}),
+    "createColumn"
+  );
 });
 
-export const deleteColumn = createAsyncThunk<void, string, {state: ApplicationState}>("columns/deleteColumn", async (payload, {getState}) => {
+export const deleteColumn = createAsyncThunk<void, string, {state: ApplicationState}>("columns/deleteColumn", async (payload, {dispatch, getState}) => {
   const boardId = getState().board.data!.id;
-  await API.deleteColumn(boardId, payload);
+  await retryable(
+    () => API.deleteColumn(boardId, payload),
+    dispatch,
+    () => deleteColumn(payload),
+    "deleteColumn"
+  );
 });
