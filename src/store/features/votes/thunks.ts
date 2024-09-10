11188837000661eae9a58b4039c5c30ebe1dec71
Jakob Schwehn <schwehn42@gmail.com>
2024-09-10 11:21:36 +0200

    retryable votes thunks

--- src/store/features/votes/thunks.ts
+++ src/store/features/votes/thunks.ts
@@ -1,19 +1,27 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import {API} from "api";
-import {ApplicationState} from "store";
+import {ApplicationState, retryable} from "store";
 import {createdVote, deletedVote} from "./actions";
 
 export const addVote = createAsyncThunk<void, string, {state: ApplicationState}>("votes/addVote", async (payload, {dispatch, getState}) => {
   const boardId = getState().board.data!.id;
-  API.addVote(boardId, payload).then((r) => {
-    dispatch(createdVote(r));
-  });
+  await retryable(
+    () => API.addVote(boardId, payload),
+    dispatch,
+    () => addVote(payload),
+    "addVote"
+  ).then((r) => dispatch(createdVote(r)));
 });
 
 export const deleteVote = createAsyncThunk<void, string, {state: ApplicationState}>("votes/deleteVote", async (payload, {dispatch, getState}) => {
   const boardId = getState().board.data!.id;
   const voting = getState().votings.open!.id;
-  API.deleteVote(boardId, payload).then(() => {
+  await retryable(
+    () => API.deleteVote(boardId, payload),
+    dispatch,
+    () => deleteVote(payload),
+    "deleteVote"
+  ).then(() => {
     dispatch(deletedVote({voting, note: payload}));
   });
 });
