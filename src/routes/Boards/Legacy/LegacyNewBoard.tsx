3d0b61f01633898ec0398d0a6d174470bf099b3b
Jakob Schwehn <schwehn42@gmail.com>
2024-09-23 13:15:27 +0200

    (re)add legacy new board

--- src/routes/Boards/Legacy/LegacyNewBoard.tsx
+++ src/routes/Boards/Legacy/LegacyNewBoard.tsx
@@ -1,3 +1,111 @@
+import {API} from "api";
+import {useState} from "react";
+import {AccessPolicy} from "types/board";
+import {useTranslation} from "react-i18next";
+import {useNavigate} from "react-router";
+import {TextInputLabel} from "components/TextInputLabel";
+import {AccessPolicySelection} from "components/AccessPolicySelection";
+import {TextInput} from "components/TextInput";
+import {Button} from "components/Button";
+import {ScrumlrLogo} from "components/ScrumlrLogo";
+import {legacyColumnTemplates} from "./legacyColumnTemplates";
 import "./LegacyNewBoard.scss";
 
-export const LegacyNewBoard = () => <div>Hello LegacyNewBoard</div>;
+export const LegacyNewBoard = () => {
+  const {t} = useTranslation();
+  const navigate = useNavigate();
+  const [boardName, setBoardName] = useState<string | undefined>();
+  const [columnTemplate, setColumnTemplate] = useState<string | undefined>(undefined);
+  const [accessPolicy, setAccessPolicy] = useState(0);
+  const [passphrase, setPassphrase] = useState("");
+  const [extendedConfiguration, setExtendedConfiguration] = useState(false);
+
+  async function onCreateBoard() {
+    let additionalAccessPolicyOptions = {};
+    if (accessPolicy === AccessPolicy.BY_PASSPHRASE && Boolean(passphrase)) {
+      additionalAccessPolicyOptions = {
+        passphrase,
+      };
+    }
+
+    if (columnTemplate) {
+      const boardId = await API.createBoard(
+        boardName,
+        {
+          type: AccessPolicy[accessPolicy],
+          ...additionalAccessPolicyOptions,
+        },
+        legacyColumnTemplates[columnTemplate].columns
+      );
+      navigate(`/board/${boardId}`);
+    }
+  }
+
+  const isCreatedBoardDisabled = !columnTemplate || (accessPolicy === AccessPolicy.BY_PASSPHRASE && !passphrase);
+
+  return (
+    <div className="new-board__wrapper">
+      <div className="new-board">
+        <div>
+          <a href="/" aria-label="Homepage">
+            <ScrumlrLogo />
+          </a>
+
+          {!extendedConfiguration && (
+            <div>
+              <h1>{t("LegacyNewBoard.basicConfigurationTitle")}</h1>
+
+              <div className="new-board__mode-selection">
+                {Object.keys(legacyColumnTemplates).map((key) => (
+                  <label key={key} className="new-board__mode">
+                    <input
+                      className="new-board__mode-input"
+                      type="radio"
+                      name="mode"
+                      value={key}
+                      onChange={(e) => setColumnTemplate(e.target.value)}
+                      checked={columnTemplate === key}
+                    />
+                    <div className="new-board__mode-label">
+                      <div>
+                        <div className="new-board__mode-name">{legacyColumnTemplates[key].name}</div>
+                        <div className="new-board__mode-description">{legacyColumnTemplates[key].description}</div>
+                      </div>
+                    </div>
+                  </label>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {extendedConfiguration && (
+            <div>
+              <h1>{t("LegacyNewBoard.extendedConfigurationTitle")}</h1>
+
+              <TextInputLabel label={t("LegacyNewBoard.boardName")}>
+                <TextInput onChange={(e) => setBoardName(e.target.value)} />
+              </TextInputLabel>
+
+              <AccessPolicySelection accessPolicy={accessPolicy} onAccessPolicyChange={setAccessPolicy} passphrase={passphrase} onPassphraseChange={setPassphrase} />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="new-board__actions">
+        <Button className="new-board__action" onClick={onCreateBoard} color="primary" disabled={isCreatedBoardDisabled}>
+          {t("LegacyNewBoard.createNewBoard")}
+        </Button>
+        {!extendedConfiguration && (
+          <Button className="new-board__action" variant="outlined" color="primary" disabled={!columnTemplate} onClick={() => setExtendedConfiguration(true)}>
+            {t("LegacyNewBoard.extendedConfigurationButton")}
+          </Button>
+        )}
+        {extendedConfiguration && (
+          <Button className="new-board__action" variant="outlined" color="primary" onClick={() => setExtendedConfiguration(false)}>
+            {t("LegacyNewBoard.basicConfigurationButton")}
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
---
+++ src/routes/Boards/Legacy/legacyColumnTemplates.ts
@@ -0,0 +1,78 @@
+import {Color} from "constants/colors";
+
+export const legacyColumnTemplates: {[id: string]: {name: string; description?: string; columns: {name: string; hidden: boolean; color: Color}[]}} = {
+  leanCoffee: {
+    name: "Lean Coffee",
+    columns: [
+      {name: "Lean Coffee", hidden: false, color: "backlog-blue"},
+      {name: "Actions", hidden: true, color: "planning-pink"},
+    ],
+  },
+  positiveNegative: {
+    name: "Positive/Negative",
+    columns: [
+      {name: "Positive", hidden: false, color: "backlog-blue"},
+      {name: "Negative", hidden: false, color: "poker-purple"},
+      {name: "Actions", hidden: true, color: "planning-pink"},
+    ],
+  },
+  startStopContinue: {
+    name: "Start/Stop/Continue",
+    columns: [
+      {name: "Start", hidden: false, color: "backlog-blue"},
+      {name: "Stop", hidden: false, color: "goal-green"},
+      {name: "Continue", hidden: false, color: "online-orange"},
+      {name: "Actions", hidden: true, color: "planning-pink"},
+    ],
+  },
+  madSadGlad: {
+    name: "Mad/Sad/Glad",
+    columns: [
+      {name: "Mad", hidden: false, color: "backlog-blue"},
+      {name: "Sad", hidden: false, color: "online-orange"},
+      {name: "Glad", hidden: false, color: "poker-purple"},
+      {name: "Actions", hidden: true, color: "planning-pink"},
+    ],
+  },
+  plusDelta: {
+    name: "Plus/Delta",
+    columns: [
+      {name: "Plus", hidden: false, color: "backlog-blue"},
+      {name: "Delta", hidden: false, color: "poker-purple"},
+      {name: "Actions", hidden: true, color: "planning-pink"},
+    ],
+  },
+  kalm: {
+    name: "KALM",
+    description: "Keep, Add, Less, More",
+    columns: [
+      {name: "Keep", hidden: false, color: "backlog-blue"},
+      {name: "Add", hidden: false, color: "yielding-yellow"},
+      {name: "Less", hidden: false, color: "goal-green"},
+      {name: "More", hidden: false, color: "poker-purple"},
+      {name: "Actions", hidden: true, color: "planning-pink"},
+    ],
+  },
+  fourL: {
+    name: "4L",
+    description: "Liked, Learned, Lacked, Longed for",
+    columns: [
+      {name: "Liked", hidden: false, color: "backlog-blue"},
+      {name: "Learned", hidden: false, color: "yielding-yellow"},
+      {name: "Lacked", hidden: false, color: "value-violet"},
+      {name: "Longed for", hidden: false, color: "poker-purple"},
+      {name: "Actions", hidden: true, color: "planning-pink"},
+    ],
+  },
+  swot: {
+    name: "SWOT",
+    description: "Strengths, Weaknesses, Opportunities, Threats",
+    columns: [
+      {name: "Strengths", hidden: false, color: "backlog-blue"},
+      {name: "Weaknesses", hidden: false, color: "online-orange"},
+      {name: "Opportunities", hidden: false, color: "goal-green"},
+      {name: "Threats", hidden: false, color: "yielding-yellow"},
+      {name: "Actions", hidden: true, color: "poker-purple"},
+    ],
+  },
+};
