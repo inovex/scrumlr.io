61be0893729ef8871db87c42b85261003f3edd4e
Jakob Schwehn <schwehn42@gmail.com>
2024-09-24 13:38:20 +0200

    active and dropped element

--- src/components/ColumnsConfigurator/ColumnsConfigurator.tsx
+++ src/components/ColumnsConfigurator/ColumnsConfigurator.tsx
@@ -1,10 +1,10 @@
-import "./ColumnsConfigurator.scss";
 import classNames from "classnames";
 import {useState} from "react";
 import {Column} from "types/column";
-import {closestCenter, DndContext, DragEndEvent, PointerSensor, useSensor, useSensors} from "@dnd-kit/core";
+import {closestCenter, DndContext, DragEndEvent, DragOverEvent, DragStartEvent, PointerSensor, useSensor, useSensors} from "@dnd-kit/core";
 import {arrayMove, horizontalListSortingStrategy, SortableContext} from "@dnd-kit/sortable";
 import {TemplateColumn} from "./TemplateColumn/TemplateColumn";
+import "./ColumnsConfigurator.scss";
 
 type ColumnsConfiguratorProps = {
   className?: string;
@@ -35,10 +35,23 @@ export const ColumnsConfigurator = (props: ColumnsConfiguratorProps) => {
   ];
   const [templateColumns, setTemplateColumns] = useState<TemplateColumnType[]>(initialState);
 
+  // id of column which is actively being dragged
+  const [dragElementId, setDragElementId] = useState<string | null>(null);
+  const [dropElemenId, setDropElementId] = useState<string | null>(null);
+
   const sensors = useSensors(useSensor(PointerSensor));
+
+  const handleDragStart = (event: DragStartEvent) => {
+    setDragElementId(event.active.id as string);
+  };
+
+  const handleDragOver = (event: DragOverEvent) => {
+    setDropElementId((event.over?.id as string) ?? null);
+  };
 
   const handleDragEnd = (event: DragEndEvent) => {
     const {active, over} = event;
+
     if (active.id !== over?.id) {
       setTemplateColumns((columns) => {
         const oldIndex = columns.findIndex((c) => c.id === active.id);
@@ -47,6 +60,10 @@ export const ColumnsConfigurator = (props: ColumnsConfiguratorProps) => {
         return arrayMove(columns, oldIndex, newIndex);
       });
     }
+
+    // don't forget to clear states
+    setDragElementId(null);
+    setDropElementId(null);
   };
 
   const calcPlacement = (index: number) => {
@@ -60,11 +77,18 @@ export const ColumnsConfigurator = (props: ColumnsConfiguratorProps) => {
   };
 
   return (
-    <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+    <DndContext sensors={sensors} collisionDetection={closestCenter} onDragStart={handleDragStart} onDragOver={handleDragOver} onDragEnd={handleDragEnd}>
       <SortableContext items={templateColumns} strategy={horizontalListSortingStrategy}>
         <div className={classNames(props.className, "columns-configurator")}>
           {templateColumns.map((column, index) => (
-            <TemplateColumn key={column.id} column={column} placement={calcPlacement(index)} />
+            <TemplateColumn
+              className="columns-configurator__column"
+              key={column.id}
+              column={column}
+              activeDrag={column.id === dragElementId}
+              activeDrop={column.id === dropElemenId}
+              placement={calcPlacement(index)}
+            />
           ))}
         </div>
       </SortableContext>
--- src/components/ColumnsConfigurator/TemplateColumn/TemplateColumn.tsx
+++ src/components/ColumnsConfigurator/TemplateColumn/TemplateColumn.tsx
@@ -5,18 +5,22 @@ import {ReactComponent as DeleteIcon} from "assets/icons/trash.svg";
 import {ReactComponent as DnDIcon} from "assets/icons/drag-and-drop.svg";
 import {TemplateColumnType} from "components/ColumnsConfigurator/ColumnsConfigurator";
 import {getColorClassName} from "constants/colors";
-import "./TemplateColumn.scss";
 import {useSortable} from "@dnd-kit/sortable";
 import {CSSProperties} from "react";
 import {CSS} from "@dnd-kit/utilities";
+import "./TemplateColumn.scss";
 
 type TemplateColumnProps = {
+  className?: string;
   column: TemplateColumnType;
   placement: "left" | "center" | "right";
+  activeDrag: boolean;
+  activeDrop: boolean;
 };
 
-export const TemplateColumn = ({column, placement}: TemplateColumnProps) => {
-  const {attributes, listeners, setNodeRef, transform, transition} = useSortable({id: column.id});
+export const TemplateColumn = (props: TemplateColumnProps) => {
+  // the column id is used to determine sortable context
+  const {attributes, listeners, setNodeRef, transform, transition} = useSortable({id: props.column.id});
 
   const style: CSSProperties = {
     transform: CSS.Transform.toString(transform),
@@ -24,11 +28,25 @@ export const TemplateColumn = ({column, placement}: TemplateColumnProps) => {
   };
 
   return (
-    <div className={classNames("template-column", `template-column--border-${placement}`, getColorClassName(column.color))} ref={setNodeRef} style={style} {...attributes}>
-      <div className="template-column__name">{column.name}</div>
+    <div
+      className={classNames(
+        props.className,
+        "template-column",
+        `template-column--border-${props.placement}`,
+        {
+          "template-column--active-drag": props.activeDrag,
+          "template-column--active-drop": props.activeDrop,
+        },
+        getColorClassName(props.column.color)
+      )}
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+    >
+      <div className="template-column__name">{props.column.name}</div>
       <DnDIcon className="template-column__drag-element" {...listeners} />
       <div className="template-column__color" />
-      {column.visible ? <VisibleIcon /> : <HiddenIcon />}
+      {props.column.visible ? <VisibleIcon /> : <HiddenIcon />}
       <DeleteIcon />
     </div>
   );
