432bba409de496b1254491807c6c007d22f3f37d
Jakob Schwehn <schwehn42@gmail.com>
2024-09-26 09:53:23 +0200

    remove default middleware
    it's added implicitly

--- src/store/store.ts
+++ src/store/store.ts
@@ -32,7 +32,6 @@ const rootReducer = {
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   devTools: process.env.NODE_ENV !== "production",
 });
 
