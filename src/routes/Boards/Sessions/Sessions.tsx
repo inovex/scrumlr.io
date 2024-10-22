6b7baef49f2752f2da4adda39f4604b35f5b4350
jschlinger <johanna.schlinger@fau.de>
2024-10-22 15:23:19 +0200

    init sessions

--- src/routes/Boards/Sessions/Sessions.tsx
+++ src/routes/Boards/Sessions/Sessions.tsx
@@ -1,8 +1,16 @@
 import {Outlet} from "react-router-dom";
+import classNames from "classnames";
+import StanDark from "../../../assets/stan/Stan_OK_Dark.svg";
+import StanLight from "../../../assets/stan/Stan_OK_Light.svg";
 
 export const Sessions = () => (
   <>
     <Outlet /> {/* settings */}
-    <div>Hello Sessions</div>
+    <div>
+      <div className="templates__stan-container">
+        <img className={classNames("templates__stan", "templates__stan--dark")} src={StanDark} alt="Stan just hanging there with a coffee" />
+        <img className={classNames("templates__stan", "templates__stan--light")} src={StanLight} alt="Stan just hanging there with a coffee" />
+      </div>
+    </div>
   </>
 );
