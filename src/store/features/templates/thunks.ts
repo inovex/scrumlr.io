4d3b92e76bb97a08e377f6d4f0f6f5a0a96ad660
Jakob Schwehn <schwehn42@gmail.com>
2024-10-28 11:56:29 +0100

    transform dto map

--- src/store/features/templates/thunks.ts
+++ src/store/features/templates/thunks.ts
@@ -3,21 +3,19 @@ import {ApplicationState} from "store";
 import {API} from "api";
 import {Template, TemplateDto} from "./types";
 
-const transformTemplate = (templateDtos: TemplateDto[]) =>
-  templateDtos.map(
-    (dto) =>
-      ({
-        ...dto,
-        createdAt: dto.created_at,
-        accessPolicy: dto.access_policy,
-        columns: dto.ColumnTemplates.map((ctdto) => ({
-          ...ctdto,
-          template: ctdto.board_template,
-        })),
-      }) as Template
-  );
+// transforms data object as sent from backend to usable Template object
+const transformTemplate = (templateDto: TemplateDto) =>
+  ({
+    ...templateDto,
+    createdAt: templateDto.created_at,
+    accessPolicy: templateDto.access_policy,
+    columns: templateDto.ColumnTemplates.map((ctdto) => ({
+      ...ctdto,
+      template: ctdto.board_template,
+    })),
+  }) as Template;
 
 export const getTemplates = createAsyncThunk<Template[], void, {state: ApplicationState}>("reactions/addReaction", async () => {
   const templates = await API.getTemplates();
-  return transformTemplate(templates);
+  return templates.map(transformTemplate);
 });
