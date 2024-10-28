74f0ab3e5d57c8a192c0b3814a71d36c112b1398
Jakob Schwehn <schwehn42@gmail.com>
2024-10-28 16:35:51 +0100

    edit template api

--- src/api/templates.ts
+++ src/api/templates.ts
@@ -1,4 +1,4 @@
-import {TemplateDto} from "store/features";
+import {Template, TemplateDto} from "store/features";
 import {SERVER_HTTP_URL} from "config";
 
 export const TemplatesAPI = {
@@ -11,6 +11,24 @@ export const TemplatesAPI = {
 
       if (response.status === 200) {
         return (await response.json()) as TemplateDto[];
+      }
+
+      throw new Error(`get all templates request resulted in status ${response.status}`);
+    } catch (error) {
+      throw new Error(`unable to get all templates with error: ${error}`);
+    }
+  },
+
+  editTemplate: async (templateId: string, overwrite: Partial<Template>) => {
+    try {
+      const response = await fetch(`${SERVER_HTTP_URL}/templates/${templateId}`, {
+        method: "PUT",
+        credentials: "include",
+        body: JSON.stringify(overwrite),
+      });
+
+      if (response.status === 200) {
+        return (await response.json()) as Omit<Template, "columns">;
       }
 
       throw new Error(`get all templates request resulted in status ${response.status}`);
