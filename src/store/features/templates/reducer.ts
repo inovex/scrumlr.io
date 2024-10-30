5d1508373c9c97b0955a24fb5394838ee6321419
Jakob Schwehn <schwehn42@gmail.com>
2024-10-30 16:09:39 +0100

    template column store feature

--- src/store/features/templates/reducer.ts
+++ src/store/features/templates/reducer.ts
@@ -10,10 +10,7 @@ export const templatesReducer = createReducer(initialState, (builder) => {
     // template edits do not return the current columns, so they are re-added to avoid losing them
     .addCase(editTemplate.fulfilled, (state, action) =>
       state.map(
-        (t) =>
-          t.id === action.payload.id
-            ? {...action.payload, columns: t.columns} // keep columns, change the rest
-            : t // only change edited templates, other stay the same
+        (t) => (t.id === action.payload.id ? {...action.payload} : t) // only change edited templates, other stay the same
       )
     );
 });
--- src/store/features/templates/types.ts
+++ src/store/features/templates/types.ts
@@ -1,20 +1,10 @@
-import {Color} from "constants/colors";
 import {AccessPolicy} from "../board";
+import {TemplateColumn} from "../templateColumns/types";
 
 // getTemplates returns all templates with columns, but is the only endpoint to do so.
 // All other template / column endpoints are handled separately, i.e. getting/updating a template
 // does not return the associated columns.
 // Thus, it's important to be aware of this to avoid inconsistencies in the store.
-
-export type TemplateColumn = {
-  id: string; // UUID
-  template: string; // UUID
-  name: string;
-  description: string;
-  color: Color;
-  visible: boolean;
-  index: number;
-};
 
 export type Template = {
   id: string; // UUID
