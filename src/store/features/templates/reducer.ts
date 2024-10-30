94a75d2a585f66e254d981f4b48fc6f028542351
Jakob Schwehn <schwehn42@gmail.com>
2024-10-30 17:27:15 +0100

    rm irrelevant comment

--- src/store/features/templates/reducer.ts
+++ src/store/features/templates/reducer.ts
@@ -7,7 +7,6 @@ const initialState: TemplatesState = [];
 export const templatesReducer = createReducer(initialState, (builder) => {
   builder
     .addCase(getTemplates.fulfilled, (_state, action) => action.payload)
-    // template edits do not return the current columns, so they are re-added to avoid losing them
     .addCase(editTemplate.fulfilled, (state, action) =>
       state.map(
         (t) => (t.id === action.payload.id ? {...action.payload} : t) // only change edited templates, other stay the same
