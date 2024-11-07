5418e5dfa9e83672bf207cd091d48b64b97fdbbc
Jakob Schwehn <schwehn42@gmail.com>
2024-11-07 19:29:59 +0100

    add another default column

--- src/constants/templates.ts
+++ src/constants/templates.ts
@@ -2,6 +2,7 @@ import {TemplateWithColumns} from "store/features";
 import {uniqueId} from "underscore";
 
 export const DEFAULT_TEMPLATE_ID = "DEFAULT_TEMPLATE_ID";
+const DEFAULT_TEMPLATE_COLUMN_ID_PREFIX = "DEFAULT_TEMPLATE_COLUMN_ID-";
 const DEFAULT_TEMPLATE_CREATOR = "DEFAULT_CREATOR_ID";
 
 export const DEFAULT_TEMPLATE: TemplateWithColumns = {
@@ -15,13 +16,22 @@ export const DEFAULT_TEMPLATE: TemplateWithColumns = {
   },
   columns: [
     {
-      id: uniqueId("template-column-"),
+      id: uniqueId(DEFAULT_TEMPLATE_COLUMN_ID_PREFIX),
       template: DEFAULT_TEMPLATE_ID,
-      name: "Default",
+      name: "Lean Coffee",
       description: "",
       color: "backlog-blue",
       visible: true,
       index: 0,
+    },
+    {
+      id: uniqueId(DEFAULT_TEMPLATE_COLUMN_ID_PREFIX),
+      template: DEFAULT_TEMPLATE_ID,
+      name: "Actions",
+      description: "",
+      color: "planning-pink",
+      visible: false,
+      index: 1,
     },
   ],
 };
