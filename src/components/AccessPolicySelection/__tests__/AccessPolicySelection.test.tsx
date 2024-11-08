72124f9674b1997c2ef78e88cc1f38c89faba2ac
Jakob Schwehn <schwehn42@gmail.com>
2024-11-08 13:58:49 +0100

    refactor: access policy enum (#4601)
    Co-authored-by: Mateo Ivankovic <mateo.ivankovic@inovex.de>

--- src/components/AccessPolicySelection/__tests__/AccessPolicySelection.test.tsx
+++ src/components/AccessPolicySelection/__tests__/AccessPolicySelection.test.tsx
@@ -1,24 +1,21 @@
 import {fireEvent, waitFor} from "@testing-library/react";
 import {AccessPolicySelection} from "components/AccessPolicySelection/AccessPolicySelection";
-import {AccessPolicy} from "store/features/board/types";
 import {render} from "testUtils";
 
 describe("AccessPolicySelection", () => {
   test("dont show passphrase input on default state", () => {
-    const {container} = render(<AccessPolicySelection accessPolicy={AccessPolicy.PUBLIC} onAccessPolicyChange={jest.fn()} passphrase="" onPassphraseChange={jest.fn()} />);
+    const {container} = render(<AccessPolicySelection accessPolicy="PUBLIC" onAccessPolicyChange={jest.fn()} passphrase="" onPassphraseChange={jest.fn()} />);
     expect(container.querySelector(`[data-testid="passphrase-input"]`)).toBeNull();
   });
 
   test("show passphrase input on access policy by passphrase", () => {
-    const {container} = render(<AccessPolicySelection accessPolicy={AccessPolicy.BY_PASSPHRASE} onAccessPolicyChange={jest.fn()} passphrase="" onPassphraseChange={jest.fn()} />);
+    const {container} = render(<AccessPolicySelection accessPolicy="BY_PASSPHRASE" onAccessPolicyChange={jest.fn()} passphrase="" onPassphraseChange={jest.fn()} />);
     expect(container.querySelector(`[data-testid="passphrase-input"]`)).toBeDefined();
   });
 
   test("trigger on change on passphrase change", async () => {
     const onChangeOfPassphrase = jest.fn();
-    const {container} = render(
-      <AccessPolicySelection accessPolicy={AccessPolicy.BY_PASSPHRASE} onAccessPolicyChange={jest.fn()} passphrase="" onPassphraseChange={onChangeOfPassphrase} />
-    );
+    const {container} = render(<AccessPolicySelection accessPolicy="BY_PASSPHRASE" onAccessPolicyChange={jest.fn()} passphrase="" onPassphraseChange={onChangeOfPassphrase} />);
 
     fireEvent.change(container.querySelector(`[data-testid="passphrase-input"]`)!, {target: {value: "1234"}});
 
@@ -29,9 +26,7 @@ describe("AccessPolicySelection", () => {
 
   test("trigger on password change when random generator is clicked", async () => {
     const onChangeOfPassphrase = jest.fn();
-    const {container} = render(
-      <AccessPolicySelection accessPolicy={AccessPolicy.BY_PASSPHRASE} onAccessPolicyChange={jest.fn()} passphrase="" onPassphraseChange={onChangeOfPassphrase} />
-    );
+    const {container} = render(<AccessPolicySelection accessPolicy="BY_PASSPHRASE" onAccessPolicyChange={jest.fn()} passphrase="" onPassphraseChange={onChangeOfPassphrase} />);
 
     fireEvent.click(container.querySelector(`[data-testid="random-passwort-generator"]`)!);
 
