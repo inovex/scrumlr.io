36deb05daf5eab8e726930eef61120788c395889
Jakob Schwehn <schwehn42@gmail.com>
2024-11-11 13:18:14 +0100

    get template columns after creating

--- src/store/features/templates/thunks.ts
+++ src/store/features/templates/thunks.ts
@@ -2,6 +2,7 @@ import {createAsyncThunk, unwrapResult} from "@reduxjs/toolkit";
 import {ApplicationState} from "store";
 import {API} from "api";
 import {Template, TemplateWithColumns} from "./types";
+import {getTemplateColumns} from "../templateColumns/thunks";
 
 export const getTemplates = createAsyncThunk<TemplateWithColumns[], void, {state: ApplicationState}>("templates/getTemplates", async () => {
   const templates = await API.getTemplates();
@@ -14,10 +15,16 @@ export const getTemplate = createAsyncThunk<Template, string, {state: Applicatio
   return template;
 });
 
-export const createTemplateWithColumns = createAsyncThunk<Template, TemplateWithColumns, {state: ApplicationState}>("templates/editTemplate", async (payload) => {
-  const template = await API.createTemplate(payload);
-  return template;
-});
+export const createTemplateWithColumns = createAsyncThunk<Template, TemplateWithColumns, {state: ApplicationState}>(
+  "templates/createTemplateWithColumns",
+  async (payload, {dispatch}) => {
+    const template = await API.createTemplate(payload);
+    // after creating, we need to retrieve the columns from backend and add them to the state,
+    // since the columns aren't returned by the createTemplate request.
+    dispatch(getTemplateColumns(template.id));
+    return template;
+  }
+);
 
 export const editTemplate = createAsyncThunk<Template, {id: string; overwrite: Partial<Template>}, {state: ApplicationState}>("templates/editTemplate", async (payload) => {
   const template = await API.editTemplate(payload.id, payload.overwrite);
