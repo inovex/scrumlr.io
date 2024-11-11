1f91cbbaa157d83aed3be2a72c1f356c846037dc
Jakob Schwehn <schwehn42@gmail.com>
2024-11-11 15:25:08 +0100

    template column thunks: edit and create

--- src/store/features/templateColumns/thunks.ts
+++ src/store/features/templateColumns/thunks.ts
@@ -7,3 +7,19 @@ export const getTemplateColumns = createAsyncThunk<TemplateColumn[], string, {st
   const columns = await API.getTemplateColumns(payload);
   return columns;
 });
+
+export const createTemplateColumn = createAsyncThunk<TemplateColumn, {templateId: string; templateColumn: TemplateColumn}, {state: ApplicationState}>(
+  "templateColumns/createColumn",
+  async (payload) => {
+    const column = await API.createTemplateColumn(payload.templateId, payload.templateColumn);
+    return column;
+  }
+);
+
+export const editTemplateColumn = createAsyncThunk<TemplateColumn, {templateId: string; columnId: string; overwrite: Partial<TemplateColumn>}, {state: ApplicationState}>(
+  "templateColumns/editColumn",
+  async (payload) => {
+    const column = await API.editTemplateColumn(payload.templateId, payload.columnId, payload.overwrite);
+    return column;
+  }
+);
