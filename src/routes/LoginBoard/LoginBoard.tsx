b35e3b3e498da2737ea92c8bdaab31f55190091b
Mateo Ivankovic <mateo.ivankovic@inovex.de>
2024-11-11 10:54:40 +0100

    feat: import exported boards (#4392)
    Co-authored-by: Jakob Schwehn <schwehn42@gmail.com>

--- src/routes/LoginBoard/LoginBoard.tsx
+++ src/routes/LoginBoard/LoginBoard.tsx
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import {getRandomName} from "constants/nameList";
+import {getRandomName} from "utils/random";
 import {Auth} from "utils/auth";
 import {Toast} from "utils/Toast";
 import {useState} from "react";
