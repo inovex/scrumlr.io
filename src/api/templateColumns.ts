ac0147bf7b8c1b6fcc18b3d436a437a1ad30a76e
Jakob Schwehn <schwehn42@gmail.com>
2024-11-11 15:24:37 +0100

    template column api: edit and create

--- src/api/templateColumns.ts
+++ src/api/templateColumns.ts
@@ -18,4 +18,41 @@ export const TemplateColumnsAPI = {
       throw new Error(`unable to get all templates with error: ${error}`);
     }
   },
+
+  createTemplateColumn: async (templateId: string, templateColumn: TemplateColumn) => {
+    const {id, ...templateColumnWithoutId} = templateColumn;
+    try {
+      const response = await fetch(`${SERVER_HTTP_URL}/templates/${templateId}/columns`, {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(templateColumnWithoutId),
+      });
+
+      if (response.status === 201) {
+        return (await response.json()) as TemplateColumn;
+      }
+
+      throw new Error(`add template column request resulted in status ${response.status}`);
+    } catch (error) {
+      throw new Error(`unable to add template with error: ${error}`);
+    }
+  },
+
+  editTemplateColumn: async (templateId: string, columnId: string, overwrite: Partial<TemplateColumn>) => {
+    try {
+      const response = await fetch(`${SERVER_HTTP_URL}/templates/${templateId}/columns/${columnId}`, {
+        method: "PUT",
+        credentials: "include",
+        body: JSON.stringify(overwrite),
+      });
+
+      if (response.status === 200) {
+        return (await response.json()) as TemplateColumn;
+      }
+
+      throw new Error(`edit template column request resulted in status ${response.status}`);
+    } catch (error) {
+      throw new Error(`unable to edit template with error: ${error}`);
+    }
+  },
 };
