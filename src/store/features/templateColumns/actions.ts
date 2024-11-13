924c89e3f12a57ede604b8701a0d3a4d8ef0f3e2
Jakob Schwehn <schwehn42@gmail.com>
2024-11-13 12:15:47 +0100

    add local template col actions: edit and delete

--- src/store/features/templateColumns/actions.ts
+++ src/store/features/templateColumns/actions.ts
@@ -3,3 +3,5 @@ import {TemplateColumn} from "./types";
 
 export const addTemplateColumnOptimistically = createAction<{templateColumn: TemplateColumn; index: number}>("templateColumns/addTemplateColumnOptimistically");
 export const moveTemplateColumnOptimistically = createAction<{templateId: string; fromIndex: number; toIndex: number}>("templateColumns/moveTemplateColumnOptimistically");
+export const editTemplateColumnOptimistically = createAction<{columnId: string; overwrite: Partial<TemplateColumn>}>("templateColumns/editTemplateColumnOptimistically");
+export const deleteTemplateColumnOptimistically = createAction<{columnId: string}>("templateColumns/deleteTemplateColumnOptimistically");
