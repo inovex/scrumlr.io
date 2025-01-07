395b8b4646a89b5b473c128baa671ca233379d07
Jakob Schwehn <schwehn42@gmail.com>
2025-01-07 16:38:04 +0100

    add provisional mini column flex view

--- src/components/ColumnsConfigurator/ColumnsMiniView/ColumnsMiniView.tsx
+++ src/components/ColumnsConfigurator/ColumnsMiniView/ColumnsMiniView.tsx
@@ -1,10 +1,31 @@
-import "./ColumnsMiniView.scss";
 import {EditableTemplateColumn} from "store/features";
 import classNames from "classnames";
+import {getColorClassName} from "constants/colors";
+import "./ColumnsMiniView.scss";
 
 type ColumnsMiniViewProps = {
   className: string;
   columns: EditableTemplateColumn[];
 };
 
-export const ColumnsMiniView = (props: ColumnsMiniViewProps) => <div className={classNames(props.className, "columns-mini-view")}>Hello ColumnsMiniView</div>;
+export const ColumnsMiniView = (props: ColumnsMiniViewProps) => {
+  const calcPlacement = (index: number) => {
+    if (index === 0) {
+      return "first";
+    }
+    if (index === props.columns.length - 1) {
+      return "last";
+    }
+    return "center";
+  };
+
+  return (
+    <div className={classNames(props.className, "columns-mini-view")}>
+      {props.columns.map((column) => (
+        <div className={classNames("columns-mini-view__column", `columns-mini-view__column--${calcPlacement(column.index)}`, getColorClassName(column.color))}>
+          {column.index + 1}
+        </div>
+      ))}
+    </div>
+  );
+};
