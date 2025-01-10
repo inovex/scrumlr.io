8f5d0da3d78a7e829f755314ef8635ff796c551a
Jakob Schwehn <schwehn42@gmail.com>
2025-01-10 17:01:03 +0100

    add isVerified logic

--- src/routes/Guards/VerifiedAccountGuard.tsx
+++ src/routes/Guards/VerifiedAccountGuard.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import {Navigate} from "react-router";
+import {useAppSelector} from "store";
 
 type VerifiedAccountGuardProps = {
   children: React.ReactNode;
 };
 
 export const VerifiedAccountGuard = ({children}: VerifiedAccountGuardProps) => {
-  const isAuthenticated = (): boolean => 
-    // Replace this with your actual authentication logic
-     false // or false based on your logic
-  ;
+  const isAnonymous = useAppSelector((state) => state.auth.user?.isAnonymous) ?? true;
 
-  return isAuthenticated() ? (
+  const isVerified = () => !isAnonymous;
+
+  return isVerified() ? (
     // eslint-disable-next-line react/jsx-no-useless-fragment
     <>{children}</>
   ) : (
+    // TODO toast which tells users they have to be verified?
     <Navigate to="/" />
   );
 };
