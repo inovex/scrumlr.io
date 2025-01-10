e052f383becf5bb7bf08f1e379e345964ff63f3c
Jakob Schwehn <schwehn42@gmail.com>
2025-01-10 11:06:49 +0100

    review: search bar clear button

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -24,14 +24,14 @@ export const SearchBar = (props: SearchBarProps) => {
 
   return (
     <div className={classNames(props.className, "search-bar", {"search-bar--disabled": props.disabled})}>
-      <div className="search-bar__icon-container search-bar__icon-container--search-icon">
+      <div className="search-bar__button search-bar__icon-container--search-icon">
         <SearchIcon className="search-bar__icon" aria-label="logo of magnifying glass" />
       </div>
       <input className="search-bar__input" type="text" placeholder={t("SearchBar.placeholder")} disabled={props.disabled} tabIndex={0} value={props.value} onInput={updateInput} />
       {props.value && (
-        <div className="search-bar__icon-container search-bar__icon-container--clear-icon" role="button" tabIndex={0} onClick={clearInput}>
+        <button className="search-bar__button search-bar__button--clear-icon" tabIndex={0} onClick={clearInput}>
           <ClearIcon className="search-bar__icon" aria-label="clear button" />
-        </div>
+        </button>
       )}
     </div>
   );
