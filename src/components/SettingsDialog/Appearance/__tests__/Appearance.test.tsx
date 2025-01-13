f9dea5a3a539a8ef25436acf5a02d59badee01ee
Jakob Schwehn <schwehn42@gmail.com>
2025-01-13 12:23:37 +0100

    add element check to tests

--- src/components/SettingsDialog/Appearance/__tests__/Appearance.test.tsx
+++ src/components/SettingsDialog/Appearance/__tests__/Appearance.test.tsx
@@ -5,6 +5,8 @@ import getTestStore from "utils/test/getTestStore";
 import {Appearance} from "../Appearance";
 import {getByLabelText} from "@testing-library/dom";
 import i18n from "i18nTest";
+import {within} from "@testing-library/react";
+import {t} from "i18next";
 
 const createAppearance = () => (
   <Provider store={getTestStore()}>
@@ -17,12 +19,14 @@ describe("Appearance", () => {
     jest.useFakeTimers().setSystemTime(new Date(2025, 11, 24)); // Christmas!
     const {container} = render(createAppearance());
     expect(container.firstChild).toMatchSnapshot();
+    expect(within(container.getElementsByClassName("appearance-container")[0] as HTMLDivElement).queryByLabelText(t("Appearance.showSnowfall"))).toBeInTheDocument();
   });
 
   test("should render all Settings correctly without Snowfall", () => {
     jest.useFakeTimers().setSystemTime(new Date(2025, 5, 7)); // my birthday!
     const {container} = render(createAppearance());
     expect(container.firstChild).toMatchSnapshot();
+    expect(within(container.getElementsByClassName("appearance-container")[0] as HTMLDivElement).queryByLabelText(t("Appearance.showSnowfall"))).not.toBeInTheDocument();
   });
 
   test("should have a notifications toggle", () => {
