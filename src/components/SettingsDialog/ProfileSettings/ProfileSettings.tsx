293a60b483f0bda55e58f9c3634c25767d9b01d5
Jakob Schwehn <schwehn42@gmail.com>
2025-01-14 11:54:25 +0100

    chore: move timer and hotkey
    to $public/assets/

--- src/components/SettingsDialog/ProfileSettings/ProfileSettings.tsx
+++ src/components/SettingsDialog/ProfileSettings/ProfileSettings.tsx
@@ -59,7 +59,7 @@ export const ProfileSettings = () => {
             >
               <Toggle active={state.hotkeysAreActive} />
             </SettingsButton>
-            <a className="profile-settings__open-cheat-sheet-button" href={`${process.env.PUBLIC_URL}/hotkeys.pdf`} target="_blank" rel="noopener noreferrer">
+            <a className="profile-settings__open-cheat-sheet-button" href={`${process.env.PUBLIC_URL}/assets/hotkeys.pdf`} target="_blank" rel="noopener noreferrer">
               <p>{t("Hotkeys.cheatSheet")}</p>
               <Info />
             </a>
