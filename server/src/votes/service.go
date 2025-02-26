b4150d128154a5b4ad6d6be08d42a9ae310d00b1
Yaazizi <yasser.azizi@inovex.de>
2025-02-26 12:56:02 +0100

    voting anonymous/non-anonymous back and frontend

--- server/src/votes/service.go
+++ server/src/votes/service.go
@@ -93,7 +93,7 @@ func getVotingWithResults(voting database.Voting, votes []database.Vote) *Voting
 		result := VotingResultsPerNote{
 			Total: total,
 		}
-		if voting.ShowVotesOfOthers {
+		if !voting.IsAnonymous {
 			userVotes := map[uuid.UUID]int{}
 			for _, user := range votesPerUser[note] {
 				if _, ok := userVotes[user]; ok {
