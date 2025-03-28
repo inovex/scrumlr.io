c38b80cacdd9b28a53c104170063bd3ff8702401
Flo <florian.schwarz@inovex.de>
2025-03-28 10:17:40 +0100

    refactor: Add feedback package (#4954)

--- server/src/api/feedback.go
+++ server/src/api/feedback.go
@@ -1,44 +1,16 @@
 package api
 
 import (
-	"encoding/json"
-	"errors"
-	"github.com/go-chi/render"
 	"net/http"
+
+	"github.com/go-chi/render"
+	"scrumlr.io/server/feedback"
 	"scrumlr.io/server/logger"
 )
 
-type FeedbackType string
-
-const (
-	FeedbackTypeBugReport      FeedbackType = "BUG_REPORT"
-	FeedbackTypeFeatureRequest FeedbackType = "FEATURE_REQUEST"
-	FeedbackTypePraise         FeedbackType = "PRAISE"
-)
-
-func (feedbackType *FeedbackType) UnmarshalJSON(b []byte) error {
-	var s string
-	if err := json.Unmarshal(b, &s); err != nil {
-		return err
-	}
-	unmarshalledFeedbackType := FeedbackType(s)
-	switch unmarshalledFeedbackType {
-	case FeedbackTypePraise, FeedbackTypeBugReport, FeedbackTypeFeatureRequest:
-		*feedbackType = unmarshalledFeedbackType
-		return nil
-	}
-	return errors.New("Invalid feedback type")
-}
-
-type FeedbackRequest struct {
-	Contact *string      `json:"contact"`
-	Text    *string      `json:"text"`
-	Type    FeedbackType `json:"type"`
-}
-
 func (s *Server) createFeedback(w http.ResponseWriter, r *http.Request) {
 	log := logger.FromRequest(r)
-	var body FeedbackRequest
+	var body feedback.FeedbackRequest
 	if err := render.Decode(r, &body); err != nil {
 		log.Errorw("Unable to decode body", "err", err)
 		w.WriteHeader(http.StatusBadRequest)
@@ -52,11 +24,11 @@ func (s *Server) createFeedback(w http.ResponseWriter, r *http.Request) {
 		str := "/"
 		body.Text = &str
 	}
-	if body.Type == FeedbackTypeFeatureRequest && *body.Text == "/" {
+	if body.Type == feedback.FeedbackTypeFeatureRequest && *body.Text == "/" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	if body.Type == FeedbackTypeBugReport && *body.Text == "/" {
+	if body.Type == feedback.FeedbackTypeBugReport && *body.Text == "/" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
