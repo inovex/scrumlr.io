aea98755c915501c85082dacac103f364d31539c
Jakob Schwehn <schwehn42@gmail.com>
2025-04-03 11:56:32 +0200

    review patch 1

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,20 @@
-import React from "react";
 import classNames from "classnames";
 import {Color, getColorClassName} from "constants/colors";
+import {MouseEvent, ReactNode} from "react";
 import "./Button.scss";
 
 type ButtonType = "primary" | "secondary" | "tertiary";
 
 type ButtonProps = {
   type?: ButtonType;
-  children?: React.ReactNode;
+  children?: ReactNode;
   className?: string;
   color?: Color;
   disabled?: boolean;
   small?: boolean;
-  icon?: React.ReactNode;
+  icon?: ReactNode;
 
-  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
 };
 
 export const Button = (props: ButtonProps) => (
