84741410653732313789ce6a03b54ef3aea21b85
Flo <florian.schwarz@inovex.de>
2025-04-16 17:42:59 +0200

    fix: naming of refactored packages (#4990)

--- server/src/database/database.go
+++ server/src/database/database.go
@@ -83,7 +83,7 @@ func (d *Database) Get(id uuid.UUID) (FullBoard, error) {
 		case getNotes:
 			notes, err = d.GetNotes(id)
 		case getReactions:
-			reactions, err = d.reactionsDb.GetReactions(id)
+			reactions, err = d.reactionsDb.GetAll(id)
 		case getVotings:
 			votings, votes, err = d.votingDB.GetVotings(id)
 		}
