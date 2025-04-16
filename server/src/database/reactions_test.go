68c99f3b9c9f688c7e3542d84e6f76fc9ad371cb
Flo <florian.schwarz@inovex.de>
2025-04-16 17:42:59 +0200

    fix: naming of refactored packages (#4990)

--- server/src/database/reactions_test.go
+++ server/src/database/reactions_test.go
@@ -22,7 +22,7 @@ func TestRunnerForReactions(t *testing.T) {
 	t.Run("Delete=1", testDeleteReactionFailsBecauseForbidden)
 }
 
-var notesTestA1 *notes.NoteDB
+var notesTestA1 *Note
 var boardTestBoard *Board
 var reactionUserJay *users.DatabaseUser
 var reactionUserJack *users.DatabaseUser
@@ -41,6 +41,8 @@ func testGetReaction(t *testing.T) {
 func testGetReactionsForNote(t *testing.T) {
 	notesTestA1 = fixture.MustRow("NoteDB.notesTestA1").(*notes.NoteDB)
 	r, err := reactionDb.GetReactionsForNote(notesTestA1.ID)
+	notesTestA1 = fixture.MustRow("Note.notesTestA1").(*Note)
+	r, err := reactionDb.GetAllForNote(notesTestA1.ID)
 
 	assert.Nil(t, err)
 	assert.Equal(t, 2, len(r))
@@ -48,7 +50,7 @@ func testGetReactionsForNote(t *testing.T) {
 
 func testGetReactions(t *testing.T) {
 	boardTestBoard = fixture.MustRow("Board.notesTestBoard").(*Board)
-	r, err := reactionDb.GetAll(boardTestBoard.ID)
+	r, err := reactionDb.GetReactions(boardTestBoard.ID)
 
 	assert.Nil(t, err)
 	assert.Equal(t, 3, len(r))
