8533d80e9ba72d466f2d2eb1d391af2a022451a8
Jakob Schwehn <schwehn42@gmail.com>
2025-05-06 15:47:59 +0200

    preinit test store

--- src/components/ColumnsConfigurator/ColumnsConfiguratorColumn/__tests__/ColumnConfiguratorColumn.test.tsx
+++ src/components/ColumnsConfigurator/ColumnsConfiguratorColumn/__tests__/ColumnConfiguratorColumn.test.tsx
@@ -3,25 +3,46 @@ import {Provider} from "react-redux";
 import getTestStore from "utils/test/getTestStore";
 import {ColumnsConfiguratorColumn} from "../ColumnsConfiguratorColumn";
 import {EditableTemplateColumn} from "store/features";
+import {MockStoreEnhanced} from "redux-mock-store";
+import {ApplicationState} from "store";
 
-const renderColumnConfiguratorColumn = (templateId: string, columnId: string, potentialIndex: number) => {
-  const testStore = getTestStore();
-  // some preprocessing to get the correct column and convert to editableColumn, which would normally be done by TemplateEditor component
-  const columns = testStore.getState().templatesColumns.filter((tc) => tc.template === templateId);
-  const editableColumns = columns.map((bc) => ({...bc, persisted: false, mode: undefined}) as EditableTemplateColumn);
-  const column = editableColumns.find((c) => c.id === columnId)!;
+describe("ColumnConfiguratorColumn render", () => {
+  let testStore: MockStoreEnhanced<ApplicationState>;
 
-  return render(
-    <Provider store={testStore}>
-      <ColumnsConfiguratorColumn column={column} index={potentialIndex} allColumns={editableColumns} />
-    </Provider>
-  );
-};
+  beforeEach(() => {
+    testStore = getTestStore();
+  });
 
-describe("ColumnConfiguratorColumn", () => {
+  const renderColumnConfiguratorColumn = (templateId: string, columnId: string, potentialIndex: number) => {
+    // some preprocessing to get the correct column and convert to editableColumn, which would normally be done by TemplateEditor component
+    const columns = testStore.getState().templatesColumns.filter((tc) => tc.template === templateId);
+    const editableColumns = columns.map((bc) => ({...bc, persisted: false, mode: undefined}) as EditableTemplateColumn);
+    const column = editableColumns.find((c) => c.id === columnId)!;
+
+    return render(
+      <Provider store={testStore}>
+        <ColumnsConfiguratorColumn column={column} index={potentialIndex} allColumns={editableColumns} />
+      </Provider>
+    );
+  };
+
   it("should render correctly", () => {
     const {container} = renderColumnConfiguratorColumn("test-templates-id-1", "test-templates-columns-id-1", 0);
 
     expect(container).toMatchSnapshot();
+  });
+
+  it("should be an even column with index 0", () => {
+    const {container} = renderColumnConfiguratorColumn("test-templates-id-1", "test-templates-columns-id-1", 0);
+    const rootNode = container.firstChild!;
+
+    expect(rootNode).toHaveClass("columns-configurator-column--even");
+  });
+
+  it("should be an odd column with index 1", () => {
+    const {container} = renderColumnConfiguratorColumn("test-templates-id-1", "test-templates-columns-id-1", 1);
+    const rootNode = container.firstChild!;
+
+    expect(rootNode).toHaveClass("columns-configurator-column--odd");
   });
 });
