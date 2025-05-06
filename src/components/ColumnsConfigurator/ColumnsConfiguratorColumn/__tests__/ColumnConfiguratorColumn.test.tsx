ea081200197f76f7d8d43a2cef3f651f0b5e38e9
Jakob Schwehn <schwehn42@gmail.com>
2025-05-06 17:37:24 +0200

    add test trying out edit column color

--- src/components/ColumnsConfigurator/ColumnsConfiguratorColumn/__tests__/ColumnConfiguratorColumn.test.tsx
+++ src/components/ColumnsConfigurator/ColumnsConfiguratorColumn/__tests__/ColumnConfiguratorColumn.test.tsx
@@ -5,6 +5,7 @@ import {ColumnsConfiguratorColumn} from "../ColumnsConfiguratorColumn";
 import {EditableTemplateColumn} from "store/features";
 import {MockStoreEnhanced} from "redux-mock-store";
 import {ApplicationState} from "store";
+import {fireEvent} from "@testing-library/react";
 
 describe("ColumnConfiguratorColumn render", () => {
   let testStore: MockStoreEnhanced<ApplicationState>;
@@ -13,7 +14,12 @@ describe("ColumnConfiguratorColumn render", () => {
     testStore = getTestStore();
   });
 
-  const renderColumnConfiguratorColumn = (templateId: string, columnId: string, potentialIndex: number) => {
+  const renderColumnConfiguratorColumn = (
+    templateId: string,
+    columnId: string,
+    potentialIndex: number,
+    editColumn?: (templateColumn: EditableTemplateColumn, overwrite: Partial<EditableTemplateColumn>) => void
+  ) => {
     // some preprocessing to get the correct column and convert to editableColumn, which would normally be done by TemplateEditor component
     const columns = testStore.getState().templatesColumns.filter((tc) => tc.template === templateId);
     const editableColumns = columns.map((bc) => ({...bc, persisted: false, mode: undefined}) as EditableTemplateColumn);
@@ -21,7 +27,7 @@ describe("ColumnConfiguratorColumn render", () => {
 
     return render(
       <Provider store={testStore}>
-        <ColumnsConfiguratorColumn column={column} index={potentialIndex} allColumns={editableColumns} />
+        <ColumnsConfiguratorColumn column={column} index={potentialIndex} allColumns={editableColumns} editColumn={editColumn} />
       </Provider>
     );
   };
@@ -44,5 +50,22 @@ describe("ColumnConfiguratorColumn render", () => {
     const rootNode = container.firstChild!;
 
     expect(rootNode).toHaveClass("columns-configurator-column--odd");
+  });
+
+  // opens color picker, select pink color, then checks if editColumn fn has been called
+  it("should be able to change color", async () => {
+    const columnId = "test-templates-columns-id-1";
+    const editColumnSpy: jest.Mock<(templateColumn: EditableTemplateColumn, overwrite: Partial<EditableTemplateColumn>) => void> = jest.fn();
+    const {container} = renderColumnConfiguratorColumn("test-templates-id-1", columnId, 0, editColumnSpy);
+
+    const colorPickerElement = container.querySelector(".columns-configurator-column__color-picker")!;
+    expect(colorPickerElement).toBeInTheDocument();
+    fireEvent.click(colorPickerElement);
+
+    const colorPickerButtonPlanningPink = container.querySelector<HTMLButtonElement>("li.accent-color__planning-pink button")!;
+
+    fireEvent.mouseDown(colorPickerButtonPlanningPink);
+
+    expect(editColumnSpy).toHaveBeenCalledWith(expect.anything(), expect.objectContaining({color: "planning-pink"}));
   });
 });
