8c9ab2c9927e60a386b22dc0ef3dfb07f31fd83e
Jakob Schwehn <schwehn42@gmail.com>
2025-05-13 15:42:43 +0200

    add custom test renderer with outlet context

--- src/testUtils.tsx
+++ src/testUtils.tsx
@@ -1,7 +1,7 @@
 import {render, RenderOptions} from "@testing-library/react";
 import {I18nextProvider} from "react-i18next";
-import {FC, PropsWithChildren, ReactElement} from "react";
-import {MemoryRouter} from "react-router";
+import {FC, PropsWithChildren, ReactElement, ReactNode} from "react";
+import {MemoryRouter, Outlet, Route, Routes} from "react-router";
 import {Provider} from "react-redux";
 import getTestStore from "utils/test/getTestStore";
 import i18n from "./i18nTest";
@@ -20,8 +20,27 @@ const AllTheProviders: FC<PropsWithChildren> = ({children}) => (
   </MemoryRouter>
 );
 
+interface AllTheProvidersProps {
+  children: ReactNode;
+  context: unknown;
+}
+
+const AllTheProvidersWithContext: FC<AllTheProvidersProps> = ({children, context}) => (
+  <MemoryRouter>
+    <Routes>
+      <Route path="/" element={<Outlet context={context} />}>
+        <Route index element={<AllTheProvidersWithoutRouter>{children}</AllTheProvidersWithoutRouter>} />
+      </Route>
+    </Routes>
+  </MemoryRouter>
+);
+
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions & {container: Element}, "wrapper">) => render(ui, {wrapper: AllTheProviders, ...options});
+
+const customRenderWithContext = (ui: ReactElement, context: unknown, options?: Omit<RenderOptions & {container: Element}, "wrapper">) =>
+  render(ui, {wrapper: (props) => <AllTheProvidersWithContext context={context} {...props} />, ...options});
+
 const customRenderWithoutRouter = (ui: ReactElement, options?: Omit<RenderOptions & {container: Element}, "wrapper">) =>
   render(ui, {wrapper: AllTheProvidersWithoutRouter, ...options});
 
-export {customRender as render, customRenderWithoutRouter as renderWithoutRouter};
+export {customRender as render, customRenderWithContext as renderWithContext, customRenderWithoutRouter as renderWithoutRouter};
