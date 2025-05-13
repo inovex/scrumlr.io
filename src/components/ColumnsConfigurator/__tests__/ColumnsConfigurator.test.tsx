1c9176c7adc784f00968f8c4c4e91a2f62fde5b5
Jakob Schwehn <schwehn42@gmail.com>
2025-05-13 11:12:50 +0200

    test ColumnsConfigurator add spy

--- src/components/ColumnsConfigurator/__tests__/ColumnsConfigurator.test.tsx
+++ src/components/ColumnsConfigurator/__tests__/ColumnsConfigurator.test.tsx
@@ -3,6 +3,7 @@ import {Partial} from "@react-spring/web";
 import {render} from "testUtils";
 import getTestApplicationState from "utils/test/getTestApplicationState";
 import {convertToEditableColumn} from "components/ColumnsConfigurator/ColumnsConfigurator.utils";
+import {fireEvent} from "@testing-library/react";
 
 const renderColumnsConfigurator = (templateId: string, override?: Partial<Omit<ColumnsConfiguratorProps, "templateId" | "columns">>) => {
   const defaultProps: ColumnsConfiguratorProps = {
@@ -24,5 +25,49 @@ describe("ColumnsConfigurator", () => {
   it("should render correctly", () => {
     const {container} = renderColumnsConfigurator("test-templates-id-1");
     expect(container).toMatchSnapshot();
+
+    const columnsNodeList = container.querySelectorAll(".columns-configurator__column");
+    expect(columnsNodeList).toHaveLength(2);
+  });
+
+  it("should fire correct add new template col signal", () => {
+    const addColumnSpy = jest.fn();
+    const {container} = renderColumnsConfigurator("test-templates-id-1", {addColumn: addColumnSpy});
+    const addTemplateColumnLeft = container.querySelector(".add-template-column--left")!;
+    const addTemplateColumnRight = container.querySelector(".add-template-column--right")!;
+    const addTemplateColumnLeftButton = addTemplateColumnLeft.querySelector("button")!;
+    const addTemplateColumnRightButton = addTemplateColumnRight.querySelector("button")!;
+
+    // template columns are backlog-blue and planning-pink, so the colors left and right to that are online-orange and poker-purple respectively
+    expect(addTemplateColumnLeft).toHaveClass("accent-color__online-orange");
+    expect(addTemplateColumnRight).toHaveClass("accent-color__poker-purple");
+
+    fireEvent.click(addTemplateColumnLeftButton);
+    expect(addColumnSpy).toHaveBeenCalledWith(
+      {
+        id: expect.stringMatching(/col(\d+)/),
+        template: "test-templates-id-1",
+        name: "Column 3",
+        description: "",
+        color: "online-orange",
+        visible: true,
+        index: -1,
+      },
+      0
+    );
+
+    fireEvent.click(addTemplateColumnRightButton);
+    expect(addColumnSpy).toHaveBeenCalledWith(
+      {
+        id: expect.stringMatching(/col(\d+)/),
+        template: "test-templates-id-1",
+        name: "Column 3", // note hat since the state does not change, column length remains the same
+        description: "",
+        color: "poker-purple",
+        visible: true,
+        index: -1,
+      },
+      2
+    );
   });
 });
