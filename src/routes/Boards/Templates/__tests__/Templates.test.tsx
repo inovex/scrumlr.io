255c0b66d51c42ee4e2bde86476e21da5aa86571
Jakob Schwehn <schwehn42@gmail.com>
2025-05-14 11:52:45 +0200

    skip Boards and Templates tests
    requires #5079

--- src/routes/Boards/Templates/__tests__/Templates.test.tsx
+++ src/routes/Boards/Templates/__tests__/Templates.test.tsx
@@ -1,11 +1,32 @@
 import {renderWithContext} from "testUtils";
 import {Templates} from "routes/Boards/Templates/Templates";
+import {API} from "api";
+import getTestApplicationState from "utils/test/getTestApplicationState";
+import {TemplateWithColumns} from "store/features";
 
 const contextData = {searchBarInput: "test1"};
 
+const {templates: templatesPreloaded, templatesColumns: templatesColumnsPreloaded} = getTestApplicationState();
+const mockedTemplateWithColumns: TemplateWithColumns[] = templatesPreloaded.map((template) => ({
+  template,
+  columns: templatesColumnsPreloaded.filter((column) => column.template === template.id),
+}));
+
 const renderTemplates = () => renderWithContext(<Templates />, contextData);
 
-describe("Templates", () => {
+jest.mock("api", () => ({API: {getTemplates: jest.fn()}}));
+
+// thunks are currently not supported within our test utils.
+// wait for https://github.com/inovex/scrumlr.io/issues/5079
+describe.skip("Templates", () => {
+  beforeEach(() => {
+    (API.getTemplates as jest.Mock).mockImplementation(() => Promise.resolve(mockedTemplateWithColumns));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("should render correctly", () => {
     const {container} = renderTemplates();
     expect(container).toMatchSnapshot();
