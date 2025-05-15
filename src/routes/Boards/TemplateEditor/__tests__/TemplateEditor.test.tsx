0f81106ca463fc70000f23b364e36e3e54b80bb4
Jakob Schwehn <schwehn42@gmail.com>
2025-05-15 10:45:08 +0200

    more tests

--- src/routes/Boards/TemplateEditor/__tests__/TemplateEditor.test.tsx
+++ src/routes/Boards/TemplateEditor/__tests__/TemplateEditor.test.tsx
@@ -1,5 +1,6 @@
 import {TemplateEditor, TemplateEditorProps} from "routes/Boards/TemplateEditor/TemplateEditor";
 import {renderWithContext} from "testUtils";
+import {fireEvent} from "@testing-library/react";
 
 const renderTemplateEditor = (props: TemplateEditorProps, editTemplateId?: string) =>
   props.mode === "create"
@@ -15,6 +16,21 @@ describe("TemplateEditor create", () => {
     const {container} = renderTemplateEditor({mode: "create"});
     expect(container).toMatchSnapshot();
   });
+
+  it("should only allow clicking create if name is set", () => {
+    const {container} = renderTemplateEditor({mode: "create"});
+    const nameInputNode = container.querySelector<HTMLInputElement>(".template-editor__name-input input")!;
+    const createTemplateButtonNode = container.querySelector<HTMLButtonElement>(".template-editor__button--create")!;
+
+    expect(createTemplateButtonNode).toBeDisabled();
+
+    fireEvent.input(nameInputNode, {target: {value: "hello world"}});
+
+    expect(createTemplateButtonNode).not.toBeDisabled();
+  });
+
+  // need to spy on store
+  it.skip("should dispatch create template", () => {});
 });
 
 describe("TemplateEditor edit", () => {
@@ -23,5 +39,16 @@ describe("TemplateEditor edit", () => {
     expect(container).toMatchSnapshot();
   });
 
-  it("fill form with data from the corresponding template", () => {});
+  it("fill form with data from the corresponding template", () => {
+    const {container} = renderTemplateEditor({mode: "edit"}, "test-templates-id-1");
+
+    const nameInputNode = container.querySelector<HTMLInputElement>(".template-editor__name-input input")!;
+    const descriptionInputNode = container.querySelector<HTMLTextAreaElement>(".template-editor__description-text-area")!;
+
+    expect(nameInputNode).toHaveValue("sample name 1");
+    expect(descriptionInputNode).toHaveValue("sample description 1");
+  });
+
+  // need to spy on store
+  it.skip("should dispatch edit template", () => {});
 });
