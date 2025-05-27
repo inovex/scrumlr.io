d6565644cad011dbcc888745ea6ef096ba2a0104
Jakob Schwehn <schwehn42@gmail.com>
2025-05-27 15:17:02 +0200

    flex select

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,10 +1,10 @@
-import "./Select.scss";
 import {Children, cloneElement, isValidElement, ReactElement} from "react";
-import {SelectOption, SelectOptionProps} from "components/Select/SelectOption/SelectOption";
+import {SelectOptionProps} from "components/Select/SelectOption/SelectOption";
 import {SelectContext} from "utils/hooks/useSelect";
+import "./Select.scss";
 
 type SelectProps = {
-  children: ReactElement<typeof SelectOption> | ReactElement<typeof SelectOption>[];
+  children: ReactElement<SelectOptionProps> | ReactElement<SelectOptionProps>[];
   activeIndex: number;
   setActiveIndex: (index: number) => void;
 };
@@ -12,7 +12,7 @@ type SelectProps = {
 // general select component
 // add <SelectOption> elements as children
 export const Select = ({children, activeIndex, setActiveIndex}: SelectProps) => (
-    <SelectContext.Provider value={{activeIndex, setActiveIndex}}>
-      <div className="select">{Children.map(children, (child, index) => (isValidElement<SelectOptionProps>(child) ? cloneElement(child, {index}) : null))}</div>
-    </SelectContext.Provider>
-  );
+  <SelectContext.Provider value={{activeIndex, setActiveIndex}}>
+    <div className="select">{Children.map(children, (child, index) => (isValidElement<SelectOptionProps>(child) ? cloneElement(child, {index}) : null))}</div>
+  </SelectContext.Provider>
+);
