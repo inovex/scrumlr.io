e8d1b52aa5c328ec5fdac43735f0d0d1423b674f
Jakob Schwehn <schwehn42@gmail.com>
2025-05-27 15:49:30 +0200

    access options localization
    only en for now

--- src/components/Templates/AccessSettings/AccessSettings.tsx
+++ src/components/Templates/AccessSettings/AccessSettings.tsx
@@ -1,11 +1,13 @@
-import "./AccessSettings.scss";
 import {Select} from "components/Select/Select";
 import {SelectOption} from "components/Select/SelectOption/SelectOption";
 import {ReactComponent as Icon} from "assets/icons/info.svg";
 import {useState} from "react";
+import {useTranslation} from "react-i18next";
+import "./AccessSettings.scss";
 
 // yes, this modal can also be abstracted / generalized if need be
 export const AccessSettings = () => {
+  const {t} = useTranslation();
   const [activeAccessSettingIndex, setActiveAccessSettingIndex] = useState(0);
 
   return (
@@ -16,8 +18,9 @@ export const AccessSettings = () => {
         </header>
         <main className="access-settings__main">
           <Select activeIndex={activeAccessSettingIndex} setActiveIndex={setActiveAccessSettingIndex}>
-            <SelectOption label="test" description="wow" icon={<Icon />} />
-            <SelectOption label="another" description="amazing" icon={<Icon />} />
+            <SelectOption label={t("Templates.AccessSettings.Public.title")} description={t("Templates.AccessSettings.Public.description")} icon={<Icon />} />
+            <SelectOption label={t("Templates.AccessSettings.By_Invite.title")} description={t("Templates.AccessSettings.By_Invite.description")} icon={<Icon />} />
+            <SelectOption label={t("Templates.AccessSettings.By_Passphrase.title")} description={t("Templates.AccessSettings.By_Passphrase.description")} icon={<Icon />} />
           </Select>
         </main>
         <footer className="access-settings__footer">
