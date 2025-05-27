7db28d8bf02c672601e9007bf3cad0093fc1e2d0
Jakob Schwehn <schwehn42@gmail.com>
2025-05-27 16:04:09 +0200

    use icons from design

--- src/components/Templates/AccessSettings/AccessSettings.tsx
+++ src/components/Templates/AccessSettings/AccessSettings.tsx
@@ -1,10 +1,12 @@
 import {Select} from "components/Select/Select";
 import {SelectOption} from "components/Select/SelectOption/SelectOption";
-import {ReactComponent as Icon} from "assets/icons/info.svg";
+import {ReactComponent as GlobeIcon} from "assets/icons/open.svg";
+import {ReactComponent as LockIcon} from "assets/icons/lock-closed.svg";
+import {ReactComponent as KeyIcon} from "assets/icons/key-protected.svg";
 import {useState} from "react";
 import {useTranslation} from "react-i18next";
-import "./AccessSettings.scss";
 import {Button} from "components/Button";
+import "./AccessSettings.scss";
 
 // yes, this modal can also be abstracted / generalized if need be
 export const AccessSettings = () => {
@@ -19,9 +21,9 @@ export const AccessSettings = () => {
         </header>
         <main className="access-settings__main">
           <Select activeIndex={activeAccessSettingIndex} setActiveIndex={setActiveAccessSettingIndex}>
-            <SelectOption label={t("Templates.AccessSettings.Public.title")} description={t("Templates.AccessSettings.Public.description")} icon={<Icon />} />
-            <SelectOption label={t("Templates.AccessSettings.By_Invite.title")} description={t("Templates.AccessSettings.By_Invite.description")} icon={<Icon />} />
-            <SelectOption label={t("Templates.AccessSettings.By_Passphrase.title")} description={t("Templates.AccessSettings.By_Passphrase.description")} icon={<Icon />} />
+            <SelectOption label={t("Templates.AccessSettings.Public.title")} description={t("Templates.AccessSettings.Public.description")} icon={<GlobeIcon />} />
+            <SelectOption label={t("Templates.AccessSettings.By_Invite.title")} description={t("Templates.AccessSettings.By_Invite.description")} icon={<LockIcon />} />
+            <SelectOption label={t("Templates.AccessSettings.By_Passphrase.title")} description={t("Templates.AccessSettings.By_Passphrase.description")} icon={<KeyIcon />} />
           </Select>
         </main>
         <footer className="access-settings__footer">
