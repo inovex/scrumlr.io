8f960a3c1e162ed9eb6afe069e4351a1f5aa8a03
Jakob Schwehn <schwehn42@gmail.com>
2025-05-27 14:36:55 +0200

    delegate active index to access settings component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,20 +1,18 @@
 import "./Select.scss";
-import {Children, cloneElement, isValidElement, ReactElement, useState} from "react";
+import {Children, cloneElement, isValidElement, ReactElement} from "react";
 import {SelectOption, SelectOptionProps} from "components/Select/SelectOption/SelectOption";
 import {SelectContext} from "utils/hooks/useSelect";
 
 type SelectProps = {
   children: ReactElement<typeof SelectOption> | ReactElement<typeof SelectOption>[];
+  activeIndex: number;
+  setActiveIndex: (index: number) => void;
 };
 
 // general select component
 // add <SelectOption> elements as children
-export const Select = ({children}: SelectProps) => {
-  const [activeIndex, setActiveIndex] = useState(0); // pre-select first element
-
-  return (
+export const Select = ({children, activeIndex, setActiveIndex}: SelectProps) => (
     <SelectContext.Provider value={{activeIndex, setActiveIndex}}>
       <div className="select">{Children.map(children, (child, index) => (isValidElement<SelectOptionProps>(child) ? cloneElement(child, {index}) : null))}</div>
     </SelectContext.Provider>
   );
-};
--- src/components/Templates/AccessSettings/AccessSettings.tsx
+++ src/components/Templates/AccessSettings/AccessSettings.tsx
@@ -2,24 +2,29 @@ import "./AccessSettings.scss";
 import {Select} from "components/Select/Select";
 import {SelectOption} from "components/Select/SelectOption/SelectOption";
 import {ReactComponent as Icon} from "assets/icons/info.svg";
+import {useState} from "react";
 
 // yes, this modal can also be abstracted / generalized if need be
-export const AccessSettings = () => (
-  <div className="access-settings__wrapper">
-    <div className="access-settings">
-      <header className="access-settings__header">
-        <div className="access-settings__title">Access Settings</div>
-      </header>
-      <main className="access-settings__main">
-        <Select>
-          <SelectOption label="test" description="wow" icon={<Icon />} />
-          <SelectOption label="another" description="amazing" icon={<Icon />} />
-        </Select>
-      </main>
-      <footer className="access-settings__footer">
-        <button className="access-settings__cancel">Go back</button>
-        <button className="access-settings__start">Start Session</button>
-      </footer>
+export const AccessSettings = () => {
+  const [activeAccessSettingIndex, setActiveAccessSettingIndex] = useState(0);
+
+  return (
+    <div className="access-settings__wrapper">
+      <div className="access-settings">
+        <header className="access-settings__header">
+          <div className="access-settings__title">Access Settings</div>
+        </header>
+        <main className="access-settings__main">
+          <Select activeIndex={activeAccessSettingIndex} setActiveIndex={setActiveAccessSettingIndex}>
+            <SelectOption label="test" description="wow" icon={<Icon />} />
+            <SelectOption label="another" description="amazing" icon={<Icon />} />
+          </Select>
+        </main>
+        <footer className="access-settings__footer">
+          <button className="access-settings__cancel">Go back</button>
+          <button className="access-settings__start">Start Session</button>
+        </footer>
+      </div>
     </div>
-  </div>
-);
+  );
+};
