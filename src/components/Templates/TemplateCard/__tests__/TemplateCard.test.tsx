63515c36ffa50aacad5152b5329af6dc6b3cba9b
Jakob Schwehn <schwehn42@gmail.com>
2025-05-28 13:45:56 +0200

    separate template card render funcs

--- src/components/Templates/TemplateCard/__tests__/TemplateCard.test.tsx
+++ src/components/Templates/TemplateCard/__tests__/TemplateCard.test.tsx
@@ -1,23 +1,44 @@
 import {render} from "testUtils";
-import {TemplateCard, TemplateCardType} from "components/Templates/TemplateCard/TemplateCard";
+import {TemplateCard} from "components/Templates/TemplateCard/TemplateCard";
 import {getTemplateAndColumnsByTemplateId} from "components/ColumnsConfigurator/ColumnsConfigurator.utils";
 import getTestApplicationState from "utils/test/getTestApplicationState";
+import {TemplateWithColumns} from "store/features";
 
-const renderTemplateCard = (templateId: string, templateType: TemplateCardType) => {
+const renderRecommendedTemplateCard = (templateId: string, onSelectTemplate: (template: TemplateWithColumns) => void = jest.fn()) => {
   const templateWithColumns = getTemplateAndColumnsByTemplateId({...getTestApplicationState()}, templateId)!;
-  return render(<TemplateCard template={templateWithColumns} templateType={templateType} />);
+
+  return render(<TemplateCard template={templateWithColumns} templateType={"RECOMMENDED"} onSelectTemplate={onSelectTemplate} />);
+};
+
+const renderCustomTemplateCard = (
+  templateId: string,
+  onSelectTemplate: (template: TemplateWithColumns) => void = jest.fn(),
+  onDeleteTemplate: (templateId: string) => void = jest.fn(),
+  onToggleFavourite: (templateId: string, favourite: boolean) => void = jest.fn(),
+  onNavigateToEdit: (templateId: string) => void = jest.fn()
+) => {
+  const templateWithColumns = getTemplateAndColumnsByTemplateId({...getTestApplicationState()}, templateId)!;
+
+  return render(
+    <TemplateCard
+      template={templateWithColumns}
+      templateType={"CUSTOM"}
+      onSelectTemplate={onSelectTemplate}
+      onDeleteTemplate={onDeleteTemplate}
+      onToggleFavourite={onToggleFavourite}
+      onNavigateToEdit={onNavigateToEdit}
+    />
+  );
 };
 
 describe("TemplateCard", () => {
   it("should render correctly (recommended)", () => {
-    const {container} = renderTemplateCard("test-templates-id-1", "RECOMMENDED");
+    const {container} = renderRecommendedTemplateCard("test-templates-id-1");
     expect(container).toMatchSnapshot();
   });
 
   it("should render correctly (custom)", () => {
-    const {container} = renderTemplateCard("test-templates-id-1", "CUSTOM");
+    const {container} = renderCustomTemplateCard("test-templates-id-1");
     expect(container).toMatchSnapshot();
   });
-
-  // more tests could include dispatch spies, which we don't support yet (#5079)
 });
