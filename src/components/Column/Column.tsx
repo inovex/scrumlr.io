863dc1c50f80337fbd491e33c821b57c30fe8cfc
Jakob Schwehn <schwehn42@gmail.com>
2025-06-02 13:06:39 +0200

    remove remains of name details from column

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,21 +1,13 @@
-import {useTranslation} from "react-i18next";
 import _ from "underscore";
 import {useEffect, useRef, useState} from "react";
 import classNames from "classnames";
-import {Tooltip} from "react-tooltip";
-import {useAppDispatch, useAppSelector} from "store";
-import {createColumn, deleteColumnOptimistically, editColumn, editColumnOptimistically} from "store/features";
+import {useAppSelector} from "store";
 import {Color, getColorClassName} from "constants/colors";
-import {NoteInput} from "components/NoteInput";
 import {Droppable} from "components/DragAndDrop/Droppable";
-import {EmojiSuggestions} from "components/EmojiSuggestions";
-import {ColumnSettings} from "components/Column/ColumnSettings";
-import {Close, MarkAsDone, Hidden, ThreeDots} from "components/Icon";
 import {Note} from "components/Note";
-import {useEmojiAutocomplete} from "utils/hooks/useEmojiAutocomplete";
 import {useStripeOffset} from "utils/hooks/useStripeOffset";
-import {useTextOverflow} from "utils/hooks/useTextOverflow";
 import "./Column.scss";
+import {ColumnHeader} from "components/Column/ColumnHeader/ColumnHeader";
 
 export interface ColumnProps {
   id: string;
@@ -26,10 +18,7 @@ export interface ColumnProps {
 }
 
 export const Column = ({id, name, color, visible, index}: ColumnProps) => {
-  const {t} = useTranslation();
-  const dispatch = useAppDispatch();
-
-  const {isTextTruncated, textRef} = useTextOverflow<HTMLHeadingElement>(name);
+  // const {isTextTruncated, textRef} = useTextOverflow<HTMLHeadingElement>(name);
 
   const notes = useAppSelector(
     (state) =>
@@ -46,16 +35,9 @@ export const Column = ({id, name, color, visible, index}: ColumnProps) => {
 
   const colorClassName = getColorClassName(color);
   const isModerator = viewer?.role === "OWNER" || viewer?.role === "MODERATOR";
-  const {value: columnName, ...emoji} = useEmojiAutocomplete<HTMLDivElement>({maxInputLength: 32, initialValue: name});
+  // const {value: columnName, ...emoji} = useEmojiAutocomplete<HTMLDivElement>({maxInputLength: 32, initialValue: name});
 
-  const [columnNameMode, setColumnNameMode] = useState<"VIEW" | "EDIT">("VIEW");
-  const [openedColumnSettings, setOpenedColumnSettings] = useState(false);
-  const [isTemporary, setIsTemporary] = useState(id === "TEMP_ID");
-
-  const inputRef = useRef<HTMLInputElement>();
   const columnRef = useRef<HTMLElement | null>(null);
-  const closeButtonRef = useRef<HTMLButtonElement>(null);
-
   const [localNotes, setLocalNotes] = useState(notes);
   useEffect(() => {
     setLocalNotes(notes);
@@ -64,151 +46,6 @@ export const Column = ({id, name, color, visible, index}: ColumnProps) => {
   const setItems = (items: string[]) => {
     setLocalNotes(items);
   };
-
-  useEffect(() => {
-    if (isTemporary) {
-      setColumnNameMode("EDIT");
-      columnRef?.current?.scrollIntoView({inline: "start", behavior: "smooth"});
-    }
-  }, [isTemporary]);
-
-  const handleEditColumnName = (newName: string) => {
-    if (isTemporary) {
-      if (!newName) {
-        dispatch(deleteColumnOptimistically(id));
-      } else {
-        dispatch(
-          editColumnOptimistically({
-            id,
-            column: {
-              name: newName,
-              color,
-              visible,
-              index,
-            },
-          })
-        ); // Prevents flicker when submitting a new column
-        dispatch(createColumn({name: newName, color, visible, index}));
-        setIsTemporary(false);
-      }
-    } else {
-      dispatch(
-        editColumn({
-          id,
-          column: {
-            name: newName,
-            color,
-            visible,
-            index,
-          },
-        })
-      );
-    }
-    setColumnNameMode("VIEW");
-  };
-
-  const renderColumnName = () =>
-    columnNameMode === "VIEW" ? (
-      <div className={classNames("column__header-text-wrapper", {"column__header-text-wrapper--hidden": !visible})}>
-        {!visible && (
-          <Hidden
-            className="column__header-hidden-icon"
-            title={t("Column.hiddenColumn")}
-            onClick={() => {
-              throw new Error("implement in ColumnHeader");
-            }}
-          />
-        )}
-        <h2
-          data-clarity-mask="True"
-          ref={textRef}
-          id={`column-${id}`}
-          onDoubleClick={() => {
-            if (isModerator) {
-              setColumnNameMode("EDIT");
-            }
-          }}
-          className={classNames("column__header-text", {"column__header-text--hidden": !visible})}
-        >
-          {name}
-        </h2>
-        {isTextTruncated && (
-          <Tooltip className="column__tooltip" anchorSelect={`#column-${id}`}>
-            {name}
-          </Tooltip>
-        )}
-      </div>
-    ) : (
-      <>
-        <input
-          {...emoji.inputBindings}
-          data-clarity-mask="True"
-          className="column__header-input"
-          type="text"
-          onKeyDown={(e) => {
-            emoji.inputBindings.onKeyDown(e);
-            if (e.defaultPrevented) return;
-
-            if (e.key === "Escape") {
-              if (isTemporary) {
-                dispatch(deleteColumnOptimistically(id));
-              }
-              setColumnNameMode("VIEW");
-            } else if (e.key === "Enter") {
-              handleEditColumnName((e.target as HTMLInputElement).value);
-            }
-          }}
-          ref={(ref) => {
-            ref?.focus();
-            inputRef.current = ref!;
-          }}
-          onFocus={(e) => e.target.select()}
-          onBlur={(e) => {
-            if (e.relatedTarget !== closeButtonRef.current) handleEditColumnName((e.target as HTMLInputElement).value);
-          }}
-        />
-        <EmojiSuggestions {...emoji.suggestionsProps} />
-      </>
-    );
-
-  const renderColumnModifiers = () => (
-    <>
-      {columnNameMode === "EDIT" && columnName && (
-        <button
-          title={t("Column.submitName")}
-          className="column__header-edit-button"
-          onClick={() => {
-            handleEditColumnName(inputRef.current?.value ?? "");
-          }}
-          aria-label={t("Column.submitName")}
-        >
-          <MarkAsDone className="column__header-edit-button-icon" />
-        </button>
-      )}
-      {columnNameMode === "EDIT" && (
-        <button
-          title={t("Column.resetName")}
-          className="column__header-edit-button"
-          ref={closeButtonRef}
-          onClick={() => {
-            if (isTemporary) {
-              dispatch(deleteColumnOptimistically(id));
-            }
-            setColumnNameMode("VIEW");
-          }}
-          aria-label={t("Column.resetName")}
-        >
-          <Close className="column__header-edit-button-icon" />
-        </button>
-      )}
-      {!isTemporary && !openedColumnSettings && (
-        <button title={t("Column.settings")} className="column__header-edit-button" onClick={() => setOpenedColumnSettings((o) => !o)}>
-          <ThreeDots className="column__header-edit-button-icon" style={{transform: "rotate(90deg)"}} /> {/* inline style to avoid funky rotating behaviour when hovering */}
-        </button>
-      )}
-    </>
-  );
-
   const {
     bindings: {ref: stripeRef, style: stripeStyle},
     updateOffset,
@@ -232,21 +69,7 @@ export const Column = ({id, name, color, visible, index}: ColumnProps) => {
       }}
     >
       <div className="column__content">
-        <div className="column__header">
-          <div className="column__header-title" ref={emoji.containerRef}>
-            {renderColumnName()}
-            {columnNameMode === "VIEW" && notes.length > 0 && (
-              <span className="column__header-card-number" title={t("Column.notes", {count: notes.length})}>
-                {notes.length}
-              </span>
-            )}
-            {!openedColumnSettings && isModerator && renderColumnModifiers()}
-            {openedColumnSettings && (
-              <ColumnSettings column={{id, name, color, visible, index}} onClose={() => setOpenedColumnSettings(false)} onNameEdit={() => setColumnNameMode("EDIT")} />
-            )}
-          </div>
-          <NoteInput column={{id, index, name, color, visible}} />
-        </div>
+        <ColumnHeader column={{id, index, name, color, visible}} />
         <Droppable id={id} items={localNotes} setItems={setItems} globalNotes={notes} className="column__notes-wrapper">
           <ul className="column__note-list">
             {localNotes.map((note) => (
--- src/components/Column/ColumnHeader/ColumnHeader.tsx
+++ src/components/Column/ColumnHeader/ColumnHeader.tsx
@@ -1,14 +1,15 @@
 import {Column} from "store/features";
 import {NoteInput} from "components/NoteInput";
-
 import "./ColumnHeader.scss";
+import {ColumnNameDetails} from "components/Column/ColumnNameDetails/ColumnNameDetails";
 
 type ColumnProps = {
   column: Column;
 };
 
 export const ColumnHeader = ({column}: ColumnProps) => (
-    <div className="column__header">
-      <NoteInput column={column} />
-    </div>
-  );
+  <div className="column__header">
+    <ColumnNameDetails />
+    <NoteInput column={column} />
+  </div>
+);
