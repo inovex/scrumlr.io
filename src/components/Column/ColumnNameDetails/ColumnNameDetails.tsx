24c633a4f5dedd939a02adbb931e2a1eddb7ccad
Jakob Schwehn <schwehn42@gmail.com>
2025-06-06 13:28:20 +0200

    description content / placeholder

--- src/components/Column/ColumnNameDetails/ColumnNameDetails.tsx
+++ src/components/Column/ColumnNameDetails/ColumnNameDetails.tsx
@@ -1,4 +1,5 @@
 import {Column} from "store/features";
+import {useTranslation} from "react-i18next";
 import "./ColumnNameDetails.scss";
 
 type ColumnNameDetailsProps = {
@@ -6,12 +7,20 @@ type ColumnNameDetailsProps = {
   notesCount: number;
 };
 
-export const ColumnNameDetails = (props: ColumnNameDetailsProps) => (
-  <div className="column-name-details">
-    <div className="column-name-details__name-wrapper">
-      <div className="column-name-details__name">{props.column.name}</div>
-      <div className="column-name-details__notes-count">{props.notesCount}</div>
+export const ColumnNameDetails = (props: ColumnNameDetailsProps) => {
+  const {t} = useTranslation();
+
+  return (
+    <div className="column-name-details">
+      <div className="column-name-details__name-wrapper">
+        <div className="column-name-details__name">{props.column.name}</div>
+        <div className="column-name-details__notes-count">{props.notesCount}</div>
+      </div>
+      {props.column.description ? (
+        <div className="column-name-details__description">{props.column.description}</div>
+      ) : (
+        <div className="column-name-details__description--placeholder">{t("Column.Header.descriptionPlaceholder")}</div>
+      )}
     </div>
-    <div className="column-name-details__description">{props.column.description}</div>
-  </div>
-);
+  );
+};
