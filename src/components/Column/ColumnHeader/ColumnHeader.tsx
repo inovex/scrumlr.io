2f1e86998af21afd839f2af696a074c97bfae665
Jakob Schwehn <schwehn42@gmail.com>
2025-06-06 15:54:48 +0200

    move columns settings to column name details

--- src/components/Column/ColumnHeader/ColumnHeader.tsx
+++ src/components/Column/ColumnHeader/ColumnHeader.tsx
@@ -3,7 +3,6 @@ import {NoteInput} from "components/NoteInput";
 import {ColumnNameDetails} from "components/Column/ColumnNameDetails/ColumnNameDetails";
 import {ColumnSettings} from "components/Column/ColumnSettings";
 import {useState} from "react";
-import {ReactComponent as SettingsIcon} from "assets/icons/three-dots.svg";
 import classNames from "classnames";
 import "./ColumnHeader.scss";
 
@@ -12,29 +11,9 @@ type ColumnProps = {
   notesCount: number;
 };
 
-export const ColumnHeader = ({column, notesCount}: ColumnProps) => {
-  const [openSettings, setOpenSettings] = useState(false);
-  const onNameEdit = () => {};
-
-  return (
+export const ColumnHeader = ({column, notesCount}: ColumnProps) => (
     <div className="column-header">
       <ColumnNameDetails column={column} notesCount={notesCount} />
       <NoteInput column={column} />
-      {openSettings ? (
-        <ColumnSettings
-          className={classNames("column-header__settings", "column-header__settings--open")}
-          column={column}
-          onClose={() => setOpenSettings(false)}
-          onNameEdit={onNameEdit}
-        />
-      ) : (
-        <button
-          className={classNames("column-header__settings", "column-header__settings--closed", "column-header__settings-icon-container")}
-          onClick={() => setOpenSettings(true)}
-        >
-          <SettingsIcon className="column-header__settings-icon" />
-        </button>
-      )}
     </div>
   );
-};
--- src/components/Column/ColumnNameDetails/ColumnNameDetails.tsx
+++ src/components/Column/ColumnNameDetails/ColumnNameDetails.tsx
@@ -3,8 +3,10 @@ import {useTranslation} from "react-i18next";
 import {useState} from "react";
 import classNames from "classnames";
 import {ReactComponent as ArrowIcon} from "assets/icons/arrow-down.svg";
+import {ReactComponent as SettingsIcon} from "assets/icons/three-dots.svg";
 import {useTextOverflow} from "utils/hooks/useTextOverflow";
 import "./ColumnNameDetails.scss";
+import {ColumnSettings} from "components/Column/ColumnSettings";
 
 type ColumnNameDetailsProps = {
   column: Column;
@@ -16,13 +18,33 @@ export const ColumnNameDetails = (props: ColumnNameDetailsProps) => {
 
   const {isTextTruncated, textRef: descriptionRef} = useTextOverflow<HTMLDivElement>(props.column.description);
 
+  const [openSettings, setOpenSettings] = useState(false);
   const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
+
+  const onNameEdit = () => {};
 
   return (
     <div className="column-name-details">
-      <div className="column-name-details__name-wrapper">
-        <div className="column-name-details__name">{props.column.name}</div>
-        <div className="column-name-details__notes-count">{props.notesCount}</div>
+      <div className="column-name-details__name-and-settings-wrapper">
+        <div className="column-name-details__name-wrapper">
+          <div className="column-name-details__name">{props.column.name}</div>
+          <div className="column-name-details__notes-count">{props.notesCount}</div>
+        </div>
+        {openSettings ? (
+          <ColumnSettings
+            className={classNames("column-name-details__settings", "column-name-details__settings--open")}
+            column={props.column}
+            onClose={() => setOpenSettings(false)}
+            onNameEdit={onNameEdit}
+          />
+        ) : (
+          <button
+            className={classNames("column-name-details__settings", "column-name-details__settings--closed", "column-name-details__settings-icon-container")}
+            onClick={() => setOpenSettings(true)}
+          >
+            <SettingsIcon className="column-name-details__settings-icon" />
+          </button>
+        )}
       </div>
       {props.column.description ? (
         <div className="column-name-details__description-wrapper">
