930c874d71e5f717802c3b8fbf3dd31392f8f69a
Jakob Schwehn <schwehn42@gmail.com>
2025-06-16 12:13:49 +0200

    stack view add description

--- src/components/NoteDialogComponents/NoteDialogHeader.tsx
+++ src/components/NoteDialogComponents/NoteDialogHeader.tsx
@@ -3,12 +3,14 @@ import "./NoteDialogHeader.scss";
 
 type NoteDialogHeaderProps = {
   columnName: string;
+  columnDescription: string;
 };
 
-export const NoteDialogHeader: FC<NoteDialogHeaderProps> = ({columnName}: NoteDialogHeaderProps) => (
-  <div className="note-dialog__header">
+export const NoteDialogHeader: FC<NoteDialogHeaderProps> = ({columnName, columnDescription}: NoteDialogHeaderProps) => (
+  <div className="note-dialog-header">
     <h2 data-clarity-mask="True" onClick={(e) => e.stopPropagation()}>
       {columnName}
     </h2>
+    <div className="note-dialog-header__description">{columnDescription}</div>
   </div>
 );
