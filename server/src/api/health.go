a0505632eb300c3bdff09c33e584183a27a61d77
Florian Schwarz <florian.schwarz@inovex.de>
2025-08-27 08:53:57 +0200

    Add otel instrumentation for health package

--- server/src/api/health.go
+++ server/src/api/health.go
@@ -1,21 +1,37 @@
 package api
 
 import (
-	"github.com/go-chi/render"
 	"net/http"
+
+	"github.com/go-chi/render"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 	"scrumlr.io/server/logger"
 )
 
+//var tracer trace.Tracer = otel.Tracer("scrumlr.io/server/api")
+
 func (s *Server) healthCheck(w http.ResponseWriter, r *http.Request) {
+	ctx, span := tracer.Start(r.Context(), "scrumlr.health.api")
+	defer span.End()
+	log := logger.FromContext(ctx)
+
 	realtimeHealthy := s.health.IsRealtimeHealthy()
-	databaseHealthy := s.health.IsDatabaseHealthy()
+	databaseHealthy := s.health.IsDatabaseHealthy(ctx)
+
+	span.SetAttributes(
+		attribute.Bool("scrumlr.health.api.database.healthy", databaseHealthy),
+		attribute.Bool("scrumlr.health.api.realtime.healthy", realtimeHealthy),
+	)
 
 	if realtimeHealthy && databaseHealthy {
 		render.Status(r, http.StatusNoContent)
 		render.Respond(w, r, nil)
 		return
 	}
-	logger.Get().Errorw("service is not healthy", "realtime", realtimeHealthy, "database", databaseHealthy)
+
+	span.SetStatus(codes.Error, "service not healthy")
+	log.Errorw("service is not healthy", "realtime", realtimeHealthy, "database", databaseHealthy)
 	render.Status(r, http.StatusServiceUnavailable)
 	render.Respond(w, r, nil)
 }
--- server/src/health/service_test.go
+++ server/src/health/service_test.go
@@ -1,37 +1,39 @@
 package health
 
 import (
+	"context"
 	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"scrumlr.io/server/realtime"
 )
 
 func TestDatabaseHealthy(t *testing.T) {
 	mockHealthDb := NewMockHealthDatabase(t)
-	mockHealthDb.EXPECT().IsHealthy().Return(true)
+	mockHealthDb.EXPECT().IsHealthy(mock.Anything).Return(true)
 
 	mockBroker := realtime.NewMockClient(t)
 	broker := new(realtime.Broker)
 	broker.Con = mockBroker
 
 	healthService := NewHealthService(mockHealthDb, broker)
-	healthDb := healthService.IsDatabaseHealthy()
+	healthDb := healthService.IsDatabaseHealthy(context.Background())
 
 	assert.True(t, healthDb)
 }
 
 func TestDatabaseNotHealthy(t *testing.T) {
 	mockHealthDb := NewMockHealthDatabase(t)
-	mockHealthDb.EXPECT().IsHealthy().Return(false)
+	mockHealthDb.EXPECT().IsHealthy(mock.Anything).Return(false)
 
 	mockBroker := realtime.NewMockClient(t)
 	broker := new(realtime.Broker)
 	broker.Con = mockBroker
 
 	healthService := NewHealthService(mockHealthDb, broker)
-	healthDb := healthService.IsDatabaseHealthy()
+	healthDb := healthService.IsDatabaseHealthy(context.Background())
 
 	assert.False(t, healthDb)
 }
