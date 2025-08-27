a094b08a5b9fb88337d4f1a03c018c5720a82def
Florian Schwarz <florian.schwarz@inovex.de>
2025-08-27 13:23:11 +0200

    Add otel instrumentation for board reaction package

--- server/src/api/board_reaction.go
+++ server/src/api/board_reaction.go
@@ -5,29 +5,37 @@ import (
 
 	"github.com/go-chi/render"
 	"github.com/google/uuid"
+	"go.opentelemetry.io/otel/codes"
 	"scrumlr.io/server/boardreactions"
 	"scrumlr.io/server/common"
 	"scrumlr.io/server/identifiers"
 	"scrumlr.io/server/logger"
 )
 
+//var tracer trace.Tracer = otel.Tracer("scrumlr.io/server/api")
+
 // createBoardReaction creates a new board reaction
 func (s *Server) createBoardReaction(w http.ResponseWriter, r *http.Request) {
-	log := logger.FromRequest(r)
-	board := r.Context().Value(identifiers.BoardIdentifier).(uuid.UUID)
-	user := r.Context().Value(identifiers.UserIdentifier).(uuid.UUID)
+	ctx, span := tracer.Start(r.Context(), "scrumlr.board_reactions.api.create")
+	defer span.End()
+	log := logger.FromContext(ctx)
+
+	board := ctx.Value(identifiers.BoardIdentifier).(uuid.UUID)
+	user := ctx.Value(identifiers.UserIdentifier).(uuid.UUID)
 
 	var body boardreactions.BoardReactionCreateRequest
 	if err := render.Decode(r, &body); err != nil {
-		common.Throw(w, r, common.BadRequestError(err))
+		span.SetStatus(codes.Error, "unable to decode body")
+		span.RecordError(err)
 		log.Errorw("unable to create board reaction", "err", err)
+		common.Throw(w, r, common.BadRequestError(err))
 		return
 	}
 
 	// user is filled from context
 	body.User = user
 
-	s.boardReactions.Create(r.Context(), board, body)
+	s.boardReactions.Create(ctx, board, body)
 
 	render.Status(r, http.StatusCreated)
 	render.Respond(w, r, nil)
