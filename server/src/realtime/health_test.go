638638bdaa9818689c380666c3cb7244b974f8b1
Florian Schwarz <florian.schwarz@inovex.de>
2025-09-05 10:15:23 +0200

    Rewrite realtime helth test

--- server/src/realtime/health_test.go
+++ server/src/realtime/health_test.go
@@ -1,43 +1,83 @@
-package realtime_test
+package realtime
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"github.com/testcontainers/testcontainers-go/modules/nats"
+	"github.com/testcontainers/testcontainers-go/modules/redis"
 
-	"scrumlr.io/server/realtime"
+	"scrumlr.io/server/initialize"
 )
 
-func TestBroker_IsHealthy(t *testing.T) {
-	type testCase struct {
-		name        string
-		setupBroker func(t *testing.T) *realtime.Broker
-		expected    bool
-	}
-	testcases := []testCase{
-		{
-			name: "nats client has has wrong url",
-			setupBroker: func(t *testing.T) *realtime.Broker {
-				rt, err := realtime.NewNats("foo")
-				require.NotNil(t, err)
-				return rt
-			},
-			expected: false,
-		},
-		{
-			name: "nats client is setup correctly",
-			setupBroker: func(t *testing.T) *realtime.Broker {
-				rt, err := realtime.NewNats(SetupNatsContainer(t))
-				require.Nil(t, err)
-				return rt
-			},
-			expected: true,
-		},
-	}
-	for _, tt := range testcases {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.expected, tt.setupBroker(t).IsHealthy(), "healthy didn't return expected result")
-		})
-	}
+type RealtimeHealthTestSuite struct {
+	suite.Suite
+	natsContainer         *nats.NATSContainer
+	redisContainer        *redis.RedisContainer
+	natsConnectionString  string
+	redisConnectionString string
+}
+
+func TestRealtimeHealthTestSuite(t *testing.T) {
+	suite.Run(t, new(RealtimeHealthTestSuite))
+}
+
+func (suite *RealtimeHealthTestSuite) SetupSuite() {
+	natsContainer, natsConnectionString := initialize.StartTestNats()
+	redisContainer, redisConnectionString := initialize.StartTestRedis()
+
+	suite.natsContainer = natsContainer
+	suite.natsConnectionString = natsConnectionString
+	suite.redisContainer = redisContainer
+	suite.redisConnectionString = redisConnectionString
+}
+
+func (suite *RealtimeHealthTestSuite) TearDownSuite() {
+	initialize.StopTestNats(suite.natsContainer)
+	initialize.StopTestRedis(suite.redisContainer)
+}
+
+func (suite *RealtimeHealthTestSuite) Test_Nats_Healthy() {
+	t := suite.T()
+
+	broker, err := NewNats(suite.natsConnectionString)
+	assert.Nil(t, err)
+
+	healthy := broker.IsHealthy()
+
+	assert.True(t, healthy)
+}
+
+func (suite *RealtimeHealthTestSuite) Test_Redis_Healthy() {
+	t := suite.T()
+
+	broker, err := NewRedis(RedisServer{Addr: suite.redisConnectionString})
+	assert.Nil(t, err)
+
+	healthy := broker.IsHealthy()
+
+	assert.True(t, healthy)
+}
+
+func (suite *RealtimeHealthTestSuite) Test_Nats_WrongUrl() {
+	t := suite.T()
+
+	broker, err := NewNats("foo")
+
+	assert.Nil(t, broker)
+	assert.NotNil(t, err)
+}
+
+func (suite *RealtimeHealthTestSuite) Test_Redis_WrongUrl() {
+	t := suite.T()
+
+	broker, err := NewRedis(RedisServer{Addr: "foo"})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, broker)
+
+	healthy := broker.IsHealthy()
+
+	assert.False(t, healthy)
 }
