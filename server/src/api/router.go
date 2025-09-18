88a8a7841f95187a277fdb0c887f2044a7ebbd94
ffischbach <fynn.fischbach@inovex.de>
2025-09-18 12:23:11 +0200

    feat: add drag state API endpoint with lock management
    - Replace separate drag-start/drag-end endpoints with unified drag-state
    - Implement lock acquisition with HTTP 409 conflict responses
    - Integrate with drag lock service for coordination
    - Broadcast realtime events for drag state changes

--- server/src/api/router.go
+++ server/src/api/router.go
@@ -15,6 +15,7 @@ import (
 	"scrumlr.io/server/boardtemplates"
 	"scrumlr.io/server/columns"
 	"scrumlr.io/server/columntemplates"
+	"scrumlr.io/server/draglocks"
 	"scrumlr.io/server/notes"
 
 	"github.com/go-chi/chi/v5"
@@ -58,6 +59,7 @@ type Server struct {
 	boardReactions  boardreactions.BoardReactionService
 	boardTemplates  boardtemplates.BoardTemplateService
 	columntemplates columntemplates.ColumnTemplateService
+	dragLocks       draglocks.DragLockService
 
 	upgrader websocket.Upgrader
 
@@ -90,6 +92,7 @@ func New(
 	boardReactions boardreactions.BoardReactionService,
 	boardTemplates boardtemplates.BoardTemplateService,
 	columntemplates columntemplates.ColumnTemplateService,
+	dragLocks draglocks.DragLockService,
 
 	verbose bool,
 	checkOrigin bool,
@@ -141,6 +144,7 @@ func New(
 		boardReactions:                   boardReactions,
 		boardTemplates:                   boardTemplates,
 		columntemplates:                  columntemplates,
+		dragLocks:                        dragLocks,
 
 		anonymousLoginDisabled:        anonymousLoginDisabled,
 		allowAnonymousCustomTemplates: allowAnonymousCustomTemplates,
@@ -359,8 +363,7 @@ func (s *Server) initNoteResources(r chi.Router) {
 			r.Get("/", s.getNote)
 			r.With(s.BoardEditableContext).Put("/", s.updateNote)
 			r.With(s.BoardEditableContext).Delete("/", s.deleteNote)
-			r.With(s.BoardEditableContext).Post("/drag-start", s.noteDragStart)
-			r.With(s.BoardEditableContext).Post("/drag-end", s.noteDragEnd)
+			r.With(s.BoardEditableContext).Put("/drag-state", s.updateNoteDragState)
 		})
 	})
 }
