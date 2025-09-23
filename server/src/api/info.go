db4059affc668a3ae272711b88e8d7e17d1a5a8c
ffischbach <fynn.fischbach@inovex.de>
2025-09-23 15:36:39 +0200

    style: apply gofmt formatting fixes
    - Fix Go code formatting using gofmt
    - Standardize indentation and spacing
    - Add missing newlines at end of files
    - Improve code readability and consistency

--- server/src/api/info.go
+++ server/src/api/info.go
@@ -1,59 +1,59 @@
 package api
 
 import (
-  "net/http"
-  "time"
+	"net/http"
+	"time"
 
-  "github.com/go-chi/render"
-  "scrumlr.io/server/common"
+	"github.com/go-chi/render"
+	"scrumlr.io/server/common"
 )
 
 type Info struct {
-  AuthProvider                  []common.AccountType `json:"authProvider"`
-  AnonymousLoginDisabled        bool                 `json:"anonymousLoginDisabled"`
-  AllowAnonymousCustomTemplates bool                 `json:"allowAnonymousCustomTemplates"`
-  AllowAnonymousBoardCreation   bool                 `json:"allowAnonymousBoardCreation"`
-  ServerTime                    time.Time            `json:"serverTime"`
-  FeedbackEnabled               bool                 `json:"feedbackEnabled"`
+	AuthProvider                  []common.AccountType `json:"authProvider"`
+	AnonymousLoginDisabled        bool                 `json:"anonymousLoginDisabled"`
+	AllowAnonymousCustomTemplates bool                 `json:"allowAnonymousCustomTemplates"`
+	AllowAnonymousBoardCreation   bool                 `json:"allowAnonymousBoardCreation"`
+	ServerTime                    time.Time            `json:"serverTime"`
+	FeedbackEnabled               bool                 `json:"feedbackEnabled"`
 }
 
 func (s *Server) getServerInfo(w http.ResponseWriter, r *http.Request) {
-  info := Info{}
-  info.AuthProvider = []common.AccountType{}
+	info := Info{}
+	info.AuthProvider = []common.AccountType{}
 
-  info.AnonymousLoginDisabled = s.anonymousLoginDisabled
+	info.AnonymousLoginDisabled = s.anonymousLoginDisabled
 
-  info.AllowAnonymousCustomTemplates = s.allowAnonymousCustomTemplates
+	info.AllowAnonymousCustomTemplates = s.allowAnonymousCustomTemplates
 
-  info.AllowAnonymousBoardCreation = s.allowAnonymousBoardCreation
+	info.AllowAnonymousBoardCreation = s.allowAnonymousBoardCreation
 
-  if s.auth.Exists(common.Google) {
-    info.AuthProvider = append(info.AuthProvider, common.Google)
-  }
-  if s.auth.Exists(common.GitHub) {
-    info.AuthProvider = append(info.AuthProvider, common.GitHub)
-  }
-  if s.auth.Exists(common.Microsoft) {
-    info.AuthProvider = append(info.AuthProvider, common.Microsoft)
-  }
-  if s.auth.Exists(common.AzureAd) {
-    info.AuthProvider = append(info.AuthProvider, common.AzureAd)
-  }
-  if s.auth.Exists(common.Apple) {
-    info.AuthProvider = append(info.AuthProvider, common.Apple)
-  }
-  if s.auth.Exists(common.TypeOIDC) {
-    info.AuthProvider = append(info.AuthProvider, common.TypeOIDC)
-  }
+	if s.auth.Exists(common.Google) {
+		info.AuthProvider = append(info.AuthProvider, common.Google)
+	}
+	if s.auth.Exists(common.GitHub) {
+		info.AuthProvider = append(info.AuthProvider, common.GitHub)
+	}
+	if s.auth.Exists(common.Microsoft) {
+		info.AuthProvider = append(info.AuthProvider, common.Microsoft)
+	}
+	if s.auth.Exists(common.AzureAd) {
+		info.AuthProvider = append(info.AuthProvider, common.AzureAd)
+	}
+	if s.auth.Exists(common.Apple) {
+		info.AuthProvider = append(info.AuthProvider, common.Apple)
+	}
+	if s.auth.Exists(common.TypeOIDC) {
+		info.AuthProvider = append(info.AuthProvider, common.TypeOIDC)
+	}
 
-  info.ServerTime = time.Now()
+	info.ServerTime = time.Now()
 
-  if s.feedback.Enabled() {
-    info.FeedbackEnabled = true
-  } else {
-    info.FeedbackEnabled = false
-  }
+	if s.feedback.Enabled() {
+		info.FeedbackEnabled = true
+	} else {
+		info.FeedbackEnabled = false
+	}
 
-  render.Status(r, http.StatusOK)
-  render.Respond(w, r, info)
+	render.Status(r, http.StatusOK)
+	render.Respond(w, r, info)
 }
--- server/src/notes/helpers.go
+++ server/src/notes/helpers.go
@@ -1,52 +1,52 @@
 package notes
 
 import (
-  "github.com/google/uuid"
-  "net/http"
-  "scrumlr.io/server/technical_helper"
+	"github.com/google/uuid"
+	"net/http"
+	"scrumlr.io/server/technical_helper"
 )
 
 type ColumnVisability struct {
-  ID      uuid.UUID
-  Visible bool
+	ID      uuid.UUID
+	Visible bool
 }
 
 func (n NoteSlice) FilterNotesByBoardSettingsOrAuthorInformation(userID uuid.UUID, showNotesOfOtherUsers bool, showAuthors bool, columns []ColumnVisability) NoteSlice {
-  visibleNotes := technical_helper.Filter[*Note](n, func(note *Note) bool {
-    for _, column := range columns {
-      if (note.Position.Column == column.ID) && column.Visible {
-        // BoardSettings -> Remove other participant cards
-        if showNotesOfOtherUsers {
-          return true
-        } else if userID == note.Author {
-          return true
-        }
-      }
-    }
-    return false
-  })
-  n.hideOtherAuthors(userID, showAuthors, visibleNotes)
-  return visibleNotes
+	visibleNotes := technical_helper.Filter[*Note](n, func(note *Note) bool {
+		for _, column := range columns {
+			if (note.Position.Column == column.ID) && column.Visible {
+				// BoardSettings -> Remove other participant cards
+				if showNotesOfOtherUsers {
+					return true
+				} else if userID == note.Author {
+					return true
+				}
+			}
+		}
+		return false
+	})
+	n.hideOtherAuthors(userID, showAuthors, visibleNotes)
+	return visibleNotes
 }
 
 func UnmarshallNotaData(data interface{}) (NoteSlice, error) {
-  notes, err := technical_helper.UnmarshalSlice[Note](data)
+	notes, err := technical_helper.UnmarshalSlice[Note](data)
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  return notes, nil
+	return notes, nil
 }
 
 func (*Note) Render(_ http.ResponseWriter, _ *http.Request) error {
-  return nil
+	return nil
 }
 
 func (n NoteSlice) hideOtherAuthors(userID uuid.UUID, showAuthors bool, visibleNotes []*Note) {
-  for _, note := range visibleNotes {
-    if !showAuthors && note.Author != userID {
-      note.Author = uuid.Nil
-    }
-  }
+	for _, note := range visibleNotes {
+		if !showAuthors && note.Author != userID {
+			note.Author = uuid.Nil
+		}
+	}
 }
