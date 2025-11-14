c3923164104df70d804fe9d5ddaef5d3b3b547c6
Jakob Schwehn <schwehn42@gmail.com>
2025-11-14 15:50:06 +0100

    test(noteReducer): expand delete note test cases to cover all scenarios

--- src/store/features/notes/__tests__/reducer.test.ts
+++ src/store/features/notes/__tests__/reducer.test.ts
@@ -7,10 +7,10 @@ import {ApplicationState} from "store/store";
 describe("noteReducer", () => {
   // test data: one single note; one note stack consisting of one parent and two children
   const testNotes: NotesState = [
-    {id: "note-id-1", author: "author-id-1", edited: false, position: {column: "column-id-1", rank: 0, stack: null}, text: "single note"},
-    {id: "note-id-2", author: "author-id-1", edited: false, position: {column: "column-id-1", rank: 0, stack: null}, text: "stack parent"},
-    {id: "note-id-3", author: "author-id-1", edited: false, position: {column: "column-id-1", rank: 0, stack: "note-id-2"}, text: "stack child 1"},
-    {id: "note-id-4", author: "author-id-1", edited: false, position: {column: "column-id-1", rank: 1, stack: "note-id-2"}, text: "stack child 2"},
+    {id: "note-id-single", author: "author-id-1", edited: false, position: {column: "column-id-1", rank: 0, stack: null}, text: "single note"},
+    {id: "note-id-parent", author: "author-id-1", edited: false, position: {column: "column-id-1", rank: 0, stack: null}, text: "stack parent"},
+    {id: "note-id-child-1", author: "author-id-1", edited: false, position: {column: "column-id-1", rank: 0, stack: "note-id-parent"}, text: "stack child 1"},
+    {id: "note-id-child-2", author: "author-id-1", edited: false, position: {column: "column-id-1", rank: 1, stack: "note-id-parent"}, text: "stack child 2"},
   ];
 
   let mockStore: EnhancedStore<ApplicationState>;
@@ -19,8 +19,32 @@ describe("noteReducer", () => {
     mockStore = getTestStore({notes: testNotes});
   });
 
-  it("delete single note", () => {
-    mockStore.dispatch(deletedNote("note-id-1"));
-    expect(mockStore.getState().notes).not.toEqual(expect.arrayContaining([expect.objectContaining({id: "note-id-1"})]));
+  test("delete nothing", () => {
+    mockStore.dispatch(deletedNote("nothing"));
+    expect(mockStore.getState().notes).toEqual(testNotes);
+  });
+
+  test("delete single note", () => {
+    mockStore.dispatch(deletedNote("note-id-single"));
+    expect(mockStore.getState().notes).toEqual(expect.not.arrayContaining([expect.objectContaining({id: "note-id-1"})]));
+  });
+
+  // stack should remain, but rank should be adjusted
+  test("delete child note", () => {
+    mockStore.dispatch(deletedNote("note-id-child-1"));
+    expect(mockStore.getState().notes).toEqual(expect.not.arrayContaining([expect.objectContaining({id: "note-id-child-1"})]));
+    expect(mockStore.getState().notes).toEqual(expect.arrayContaining([expect.objectContaining({id: "note-id-child-2", position: expect.objectContaining({rank: 0})})]));
+  });
+
+  // first child note becomes the new parent; stack should be updated accordingly
+  test("delete parent note", () => {
+    mockStore.dispatch(deletedNote("note-id-parent"));
+    expect(mockStore.getState().notes).toEqual(expect.not.arrayContaining([expect.objectContaining({id: "note-id-parent"})]));
+    expect(mockStore.getState().notes).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({id: "note-id-child-1", position: expect.objectContaining({stack: "note-id-child-2"})}),
+        expect.objectContaining({id: "note-id-child-2", position: expect.objectContaining({stack: null})}),
+      ])
+    );
   });
 });
