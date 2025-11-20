62da5001cf87eadef77cd3582c0bd06d05cfc1fb
Jakob Schwehn <schwehn42@gmail.com>
2025-11-20 17:00:30 +0100

    config: enhance vite config with paths and SCSS support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from 'vite-plugin-svgr';
+import * as path from "node:path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths(), svgr()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        loadPaths: [path.resolve(__dirname, 'src'),
+        ]
+
+      },
+    },
+  },
   build: {
     outDir: 'build',
   },
