7f3865358493ddca7247dbd648e08f1214d22d77
Jakob Schwehn <schwehn42@gmail.com>
2025-12-19 14:55:20 +0100

    vite env test

--- vite-env-override.d.ts
+++ vite-env-override.d.ts
@@ -2,3 +2,10 @@ declare module '*.svg' {
   const content: React.FC<React.SVGProps<SVGElement>>
   export default content
 }
+
+/* not needed?
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+ */
