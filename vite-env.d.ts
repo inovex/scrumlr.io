f99d5653f51eedccc7f63a6a2be3f378b16ba2de
Jakob Schwehn <schwehn42@gmail.com>
2025-12-19 14:55:20 +0100

    vite env test

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="./vite-env-override.d.ts" />
+/// <reference types="vite-env-override.d.ts" />
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 
