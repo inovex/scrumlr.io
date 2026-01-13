dcb96c71077d99c8204ce9634c9f8bf78477c9f1
Jakob Moehler <44205136+jmoehler@users.noreply.github.com>
2026-01-13 14:44:23 +0100

    feat: enhance Button component with ghost type and icon-only variant

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import {Color, getColorClassName} from "constants/colors";
 import {MouseEvent, ReactNode} from "react";
 import "./Button.scss";
 
-type ButtonType = "primary" | "secondary" | "tertiary";
+type ButtonType = "primary" | "secondary" | "tertiary" | "ghost";
 
 type ButtonProps = {
   type?: ButtonType;
@@ -23,28 +23,35 @@ type ButtonProps = {
   onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
 
   dataCy?: string;
+
+  hideLabel?: boolean;
 };
 
-export const Button = (props: ButtonProps) => (
-  <button
-    className={classNames(
-      props.className,
-      "button",
-      `button--${props.type ?? "primary"}`,
-      {
-        "button--small": props.small,
-        "button--with-icon": props.icon,
-      },
-      getColorClassName(props.color ?? "planning-pink")
-    )}
-    disabled={props.disabled}
-    onClick={props.onClick}
-    title={props.title}
-    data-tooltip-id={props.dataTooltipId}
-    data-tooltip-content={props.dataTooltipContent}
-    data-cy={props.dataCy}
-  >
-    {props.children}
-    {props.icon}
-  </button>
-);
+export const Button = (props: ButtonProps) => {
+  const isIconOnly = props.hideLabel && props.icon;
+
+  return (
+    <button
+      className={classNames(
+        props.className,
+        "button",
+        `button--${props.type ?? "primary"}`,
+        {
+          "button--small": props.small,
+          "button--with-icon": props.icon && !isIconOnly,
+          "button--icon-only": isIconOnly,
+        },
+        getColorClassName(props.color ?? "planning-pink")
+      )}
+      disabled={props.disabled}
+      onClick={props.onClick}
+      title={props.title}
+      data-tooltip-id={props.dataTooltipId}
+      data-tooltip-content={props.dataTooltipContent}
+      data-cy={props.dataCy}
+    >
+      {!props.hideLabel && props.children}
+      {props.icon}
+    </button>
+  );
+};
