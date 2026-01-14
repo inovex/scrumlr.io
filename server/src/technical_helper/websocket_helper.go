afa4da343c6d377d8f469138530a56faaac40995
Simon Röcker <simon.roecker@inovex.de>
2026-01-14 18:41:23 +0100

    chore: Changed unit test to integration tests

--- server/src/technical_helper/websocket_helper.go
+++ server/src/technical_helper/websocket_helper.go
@@ -1,20 +1,20 @@
 package technical_helper
 
 import (
-  "github.com/coder/websocket"
-  "net/http"
+	"github.com/coder/websocket"
+	"net/http"
 )
 
 func AcceptWebSocket(w http.ResponseWriter, r *http.Request, checkOrigin bool) (*websocket.Conn, error) {
-  return websocket.Accept(w, r, &websocket.AcceptOptions{
-    InsecureSkipVerify: !checkOrigin,
-  })
+	return websocket.Accept(w, r, &websocket.AcceptOptions{
+		InsecureSkipVerify: !checkOrigin,
+	})
 }
 
 func IsNormalClose(err error) bool {
-  if err == nil {
-    return false
-  }
-  status := websocket.CloseStatus(err)
-  return status == websocket.StatusNormalClosure || status == websocket.StatusGoingAway
+	if err == nil {
+		return false
+	}
+	status := websocket.CloseStatus(err)
+	return status == websocket.StatusNormalClosure || status == websocket.StatusGoingAway
 }
