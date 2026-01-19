61ee88b6f33bfdd0cb9f6e8fe5a524fbde5cbcd3
Mateo Ivankovic <mateo.ivankovic@inovex.de>
2026-01-19 09:00:20 +0100

    tests

--- server/src/serviceinitialize/api.go
+++ server/src/serviceinitialize/api.go
@@ -1,42 +1,42 @@
 package serviceinitialize
 
 import (
-  "scrumlr.io/server/auth"
-  "scrumlr.io/server/sessions"
-  "scrumlr.io/server/users"
+	"scrumlr.io/server/auth"
+	"scrumlr.io/server/sessions"
+	"scrumlr.io/server/users"
 )
 
 type ApiInitializer struct {
-  basePath string
+	basePath string
 }
 
 func NewApiInitializer(basePath string) ApiInitializer {
-  initializer := new(ApiInitializer)
-  initializer.basePath = basePath
-  return *initializer
+	initializer := new(ApiInitializer)
+	initializer.basePath = basePath
+	return *initializer
 }
 
 func (init *ApiInitializer) InitializeAuthApi(authService auth.AuthService, userService users.UserService) auth.AuthApi {
-  authApi := auth.NewAuthApi(authService, userService)
-  return authApi
+	authApi := auth.NewAuthApi(authService, userService)
+	return authApi
 }
 
 func (init *ApiInitializer) InitializeBoardApi() {
-  panic("Not implemented")
+	panic("Not implemented")
 }
 
 func (init *ApiInitializer) InitializeColumnApi() {
-  panic("Not implemented")
+	panic("Not implemented")
 
 }
 
 func (init *ApiInitializer) InitializeBoardReactionApi() {
-  panic("Not implemented")
+	panic("Not implemented")
 
 }
 
 func (init *ApiInitializer) InitializeBoardTemplateApi() {
-  panic("Not implemented")
+	panic("Not implemented")
 
 }
 
@@ -49,34 +49,34 @@ func (init *ApiInitializer) InitializeFeedbackApi() {
 }
 
 func (init *ApiInitializer) InitializeHealthApi() {
-  panic("Not implemented")
+	panic("Not implemented")
 
 }
 
 func (init *ApiInitializer) InitializeReactionApi() {
-  panic("Not implemented")
+	panic("Not implemented")
 
 }
 
 func (init *ApiInitializer) InitializeSessionApi(sessionService sessions.SessionService) sessions.SessionApi {
-  sessionApi := sessions.NewSessionApi(sessionService)
-  return sessionApi
+	sessionApi := sessions.NewSessionApi(sessionService)
+	return sessionApi
 }
 
 func (init *ApiInitializer) InitializeSessionRequestApi() {
-  panic("Not implemented")
+	panic("Not implemented")
 
 }
 
 func (init *ApiInitializer) InitializeUserApi(userService users.UserService, sessionService sessions.SessionService, allowAnonymousBoardCreation, allowAnonymousCustomTemplates bool) users.UsersApi {
-  usersApi := users.NewUserApi(userService, sessionService, allowAnonymousBoardCreation, allowAnonymousCustomTemplates)
-  return usersApi
+	usersApi := users.NewUserApi(userService, sessionService, allowAnonymousBoardCreation, allowAnonymousCustomTemplates)
+	return usersApi
 }
 
 func (init *ApiInitializer) InitializeNotesApi() {
-  panic("Not implemented")
+	panic("Not implemented")
 }
 
 func (init *ApiInitializer) InitializeVotingApi() {
-  panic("Not implemented")
+	panic("Not implemented")
 }
