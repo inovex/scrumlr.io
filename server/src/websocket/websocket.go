a9ba4b7080a0250eccc14063571b5024ad81bdf2
Simon Röcker <simon.roecker@inovex.de>
2026-01-20 16:56:55 +0100

    chore: Added binary message type

--- server/src/websocket/websocket.go
+++ server/src/websocket/websocket.go
@@ -1,24 +1,25 @@
 package websocket
 
 import (
-	"context"
-	"net/http"
+  "context"
+  "net/http"
 )
 
 type MessageType int
 
 const (
-	MessageText MessageType = 1
+  MessageText   MessageType = 1
+  MessageBinary MessageType = 2
 )
 
 // Connection abstracts a WebSocket connection
 type Connection interface {
-	WriteJSON(ctx context.Context, data interface{}) error
-	Read(ctx context.Context) (MessageType, []byte, error)
-	Close(reason string) error
+  WriteJSON(ctx context.Context, data interface{}) error
+  Read(ctx context.Context) (MessageType, []byte, error)
+  Close(reason string) error
 }
 
 type WebSocketService interface {
-	Accept(w http.ResponseWriter, r *http.Request, checkOrigin bool) (Connection, error)
-	IsNormalClose(err error) bool
+  Accept(w http.ResponseWriter, r *http.Request, checkOrigin bool) (Connection, error)
+  IsNormalClose(err error) bool
 }
