fe74413f04070b0450a2a5382e80745f9b16a9b9
Jakob Schwehn <schwehn42@gmail.com>
2026-01-21 13:58:24 +0100

    chore: remove cra/jest related stuff

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,27 +1,9 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// vitest-dom adds custom matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 import { vi } from "vitest";
-/*
-
-// import {TextEncoder} from "util";
-
-import ResizeObserver from "resize-observer-polyfill";
-
-global.ResizeObserver = ResizeObserver
-
-// global.TextDecoder = require('util').TextDecoder;
-global.TextEncoder = TextEncoder;
-
-// jest can't handle match media, and the official workaround doesn't work (lol):
-// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-// the package to mock it correctly almost works, but not quite:
-// https://yarnpkg.com/package?q=jest-matchmedia&name=jest-matchmedia-mock
-// fortunately, this seems to do the trick:
-// https://github.com/facebook/create-react-app/issues/10126#issuecomment-735272763
-*/
 
 class ResizeObserverMock {
   observe = vi.fn();
