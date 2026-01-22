b7ac0aec213d68b9385bde90a23037ac77d0bcf7
Jakob Schwehn <schwehn42@gmail.com>
2026-01-22 14:13:59 +0100

    refactor: merge svg declaration module

--- vite-env-override.d.ts
+++ vite-env-override.d.ts
@@ -1,11 +1,13 @@
-declare module '*.svg' {
-  const content: React.FC<React.SVGProps<SVGElement>>
-  export default content
+/// <reference types="vite/client" />
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
 }
 
-/* not needed?
-declare module "*.png" {
-  const content: string;
-  export default content;
+declare module "*.svg?react" {
+  import * as React from "react";
+
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement> & { title?: string }>;
+  export default ReactComponent;
 }
- */
