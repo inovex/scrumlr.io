3ec5c20647f1c79ef53c94f21fce5b18a0d8a6cf
Jakob <44205136+jmoehler@users.noreply.github.com>
2026-02-02 14:01:25 +0100

    fix: added reindexing after column deletion to ensure correct ordering when columns are added (#5776)

--- src/store/features/columns/reducer.ts
+++ src/store/features/columns/reducer.ts
@@ -12,10 +12,10 @@ export const columnsReducer = createReducer(initialState, (builder) =>
     .addCase(updatedColumns, (_state, action) => action.payload)
     // .toSpliced instead .splice because of Immer
     .addCase(createColumnOptimistically, (state, action) => state.toSpliced(action.payload.index, 0, action.payload))
-    .addCase(deleteColumnOptimistically, (state) => state.filter((c) => c.id !== TEMPORARY_COLUMN_ID))
+    .addCase(deleteColumnOptimistically, (state) => state.filter((c) => c.id !== TEMPORARY_COLUMN_ID).map((col, idx) => ({...col, index: idx})))
     .addCase(editColumnOptimistically, (state, action) => {
       const col = state.find((c) => c.id === TEMPORARY_COLUMN_ID);
       if (col) col.name = action.payload.column.name;
     })
-    .addCase(deletedColumn, (state, action) => state.filter((c) => c.id !== action.payload))
+    .addCase(deletedColumn, (state, action) => state.filter((c) => c.id !== action.payload).map((col, idx) => ({...col, index: idx})))
 );
