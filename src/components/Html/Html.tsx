88c6bb4e7bbb3694fa0c9bf33b9086920dc2ca43
ffischbach <fynn.fischbach@inovex.de>
2026-02-10 17:25:47 +0100

    feat: migrate from react-helmet to react-helmet-async

--- src/components/Html/Html.tsx
+++ src/components/Html/Html.tsx
@@ -1,22 +1,25 @@
 import {useEffect} from "react";
-import {Helmet, HelmetProps} from "react-helmet";
+import {Helmet} from "react-helmet-async";
 import {useAppSelector} from "store";
 import {useAutoTheme} from "utils/hooks/useAutoTheme";
 
-const HelmetWorkaround = ({...rest}: HelmetProps) => <Helmet {...rest} />;
-
 export const Html = () => {
   const lang = useAppSelector((state) => state.view.language);
-  let title = useAppSelector((state) => state.board.data?.name);
-  if (title) title = `scrumlr.io - ${title}`;
+  const title = useAppSelector((state) => state.board.data?.name);
+
+  const displayTitle = title ? `scrumlr.io - ${title}` : "scrumlr.io";
 
   const theme = useAppSelector((state) => state.view.theme);
   const autoTheme = useAutoTheme(theme);
 
-  // set the theme as an attribute, which can then be used inside stylesheets, e.g. [theme="dark"] {...}
   useEffect(() => {
     document.documentElement.setAttribute("theme", autoTheme.toString());
   }, [autoTheme]);
 
-  return <HelmetWorkaround title={title} htmlAttributes={{lang, theme: autoTheme.toString()}} />;
+  return (
+    <Helmet>
+      <title>{displayTitle}</title>
+      <html lang={lang} data-theme={autoTheme.toString()} />
+    </Helmet>
+  );
 };
