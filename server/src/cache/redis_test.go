0f5a90e8b382f45d6c765099e2b85a258acd07f4
ffischbach <fynn.fischbach@inovex.de>
2026-02-12 17:45:57 +0100

    refactor: move duplicated code into CacheRedisTestSuite and SetupTest function

--- server/src/cache/redis_test.go
+++ server/src/cache/redis_test.go
@@ -1,189 +1,150 @@
 package cache
 
 import (
-	"context"
-	"encoding/json"
-	"testing"
-	"time"
+  "context"
+  "encoding/json"
+  "time"
 
-	"github.com/google/uuid"
-	"github.com/redis/go-redis/v9"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
-	testcontainer "github.com/testcontainers/testcontainers-go/modules/redis"
-	"scrumlr.io/server/initialize"
+  "testing"
+
+  "github.com/google/uuid"
+  "github.com/redis/go-redis/v9"
+  "github.com/stretchr/testify/assert"
+  "github.com/stretchr/testify/suite"
+  testcontainer "github.com/testcontainers/testcontainers-go/modules/redis"
+  "scrumlr.io/server/initialize"
 )
 
 type CacheRedisTestSuite struct {
-	suite.Suite
-	redisContainer        *testcontainer.RedisContainer
-	redisConnectionString string
+  suite.Suite
+  redisContainer        *testcontainer.RedisContainer
+  redisConnectionString string
+  cache                 *Cache
+  ctx                   context.Context
+  key                   string
 }
 
 func TestCacheRedisTestSuite(t *testing.T) {
-	suite.Run(t, new(CacheRedisTestSuite))
+  suite.Run(t, new(CacheRedisTestSuite))
 }
 
 func (suite *CacheRedisTestSuite) SetupSuite() {
-	redisContainer, redisConnectionString := initialize.StartTestRedis()
+  redisContainer, redisConnectionString := initialize.StartTestRedis()
 
-	suite.redisContainer = redisContainer
-	suite.redisConnectionString = redisConnectionString
+  suite.redisContainer = redisContainer
+  suite.redisConnectionString = redisConnectionString
 }
 
 func (suite *CacheRedisTestSuite) TearDownSuite() {
-	initialize.StopTestRedis(suite.redisContainer)
+  initialize.StopTestRedis(suite.redisContainer)
+}
+
+func (suite *CacheRedisTestSuite) SetupTest() {
+  suite.ctx = context.Background()
+  suite.key = uuid.New().String()
+
+  cache, err := NewRedis(RedisServer{Addr: suite.redisConnectionString})
+  suite.Require().NoError(err)
+  suite.cache = cache
 }
 
 func (suite *CacheRedisTestSuite) TestRedisCreate() {
-	t := suite.T()
-	ctx := context.Background()
-	key := uuid.New()
-	value := "test"
+  value := "test"
 
-	cache, err := NewRedis(RedisServer{Addr: suite.redisConnectionString})
-	assert.Nil(t, err)
+  err := suite.cache.Con.Create(suite.ctx, suite.key, value, time.Second)
+  assert.Nil(suite.T(), err)
 
-	err = cache.Con.Create(ctx, key.String(), value, time.Second)
-	assert.Nil(t, err)
+  ret, err := suite.cache.Con.Get(suite.ctx, suite.key)
+  assert.Nil(suite.T(), err)
 
-	ret, err := cache.Con.Get(ctx, key.String())
-	assert.Nil(t, err)
+  var retVal string
+  err = json.Unmarshal(ret, &retVal)
+  assert.Nil(suite.T(), err)
 
-	var retVal string
-	err = json.Unmarshal(ret, &retVal)
-	assert.Nil(t, err)
-
-	assert.Equal(t, value, retVal)
+  assert.Equal(suite.T(), value, retVal)
 }
 
 func (suite *CacheRedisTestSuite) TestRedisCreateAlreadyExists() {
-	t := suite.T()
-	ctx := context.Background()
-	key := uuid.New()
+  err := suite.cache.Con.Create(suite.ctx, suite.key, "test", 0)
+  assert.Nil(suite.T(), err)
 
-	cache, err := NewRedis(RedisServer{Addr: suite.redisConnectionString})
-	assert.Nil(t, err)
-
-	err = cache.Con.Create(ctx, key.String(), "test", 0)
-	assert.Nil(t, err)
-
-	err = cache.Con.Create(ctx, key.String(), "this should not work", 0)
-	assert.NotNil(t, err)
-	assert.Equal(t, &KeyAlreadyExists{redis.Nil}, err)
+  err = suite.cache.Con.Create(suite.ctx, suite.key, "this should not work", 0)
+  assert.NotNil(suite.T(), err)
+  assert.Equal(suite.T(), &KeyAlreadyExists{redis.Nil}, err)
 }
 
 func (suite *CacheRedisTestSuite) TestRedisPut() {
-	t := suite.T()
-	ctx := context.Background()
-	key := uuid.New()
-	value := "put"
+  value := "put"
 
-	cache, err := NewRedis(RedisServer{Addr: suite.redisConnectionString})
-	assert.Nil(t, err)
+  err := suite.cache.Con.Put(suite.ctx, suite.key, value)
+  assert.Nil(suite.T(), err)
 
-	err = cache.Con.Put(ctx, key.String(), value)
-	assert.Nil(t, err)
+  ret, err := suite.cache.Con.Get(suite.ctx, suite.key)
+  assert.Nil(suite.T(), err)
 
-	ret, err := cache.Con.Get(ctx, key.String())
-	assert.Nil(t, err)
+  var retVal string
+  err = json.Unmarshal(ret, &retVal)
+  assert.Nil(suite.T(), err)
 
-	var retVal string
-	err = json.Unmarshal(ret, &retVal)
-	assert.Nil(t, err)
-
-	assert.Equal(t, value, retVal)
+  assert.Equal(suite.T(), value, retVal)
 }
 
 func (suite *CacheRedisTestSuite) TestRedisPutOverride() {
-	t := suite.T()
-	ctx := context.Background()
-	key := uuid.New()
-	value := "this should work"
+  value := "this should work"
 
-	cache, err := NewRedis(RedisServer{Addr: suite.redisConnectionString})
-	assert.Nil(t, err)
+  err := suite.cache.Con.Put(suite.ctx, suite.key, "put")
+  assert.Nil(suite.T(), err)
 
-	err = cache.Con.Put(ctx, key.String(), "put")
-	assert.Nil(t, err)
+  err = suite.cache.Con.Put(suite.ctx, suite.key, value)
+  assert.Nil(suite.T(), err)
 
-	err = cache.Con.Put(ctx, key.String(), value)
-	assert.Nil(t, err)
+  ret, err := suite.cache.Con.Get(suite.ctx, suite.key)
+  assert.Nil(suite.T(), err)
 
-	ret, err := cache.Con.Get(ctx, key.String())
-	assert.Nil(t, err)
+  var retVal string
+  err = json.Unmarshal(ret, &retVal)
+  assert.Nil(suite.T(), err)
 
-	var retVal string
-	err = json.Unmarshal(ret, &retVal)
-	assert.Nil(t, err)
-
-	assert.Equal(t, value, retVal)
+  assert.Equal(suite.T(), value, retVal)
 }
 
 func (suite *CacheRedisTestSuite) TestRedisGet() {
-	t := suite.T()
-	ctx := context.Background()
-	key := uuid.New()
-	value := "test"
+  value := "test"
 
-	cache, err := NewRedis(RedisServer{Addr: suite.redisConnectionString})
-	assert.Nil(t, err)
+  err := suite.cache.Con.Create(suite.ctx, suite.key, value, 0)
+  assert.Nil(suite.T(), err)
 
-	err = cache.Con.Create(ctx, key.String(), value, 0)
-	assert.Nil(t, err)
+  ret, err := suite.cache.Con.Get(suite.ctx, suite.key)
+  assert.Nil(suite.T(), err)
 
-	ret, err := cache.Con.Get(ctx, key.String())
-	assert.Nil(t, err)
+  var retVal string
+  err = json.Unmarshal(ret, &retVal)
+  assert.Nil(suite.T(), err)
 
-	var retVal string
-	err = json.Unmarshal(ret, &retVal)
-	assert.Nil(t, err)
-
-	assert.Equal(t, value, retVal)
+  assert.Equal(suite.T(), value, retVal)
 }
 
 func (suite *CacheRedisTestSuite) TestRedisGetNotFound() {
-	t := suite.T()
-	ctx := context.Background()
-	key := uuid.New()
+  val, err := suite.cache.Con.Get(suite.ctx, suite.key)
 
-	cache, err := NewRedis(RedisServer{Addr: suite.redisConnectionString})
-	assert.Nil(t, err)
-
-	val, err := cache.Con.Get(ctx, key.String())
-
-	assert.Nil(t, val)
-	assert.NotNil(t, err)
-	assert.Equal(t, &KeyNotFound{redis.Nil}, err)
+  assert.Nil(suite.T(), val)
+  assert.NotNil(suite.T(), err)
+  assert.Equal(suite.T(), &KeyNotFound{redis.Nil}, err)
 }
 
 func (suite *CacheRedisTestSuite) TestRedisDelete() {
-	t := suite.T()
-	ctx := context.Background()
-	key := uuid.New()
+  err := suite.cache.Con.Create(suite.ctx, suite.key, "test", 0)
+  assert.Nil(suite.T(), err)
 
-	cache, err := NewRedis(RedisServer{Addr: suite.redisConnectionString})
-	assert.Nil(t, err)
+  err = suite.cache.Con.Delete(suite.ctx, suite.key)
+  assert.Nil(suite.T(), err)
 
-	err = cache.Con.Create(ctx, key.String(), "test", 0)
-	assert.Nil(t, err)
-
-	err = cache.Con.Delete(ctx, key.String())
-	assert.Nil(t, err)
-
-	ret, err := cache.Con.Get(ctx, key.String())
-	assert.Nil(t, ret)
-	assert.NotNil(t, err)
+  ret, err := suite.cache.Con.Get(suite.ctx, suite.key)
+  assert.Nil(suite.T(), ret)
+  assert.NotNil(suite.T(), err)
 }
 
 func (suite *CacheRedisTestSuite) TestRedisDeleteNotFound() {
-	t := suite.T()
-	ctx := context.Background()
-	key := uuid.New()
-
-	cache, err := NewRedis(RedisServer{Addr: suite.redisConnectionString})
-	assert.Nil(t, err)
-
-	err = cache.Con.Delete(ctx, key.String())
-	assert.Nil(t, err)
+  err := suite.cache.Con.Delete(suite.ctx, suite.key)
+  assert.Nil(suite.T(), err)
 }
