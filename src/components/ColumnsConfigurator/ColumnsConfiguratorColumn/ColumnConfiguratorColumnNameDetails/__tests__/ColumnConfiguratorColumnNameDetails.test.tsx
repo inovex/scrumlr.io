35d2dd30818e2d2a1697b47e6ad6ecd6c36806a8
Jakob Schwehn <schwehn42@gmail.com>
2026-03-02 11:25:58 +0100

    change: column details save behaviour (#5831)

--- src/components/ColumnsConfigurator/ColumnsConfiguratorColumn/ColumnConfiguratorColumnNameDetails/__tests__/ColumnConfiguratorColumnNameDetails.test.tsx
+++ src/components/ColumnsConfigurator/ColumnsConfiguratorColumn/ColumnConfiguratorColumnNameDetails/__tests__/ColumnConfiguratorColumnNameDetails.test.tsx
@@ -12,34 +12,34 @@ const renderColumnConfiguratorColumnNameDetails = (override?: Partial<ColumnConf
     updateColumnTitle: jest.fn(),
   };
 
-  return render(<ColumnConfiguratorColumnNameDetails {...defaultProps} {...override} />);
+  return <ColumnConfiguratorColumnNameDetails {...defaultProps} {...override} />;
 };
 
 describe("ColumnConfiguratorColumnNameDetails render", () => {
   it("should render correctly (closed)", () => {
-    const {container} = renderColumnConfiguratorColumnNameDetails({openState: "closed"});
+    const {container} = render(renderColumnConfiguratorColumnNameDetails({openState: "closed"}));
     expect(container).toMatchSnapshot();
   });
 
   it("should render correctly (visualFeedback)", () => {
-    const {container} = renderColumnConfiguratorColumnNameDetails({openState: "visualFeedback"});
+    const {container} = render(renderColumnConfiguratorColumnNameDetails({openState: "visualFeedback"}));
     expect(container).toMatchSnapshot();
   });
 
   it("should render correctly (nameFirst)", () => {
-    const {container} = renderColumnConfiguratorColumnNameDetails({openState: "nameFirst"});
+    const {container} = render(renderColumnConfiguratorColumnNameDetails({openState: "nameFirst"}));
     expect(container).toMatchSnapshot();
   });
 
   it("should render correctly (descriptionFirst)", () => {
-    const {container} = renderColumnConfiguratorColumnNameDetails({openState: "descriptionFirst"});
+    const {container} = render(renderColumnConfiguratorColumnNameDetails({openState: "descriptionFirst"}));
     expect(container).toMatchSnapshot();
   });
 
   it("should match title and description", () => {
     const name = "Custom Title";
     const description = "Custom Description";
-    const {container} = renderColumnConfiguratorColumnNameDetails({name, description});
+    const {container} = render(renderColumnConfiguratorColumnNameDetails({name, description}));
 
     expect(container.querySelector<HTMLInputElement>(".column-configurator-column-name-details__name")).toHaveValue(name);
     expect(container.querySelector<HTMLDivElement>(".column-configurator-column-name-details__inline-description")).toHaveTextContent(description);
@@ -47,9 +47,42 @@ describe("ColumnConfiguratorColumnNameDetails render", () => {
 });
 
 describe("ColumnConfiguratorColumnNameDetails behaviour", () => {
+  it("should make title editable after focusing", () => {
+    const setOpenStateSpy = jest.fn();
+    const {container, rerender} = render(renderColumnConfiguratorColumnNameDetails({openState: "closed", setOpenState: setOpenStateSpy}));
+
+    const inputElement = container.querySelector<HTMLInputElement>(".column-configurator-column-name-details__name")!;
+
+    expect(inputElement).not.toHaveFocus();
+
+    fireEvent.focus(inputElement);
+
+    expect(setOpenStateSpy).toHaveBeenCalledWith("nameFirst");
+
+    rerender(renderColumnConfiguratorColumnNameDetails({openState: "nameFirst"}));
+
+    expect(inputElement).toHaveClass("column-configurator-column-name-details__name--editing");
+  });
+
+  it("should make description editable after clicking", () => {
+    const setOpenStateSpy = jest.fn();
+    const {container, rerender} = render(renderColumnConfiguratorColumnNameDetails({openState: "closed", setOpenState: setOpenStateSpy}));
+
+    const inlineDescriptionElement = container.querySelector<HTMLDivElement>(".column-configurator-column-name-details__inline-description")!;
+
+    expect(inlineDescriptionElement).toBeInTheDocument();
+
+    fireEvent.click(inlineDescriptionElement);
+
+    expect(setOpenStateSpy).toHaveBeenCalledWith("descriptionFirst");
+    rerender(renderColumnConfiguratorColumnNameDetails({openState: "descriptionFirst"}));
+
+    expect(container.querySelector(".column-configurator-column-name-details__inline-description")).not.toBeInTheDocument();
+  });
+
   it("should close after losing focus", () => {
     const setOpenStateSpy = jest.fn();
-    const {container} = renderColumnConfiguratorColumnNameDetails({openState: "nameFirst", setOpenState: setOpenStateSpy});
+    const {container} = render(renderColumnConfiguratorColumnNameDetails({openState: "nameFirst", setOpenState: setOpenStateSpy}));
 
     // note: useOnBlur does not actually use the native blur event, but scans for clicks outside the element instead.
     // this is why to simulate the blur, we just click somewhere on the document.
@@ -60,15 +93,31 @@ describe("ColumnConfiguratorColumnNameDetails behaviour", () => {
     expect(setOpenStateSpy).toHaveBeenCalledWith("closed");
   });
 
+  it("should close without saving when there are no actual changes", () => {
+    const setOpenStateSpy = jest.fn();
+    const {container} = render(renderColumnConfiguratorColumnNameDetails({openState: "nameFirst", setOpenState: setOpenStateSpy}));
+
+    const saveChangesButtonElement = container.querySelector<HTMLButtonElement>(".mini-menu-item--save")!;
+    fireEvent.mouseDown(saveChangesButtonElement);
+
+    expect(setOpenStateSpy).toHaveBeenCalledWith("closed");
+  });
+
   it("should set openState to visualFeedback, then to closed upon confirming changes", () => {
     jest.useFakeTimers();
     const setOpenStateSpy = jest.fn();
-    const {container} = renderColumnConfiguratorColumnNameDetails({openState: "nameFirst", setOpenState: setOpenStateSpy});
+    const updateColumnTitleSpy = jest.fn();
+    const {container} = render(renderColumnConfiguratorColumnNameDetails({openState: "nameFirst", setOpenState: setOpenStateSpy, updateColumnTitle: updateColumnTitleSpy}));
+
+    // make sure to insert actual changes
+    const inputElement = container.querySelector<HTMLInputElement>(".column-configurator-column-name-details__name")!;
+    fireEvent.input(inputElement, {target: {value: "Custom Title"}});
 
     const saveChangesButtonElement = container.querySelector<HTMLButtonElement>(".mini-menu-item--save")!;
     fireEvent.mouseDown(saveChangesButtonElement);
 
     expect(setOpenStateSpy).toHaveBeenCalledWith("visualFeedback");
+    expect(updateColumnTitleSpy).toHaveBeenCalledWith("Custom Title", expect.anything());
 
     setTimeout(() => {
       expect(setOpenStateSpy).toHaveBeenCalledWith("closed");
@@ -79,7 +128,7 @@ describe("ColumnConfiguratorColumnNameDetails behaviour", () => {
 
   it("should call save changes with new input title", () => {
     const updateColumnTitleSpy = jest.fn();
-    const {container} = renderColumnConfiguratorColumnNameDetails({openState: "nameFirst", updateColumnTitle: updateColumnTitleSpy});
+    const {container} = render(renderColumnConfiguratorColumnNameDetails({openState: "nameFirst", updateColumnTitle: updateColumnTitleSpy}));
 
     const inputElement = container.querySelector<HTMLInputElement>(".column-configurator-column-name-details__name")!;
     fireEvent.input(inputElement, {target: {value: "Custom Title"}});
@@ -88,5 +137,31 @@ describe("ColumnConfiguratorColumnNameDetails behaviour", () => {
     fireEvent.mouseDown(saveChangesButtonElement);
 
     expect(updateColumnTitleSpy).toHaveBeenCalledWith("Custom Title", expect.anything());
+  });
+
+  it("should also save after pressing enter", () => {
+    const updateColumnTitleSpy = jest.fn();
+    const {container} = render(renderColumnConfiguratorColumnNameDetails({openState: "nameFirst", updateColumnTitle: updateColumnTitleSpy}));
+
+    const inputElement = container.querySelector<HTMLInputElement>(".column-configurator-column-name-details__name")!;
+    fireEvent.input(inputElement, {target: {value: "Custom Title"}});
+
+    fireEvent.keyUp(inputElement, {key: "Enter"});
+
+    expect(updateColumnTitleSpy).toHaveBeenCalledWith("Custom Title", expect.anything());
+  });
+
+  it("should reset after pressing escape", () => {
+    const updateColumnTitleSpy = jest.fn();
+    const setOpenStateSpy = jest.fn();
+    const {container} = render(renderColumnConfiguratorColumnNameDetails({openState: "nameFirst", setOpenState: setOpenStateSpy, updateColumnTitle: updateColumnTitleSpy}));
+
+    const inputElement = container.querySelector<HTMLInputElement>(".column-configurator-column-name-details__name")!;
+    fireEvent.input(inputElement, {target: {value: "Custom Title"}});
+
+    fireEvent.keyUp(inputElement, {key: "Escape"});
+
+    expect(updateColumnTitleSpy).not.toHaveBeenCalled();
+    expect(setOpenStateSpy).toHaveBeenCalledWith("closed");
   });
 });
