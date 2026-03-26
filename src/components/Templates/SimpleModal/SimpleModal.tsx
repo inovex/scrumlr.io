3083700c9b5c872386a4a989b9ebbf091c5a9a5b
Jakob Schwehn <schwehn42@gmail.com>
2026-03-26 15:39:51 +0100

    feat: add SimpleModal component

---
+++ src/components/Templates/SimpleModal/SimpleModal.tsx
@@ -0,0 +1,36 @@
+import {ReactNode} from "react";
+import {Button} from "components/Button";
+import "./SimpleModal.scss";
+import classNames from "classnames";
+
+type SimpleModalButtonProps = {
+  label: string;
+  onClick: () => void;
+  disabled?: boolean;
+};
+
+type SimpleModalProps = {
+  className: string;
+  title: string;
+  children: ReactNode;
+
+  secondaryButton: SimpleModalButtonProps;
+  primaryButton: SimpleModalButtonProps;
+};
+
+export const SimpleModal = (props: SimpleModalProps) => (
+    <div className={classNames(props.className, "simple-modal")}>
+      <header className="simple-modal__header">
+        <div className="simple-modal__title">{props.title}</div>
+      </header>
+      <main className="simple-modal__main">{props.children}</main>
+      <footer className="simple-modal__footer">
+        <Button variant="secondary" onClick={props.secondaryButton.onClick} disabled={props.secondaryButton.disabled} dataCy="simple-modal__secondary-button">
+          {props.secondaryButton.label}
+        </Button>
+        <Button variant="primary" onClick={props.primaryButton.onClick} disabled={props.primaryButton.disabled} dataCy="simple-modal__primary-button">
+          {props.primaryButton.label}
+        </Button>
+      </footer>
+    </div>
+  );
