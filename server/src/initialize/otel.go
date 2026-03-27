e6d65d967213fb1785f4142cd56bcd3b9d76d72e
Flo <florian.schwarz@inovex.de>
2026-03-27 14:57:53 +0100

    chore: Update scrumlr version to 5.0.1 (#5937)

--- server/src/initialize/otel.go
+++ server/src/initialize/otel.go
@@ -256,7 +256,7 @@ func newResource() (*resource.Resource, error) {
 		resource.NewWithAttributes(
 			semconv.SchemaURL,
 			semconv.ServiceName("scrumlr"),
-			semconv.ServiceVersion("5.0.0"),
+			semconv.ServiceVersion("5.0.1"),
 		),
 	)
 }
