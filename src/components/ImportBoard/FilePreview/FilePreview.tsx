c59e3e2956d5b299decffb355b412d68c9015245
Jakob Schwehn <schwehn42@gmail.com>
2026-03-31 18:05:59 +0200

    file state

--- src/components/ImportBoard/FilePreview/FilePreview.tsx
+++ src/components/ImportBoard/FilePreview/FilePreview.tsx
@@ -1,4 +1,5 @@
 import {CheckDoneIcon, CloseIcon, FileJsonIcon} from "components/Icon";
+import {useTranslation} from "react-i18next";
 import "./FilePreview.scss";
 
 type FileState = "loading" | "ready";
@@ -11,7 +12,10 @@ type FilePreviewProps = {
 };
 
 // file preview currently only supports one file, could be extended if needed
-export const FilePreview = (props: FilePreviewProps) => (
+export const FilePreview = (props: FilePreviewProps) => {
+  const {t} = useTranslation();
+
+  return (
     <div className="file-preview">
       <div className="file-preview__file">
         <div className="file-preview__file-icon-container">
@@ -19,7 +23,17 @@ export const FilePreview = (props: FilePreviewProps) => (
         </div>
         <div className="file-preview__file-info">
           <div className="file-preview__file-name">{props.name}</div>
-          <div className="file-preview__file-state">{props.state}</div>
+          <div className="file-preview__file-state">
+            {props.state === "loading" && <span className="file-preview__file-state-text">{t("ImportBoard.FilePreview.loadingState")}</span>}
+            {props.state === "ready" && (
+              <>
+                <div className="file-preview__file-state-ready-icon-container">
+                  <CheckDoneIcon className="file-preview__file-state-ready-icon" />
+                </div>
+                <span className="file-preview__file-state-text">{t("ImportBoard.FilePreview.uploadedState")}</span>
+              </>
+            )}
+          </div>
         </div>
         <button className="file-preview__file-remove-button" onClick={props.onRemove}>
           <CloseIcon className="file-preview__file-remove-icon" />
@@ -27,3 +41,4 @@ export const FilePreview = (props: FilePreviewProps) => (
       </div>
     </div>
   );
+};
