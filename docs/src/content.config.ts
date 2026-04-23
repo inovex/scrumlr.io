28fec1dd56d70589d581819caa9847d05799ab11
Jakob Schwehn <schwehn42@gmail.com>
2026-04-23 13:04:01 +0200

    docs: use collection loader
    refer to https://docs.astro.build/en/guides/content-collections/#build-time-collection-loaders

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,6 +1,13 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema } from '@astrojs/starlight/schema';
+import {defineCollection} from 'astro:content';
+import {docsSchema} from '@astrojs/starlight/schema';
+import {glob} from "astro/loaders";
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({
+    schema: docsSchema(),
+    loader: glob({
+      pattern: "**/*.{md,mdx}",
+      base: "src/content/docs"
+    }),
+  }),
 };
