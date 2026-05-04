7626fe84ae486509649acc82b4a6f6405bec5141
Jakob Schwehn <schwehn42@gmail.com>
2026-05-04 11:11:05 +0200

    build: ts config path (#6054)

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import {defineConfig, type ViteUserConfig} from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from 'vite-plugin-svgr';
 import * as path from "node:path";
 
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths({
-      projects: ["./tsconfig.json"]
-    }),
     svgr({
       include: '**/*.svg?react',
     })
@@ -22,6 +18,9 @@ export default defineConfig({
 
       },
     },
+  },
+  resolve: {
+    tsconfigPaths: true
   },
   build: {
     outDir: 'build',
