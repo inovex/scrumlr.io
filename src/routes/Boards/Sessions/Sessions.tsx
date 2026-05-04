b540647f0805f682f8475eeb3233999f5a606e14
Jakob Schwehn <schwehn42@gmail.com>
2026-05-04 14:22:32 +0200

    clean up Sessions, remove info

--- src/routes/Boards/Sessions/Sessions.tsx
+++ src/routes/Boards/Sessions/Sessions.tsx
@@ -1,7 +1,6 @@
 import {Outlet} from "react-router";
 import {useTranslation} from "react-i18next";
 import classNames from "classnames";
-import {InfoIcon} from "components/Icon";
 import StanDark from "assets/stan/Stan_Okay_Cutted_Darkblue_Shirt.svg";
 import StanLight from "assets/stan/Stan_Okay_Cutted_White_Shirt.svg";
 import "./Sessions.scss";
@@ -11,20 +10,17 @@ export const Sessions = () => {
   return (
     <>
       <Outlet /> {/* settings */}
-      <div className="sessions__card">
+      <div className="sessions">
         <div className="sessions__stan-container">
           <div className="sessions__stan-spacing" />
           <img className={classNames("sessions__stan", "sessions__stan--dark")} src={StanDark} alt="" />
           <img className={classNames("sessions__stan", "sessions__stan--light")} src={StanLight} alt="" />
         </div>
-        <div className="sessions__teaser">
+        <div className="sessions_container">
           <header className="sessions__container-header">
             <div className="templates__container-title">{t("Sessions.savedBoards")}</div>
           </header>
-          <div className="sessions__teaser-info">
-            <InfoIcon className="sessions__teaser-icon" />
-            <p className="sessions__teaser-text">{t("Sessions.teaserText")}</p>
-          </div>
+          <div className="sessions__card-container" />
         </div>
       </div>
     </>
