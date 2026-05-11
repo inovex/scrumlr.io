68b93c85e258015571a7aaed67b51a7f73be9509
Jakob Schwehn <schwehn42@gmail.com>
2026-05-11 11:02:25 +0200

    feat: render access policy icon

--- src/routes/Boards/Sessions/HistoryCard/HistoryCard.tsx
+++ src/routes/Boards/Sessions/HistoryCard/HistoryCard.tsx
@@ -4,80 +4,92 @@ import classNames from "classnames";
 import {CalendarIcon, ColumnsIcon, KeyWithLockIcon, MultipleUserIcon, NextIcon, NoteIcon, OpenIcon, ThreeDotsIcon as MenuIcon} from "components/Icon";
 import {TextArea} from "components/TextArea/TextArea";
 import {Button} from "components/Button";
-import "./HistoryCard.scss";
 import {UserRoleChip} from "routes/Boards/Sessions/HistoryCard/AccessPolicyChip/UserRoleChip";
+import {AccessPolicy} from "store/features";
+import {ReactElement} from "react";
+import "./HistoryCard.scss";
 
 type HistoryCardProps = {
   board: HistoryBoard;
   favourite: boolean;
 };
 
-export const HistoryCard = (props: HistoryCardProps) => (
-  <div className="history-card">
-    <FavouriteButton
-      className="history-card__favourite"
-      active={props.favourite}
-      onClick={() => {
-        throw new Error("Not implemented yet");
-      }}
-    />
+const accessPolicyIconMap: Record<AccessPolicy, ReactElement> = {
+  PUBLIC: <OpenIcon className={classNames("history-card__access-policy", "history-card__icon", "history-card__icon--public")} />,
+  BY_PASSPHRASE: <KeyWithLockIcon className={classNames("history-card__access-policy", "history-card__icon", "history-card__icon--passphrase")} />,
+  BY_INVITE: <MultipleUserIcon className={classNames("history-card__access-policy", "history-card__icon", "history-card__icon--private")} />,
+};
 
-    <div className={classNames("history-card__head")}>
-      <div className="history-card__title">{props.board.name}</div>
+export const HistoryCard = (props: HistoryCardProps) => {
+  const renderAccessPolicyIcon = (accessPolicy: AccessPolicy) => accessPolicyIconMap[accessPolicy];
 
-      <OpenIcon className={classNames("history-card__access-policy", "history-card__icon", "history-card__icon--public")} />
+  return (
+    <div className="history-card">
+      <FavouriteButton
+        className="history-card__favourite"
+        active={props.favourite}
+        onClick={() => {
+          throw new Error("Not implemented yet");
+        }}
+      />
 
-      <UserRoleChip className="history-card__user-role-chip" userRole={props.board.userRole} />
-    </div>
+      <div className={classNames("history-card__head")}>
+        <div className="history-card__title">{props.board.name}</div>
 
-    <MenuIcon className={classNames("history-card__menu", "history-card__icon", "history-card__icon--menu")} />
+        {renderAccessPolicyIcon(props.board.accessPolicy)}
 
-    <TextArea className={classNames("history-card__description")} input={props.board.description} setInput={() => {}} readOnly border="none" embedded />
-
-    <ColumnsIcon className={classNames("history-card__icon", "history-card__icon--columns")} />
-
-    <div className={classNames("history-card__info-footer")}>
-      <div className="history-card__info-item-data-container history-card__info-item-data-container--columns">
-        <div className="history-card__info-item-data-title">Columns: {props.board.columns.length}</div>
-        <div className="history-card__info-item-data-subtitle">{props.board.columns.join(", ")}</div>
+        <UserRoleChip className="history-card__user-role-chip" userRole={props.board.userRole} />
       </div>
 
-      <div className="history-card__info-item">
-        <CalendarIcon className={classNames("history-card__icon", "history-card__icon--calendar")} />
-        <div className="history-card__info-item-data-container history-card__info-item-data-container--timestamps">
-          <div className="history-card__info-item-data-title">
-            {props.board.createdAt.toLocaleDateString(undefined, {weekday: "long", year: "numeric", month: "2-digit", day: "2-digit"})}
+      <MenuIcon className={classNames("history-card__menu", "history-card__icon", "history-card__icon--menu")} />
+
+      <TextArea className={classNames("history-card__description")} input={props.board.description} setInput={() => {}} readOnly border="none" embedded />
+
+      <ColumnsIcon className={classNames("history-card__icon", "history-card__icon--columns")} />
+
+      <div className={classNames("history-card__info-footer")}>
+        <div className="history-card__info-item-data-container history-card__info-item-data-container--columns">
+          <div className="history-card__info-item-data-title">Columns: {props.board.columns.length}</div>
+          <div className="history-card__info-item-data-subtitle">{props.board.columns.join(", ")}</div>
+        </div>
+
+        <div className="history-card__info-item">
+          <CalendarIcon className={classNames("history-card__icon", "history-card__icon--calendar")} />
+          <div className="history-card__info-item-data-container history-card__info-item-data-container--timestamps">
+            <div className="history-card__info-item-data-title">
+              {props.board.createdAt.toLocaleDateString(undefined, {weekday: "long", year: "numeric", month: "2-digit", day: "2-digit"})}
+            </div>
+            <div className="history-card__info-item-data-subtitle">Last Changed: {props.board.modifiedAt.toLocaleDateString()}</div>
           </div>
-          <div className="history-card__info-item-data-subtitle">Last Changed: {props.board.modifiedAt.toLocaleDateString()}</div>
+        </div>
+
+        <div className="history-card__info-item">
+          <MultipleUserIcon className={classNames("history-card__icon", "history-card__icon--participants")} />
+          <div className="history-card__info-item-data-container history-card__info-item-data-container--participants">
+            <div className="history-card__info-item-data-title">{props.board.participants} Participants</div>
+          </div>
+        </div>
+
+        <div className="history-card__info-item">
+          <NoteIcon className={classNames("history-card__icon", "history-card__icon--notes")} />
+          <div className="history-card__info-item-data-container history-card__info-item-data-container--notes">
+            <div className="history-card__info-item-data-title">{props.board.participants} Notes</div>
+          </div>
         </div>
       </div>
 
-      <div className="history-card__info-item">
-        <MultipleUserIcon className={classNames("history-card__icon", "history-card__icon--participants")} />
-        <div className="history-card__info-item-data-container history-card__info-item-data-container--participants">
-          <div className="history-card__info-item-data-title">{props.board.participants} Participants</div>
-        </div>
-      </div>
+      {props.board.isLocked && <KeyWithLockIcon className={classNames("history-card__icon", "history-card__icon--locked")} />}
 
-      <div className="history-card__info-item">
-        <NoteIcon className={classNames("history-card__icon", "history-card__icon--notes")} />
-        <div className="history-card__info-item-data-container history-card__info-item-data-container--notes">
-          <div className="history-card__info-item-data-title">{props.board.participants} Notes</div>
-        </div>
-      </div>
+      <Button
+        className={classNames("history-card__button", "history-card__button--start")}
+        small
+        icon={<NextIcon />}
+        onClick={() => {
+          throw new Error("Not implemented yet");
+        }}
+      >
+        Go to Session
+      </Button>
     </div>
-
-    {props.board.isLocked && <KeyWithLockIcon className={classNames("history-card__icon", "history-card__icon--locked")} />}
-
-    <Button
-      className={classNames("history-card__button", "history-card__button--start")}
-      small
-      icon={<NextIcon />}
-      onClick={() => {
-        throw new Error("Not implemented yet");
-      }}
-    >
-      Go to Session
-    </Button>
-  </div>
-);
+  );
+};
