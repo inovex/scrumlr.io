9285b150b6f9fabced95c7038dfa75b69f069b34
Jakob Schwehn <schwehn42@gmail.com>
2026-05-13 15:59:49 +0200

    feat: bring back teaser for history page and show conditionally

--- src/routes/Boards/Sessions/History.tsx
+++ src/routes/Boards/Sessions/History.tsx
@@ -3,6 +3,7 @@ import {useTranslation} from "react-i18next";
 import classNames from "classnames";
 import StanDark from "assets/stan/Stan_Okay_Cutted_Darkblue_Shirt.svg";
 import StanLight from "assets/stan/Stan_Okay_Cutted_White_Shirt.svg";
+import {InfoIcon} from "components/Icon";
 import {AccessPolicy, ParticipantRole} from "store/features";
 import {HistoryCard} from "./HistoryCard/HistoryCard";
 import "./History.scss";
@@ -61,6 +62,19 @@ export const History = () => {
 
   const matchSearchInput = (historyBoard: HistoryBoard) => historyBoard.name.toLowerCase().includes(searchBarInput.toLowerCase());
 
+  // for development, only show history boards if the env variable is set to true, otherwise show the teaser
+  const renderHistoryContent = () =>
+    import.meta.env.VITE_SHOW_HISTORY_PAGE === "true" ? (
+      TEST_HISTORY_BOARDS.filter(matchSearchInput)
+        .sort((a, b) => Number(b.favourite) - Number(a.favourite)) // move favourites to the top using the fact that true is 1 and false is 0
+        .map((hb) => <HistoryCard key={hb.id} board={hb} />)
+    ) : (
+      <div className="history__teaser-info">
+        <InfoIcon className="history__teaser-icon" />
+        <p className="history__teaser-text">{t("History.teaserText")}</p>
+      </div>
+    );
+
   return (
     <>
       <Outlet /> {/* settings */}
@@ -74,13 +88,7 @@ export const History = () => {
           <header className="history__container-header">
             <div className="templates__container-title">{t("History.savedBoards")}</div>
           </header>
-          <div className="history__card-container">
-            {TEST_HISTORY_BOARDS.filter(matchSearchInput)
-              .sort((a, b) => Number(b.favourite) - Number(a.favourite)) // move favourites to the top using the fact that true is 1 and false is 0
-              .map((hb) => (
-                <HistoryCard key={hb.id} board={hb} />
-              ))}
-          </div>
+          <div className="history__card-container">{renderHistoryContent()}</div>
         </section>
       </div>
     </>
