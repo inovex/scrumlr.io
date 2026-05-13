6539e82a86a8b01586b574f3a1d809b96ba0af89
Matin Gohar Far <mgohar-far@Matins-MacBook-Pro.local>
2026-05-13 13:33:59 +0200

    mapError function in api package to include all domain error types, adapted service_test for board package

--- server/src/api/errors.go
+++ server/src/api/errors.go
@@ -5,7 +5,14 @@ import (
 	"errors"
 
 	"scrumlr.io/server/boards"
+	"scrumlr.io/server/columns"
 	"scrumlr.io/server/common"
+	"scrumlr.io/server/notes"
+	"scrumlr.io/server/reactions"
+	"scrumlr.io/server/sessionrequests"
+	"scrumlr.io/server/sessions"
+	"scrumlr.io/server/users"
+	"scrumlr.io/server/votings"
 )
 
 // mapError translates domain and database errors to HTTP API Errors.
@@ -14,24 +21,54 @@ func mapError(err error) error {
 		return nil
 	}
 
-	// Check for specific domain errors
+	// Group errors by HTTP semantics
 	switch {
+	// 400 Bad Request
 	case errors.Is(err, boards.ErrPassphraseForbidden),
 		errors.Is(err, boards.ErrPassphraseRequired),
 		errors.Is(err, boards.ErrNameEmpty),
-		errors.Is(err, boards.ErrInvalidPassphrase):
+		errors.Is(err, boards.ErrInvalidPassphrase),
+		errors.Is(err, boards.ErrInvalidBoardSettings),
+		errors.Is(err, notes.ErrEmptyTextCreate),
+		errors.Is(err, notes.ErrEmptyTextImport),
+		errors.Is(err, sessionrequests.ErrInvalidBoardStatusFilter),
+		errors.Is(err, votings.ErrVotingLimitNegative),
+		errors.Is(err, votings.ErrVoteLimitTooHigh),
+		errors.Is(err, users.ErrInvalidUserName):
 		return common.BadRequestError(err)
 
-	case errors.Is(err, boards.ErrNotAuthorized):
+	// 403 Forbidden
+	case errors.Is(err, boards.ErrNotAuthorized),
+		errors.Is(err, sessions.ErrForbiddenSessionChange),
+		errors.Is(err, sessions.ErrForbiddenRolePromotion),
+		errors.Is(err, sessions.ErrForbiddenOwnerChange),
+		errors.Is(err, sessions.ErrForbiddenOwnerPromotion),
+		errors.Is(err, notes.ErrNotAllowedTextChange),
+		errors.Is(err, notes.ErrForbiddenStackNotes),
+		errors.Is(err, notes.ErrForbiddenStackOnSelf),
+		errors.Is(err, notes.ErrForbiddenDeleteOtherUserNote),
+		errors.Is(err, reactions.ErrForbiddenReactionDelete),
+		errors.Is(err, reactions.ErrForbiddenReactionUpdate):
 		return common.ForbiddenError(err)
 
+	// 404 Not Found
 	case errors.Is(err, boards.ErrBoardNotFound),
-		errors.Is(err, sql.ErrNoRows): // Catch database "not found"
+		errors.Is(err, sql.ErrNoRows), // Catch database "not found"
+		errors.Is(err, columns.ErrColumnNotFound),
+		errors.Is(err, sessionrequests.ErrSessionRequestNotFound),
+		errors.Is(err, sessions.ErrSessionNotFound),
+		errors.Is(err, users.ErrUserNotFound),
+		errors.Is(err, reactions.ErrReactionNotFound),
+		errors.Is(err, votings.ErrVotingNotFound):
 		return common.NotFoundError
 
+	// 409 Conflict
+	case errors.Is(err, notes.ErrNoteLocked),
+		errors.Is(err, reactions.ErrReactionAlreadyExists):
+		return common.ConflictError(err)
+
 	default:
-		// If it's an unmapped error (like a DB connection failure),
-		// return a generic 500 without leaking DB internals to the client.
+		// Unmapped errors -> 500
 		return common.InternalServerError
 	}
 }
--- server/src/votings/errors.go
+++ server/src/votings/errors.go
@@ -8,4 +8,5 @@ var (
 	// Bad Request Errors
 	ErrVotingLimitNegative = errors.New("vote limit cannot be smaller than 0")
 	ErrVoteLimitTooHigh    = errors.New("vote limit cannot be greater than 100")
+	ErrOnlyOneOpenVoting   = errors.New("only one open voting per session is allowed")
 )
