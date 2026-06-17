37b0c1878f1b1ec55d8e02b997bd07f72cd2e456
Jakob Schwehn <schwehn42@gmail.com>
2026-06-17 13:52:02 +0200

    fix: sync language state with redux and i18n integration (#6142)
    Co-authored-by: Jakob <44205136+jmoehler@users.noreply.github.com>

--- src/components/SettingsDialog/Components/LanguageSettingsDropdown.tsx
+++ src/components/SettingsDialog/Components/LanguageSettingsDropdown.tsx
@@ -1,20 +1,18 @@
-import {useState} from "react";
 import German from "assets/flags/DE.svg?react";
 import English from "assets/flags/US.svg?react";
 import French from "assets/flags/FR.svg?react";
 import {useTranslation} from "react-i18next";
-import i18n from "i18n";
+import {useAppDispatch, useAppSelector} from "store";
+import {setLanguage} from "store/features";
 import {SettingsDropdown} from "./SettingsDropdown";
 
 export const LanguageSettingsDropdown = () => {
   const {t} = useTranslation();
-  const [currentlanguage, setCurrentlanguage] = useState(i18n.resolvedLanguage);
+  const dispatch = useAppDispatch();
+  const currentLanguage = useAppSelector((state) => state.view.language);
 
   const changeLanguage = (language: string) => {
-    setCurrentlanguage(language);
-    i18n.changeLanguage(language).then(() => {
-      document.documentElement.lang = i18n.language;
-    });
+    dispatch(setLanguage(language));
   };
 
   const languages = [
@@ -23,5 +21,5 @@ export const LanguageSettingsDropdown = () => {
     {icon: French, text: t("Language.french"), callback: () => changeLanguage("fr"), code: "fr"},
   ];
 
-  return <SettingsDropdown items={languages} label={t("Appearance.Language")} current={languages.find((item) => item.code === currentlanguage) ?? languages[0]} />;
+  return <SettingsDropdown items={languages} label={t("Appearance.Language")} current={languages.find((item) => item.code === currentLanguage) ?? languages[0]} />;
 };
