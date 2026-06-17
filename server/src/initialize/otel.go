3ba15a2d302402913021dcdb56ef758374b0a583
Florian Schwarz <florian.schwarz@inovex.de>
2026-06-17 11:19:59 +0200

    Update scrumlr version

--- server/src/initialize/otel.go
+++ server/src/initialize/otel.go
@@ -17,7 +17,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.41.0"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"scrumlr.io/server/logger"
@@ -256,7 +256,7 @@ func newResource() (*resource.Resource, error) {
 		resource.NewWithAttributes(
 			semconv.SchemaURL,
 			semconv.ServiceName("scrumlr"),
-			semconv.ServiceVersion("5.2.0"),
+			semconv.ServiceVersion("5.2.1"),
 		),
 	)
 }
