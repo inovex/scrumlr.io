92a2b77a6625bcfe905bce6af2ca3c62581c0910
Flo <florian.schwarz@inovex.de>
2026-07-06 12:49:47 +0200

    refactor: Comment unimplemented methods (#6271)

--- server/src/serviceinitialize/routes.go
+++ server/src/serviceinitialize/routes.go
@@ -16,40 +16,43 @@ func NewRoutesInitializer() RoutesInitializer {
 }
 
 func (init *RoutesInitializer) InitializeBoardRoutes() {
+	// board routes are currently not initialized through the route initializer
 	panic("Not implemented")
 }
 
 func (init *RoutesInitializer) InitializeColumnRoutes() {
+	// column routes are currently not initialized through the route initializer
 	panic("Not implemented")
-
 }
 
 func (init *RoutesInitializer) InitializeBoardReactionRoutes() {
+	// board reaction routes are currently not initialized through the route initializer
 	panic("Not implemented")
-
 }
 
 func (init *RoutesInitializer) InitializeBoardTemplateRoutes() {
+	// board template routes are currently not initialized through the route initializer
 	panic("Not implemented")
-
 }
 
 func (init *RoutesInitializer) InitializeColumnTemplateRoutes() {
-
+	// column template routes are currently not initialized through the route initializer
+	panic("Not implemented")
 }
 
 func (init *RoutesInitializer) InitializeFeedbackRoutes() {
-
+	// feedback routes are currently not initialized through the route initializer
+	panic("Not implemented")
 }
 
 func (init *RoutesInitializer) InitializeHealthRoutes() {
+	// health routes are currently not initialized through the route initializer
 	panic("Not implemented")
-
 }
 
 func (init *RoutesInitializer) InitializeReactionRoutes() {
+	// reaction routes are currently not initialized through the route initializer
 	panic("Not implemented")
-
 }
 
 func (init *RoutesInitializer) InitializeSessionRoutes(sessionApi sessions.SessionApi) chi.Router {
@@ -58,8 +61,8 @@ func (init *RoutesInitializer) InitializeSessionRoutes(sessionApi sessions.Sessi
 }
 
 func (init *RoutesInitializer) InitializeSessionRequestRoutes() {
+	// session request routes are currently not initialized through the route initializer
 	panic("Not implemented")
-
 }
 
 func (init *RoutesInitializer) InitializeUserRoutes(userApi users.UsersApi, sessionApi sessions.SessionApi) chi.Router {
@@ -68,9 +71,11 @@ func (init *RoutesInitializer) InitializeUserRoutes(userApi users.UsersApi, sess
 }
 
 func (init *RoutesInitializer) InitializeNotesRoutes() {
+	// note routes routes are currently not initialized through the route initializer
 	panic("Not implemented")
 }
 
 func (init *RoutesInitializer) InitializeVotingRoutes() {
+	// voting routes are currently not initialized through the route initializer
 	panic("Not implemented")
 }
