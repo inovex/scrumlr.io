d98871cc455c8ce22aa92f3b11e93a001f2be567
Matin Gohar Far <mgohar-far@Matins-MacBook-Pro.local>
2026-07-29 16:58:35 +0200

    bug fix after rebase

--- server/src/users/service_integration_test.go
+++ server/src/users/service_integration_test.go
@@ -193,7 +193,7 @@ func (suite *UserServiceIntegrationTestsuite) Test_Update() {
 	userId := suite.updateUser.ID
 	boardId := suite.updateBoard.ID
 
-	events, err := suite.broker.GetBoardChannel(suite.ctx, boardId)
+	events, err := suite.broker.GetBoardChannel(ctx, boardId)
 	require.NoError(suite.T(), err, "Failed to subscribe to board channel")
 
 	user, err := suite.userService.Update(ctx, UserUpdateRequest{ID: userId, Name: suite.testUserName})
@@ -218,10 +218,10 @@ func (suite *UserServiceIntegrationTestsuite) Test_Delete_WithNotes() {
 	suite.Nil(preDeleteErr)
 	suite.Len(preDeleteNotes, 1)
 
-	events, err := suite.broker.GetBoardChannel(suite.ctx, boardId)
+	events, err := suite.broker.GetBoardChannel(ctx, boardId)
 	require.NoError(suite.T(), err, "Failed to subscribe to board channel")
 
-	err = suite.userService.Delete(suite.ctx, userId)
+	err = suite.userService.Delete(ctx, userId)
 
 	suite.Nil(err)
 
