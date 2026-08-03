0d367b4a2bfac1340d074ff6a7255eaee951e6f9
Tobias Heim Galindo <81302108+tohega@users.noreply.github.com>
2026-08-03 10:45:42 +0200

    refactor: typescript sonarcube maintainability (#6417)
    Co-authored-by: Jakob <44205136+jmoehler@users.noreply.github.com>
    Co-authored-by: Flo <florian.schwarz@inovex.de>

--- src/components/SettingsDialog/Components/SettingsCarousel.tsx
+++ src/components/SettingsDialog/Components/SettingsCarousel.tsx
@@ -34,10 +34,7 @@ export const SettingsCarousel: FC<SettingsCarouselProps<string>> = ({carouselIte
       </button>
       <div className="settings-carousel__text">
         {label && <span className="settings-carousel__text-label">{label}</span>}
-        {/* eslint-disable */}
-        {/* @ts-ignore */}
         <span className="settings-carousel__text-value">{localizationPath !== undefined ? t(`${localizationPath}${currentValue}`) : currentValue}</span>
-        {/* eslint-enable */}
       </div>
       <button className="settings-carousel__button settings-carousel__button--right" disabled={disabled} onClick={() => handleClick()} aria-label={t("Avatar.nextOption")}>
         <ArrowRightIcon />
