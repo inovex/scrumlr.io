5236eac9b9c092182f7849dc74c4b5879cae9b2d
Jakob <44205136+jmoehler@users.noreply.github.com>
2026-08-05 14:43:02 +0200

    feat: Notify importers when notes with unavailable authors are removed (#6416)

--- src/store/features/board/types.ts
+++ src/store/features/board/types.ts
@@ -39,9 +39,17 @@ export type BoardImportData = {
   };
   columns: Column[];
   notes: Note[];
-  participants: ParticipantWithUser;
-  voting: Voting;
+  participants: ParticipantWithUser[];
+  votings: Voting | null;
 };
+
+export interface ImportWarnings {
+  removedNotesMissingAuthorCount?: number;
+}
+
+export interface ImportBoardResponse extends Board {
+  importWarnings?: ImportWarnings;
+}
 
 export type BoardActionType = {
   board: Board;
