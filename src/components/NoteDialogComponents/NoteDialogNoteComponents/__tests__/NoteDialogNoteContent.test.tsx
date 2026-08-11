602602ae5124848329204b1cf5f8997fb931f7b5
Alfonso Machado <almacbe@gmail.com>
2026-08-11 13:00:32 +0200

    feat: show character count in note dialog

---
+++ src/components/NoteDialogComponents/NoteDialogNoteComponents/__tests__/NoteDialogNoteContent.test.tsx
@@ -0,0 +1,27 @@
+import {render} from "testUtils";
+import {Provider} from "react-redux";
+import getTestStore from "utils/test/getTestStore";
+import getTestApplicationState from "utils/test/getTestApplicationState";
+import {NoteDialogNoteContent} from "components/NoteDialogComponents/NoteDialogNoteComponents/NoteDialogNoteContent";
+
+describe("NoteDialogNoteContent", () => {
+  it("should render the character count in the same row as the edited marker", () => {
+    const longText = "a".repeat(768);
+    const editedNote = {...getTestApplicationState().notes[0], text: longText}; // notes[0] is marked as edited
+    const viewer = getTestApplicationState().participants.self!;
+
+    const {container} = render(
+      <Provider store={getTestStore({notes: [editedNote]})}>
+        <NoteDialogNoteContent noteId={editedNote.id} authorId={editedNote.author} text={longText} viewer={viewer} isStackedNote={false} />
+      </Provider>
+    );
+
+    const textarea = container.querySelector<HTMLTextAreaElement>(".note-dialog__note-content-text")!;
+    expect(textarea).toBeTruthy();
+
+    const footer = textarea.nextElementSibling!;
+    expect(footer).toHaveClass("note-dialog__note-content-footer");
+    expect(footer.querySelector(".note-dialog__marker-edited")).toBeTruthy();
+    expect(footer.querySelector(".character-count-indicator")).toHaveTextContent("768/1024");
+  });
+});
