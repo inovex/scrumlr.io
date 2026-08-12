fdd8eee305995fec77ef312b9f5b576cc4d99d8d
Matin Gohar Far <mgohar-far@Matins-MacBook-Pro.local>
2026-08-12 14:06:27 +0200

    fixed wrong status in comment

--- server/src/votings/voting_status.go
+++ server/src/votings/voting_status.go
@@ -12,7 +12,7 @@ const (
 	// Open is the state for an open voting session, meaning that votes are allowed.
 	Open VotingStatus = "OPEN"
 
-	// Canceled represents a canceled voting session (the DB enum uses ABORTED)
+	// Aborted represents an aborted voting session (the DB enum uses ABORTED)
 	Aborted VotingStatus = "ABORTED"
 
 	// Closed is the state for a closed voting session.
