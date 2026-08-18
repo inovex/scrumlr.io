f8cbda06c4d26b429849b5f4da95dcf53778cc46
Alfonso Machado <almacbe@gmail.com>
2026-08-18 18:06:45 +0200

    fix: localize Spanish language label

--- src/components/SettingsDialog/Components/LanguageSettingsDropdown.tsx
+++ src/components/SettingsDialog/Components/LanguageSettingsDropdown.tsx
@@ -20,7 +20,7 @@ export const LanguageSettingsDropdown = () => {
     {icon: English, text: t("Language.english"), callback: () => changeLanguage("en"), code: "en"},
     {icon: German, text: t("Language.german"), callback: () => changeLanguage("de"), code: "de"},
     {icon: French, text: t("Language.french"), callback: () => changeLanguage("fr"), code: "fr"},
-    {icon: Spanish, text: "Español", callback: () => changeLanguage("es"), code: "es"},
+    {icon: Spanish, text: t("Language.spanish"), callback: () => changeLanguage("es"), code: "es"},
   ];
 
   return <SettingsDropdown items={languages} label={t("Appearance.Language")} current={languages.find((item) => item.code === currentLanguage) ?? languages[0]} />;
