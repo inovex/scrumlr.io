1ba3cfb3100fd81e56f4561d7cd407234f5b0730
Jakob Schwehn <schwehn42@gmail.com>
2026-09-01 10:24:56 +0200

    test: move board create test to board spec

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -17,6 +17,8 @@ declare namespace Cypress {
       // create template with given name
       createCustomTemplate(templateName: string): Chainable<void>
 
+      createBoard(): Chainable<void>
+
       // opens mini menu and clicks an item from it
       // make sure to append --closed to the mini menu icon so cyData matches
       selectMiniMenu(cyData: string, itemLabel: string): Chainable<void>
