2fd00bd014580964773f48bc0f4a84b5f5488608
Matin Gohar Far <mgohar-far@Matins-MacBook-Pro.local>
2026-09-10 17:08:06 +0200

    small style fix

--- server/src/boards/service.go
+++ server/src/boards/service.go
@@ -190,12 +190,16 @@ func (service *Service) Join(ctx context.Context, board *Board, user uuid.UUID, 
 	}
 
 	switch board.AccessPolicy {
+
 	case Public:
 		return service.joinPublic(ctx, board, user)
+
 	case ByPassphrase:
 		return service.joinByPassphrase(ctx, board, user, request)
+
 	case ByInvite:
 		return service.joinByInvite(ctx, board, user)
+
 	default:
 		err := errors.New("invalid access policy")
 		otel.RecordErrorSpan(span, err, new("invalid access policy"))
@@ -770,6 +774,7 @@ func (service *Service) buildCSVRecords(ctx context.Context, board *FullBoard, c
 func (service *Service) joinPublic(ctx context.Context, board *Board, user uuid.UUID) (bool, string, int, error) {
 	ctx, span := tracer.Start(ctx, "scrumlr.boards.service.join_public")
 	defer span.End()
+
 	_, err := service.sessionService.Create(ctx, sessions.BoardSessionCreateRequest{
 		Board: board.ID,
 		User:  user,
@@ -779,6 +784,7 @@ func (service *Service) joinPublic(ctx context.Context, board *Board, user uuid.
 		otel.RecordErrorSpan(span, err, new("failed to create session"))
 		return false, "", 0, err
 	}
+
 	return false, fmt.Sprintf("/boards/%s/participants/%s", board.ID, user), http.StatusCreated, nil
 }
 
@@ -791,17 +797,20 @@ func (service *Service) joinByPassphrase(ctx context.Context, board *Board, user
 		otel.RecordErrorSpan(span, err, new("missing passphrase"))
 		return false, "", 0, CreateBoardError(BadRequest, "missing passphrase", err)
 	}
+
 	if board.Passphrase == nil || board.Salt == nil {
 		err := errors.New("board passphrase is not configured")
 		otel.RecordErrorSpan(span, err, new("board passphrase is not configured"))
 		return false, "", 0, CreateBoardError(Internal, "board passphrase is not configured", err)
 	}
+
 	encodedPassphrase := service.hash.HashBySalt(request.Passphrase, *board.Salt)
 	if encodedPassphrase != *board.Passphrase {
 		err := errors.New("wrong passphrase")
 		otel.RecordErrorSpan(span, err, new("wrong passphrase provided"))
 		return false, "", 0, CreateBoardError(BadRequest, "wrong passphrase", err)
 	}
+
 	return service.joinPublic(ctx, board, user)
 }
 
@@ -814,12 +823,14 @@ func (service *Service) joinByInvite(ctx context.Context, board *Board, user uui
 		otel.RecordErrorSpan(span, err, new("failed to check session requests"))
 		return false, "", 0, err
 	}
+
 	if !sessionRequestExists {
 		if _, err = service.sessionRequestService.Create(ctx, board.ID, user); err != nil {
 			otel.RecordErrorSpan(span, err, new("failed to create session request"))
 			return false, "", 0, err
 		}
 	}
+
 	return false, fmt.Sprintf("/boards/%s/requests/%s", board.ID, user), http.StatusSeeOther, nil
 }
 
