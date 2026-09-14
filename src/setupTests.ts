76084f37e8e222bd13cab785ffd6766562f0fd4e
Jakob Schwehn <schwehn42@gmail.com>
2026-09-14 17:10:42 +0200

    fix: add vitest jestdom matchers

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,9 +1,7 @@
-import {vi, expect} from "vitest";
-import * as matchers from "@testing-library/jest-dom/matchers";
+import {vi} from "vitest";
+import "@testing-library/jest-dom/vitest"; // implicitly invokes expect.extend for DOM matchers
 import {LocalStorage} from "node-localstorage";
 import path from "path";
-
-expect.extend(matchers);
 
 // local storage
 const LOCALE_STORAGE_PATH = path.resolve(__dirname, "utils/test/localstorage");
